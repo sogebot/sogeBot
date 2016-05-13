f7e2ad070854753d0272de2210ef2297ff6d4546
Michal Orlik <sogehige@gmail.com>
2016-05-13 14:33:19 +0200

    #2 - added proper keyword system

--- libs/parser.js
+++ libs/parser.js
@@ -6,11 +6,22 @@ var chalk     = require('chalk'),
 function Parser() {
     this.registeredCmds = {};
     this.permissionsCmds = {};
+    this.registeredParsers = {};
+    this.permissionsParsers = {};
     this.linesParsed = 0;
 }
 
 Parser.prototype.parse = function(user, message) {
     this.linesParsed++;
+    
+    var isParseable = true;
+    for (var parser in this.registeredParsers) {
+        if (this.permissionsParsers[parser] === constants.VIEWERS || this.permissionsParsers[parser] === constants.OWNER_ONLY && this.isOwner(user)) {
+            isParseable = this.registeredParsers[parser](user, message);
+        }
+        if (!isParseable) return;
+    }
+    
     for (var cmd in this.registeredCmds) {
         if (message.startsWith(cmd)) {
             if (this.permissionsCmds[cmd] === constants.VIEWERS || this.permissionsCmds[cmd] === constants.OWNER_ONLY && this.isOwner(user)) {
@@ -25,6 +36,11 @@ Parser.prototype.parse = function(user, message) {
 Parser.prototype.register = function(cmd, fnc, permission) {
     this.registeredCmds[cmd] = fnc;
     this.permissionsCmds[cmd] = permission;
+}
+
+Parser.prototype.registerParser = function(parser, fnc, permission) {
+    this.registeredParsers[parser] = fnc;
+    this.permissionsParsers[parser] = permission;
 }
 
 Parser.prototype.unregister = function(cmd) {
