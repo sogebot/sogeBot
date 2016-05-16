cb88e8974c6d4470fd887c4956f2cc19a292f1da
Michal Orlik <sogehige@gmail.com>
2016-05-16 14:14:09 +0200

    #4 - added more commands for points system

--- libs/systems/points.js
+++ libs/systems/points.js
@@ -13,7 +13,14 @@ database.persistence.setAutocompactionInterval(60000);
 
 function Points(configuration) {        
     if (global.configuration.get().systems.points === true) {
-        global.parser.register('!points', this.getPoints, constants.VIEWERS);
+        global.parser.register('!points add',    this.addPoints,         constants.OWNER_ONLY);
+        global.parser.register('!points remove', this.removePoints,      constants.OWNER_ONLY);
+        global.parser.register('!points all',    this.allPoints,         constants.OWNER_ONLY);
+        global.parser.register('!points set',    this.setPoints,         constants.OWNER_ONLY);
+        global.parser.register('!points get',    this.getPointsFromUser, constants.OWNER_ONLY);
+        global.parser.register('!points give',   this.givePoints,        constants.VIEWERS);
+        global.parser.register('!makeitrain',    this.rainPoints,        constants.OWNER_ONLY);
+        global.parser.register('!points',        this.getPoints,         constants.VIEWERS);
         
         // add events for join/part
         var self = this;
@@ -24,6 +31,9 @@ function Points(configuration) {
         setInterval(function() {
             self.updatePoints();
         }, 30000);
+        
+        // disable counting for all users on start
+        database.update({ }, {$set: {isOnline: false, partedTime: new Date().getTime()}}, { multi: true });
     }
     
     console.log('Points system loaded and ' + (global.configuration.get().systems.points === true ? chalk.green('enabled'):chalk.red('disabled')));
@@ -43,6 +53,250 @@ Points.prototype.addEvents = function(self) {
         if (username != global.configuration.get().twitch.username) {
             self.stopCounting(username)
         }
+    });
+}
+
+Points.prototype.setPoints = function(user, text) {
+    if (text.length < 1) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: You need to specify user');
+        return;
+    }
+    
+    // check if response after keyword is set
+    if (text.split(" ").length <= 1) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: You need to specify points');
+        return;
+    }
+    
+    var user   = text.split(" ")[0],
+        points = parseInt(text.replace(user,'').trim()),
+        self   = this;
+    
+    if (!Number.isInteger(points)) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: Cannot set NaN points.');
+        return;
+    }
+    
+    database.findOne({ username: user }, function (err, item) {
+        if (typeof item === undefined || item === null) {
+            database.insert({username: user, points: points});
+        } else {
+            database.update({username: user}, {$set: {points: points}}, {});
+        }
+        global.client.action(global.configuration.get().twitch.owner, 'I just set ' + points + ' Points to ' + user);
+    });
+}
+
+Points.prototype.givePoints = function(user, text) {
+    if (text.length < 1) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: You need to specify user');
+        return;
+    }
+    
+    // check if response after keyword is set
+    if (text.split(" ").length <= 1) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: You need to specify points');
+        return;
+    }
+    
+    var user2  = text.split(" ")[0],
+        points = parseInt(text.replace(user2,'').trim()),
+        self   = this;
+        
+    if (!Number.isInteger(points)) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: Cannot set NaN points.');
+        return;
+    }
+    
+    database.findOne({ username: user.username }, function (err, item) {
+        if (typeof item === undefined || item === null) {
+            global.client.action(global.configuration.get().twitch.owner, 'You, ' + user.username + ', cannot give points as you have none.');
+        } else {
+            if (parseInt(item.points) < points) {
+                global.client.action(global.configuration.get().twitch.owner, 'You, ' + user.username + ', don\'t have enough points.');
+            }
+            database.findOne({ username: user2 }, function (err, item) {
+                if (typeof item === undefined || item === null) {
+                    database.insert({username: user2, points: points});
+                } else {
+                    database.update({username: user2}, {$set: {points: parseInt(item.points) + points}}, {});
+                }
+            });
+            database.update({username: user.username}, {$set: {points: parseInt(item.points) - points}}, {});
+            global.client.action(global.configuration.get().twitch.owner, user.username + ' just gave ' + points + ' Points to ' + user2);
+        }
+    });
+    
+}
+
+Points.prototype.getPointsFromUser = function(user, text) {
+    if (text.length < 1) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: You need to specify user');
+        return;
+    }
+    
+    var user = text.trim(),
+        self   = this;
+        
+    database.findOne({ username: user }, function (err, item) {
+        // TODO - create a function as this is used a lot
+        var points = (typeof item !== undefined && item !== null ? item.points : 0),
+            responsePattern = global.configuration.get().systems.pointsResponse,
+            pointsNames = global.configuration.get().systems.pointsName.split('|');
+            
+        var single, multi, xmulti;
+        // get single|x:multi|multi from pointsName
+        switch (pointsNames.length) {
+            case 0:
+                xmulti = null;
+                single = 'Point';
+                multi = 'Points';
+                break;
+            case 1:
+                xmulti = null;
+                single = multi = pointsNames[0];
+                break;
+            case 2:
+                single = pointsNames[0];
+                multi = pointsNames[1];
+                xmulti = null;
+                break;
+            default:
+                var len = pointsNames.length;
+                single = pointsNames[0]
+                multi = pointsNames[len-1];
+                xmulti = {}
+                
+                for (var pattern in pointsNames) {
+                    if (pointsNames.hasOwnProperty(pattern) && pattern != 0 && pattern != len-1) {
+                        var maxPts = pointsNames[pattern].split(':')[0],
+                            name   = pointsNames[pattern].split(':')[1];
+                        xmulti[maxPts] = name;    
+                    }
+                }
+                break;     
+        }        
+        
+        var pointsName = (points == 1 ? single : multi)
+        if (typeof xmulti === 'object' && points > 1 && points <= 10) {
+            for (var i=points; i<=10; i++) { 
+                if (typeof xmulti[i] === 'string') {
+                    pointsName = xmulti[i];
+                    break;
+                }
+            }
+        }
+        
+        global.client.action(global.configuration.get().twitch.owner, 
+            responsePattern.replace('(sender)', user).replace('(amount)', points + ' ' + pointsName));
+    });
+}
+
+Points.prototype.allPoints = function(user, text) {
+    if (text.length < 1) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: You need to specify points');
+        return;
+    }
+    
+    var points = parseInt(text.trim()),
+        self   = this;
+    
+    if (!Number.isInteger(points)) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: Cannot add NaN points.');
+        return;
+    }
+    
+    database.find({ isOnline: true }, function (err, items) {
+        items.forEach(function(e, i, ar) {
+            database.update({username: e.username}, {$set: {points: parseInt(e.points) + points}}, {});
+        });
+        global.client.action(global.configuration.get().twitch.owner, 'I just added ' + points + ' Points to all users');
+    });
+}
+
+Points.prototype.rainPoints = function(user, text) {
+    if (text.length < 1) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: You need to specify max points');
+        return;
+    }
+    
+    var points = parseInt(text.trim()),
+        self   = this;
+    
+    if (!Number.isInteger(points)) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: Cannot add NaN points.');
+        return;
+    }
+    
+    database.find({ isOnline: true }, function (err, items) {
+        items.forEach(function(e, i, ar) {
+            var random = Math.floor(Math.random() * points);
+            database.update({username: e.username}, {$set: {points: parseInt(e.points) + random}}, {});
+        });
+        global.client.action(global.configuration.get().twitch.owner, 'I just added 0-' + points + ' Points to all users');
+    });
+}
+
+Points.prototype.addPoints = function(user, text) {
+    if (text.length < 1) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: You need to specify user');
+        return;
+    }
+    
+    // check if response after keyword is set
+    if (text.split(" ").length <= 1) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: You need to specify points');
+        return;
+    }
+    
+    var user   = text.split(" ")[0],
+        points = parseInt(text.replace(user,'').trim()),
+        self   = this;
+    
+    if (!Number.isInteger(points)) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: Cannot add NaN points.');
+        return;
+    }
+    
+    database.findOne({ username: user }, function (err, item) {
+        if (typeof item === undefined || item === null) {
+            database.insert({username: user, points: points});
+        } else {
+            database.update({username: user}, {$set: {points: parseInt(item.points) + points}}, {});
+        }
+        global.client.action(global.configuration.get().twitch.owner, 'I just added ' + points + ' Points to ' + user);
+    });
+}
+
+Points.prototype.removePoints = function(user, text) {
+    if (text.length < 1) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: You need to specify user');
+        return;
+    }
+    
+    // check if response after keyword is set
+    if (text.split(" ").length <= 1) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: You need to specify points');
+        return;
+    }
+    
+    var user   = text.split(" ")[0],
+        points = parseInt(text.replace(user,'').trim()),
+        self   = this;
+    
+    if (!Number.isInteger(points)) {
+        global.client.action(global.configuration.get().twitch.owner, 'Points error: Cannot remove NaN points.');
+        return;
+    }
+    
+    database.findOne({ username: user }, function (err, item) {
+        if (typeof item === undefined || item === null) {
+            database.insert({username: user, points: points});
+        } else {
+            if (parseInt(item.points) - points < 0) { points = item.points };
+            database.update({username: user}, {$set: {points: parseInt(item.points) - points}}, {});
+        }
+        global.client.action(global.configuration.get().twitch.owner, 'I just removed ' + points + ' Points from ' + user);
     });
 }
 
@@ -89,7 +343,6 @@ Points.prototype.getPoints = function(user) {
         var pointsName = (points == 1 ? single : multi)
         if (typeof xmulti === 'object' && points > 1 && points <= 10) {
             for (var i=points; i<=10; i++) { 
-                console.log(typeof xmulti[i])
                 if (typeof xmulti[i] === 'string') {
                     pointsName = xmulti[i];
                     break;
