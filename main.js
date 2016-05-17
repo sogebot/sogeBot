0fe7e41766e228a0d93f21c391aa096c16f38986
Michal Orlik <orlikmi@gmail.com>
2016-05-17 12:02:11 +0200

    main -> standard compliant

--- main.js
+++ main.js
@@ -1,46 +1,44 @@
-
-'use strict';
+'use strict'
 
 // 3rd party libraries
-var chalk = require('chalk'),
-    irc   = require('tmi.js');
+var irc = require('tmi.js')
 
 // bot libraries
-var Configuration = require('./libs/configuration'),
-    Parser        = require('./libs/parser');
-    
-global.configuration = new Configuration;
-global.parser        = new Parser;
-    
+var Configuration = require('./libs/configuration')
+var Parser = require('./libs/parser')
+
+global.configuration = new Configuration()
+global.parser = new Parser()
+
 // bot systems
-global.systems = require('auto-load')('./libs/systems/');
+global.systems = require('auto-load')('./libs/systems/')
 
 var options = {
-    options: {
-        debug: true
-    },
-    connection: {
-        cluster: configuration.get().twitch.cluster,
-        reconnect: true
-    },
-    identity: {
-        username: configuration.get().twitch.username,
-        password: configuration.get().twitch.password
-    },
-    channels: ["#"+configuration.get().twitch.owner]
-};
+  options: {
+    debug: true
+  },
+  connection: {
+    cluster: global.configuration.get().twitch.cluster,
+    reconnect: true
+  },
+  identity: {
+    username: global.configuration.get().twitch.username,
+    password: global.configuration.get().twitch.password
+  },
+  channels: ['#' + global.configuration.get().twitch.owner]
+}
 
-global.client = new irc.client(options);
+global.client = new irc.client(options)
 
 // Connect the client to the server..
-client.connect()
+global.client.connect()
 
-client.on("connected", function (address, port) {
-    global.client.raw("CAP REQ :twitch.tv/commands");
-    global.client.raw("CAP REQ :twitch.tv/membership");
-    client.color("Firebrick");
-});
+global.client.on('connected', function (address, port) {
+  global.client.raw('CAP REQ :twitch.tv/commands')
+  global.client.raw('CAP REQ :twitch.tv/membership')
+  global.client.color('Firebrick')
+})
 
-client.on("chat", function (channel, user, message, self) {
-    parser.parse(user, message);
-});
+global.client.on('chat', function (channel, user, message, self) {
+  global.parser.parse(user, message)
+})
