67d4770c10485fc0bddd19233fc883630fe6146d
Michal Orlik <sogehige@gmail.com>
2016-05-20 12:28:35 +0200

    #17 - no db dir is required anymore

--- launch.sh
+++ launch.sh
@@ -1,11 +1,7 @@
-#!/bin/bash  
+#!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-
-if [ ! -d "$DIR/db" ]; then
-    mkdir $DIR/db
-fi
 
 if [ ! -d "$DIR/node_modules" ]; then
     npm install
