6258b58c2c6f0101db14e55b741a1f1be0a9f52f
sogehige <sogehige@gmail.com>
2016-05-26 18:23:18 +0200

    Added default loudness -15, when those data are not available on YT

--- libs/systems/songs.js
+++ libs/systems/songs.js
@@ -145,7 +145,7 @@ Songs.prototype.sendMeanLoudness = function (socket) {
   var count = 0
   global.botDB.find({type: 'playlist'}).exec(function (err, items) {
     if (err) console.log(err)
-    _.each(items, function (item) { count = count + 1; loudness = loudness + parseFloat(item.loudness) })
+    _.each(items, function (item) { (typeof item.loudness == 'undefined') ? loudness = loudness + -15 : loudness = loudness + parseFloat(item.loudness); count = count + 1; })
     socket.emit('meanLoudness', loudness / count)
   })
 }
