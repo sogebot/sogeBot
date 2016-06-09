21fe9f0de7770559e6ce1085cb31b4d546d45717
Michal Orlik <sogehige@gmail.com>
2016-06-09 14:05:22 +0200

    Added !uptime command ; updates npm packages

--- libs/twitch.js
+++ libs/twitch.js
@@ -1,7 +1,12 @@
 'use strict'
 
+var constants = require('./constants')
+var moment = require('moment')
+require('moment-precise-range-plugin')
+
 function Twitch () {
   this.isOnline = false
+  this.whenOnline = moment()
 
   var self = this
   setInterval(function () {
@@ -10,14 +15,27 @@ function Twitch () {
       json: true
     }, function (err, res, body) {
       if (err) console.log(err)
-      if (body.stream) self.isOnline = true
-      else self.isOnline = false
+      if (body.stream) {
+        if (!self.isOnline) self.whenOnline = moment()
+        self.isOnline = true
+      } else self.isOnline = false
     })
-  }, 60000)
+  }, 15000)
+
+  global.parser.register(this, '!uptime', this.uptime, constants.VIEWERS)
 }
 
 Twitch.prototype.isOnline = function () {
   return this.isOnline
+}
+
+Twitch.prototype.uptime = function (self, sender) {
+  var now = moment().preciseDiff(self.whenOnline, true)
+  var days = now.days > 0 ? now.days+'d' : ''
+  var hours = now.hours > 0 ? now.hours+'h' : ''
+  var minutes = now.minutes > 0 ? now.minutes+'m' : ''
+  var seconds = now.seconds > 0 ? now.seconds+'s' : ''
+  global.commons.sendMessage(self.isOnline ? global.translate('core.online').replace('(time)', days + hours + minutes + seconds) : global.translate('core.offline'))
 }
 
 module.exports = Twitch
