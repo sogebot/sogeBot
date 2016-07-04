30ee10a8142c5da29f1e57d05b0e25fe85f92590
Michal Orlik <sogehige@gmail.com>
2016-07-04 12:00:30 +0200

    user: fixed undefined id

--- libs/user.js
+++ libs/user.js
@@ -8,7 +8,8 @@ global.botDB.update({$where: function () { return this._id.startsWith('user') }}
 
 function User (username) {
   this.loaded = false
-  this.data = {username: username}
+  this.username = username
+  this.data = {}
   this.loadFromDB(username)
 }
 
@@ -25,7 +26,7 @@ User.prototype.loadFromDB = function (username) {
 User.prototype.isLoaded = function () {
   var self = this
   return new Promise(function (resolve, reject) {
-    global.botDB.findOne({_id: 'user_' + self.data.username}, function (err, item) {
+    global.botDB.findOne({_id: 'user_' + self.username}, function (err, item) {
       if (err) reject(err)
       if (!_.isNull(item)) self.data = item
       self.loaded = true
@@ -35,12 +36,12 @@ User.prototype.isLoaded = function () {
 }
 
 User.prototype.setOnline = function () {
-  global.botDB.update({_id: 'user_' + this.data.username}, {$set: {isOnline: true}}, {upsert: true})
+  global.botDB.update({_id: 'user_' + this.username}, {$set: {isOnline: true}}, {upsert: true})
   this.data.isOnline = true
 }
 
 User.prototype.setOffline = function () {
-  global.botDB.update({_id: 'user_' + this.data.username}, {$set: {isOnline: false, partedTime: new Date().getTime()}}, {upsert: true})
+  global.botDB.update({_id: 'user_' + this.username}, {$set: {isOnline: false, partedTime: new Date().getTime()}}, {upsert: true})
   this.data.isOnline = false
 }
 
@@ -51,7 +52,7 @@ User.prototype.get = function (attr) {
 User.prototype.set = function (attr, value) {
   var toUpdate = {}
   toUpdate[attr] = value
-  global.botDB.update({_id: 'user_' + this.data.username}, {$set: toUpdate}, {upsert: true})
+  global.botDB.update({_id: 'user_' + this.username}, {$set: toUpdate}, {upsert: true})
   this.data[attr] = value
 }
 
