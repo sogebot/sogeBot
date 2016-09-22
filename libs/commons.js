635315937b7e291403abe0612c683abfc84b0855
Michal Orlik <sogehige@gmail.com>
2016-09-22 12:50:32 +0200

    commons: now it should not send anything from dashboard

--- libs/commons.js
+++ libs/commons.js
@@ -89,8 +89,8 @@ Commons.prototype.runCallback = function (cb, data) {
 }
 
 Commons.prototype.sendMessage = function (message, sender) {
-  // if sender is null, we can assume, that username is from dashboard -> bot
-  if (_.isNull(sender) || (!_.isUndefined(sender) && sender.username === global.configuration.get().username)) return // we don't want to reply on bot commands
+  // if sender is null/undefined, we can assume, that username is from dashboard -> bot
+  if (_.isUndefined(sender) || _.isNull(sender) || (!_.isUndefined(sender) && sender.username === global.configuration.get().username)) return // we don't want to reply on bot commands
   message = !_.isUndefined(sender) && !_.isUndefined(sender.username) ? message.replace('(sender)', '@' + sender.username) : message
   global.client.say(global.configuration.get().twitch.owner, message)
 }
