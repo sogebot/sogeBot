7fa40448e32a1446aea5d5f9b131ed96f3f4efc2
Michal Orlik <sogehige@gmail.com>
2016-09-29 15:23:28 +0200

    panel: finished first iteration of stats

--- libs/stats.js
+++ libs/stats.js
@@ -18,18 +18,40 @@ function Stats () {
 Stats.prototype.webPanel = function () {
   global.panel.addMenu({category: 'main', name: 'Stats', id: 'stats'})
   global.panel.socketListening(this, 'getLatestStats', this.getLatestStats)
+  global.panel.socketListening(this, 'getStatsData', this.getStatsData)
+  global.panel.socketListening(this, 'getCalendarData', this.getCalendarData)
+  global.panel.socketListening(this, 'getStreamEvents', this.getStreamEvents)
 }
 
 Stats.prototype.save = function (data) {
-  if (data.timestamp - this.latestTimestamp >= 150000) {
+  if (data.timestamp - this.latestTimestamp >= 300000) {
     statsDB.update({ _id: data.whenOnline }, { $push: { stats: data } }, { upsert: true }, function () {})
     this.latestTimestamp = data.timestamp
   }
 }
 
-Stats.prototype.get = function (id) {
-  statsDB.findOne({ _id: id }).sort({ timestamp: -1 }).exec(function (err, item) {
+Stats.prototype.getStreamEvents = function (self, socket, data) {
+  statsDB.find({$where: function () { return this._id.startsWith(data) }}, function (err, items) {
+    if (err) { global.log.error(err) }
+    socket.emit('streamData', items)
+  })
+}
+
+Stats.prototype.getStatsData = function (self, socket, data) {
+  statsDB.findOne({ _id: data }).sort({ timestamp: -1 }).exec(function (err, item) {
     if (err) global.log.error(err)
+    socket.emit('statsData', item)
+  })
+}
+
+Stats.prototype.getCalendarData = function (self, socket, data) {
+  statsDB.find({}).exec(function (err, items) {
+    if (err) global.log.error(err)
+    var ids = []
+    _.each(items, function (item) {
+      ids.push(item._id)
+    })
+    socket.emit('calendarData', ids)
   })
 }
 
