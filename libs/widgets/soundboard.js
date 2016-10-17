b5ca41c6b350a0be9926b70d70414d6371eb061c
Michal Orlik <sogehige@gmail.com>
2016-10-17 11:04:50 +0200

    soundboard - added widget

---
+++ libs/widgets/soundboard.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var _ = require('lodash')
+var log = global.log
+var glob = require('glob')
+
+function SoundboardWidget () {
+  global.panel.addWidget('soundboard', 'SoundBoard', 'music')
+
+  global.panel.socketListening(this, 'getSoundBoardSounds', this.getSoundBoardSounds)
+}
+
+SoundboardWidget.prototype.getSoundBoardSounds = function (self, socket) {
+  glob('public/dist/soundboard/*.mp3', function (err, files) {
+    if (err) return
+
+    var sounds = []
+    _.each(files, function (file) {
+      sounds.push(file.split('/').pop().replace('.mp3', ''))
+    })
+    socket.emit('soundBoardSounds', sounds)
+  })
+}
+
+module.exports = new SoundboardWidget()
