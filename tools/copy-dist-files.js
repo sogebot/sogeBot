3fe8b46091ed3bae65ec838988ce6f5948e0b191
Michal Orlik <sogehige@gmail.com>
2016-12-08 15:38:53 +0100

    Removed some CDNs and added npm dependencies for jquery and bootstrap

---
+++ tools/copy-dist-files.js
@@ -0,0 +1,51 @@
+'use strict'
+var fs = require('fs')
+var path = require('path')
+var _ = require('lodash')
+
+var dist = {
+  'bootstrap': {
+    'js': 'dist/js/bootstrap.min.js',
+    'css': 'dist/css/bootstrap.min.css',
+    'fonts': [
+      'dist/fonts/glyphicons-halflings-regular.eot',
+      'dist/fonts/glyphicons-halflings-regular.svg',
+      'dist/fonts/glyphicons-halflings-regular.ttf',
+      'dist/fonts/glyphicons-halflings-regular.woff',
+      'dist/fonts/glyphicons-halflings-regular.woff2'
+    ]
+  },
+  'jquery': {
+    'js': 'dist/jquery.min.js'
+  },
+  'lodash': {
+    'js': 'lodash.min.js'
+  },
+  'bootstrap-toggle': {
+    'js': 'js/bootstrap-toggle.min.js',
+    'css': 'css/bootstrap-toggle.min.css'
+  }
+}
+
+fs.mkdirRecursive = function (dirPath) {
+  var directories = dirPath.split('/')
+  _.each(directories, function (directory, index) {
+    var dirsToAppend = []
+    for (var i = 0; i < index; i++) {
+      dirsToAppend.push(directories[i])
+    }
+    if (!fs.existsSync(path.join(dirsToAppend.join('/'), directory))) fs.mkdirSync(path.join(dirsToAppend.join('/'), directory))
+  })
+}
+
+_.each(dist, function (aList, aName) {
+  _.each(aList, function (aFiles, aType) {
+    if (typeof aFiles === 'string') aFiles = [aFiles]
+    aFiles.map(function (x, i, ar) { ar[i] = ['node_modules', aName, ar[i]].join('/') })
+    _.each(aFiles, function (aFile) {
+      fs.mkdirRecursive(['public', 'dist', aName, aType].join('/'))
+      fs.createReadStream(aFile)
+        .pipe(fs.createWriteStream([['public', 'dist', aName, aType].join('/'), aFile.split('/')[aFile.split('/').length - 1]].join('/')))
+    })
+  })
+})
