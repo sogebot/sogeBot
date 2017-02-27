b233bbbc5d2b971236271de6dacb4606734d6f99
Michal Orlik <sogehige@gmail.com>
2017-02-27 17:44:34 +0100

    alias: proper use of watcher

--- libs/systems/alias.js
+++ libs/systems/alias.js
@@ -8,6 +8,7 @@ var constants = require('../constants')
 var log = global.log
 
 function Alias () {
+  this.alias = []
   if (global.commons.isSystemEnabled(this)) {
     global.parser.register(this, '!alias add', this.add, constants.OWNER_ONLY)
     global.parser.register(this, '!alias list', this.list, constants.OWNER_ONLY)
@@ -18,8 +19,27 @@ function Alias () {
 
     global.parser.registerParser(this, 'alias', this.parse, constants.VIEWERS)
 
+    global.watcher.watch(this, 'alias', this._save)
+    this._update(this)
+
     this.webPanel()
   }
+}
+
+Alias.prototype._update = function (self) {
+  global.botDB.findOne({ _id: 'alias' }, function (err, item) {
+    if (err) return log.error(err)
+    if (_.isNull(item)) return
+
+    self.alias = item.alias
+  })
+}
+
+Alias.prototype._save = function (self) {
+  var alias = {
+    alias: self.alias
+  }
+  global.botDB.update({ _id: 'alias' }, { $set: alias }, { upsert: true })
 }
 
 Alias.prototype.webPanel = function () {
@@ -30,10 +50,7 @@ Alias.prototype.webPanel = function () {
 }
 
 Alias.prototype.sendAliases = function (self, socket) {
-  global.botDB.find({$where: function () { return this._id.startsWith('alias') }}, function (err, docs) {
-    if (err) { log.error(err) }
-    socket.emit('Alias', docs)
-  })
+  socket.emit('Alias', self.alias)
 }
 
 Alias.prototype.deleteAlias = function (self, socket, data) {
@@ -52,30 +69,44 @@ Alias.prototype.help = function (self, sender) {
 
 Alias.prototype.add = function (self, sender, text) {
   try {
-    var parsed = text.match(/^([\u0500-\u052F\u0400-\u04FF\w]+) ([\u0500-\u052F\u0400-\u04FF\w]+)$/)
-    global.commons.insertIfNotExists({__id: 'alias_' + parsed[2], _alias: parsed[2], command: parsed[1], success: 'alias.success.add', error: 'alias.failed.add'})
+    let parsed = text.match(/^([\u0500-\u052F\u0400-\u04FF\w]+) ([\u0500-\u052F\u0400-\u04FF\w]+)$/)
+    let data = {
+      alias: parsed[2],
+      command: parsed[1]
+    }
+    // check if alias is already created
+    var unique = true
+    _.each(self.alias, function (alias) {
+      if (!unique) return
+      if (alias.alias === data.alias) unique = false
+    })
+
+    if (unique) self.alias.push(data)
+    global.commons.sendMessage(global.translate(unique ? 'alias.success.add' : 'alias.failed.add'), sender)
   } catch (e) {
     global.commons.sendMessage(global.translate('alias.failed.parse'), sender)
   }
 }
 
 Alias.prototype.list = function (self, sender, text) {
-  var parsed = text.match(/^([\u0500-\u052F\u0400-\u04FF\w]+)$/)
-  if (_.isNull(parsed)) {
-    global.botDB.find({$where: function () { return this._id.startsWith('alias') }}, function (err, docs) {
-      if (err) { log.error(err) }
-      var list = []
-      docs.forEach(function (e, i, ar) { list.push('!' + e.alias) })
-      var output = (docs.length === 0 ? global.translate('alias.failed.list') : global.translate('alias.success.list') + ': ' + list.join(', '))
-      global.commons.sendMessage(output, sender)
-    })
-  } else global.commons.sendMessage(global.translate('alias.failed.parse'), sender)
+  var aliases = []
+  _.each(self.alias, function (element) { aliases.push('!' + element.alias) })
+  var output = (aliases.length === 0 ? global.translate('alias.failed.list') : global.translate('alias.success.list') + ': ' + aliases.join(', '))
+  global.commons.sendMessage(output, sender)
 }
 
 Alias.prototype.remove = function (self, sender, text) {
   try {
-    var parsed = text.match(/^([\u0500-\u052F\u0400-\u04FF\w]+)$/)
-    global.commons.remove({__id: 'alias_' + parsed[1], success: 'alias.success.remove', error: 'alias.failed.remove'})
+    let alias = text.match(/^([\u0500-\u052F\u0400-\u04FF\w]+)$/)[1]
+
+    var removed = false
+    self.alias.forEach(function (element, key) {
+      if (element.alias === alias) {
+        removed = true
+        self.alias.splice(key, 1)
+      }
+    }, this)
+    global.commons.sendMessage(global.translate(removed ? 'alias.success.remove' : 'alias.failed.remove'), sender)
   } catch (e) {
     global.commons.sendMessage(global.translate('alias.failed.parse'), sender)
   }
@@ -85,14 +116,17 @@ Alias.prototype.parse = function (self, id, sender, text) {
   try {
     var parsed = text.match(/^!([\u0500-\u052F\u0400-\u04FF\w]+) ?(.*)$/)
     var cmd = parsed[1]
-    global.botDB.findOne({$where: function () { return this._id.startsWith('alias_') && cmd === this.alias }}, function (err, item) {
-      if (err) log.error(err)
-      if (!_.isNull(item)) {
-        global.parser.parse(sender, text.replace('!' + item.alias, '!' + item.command))
+
+    var found = false
+    _.each(self.alias, function (alias) {
+      if (found) return
+      if (alias.alias === cmd) {
+        found = true
+        global.parser.parse(sender, text.replace('!' + cmd, '!' + alias.command))
         global.parser.lineParsed--
       }
-      global.updateQueue(id, _.isNull(item))
     })
+    global.updateQueue(id, true)
   } catch (e) {
     global.updateQueue(id, true)
   }
