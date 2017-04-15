58f0b728074f67d835ef5012ef47b2da44689c1f
Michal Orlik <sogehige@gmail.com>
2017-04-16 01:31:29 +0200

    fixed tests

--- libs/parser.js
+++ libs/parser.js
@@ -85,9 +85,9 @@ Parser.prototype.processQueue = async function (id) {
 }
 
 Parser.prototype.parseCommands = async function (user, message) {
-  message = message.trim().toLowerCase()
+  message = message.trim()
   for (var cmd in this.registeredCmds) {
-    if (message.startsWith(cmd)) {
+    if (message.toLowerCase().startsWith(cmd.toLowerCase())) {
       if (this.permissionsCmds[cmd] === constants.DISABLE) break
       if (_.isNil(user) || // if user is null -> we are running command through a bot
         (this.permissionsCmds[cmd] === constants.VIEWERS) ||
