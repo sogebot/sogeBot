e6fb098423b1130c4785e1ba0df39709ba8a5c2c
Michal Orlik <sogehige@gmail.com>
2017-06-07 17:59:01 +0200

    libs/commons - remove first char uppercase

--- libs/commons.js
+++ libs/commons.js
@@ -117,7 +117,6 @@ Commons.prototype.sendMessage = async function (message, sender, attr = {}) {
   if (_.isUndefined(sender) || _.isNull(sender) || (!_.isUndefined(sender) && sender.username === global.configuration.get().twitch.username && !attr.force)) return false // we don't want to reply on bot commands
   message = !_.isUndefined(sender) && !_.isUndefined(sender.username) ? message.replace('(sender)', (global.configuration.getValue('atUsername') ? '@' : '') + sender.username) : message
   if (!global.configuration.getValue('mute') || attr.force) {
-    message = message.charAt(0).toUpperCase() + message.slice(1)
     sender['message-type'] === 'whisper' ? global.log.whisperOut(message, {username: sender.username}) : global.log.chatOut(message, {username: sender.username})
     sender['message-type'] === 'whisper' ? global.client.whisper(sender.username, message) : global.client.say(global.configuration.get().twitch.channel, message)
   }
