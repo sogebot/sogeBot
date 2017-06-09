242c79c034742aff9e713937230dca4b1ee1385f
Michal Orlik <sogehige@gmail.com>
2017-06-09 14:39:21 +0200

    systems/points - add points reset button to webpanel

--- libs/systems/points.js
+++ libs/systems/points.js
@@ -47,10 +47,15 @@ Points.prototype.webPanel = function () {
 
   global.panel.socketListening(this, 'setPoints', this.setSocket)
   global.panel.socketListening(this, 'getPointsConfiguration', this.sendConfiguration)
+  global.panel.socketListening(this, 'resetPoints', this.resetPoints)
 }
 
 Points.prototype.setSocket = function (self, socket, data) {
   self.setPoints(self, {username: global.configuration.get().twitch.channel}, data.username + ' ' + data.value) // we want to show this in chat
+}
+
+Points.prototype.resetPoints = function (self, socket, data) {
+  global.users.setAll({points: 0})
 }
 
 Points.prototype.sendConfiguration = function (self, socket) {
