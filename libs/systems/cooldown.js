646bea589d6263c21795a9d49d423657b5f4b038
Michal Orlik <sogehige@gmail.com>
2017-06-16 13:35:23 +0200

    systems/cooldown - toggle cooldown and edit in webpanel

--- libs/systems/cooldown.js
+++ libs/systems/cooldown.js
@@ -9,6 +9,7 @@ var log = global.log
 
 /*
  * !cooldown [command] [seconds] [true/false] - set cooldown for command - 0 for disable, true/false set quiet mode
+ * !cooldown toggle [command]                 - enable/disable specified command cooldown
  */
 
 function Cooldown () {
@@ -17,6 +18,7 @@ function Cooldown () {
   this.viewers = {}
 
   if (global.commons.isSystemEnabled(this)) {
+    global.parser.register(this, '!cooldown toggle', this.toggle, constants.OWNER_ONLY)
     global.parser.register(this, '!cooldown', this.set, constants.OWNER_ONLY)
     global.parser.registerParser(this, '0-cooldown', this.check, constants.VIEWERS)
 
@@ -33,6 +35,14 @@ Cooldown.prototype.webPanel = function () {
   global.panel.addMenu({category: 'manage', name: 'commands-cooldowns', id: 'cooldown'})
   global.panel.socketListening(this, 'cooldown.get', this.sSend)
   global.panel.socketListening(this, 'cooldown.set', this.sSet)
+  global.panel.socketListening(this, 'cooldown.edit', this.sEdit)
+  global.panel.socketListening(this, 'cooldown.toggle', this.sToggle)
+}
+
+Cooldown.prototype.sEdit = function (self, socket, data) {
+  if (data.seconds.length === 0 || parseInt(data.seconds, 10) === 0) self.sSet(self, socket, {command: data.id, seconds: 0})
+  else _.find(self.list, function (o, k) { return k === data.id }).miliseconds = parseInt(data.seconds, 10) * 1000
+  self.sSend(self, socket)
 }
 
 Cooldown.prototype.sSend = function (self, socket) {
@@ -42,6 +52,12 @@ Cooldown.prototype.sSend = function (self, socket) {
 Cooldown.prototype.sSet = function (self, socket, data) {
   data.type = _.isUndefined(data.type) ? 'global' : data.type
   self.set(self, null, [data.command, data.type, data.seconds, data.quiet].join(' '))
+  self.sSend(self, socket)
+}
+
+Cooldown.prototype.sToggle = function (self, socket, data) {
+  console.log(data)
+  self.toggle(self, null, data)
   self.sSend(self, socket)
 }
 
@@ -68,7 +84,7 @@ Cooldown.prototype.set = function (self, sender, text) {
 
   try {
     match = text.match(/^([\u0500-\u052F\u0400-\u04FF\w]+) (global|user) (\d+) ?(\w+)?/)
-    data = {'command': match[1], 'seconds': match[3], 'type': match[2], 'quiet': match[4] !== 'false'}
+    data = {'command': match[1], 'seconds': match[3], 'type': match[2], 'quiet': match[4] !== 'false', 'enabled': true}
   } catch (e) {
     global.commons.sendMessage(global.translate('cooldown.failed.parse'), sender)
     return
@@ -76,7 +92,7 @@ Cooldown.prototype.set = function (self, sender, text) {
 
   delete self.list[data.command]
   if (parseInt(data.seconds, 10) !== 0) {
-    self.list[data.command] = { 'miliseconds': data.seconds * 1000, 'type': data.type, 'timestamp': 0, 'quiet': data.quiet }
+    self.list[data.command] = { 'miliseconds': data.seconds * 1000, 'type': data.type, 'timestamp': 0, 'quiet': data.quiet, 'enabled': data.enabled }
     global.commons.sendMessage(global.translate('cooldown.success.set')
       .replace('(command)', data.command)
       .replace('(type)', data.type)
@@ -97,8 +113,8 @@ Cooldown.prototype.check = function (self, id, sender, text) {
 
   try {
     match = text.match(/^!([\u0500-\u052F\u0400-\u04FF\w]+)/)
-    data = {'command': match[1], 'miliseconds': self.list[match[1]].miliseconds, 'type': self.list[match[1]].type, 'timestamp': self.list[match[1]].timestamp, 'quiet': self.list[match[1]].quiet}
-    if (_.isUndefined(data.miliseconds)) throw Error()
+    data = {'command': match[1], 'miliseconds': self.list[match[1]].miliseconds, 'type': self.list[match[1]].type, 'timestamp': self.list[match[1]].timestamp, 'quiet': self.list[match[1]].quiet, 'enabled': self.list[match[1]].enabled}
+    if (_.isUndefined(data.miliseconds) || !data.enabled) throw Error()
   } catch (e) {
     global.updateQueue(id, true)
     return
@@ -128,6 +144,24 @@ Cooldown.prototype.check = function (self, id, sender, text) {
         .replace('(seconds)', Math.ceil((data.miliseconds - now + timestamp) / 1000)), sender)
     }
     global.updateQueue(id, false)
+  }
+}
+
+Cooldown.prototype.toggle = function (self, sender, text) {
+  try {
+    let parsed = text.match(/^([\u0500-\u052F\u0400-\u04FF\w\S]+)$/)[1]
+    let cooldown = _.find(self.list, function (o, k) { return k === parsed })
+    if (_.isUndefined(cooldown)) {
+      global.commons.sendMessage(global.translate('cooldown.failed.toggle')
+        .replace('(command)', parsed), sender)
+      return
+    }
+
+    cooldown.enabled = !cooldown.enabled
+    global.commons.sendMessage(global.translate(cooldown.enabled ? 'cooldown.success.enabled' : 'cooldown.success.disabled')
+      .replace('(command)', parsed), sender)
+  } catch (e) {
+    global.commons.sendMessage(global.translate('cooldown.failed.parse'), sender)
   }
 }
 
