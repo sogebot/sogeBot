fb8bc86cf5a4edfd037b68b9ca64dddb0e8b4ae0
Michal Orlik <sogehige@gmail.com>
2017-06-16 12:16:57 +0200

    tools/cleanup - remove duplicate users

--- tools/cleanup.js
+++ tools/cleanup.js
@@ -52,7 +52,36 @@ async function clean_users_without_id () {
   console.log('Cleaned ' + (size - _.size(users.users) + ' users'))
 }
 
+async function clean_users_duplicates () {
+  let users = await DB.findOne({ _id: 'users' })
+  let uniqueUsernames = []
+
+  _.each(users.users, function (user) {
+    uniqueUsernames.push(user.username.toLowerCase())
+  })
+
+  let size = _.size(uniqueUsernames)
+
+  let newUsers = {}
+  for (var i = 0, len = uniqueUsernames.length; i < len; i++) {
+    newUsers[users.users[uniqueUsernames[i]].username] = users.users[uniqueUsernames[i]]
+  }
+  users = {
+    _id: 'users',
+    users: newUsers
+  }
+  await DB.remove({ _id: 'users' })
+  await DB.insert(users)
+
+  console.log('Cleaned ' + (size - _.size(_.uniq(uniqueUsernames)) + ' users'))
+
+}
+
 async function main () {
+  console.log('Cleaning up duplicates users')
+  await clean_users_duplicates()
+  console.log('- DONE')
+
   console.log('Cleaning up users without watched time')
   await clean_users()
   console.log('- DONE')
