6196ce0a9f003274b06d4c3e31c85d07afa2fae9
Michal Orlik <sogehige@gmail.com>
2017-09-29 21:37:43 -0700

    mongodb support

--- libs/databases/database.js
+++ libs/databases/database.js
@@ -3,11 +3,13 @@ const _ = require('lodash')
 const config = require('../../config.json')
 
 const INeDB = require('./nedb')
+const IMongoDB = require('./mongodb')
 
 class Database {
   constructor () {
     this.engine = null
     if (config.database.type === 'nedb') this.engine = new INeDB()
+    if (config.database.type === 'mongodb') this.engine = new IMongoDB()
 
     if (_.isNil(this.engine)) {
       global.log.warning('No database was selected - fallback to NeDB')
---
+++ libs/databases/mongodb.js
@@ -0,0 +1,102 @@
+const client = require('mongodb').MongoClient
+
+const Interface = require('./interface')
+const config = require('../../config.json')
+const flatten = require('flat')
+
+const _ = require('lodash')
+const debug = require('debug')('db:mongodb')
+
+class IMongoDB extends Interface {
+  constructor () {
+    super('mongodb')
+
+    this._connection = null
+
+    if (debug.enabled) debug('MongoDB initialized')
+  }
+
+  async connection () {
+    if (_.isNil(this._connection)) {
+      this._connection = await client.connect(config.database.mongodb.url)
+      debug(this._connection)
+    }
+    return this._connection
+  }
+
+  async find (table, where) {
+    where = where || {}
+
+    let db = await this.connection()
+    let collection = await db.collection(table)
+    let items = await collection.find(where)
+    return items.toArray()
+  }
+
+  async findOne (table, where) {
+    where = where || {}
+
+    let db = await this.connection()
+    let collection = await db.collection(table)
+    let item = await collection.findOne(where)
+    return item || {}
+  }
+
+  async insert (table, object) {
+    if (_.isEmpty(object)) throw Error('Object cannot be empty')
+
+    let db = await this.connection()
+    let collection = await db.collection(table)
+    let item = await collection.insert(object)
+    return _.size(item)
+  }
+
+  async increment (table, where, object) {
+    if (_.isEmpty(object)) throw Error('Object to update cannot be empty')
+
+    let db = await this.connection()
+    let collection = await db.collection(table)
+
+    let result = await collection.update(
+      where,
+      { $inc: flatten(object) }, {
+        upsert: true,
+        multi: _.isEmpty(where)
+      }
+    )
+    return result.nMatched
+  }
+
+  async remove (table, where) {
+    if (_.isEmpty(where)) throw Error('Object to delete cannot be empty')
+
+    let db = await this.connection()
+    let collection = await db.collection(table)
+    let result = await collection.remove(where)
+    return result.nRemoved
+  }
+
+  async update (table, where, object) {
+    if (_.isEmpty(object)) throw Error('Object to update cannot be empty')
+
+    if (debug.enabled) debug('update() \n\ttable: %s \n\twhere: %j', table, where)
+
+    let db = await this.connection()
+    let collection = await db.collection(table)
+
+    let result
+    if (_.size(where) === 0) {
+      result = await collection.updateMany({}, { $set: flatten(object) })
+    } else {
+      result = await collection.update(
+        where,
+        { $set: flatten(object) }, {
+          upsert: _.isNil(where._id)
+        }
+      )
+    }
+    return result.nMatched
+  }
+}
+
+module.exports = IMongoDB
