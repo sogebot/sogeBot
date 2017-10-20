018a23ee906410063f6db5e86443f032626a7f7a
Michal Orlik <sogehige@gmail.com>
2017-10-20 14:19:29 +0200

    tests: cleanup users

--- test/helpers/db.js
+++ test/helpers/db.js
@@ -14,5 +14,6 @@ module.exports = {
     await global.db.engine.remove('settings', {})
     await global.db.engine.remove('timers', {})
     await global.db.engine.remove('timersResponses', {})
+    await global.db.engine.remove('users', {})
   }
 }
