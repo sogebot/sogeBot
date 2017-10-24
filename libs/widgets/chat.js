3c187afd330c8dac59e95c91af6e8d696cf37cf7
Michal Orlik <sogehige@gmail.com>
2017-10-24 18:41:09 +0200

    widgets: add viewers list to chat widget

--- libs/widgets/chat.js
+++ libs/widgets/chat.js
@@ -1,11 +1,24 @@
 'use strict'
 
 const config = require('../../config.json')
+const fetch = require('snekfetch')
 
 function ChatWidget () {
   global.panel.addWidget('chat', 'widget-title-chat', 'comment')
   global.panel.socketListening(this, 'getChatRoom', this.sendChatRoom)
   global.panel.socketListening(this, 'chat.message.send', this.chatMessageSend)
+
+  this.refresh()
+  setInterval(() => this.refresh(), 30000)
+}
+
+ChatWidget.prototype.refresh = async (self, socket) => {
+  let url = `https://tmi.twitch.tv/group/user/${config.settings.broadcaster_username.toLowerCase()}/chatters`
+  let response = await fetch.get(url)
+
+  if (response.status === 200) {
+    global.panel.io.emit('chatChatters', { chatters: response.body.chatters, _total: response.body.chatter_count })
+  }
 }
 
 ChatWidget.prototype.sendChatRoom = function (self, socket) {
