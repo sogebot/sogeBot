fba5bc0e707a910dcf8e5c67762fd9316661301b
Michal Orlik <sogehige@gmail.com>
2017-11-12 17:56:46 +0100

    remove db cache

--- libs/databases/mongodb.js
+++ libs/databases/mongodb.js
@@ -4,7 +4,6 @@ const ObjectID = require('mongodb').ObjectID
 const Interface = require('./interface')
 const config = require('../../config.json')
 const flatten = require('flat')
-const cache = require('memory-cache')
 
 const _ = require('lodash')
 const debug = require('debug')('db:mongodb')
@@ -14,7 +13,6 @@ class IMongoDB extends Interface {
     super('mongodb')
 
     this._connection = {}
-    this.cache = {}
 
     if (debug.enabled) debug('MongoDB initialized')
   }
@@ -27,14 +25,7 @@ class IMongoDB extends Interface {
     return this._connection[table]
   }
 
-  on (table) {
-    if (_.isNil(this.cache[table])) {
-      this.cache[table] = new cache.Cache()
-    }
-  }
-
   async find (table, where) {
-    this.on(table) // init table
 
     where = where || {}
     if (!_.isNil(where._id)) {
@@ -47,14 +38,10 @@ class IMongoDB extends Interface {
     let collection = await db.collection(table)
     if (table === 'users') collection.createIndex({'_id': 1, 'username': 1})
     let items = await collection.find(where).toArray()
-    for (let item of items) {
-      this.cache[table].put(item._id, item)
-    }
     return items
   }
 
   async findOne (table, where) {
-    this.on(table) // init table
 
     where = where || {}
     if (!_.isNil(where._id)) {
@@ -63,24 +50,14 @@ class IMongoDB extends Interface {
       else return {}
     } else where = flatten(where)
 
-    // get from cache
-    var keys = this.cache[table].keys()
-    for (let key of keys) {
-      if (!_.isEmpty((_.filter(this.cache[table].get(key), where)))) {
-        return this.cache[table].get(key)
-      }
-    }
-
     let db = await this.connection(table)
     let collection = await db.collection(table)
     if (table === 'users') collection.createIndex({'_id': 1, 'username': 1})
     let item = await collection.findOne(where)
-    if (!_.isNil(item)) this.cache[table].put(item._id, item)
     return item || {}
   }
 
   async insert (table, object) {
-    this.on(table) // init table
 
     if (_.isEmpty(object)) throw Error('Object cannot be empty')
     delete object._id
@@ -90,12 +67,10 @@ class IMongoDB extends Interface {
     if (table === 'users') collection.createIndex({'_id': 1, 'username': 1})
     let item = await collection.insert(object)
 
-    this.cache[table].put(item._id, item)
     return item.ops[0]
   }
 
   async increment (table, where, object) {
-    this.on(table) // init table
 
     where = where || {}
     if (!_.isNil(where._id)) where._id = new ObjectID(where._id)
@@ -103,14 +78,6 @@ class IMongoDB extends Interface {
 
     if (_.isEmpty(object)) throw Error('Object to update cannot be empty')
     delete object._id
-
-    // invalidate cache on update
-    var keys = this.cache[table].keys()
-    for (let key of keys) {
-      if (!_.isEmpty((_.filter(this.cache[table].get(key), where)))) {
-        this.cache[table].del(key)
-      }
-    }
 
     let db = await this.connection(table)
     let collection = await db.collection(table)
@@ -127,18 +94,9 @@ class IMongoDB extends Interface {
   }
 
   async remove (table, where) {
-    this.on(table) // init table
 
     if (!_.isNil(where._id)) where._id = new ObjectID(where._id)
     else where = flatten(where)
-
-    // remove cache
-    var keys = this.cache[table].keys()
-    for (let key of keys) {
-      if (!_.isEmpty((_.filter(this.cache[table].get(key), where)))) {
-        this.cache[table].del(key)
-      }
-    }
 
     let db = await this.connection(table)
     let collection = await db.collection(table)
@@ -147,20 +105,11 @@ class IMongoDB extends Interface {
   }
 
   async update (table, where, object) {
-    this.on(table) // init table
 
     if (_.isEmpty(object)) throw Error('Object to update cannot be empty')
 
     if (!_.isNil(where._id)) where._id = new ObjectID(where._id)
     else where = flatten(where)
-
-    // invalidate cache on update
-    var keys = this.cache[table].keys()
-    for (let key of keys) {
-      if (!_.isEmpty((_.filter(this.cache[table].get(key), where)))) {
-        this.cache[table].del(key)
-      }
-    }
 
     if (debug.enabled) debug('update() \n\ttable: %s \n\twhere: %j', table, where)
 
