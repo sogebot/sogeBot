d5c37fc7d12772777263f3876874abdcd1d64254
Michal Orlik <sogehige@gmail.com>
2017-12-02 18:41:30 +0100

    tests: alias fix uptime change

--- test/tests/alias/run.js
+++ test/tests/alias/run.js
@@ -18,13 +18,12 @@ describe('Alias - run()', () => {
   it('!a will show !uptime', async () => {
     global.systems.alias.add(global.systems.alias, owner, '!a !uptime')
     await message.isSent('alias.alias-was-added', owner, { alias: 'a', command: 'uptime' })
-
     // force uptime to be 0 (bypass cached db)
     global.twitch.when.online = null
     global.twitch.when.offline = null
-
+    
     global.parser.parse(owner, '!a')
-    await message.isSent('core.offline', owner, { time: '' })
+    await message.isSent('uptime.offline', owner, { days: '', hours: '', minutes: '', seconds: '' })
 
     global.systems.alias.remove(global.systems.alias, owner, '!a')
     await message.isSent('alias.alias-was-removed', owner, { alias: 'a' })
