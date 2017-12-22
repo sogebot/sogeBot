54d676e4658ba662dd47d8e11844a59f76586d39
Michal Orlik <sogehige@gmail.com>
2017-12-22 03:18:59 +0100

    stats: cleanup >2h api cache
    _ref: #618

--- libs/stats.js
+++ libs/stats.js
@@ -1,5 +1,6 @@
 'use strict'
 var _ = require('lodash')
+const debug = require('debug')
 
 function Stats () {
   this.latestTimestamp = 0
@@ -29,9 +30,17 @@ Stats.prototype.getLatestStats = async function (self, socket) {
 }
 
 Stats.prototype.getApiStats = async function (self, socket) {
+  const d = debug('stats:getApiStats')
   let stats = await global.db.engine.find('APIStats')
   // return hour of data
   socket.emit('APIStats', _.filter(stats, (o) => _.now() - o.timestamp < 1000 * 60 * 60))
+
+  // remove data older than 2h
+  stats = _.filter(stats, (o) => _.now() - o.timestamp >= 2000 * 60 * 60)
+  d('Stats to delete: %j', stats)
+  for (let s of stats) {
+    await global.db.engine.remove('APIStats', { _id: s._id.toString() })
+  }
 }
 
 module.exports = Stats
