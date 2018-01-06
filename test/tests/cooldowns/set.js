a194f95cadb794d1f7955c96eb2cda6aa8cdbf1b
Michal Orlik <sogehige@gmail.com>
2018-01-06 22:14:35 +0100

    cooldown: add proper unicode support

--- test/tests/cooldowns/set.js
+++ test/tests/cooldowns/set.js
@@ -68,4 +68,44 @@ describe('Cooldowns - set()', () => {
     global.systems.cooldown.set(global.systems.cooldown, owner, '!test user 20 true')
     await message.isSent('cooldowns.cooldown-was-set', owner, { command: '!test', type: 'user', seconds: 20 })
   })
+
+  it('!한국어 global 20 true', async () => {
+    global.systems.cooldown.set(global.systems.cooldown, owner, '!한국어 global 20 true')
+    await message.isSent('cooldowns.cooldown-was-set', owner, { command: '!한국어', type: 'global', seconds: 20 })
+  })
+
+  it('!한국어 user 20 true', async () => {
+    global.systems.cooldown.set(global.systems.cooldown, owner, '!한국어 user 20 true')
+    await message.isSent('cooldowns.cooldown-was-set', owner, { command: '!한국어', type: 'user', seconds: 20 })
+  })
+
+  it('한국어 global 20 true', async () => {
+    global.systems.cooldown.set(global.systems.cooldown, owner, '한국어 global 20 true')
+    await message.isSent('cooldowns.cooldown-was-set', owner, { command: '한국어', type: 'global', seconds: 20 })
+  })
+
+  it('한국어 user 20 true', async () => {
+    global.systems.cooldown.set(global.systems.cooldown, owner, '한국어 user 20 true')
+    await message.isSent('cooldowns.cooldown-was-set', owner, { command: '한국어', type: 'user', seconds: 20 })
+  })
+
+  it('!русский global 20 true', async () => {
+    global.systems.cooldown.set(global.systems.cooldown, owner, '!русский global 20 true')
+    await message.isSent('cooldowns.cooldown-was-set', owner, { command: '!русский', type: 'global', seconds: 20 })
+  })
+
+  it('!русский user 20 true', async () => {
+    global.systems.cooldown.set(global.systems.cooldown, owner, '!русский user 20 true')
+    await message.isSent('cooldowns.cooldown-was-set', owner, { command: '!русский', type: 'user', seconds: 20 })
+  })
+
+  it('русский global 20 true', async () => {
+    global.systems.cooldown.set(global.systems.cooldown, owner, 'русский global 20 true')
+    await message.isSent('cooldowns.cooldown-was-set', owner, { command: 'русский', type: 'global', seconds: 20 })
+  })
+
+  it('русский user 20 true', async () => {
+    global.systems.cooldown.set(global.systems.cooldown, owner, 'русский user 20 true')
+    await message.isSent('cooldowns.cooldown-was-set', owner, { command: 'русский', type: 'user', seconds: 20 })
+  })
 })
