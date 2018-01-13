5883fa5907cfa01d1ea8be6251b8f1f4f6ab62c3
Michal Orlik <sogehige@gmail.com>
2018-01-13 02:46:02 +0100

    test: config move to test

--- test/general.js
+++ test/general.js
@@ -3,6 +3,8 @@ const sinon = require('sinon')
 
 // setup config
 const config = require('../config.json')
+config.settings.bot_username = 'test__________bot'
+config.settings.bot_oauth = '96wi94nziumq4fk2w3ocj8g97wb5uj'
 config.settings.bot_owners = 'soge__'
 config.settings.broadcaster_username = 'soge__'
 fs.writeFileSync('../config.json', JSON.stringify(config))
