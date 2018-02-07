9a3c63f14218616f6e62fb4d8b25de0f1a7b61d6
Michal Orlik <sogehige@gmail.com>
2018-02-07 10:00:59 +0100

    tests: when

---
+++ test/tests/twitch/when.js
@@ -0,0 +1,35 @@
+/* global describe it before */
+
+require('../../general.js')
+
+const db = require('../../general.js').db
+const tmi = require('../../general.js').tmi
+
+const _ = require('lodash')
+const assert = require('chai').assert
+
+describe('lib/twitch - when()', () => {
+  before(async () => {
+    await tmi.waitForConnection()
+    global.commons.sendMessage.reset()
+    await db.cleanup()
+  })
+
+  it('await when() 20-times at once', async () => {
+    let toAwait = []
+    for (let i = 0; i < 15; i++) {
+      toAwait.push(global.twitch.when({
+        subcribed_at: _.now(),
+        followed_at: _.now(),
+        online: null,
+        offline: _.now()
+      }))
+    }
+    await Promise.all(toAwait)
+  })
+
+  it('when should be only once in db', async () => {
+    let when = await global.db.engine.find('cache.when')
+    assert.lengthOf(when, 1, JSON.stringify(when))
+  })
+})
