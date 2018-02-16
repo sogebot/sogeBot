3dbee9da8a8415013e48896b8f3ba923f738dfb7
Michal Orlik <sogehige@gmail.com>
2018-02-16 14:07:50 +0100

    tools: generate-authors exclude authors
    AUTHORS update

--- tools/generate-authors.sh
+++ tools/generate-authors.sh
@@ -3,13 +3,30 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
 
-# see also ".mailmap" for how email addresses and names are deduplicated
+# some authors doesn't want to be in AUTHORS -> space as \\s
+EXCLUDED_AUTHORS=("alreadylostmyself")
 
+# see also ".mailmap" for how email addresses and names are deduplicated
 {
 	cat <<-'EOH'
 	# This file lists all individuals having contributed content to the repository.
 	# For how it is generated, see `tools/generate-authors.sh`.
 	EOH
+	git config --global grep.patternType perl
+
+	excluded_regexp='^(?!'
+	for i in "${!EXCLUDED_AUTHORS[@]}"
+	do
+		:
+		if [ $i -gt 0 ]
+		then
+			excluded_regexp=$excluded_regexp'|'${EXCLUDED_AUTHORS[$i]}
+		else
+			excluded_regexp=$excluded_regexp${EXCLUDED_AUTHORS[$i]}
+		fi
+	done
+	excluded_regexp=$excluded_regexp').*$'
+
 	echo
-	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
+	git log --format='%aN <%aE>' --author=$excluded_regexp | LC_ALL=C.UTF-8 sort -uf
 } > AUTHORS
