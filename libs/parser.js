75d8b0745f180fc9e32657404526ee40d03293d6
Michal Orlik <sogehige@gmail.com>
2018-03-06 11:53:45 +0100

    Parser: Fixed global variables when Songs system is enabled
    _ref: #797

--- libs/parser.js
+++ libs/parser.js
@@ -477,7 +477,7 @@ Parser.prototype.parseMessage = async function (message, attr) {
     .replace(/\$hosts/g, global.twitch.current.hosts)
     .replace(/\$subscribers/g, global.twitch.current.subscribers)
     .replace(/\$bits/g, global.twitch.current.bits)
-  if (global.commons.isSystemEnabled('songs')) msg = message.replace(/\$currentSong/g, _.get(global.systems.songs.currentSong, 'title', ''))
+  if (global.commons.isSystemEnabled('songs')) msg = msg.replace(/\$currentSong/g, _.get(global.systems.songs.currentSong, 'title', ''))
 
   msg = await this.parseMessageVariables(custom, msg); d('parseMessageEach: %s', msg)
   msg = await this.parseMessageEval(evaluate, decode(msg)); d('parseMessageEval: %s', msg)
