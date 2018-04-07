e34e2be65bb72679ae4a23356a49ce3e2fe50117
Michal Orlik <sogehige@gmail.com>
2018-04-07 23:23:08 +0200

    overlay/stats: Fix sending incorrect data

--- libs/overlays/stats.js
+++ libs/overlays/stats.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const _ = require('lodash')
+
 function Stats () {
   if (require('cluster').isMaster) {
     global.panel.addMenu({category: 'settings', name: 'overlays', id: 'overlays'})
@@ -11,10 +13,10 @@ Stats.prototype._get = async function (self, socket) {
   const when = await global.cache.when()
   const stats = {
     uptime: global.commons.getTime(await global.cache.isOnline() ? when.online : 0, false),
-    viewers: global.api.current.viewers,
-    followers: global.api.current.followers,
-    subscribers: global.api.current.subscribers,
-    bits: global.api.current.bits
+    viewers: _.get(await global.db.engine.findOne('api.current', { key: 'viewers' }), 'value', 0),
+    followers: _.get(await global.db.engine.findOne('api.current', { key: 'followers' }), 'value', 0),
+    subscribers: _.get(await global.db.engine.findOne('api.current', { key: 'subscribers' }), 'value', 0),
+    bits: _.get(await global.db.engine.findOne('api.current', { key: 'tips' }), 'value', 0)
   }
   socket.emit('overlay.stats', stats)
 }
