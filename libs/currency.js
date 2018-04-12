4b13f3676564576a553f4e89d3c2d6be81ea6436
Michal Orlik <sogehige@gmail.com>
2018-04-12 15:13:19 +0200

    lib/currency: Change rate provider to cnb.cz
    _ref: #867

--- libs/currency.js
+++ libs/currency.js
@@ -1,10 +1,11 @@
 const getSymbolFromCurrency = require('currency-symbol-map')
 const snekfetch = require('snekfetch')
 const _ = require('lodash')
+const debug = require('debug')
 
 class Currency {
   constructor () {
-    this.base = 'USD'
+    this.base = 'CZK'
     this.rates = {}
 
     global.configuration.register('currency', 'core.no-response', 'string', 'EUR')
@@ -30,8 +31,10 @@ class Currency {
       if (_.isNil(this.rates[to]) && to.toLowerCase().trim() !== this.base.toLowerCase().trim()) throw Error(`${to} code was not found`)
 
       if (to.toLowerCase().trim() !== this.base.toLowerCase().trim()) {
-        return parseFloat((value / this.rates[from]) * this.rates[to])
-      } else return parseFloat(value / this.rates[from])
+        return parseFloat((value * this.rates[from]) / this.rates[to])
+      } else {
+        return parseFloat(value * this.rates[from])
+      }
     } catch (e) {
       global.log.warning(`Currency exchange error - ${e.message}`)
       return parseFloat(value) // don't change rate if code not found
@@ -41,8 +44,18 @@ class Currency {
   async updateRates () {
     let refresh = 1000 * 60 * 60 * 24
     try {
-      const result = await snekfetch.get(`http://api.fixer.io/latest?base=${this.base}`)
-      this.rates = result.body.rates
+      // base is always CZK
+      const result = await snekfetch.get(`http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt`)
+      let linenum = 0
+      for (let line of result.body.toString().split('\n')) {
+        if (linenum < 2 || line.trim().length === 0) {
+          linenum++
+          continue
+        }
+        let [country, name, count, code, rate] = line.split('|')
+        debug('currency:updateRates')([country, name, count, code, rate])
+        this.rates[code] = parseFloat(rate.replace(',', '.') / count).toFixed(3)
+      }
     } catch (e) {
       console.error(e.stack)
       refresh = 1000
