757b1bb083c22a7fdfe306efaa85042a06cb2eb7
Michal Orlik <sogehige@gmail.com>
2018-04-14 14:57:20 +0200

    tool/cleanup: Remove duplicates cleanup
    As we are using unique username as index, this shouldn't happen anymore

--- tools/cleanup.js
+++ tools/cleanup.js
@@ -34,7 +34,6 @@ function areYouSure () {
   })
 }
 
-
 async function cleanUsersWithoutTime () {
   let users = await db.engine.find('users')
   let size = _.size(users)
@@ -65,37 +64,7 @@ async function cleanUsersWithoutID () {
   console.log('Cleaned ' + (size - _.size(users) + ' users'))
 }
 
-async function cleanUsersDuplicates () {
-  let users = await db.engine.find('users')
-  let uniqueUsernames = []
-
-  for (let user of users) {
-    uniqueUsernames.push(user.username.toLowerCase())
-  }
-
-  let size = _.size(uniqueUsernames)
-  uniqueUsernames = _.uniq(uniqueUsernames)
-
-  let newUsers = []
-  for (var i = 0, len = uniqueUsernames.length; i < len; i++) {
-    let user = _.filter(users, (o) => o.username === uniqueUsernames[i])
-    if (user.length > 0) newUsers.push(user[0])
-    else continue
-  }
-  await db.engine.remove('users', {})
-  for (let user of newUsers) {
-    delete user._id
-    await db.engine.insert('users', user)
-  }
-
-  console.log('Cleaned ' + (size - _.size(_.uniq(uniqueUsernames)) + ' users'))
-}
-
 async function main () {
-  console.log('Cleaning up duplicates users')
-  await cleanUsersDuplicates()
-  console.log('- DONE')
-
   console.log('Cleaning up users without watched time')
   await cleanUsersWithoutTime()
   console.log('- DONE')
