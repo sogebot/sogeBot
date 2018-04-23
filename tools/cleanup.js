7541346c1471db2f3d81a016ff8819643cbc0718
Michal Orlik <sogehige@gmail.com>
2018-04-23 14:14:28 +0200

    tools/cleanup: Revert duplicate user clean

--- tools/cleanup.js
+++ tools/cleanup.js
@@ -50,6 +50,31 @@ async function cleanUsersWithoutTime () {
   console.log('Cleaned ' + (size - _.size(users) + ' users'))
 }
 
+async function cleanUsersDuplicates () {
+  let users = await db.engine.find('users')
+  let uniqueUsernames = []
+
+  for (let user of users) {
+    uniqueUsernames.push(user.username.toLowerCase())
+  }
+
+  let size = _.size(uniqueUsernames)
+  uniqueUsernames = _.uniq(uniqueUsernames)
+
+  let newUsers = []
+  for (var i = 0, len = uniqueUsernames.length; i < len; i++) {
+    let user = _.filter(users, (o) => o.username === uniqueUsernames[i])
+    if (user.length > 0) newUsers.push(user[0])
+    else continue
+  }
+  await db.engine.remove('users', {})
+  for (let user of newUsers) {
+    delete user._id
+    await db.engine.insert('users', user)
+  }
+  console.log('Cleaned ' + (size - _.size(_.uniq(uniqueUsernames)) + ' users'))
+}
+
 async function cleanUsersWithoutID () {
   let users = await db.engine.find('users')
   let size = _.size(users)
@@ -65,6 +90,10 @@ async function cleanUsersWithoutID () {
 }
 
 async function main () {
+  console.log('Cleaning up duplicates users')
+  await cleanUsersDuplicates()
+  console.log('- DONE')
+
   console.log('Cleaning up users without watched time')
   await cleanUsersWithoutTime()
   console.log('- DONE')
