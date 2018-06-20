f0392269c8b0e4544a133cea9d3dcd4d92db87d0
Michal Orlik <sogehige@gmail.com>
2018-06-20 17:23:08 +0200

    lib/configuration: Add ids to commands

--- libs/configuration.js
+++ libs/configuration.js
@@ -19,15 +19,15 @@ function Configuration () {
 
 Configuration.prototype.commands = function () {
   return [
-    { this: this, command: '!set list', fnc: this.listSets, permission: constants.OWNER_ONLY },
-    { this: this, command: '!set', fnc: this.setValue, permission: constants.OWNER_ONLY },
-    { this: this, command: '!_debug', fnc: this.debug, permission: constants.OWNER_ONLY },
-    { this: this, command: '!enable', fnc: this.enable, permission: constants.OWNER_ONLY },
-    { this: this, command: '!disable', fnc: this.disable, permission: constants.OWNER_ONLY }
+    { this: this, id: '!set list', command: '!set list', fnc: this.listSets, permission: constants.OWNER_ONLY },
+    { this: this, id: '!set', command: '!set', fnc: this.setValue, permission: constants.OWNER_ONLY },
+    { this: this, id: '!_debug', command: '!_debug', fnc: this.debug, permission: constants.OWNER_ONLY },
+    { this: this, id: '!enable', command: '!enable', fnc: this.enable, permission: constants.OWNER_ONLY },
+    { this: this, id: '!disable', command: '!disable', fnc: this.disable, permission: constants.OWNER_ONLY }
   ]
 }
 
-Configuration.prototype.debug = async function (self, sender) {
+Configuration.prototype.debug = async function () {
   let widgets = await global.db.engine.find('widgets')
 
   let oauth = {
@@ -55,53 +55,53 @@ Configuration.prototype.register = function (cfgName, success, filter, defaultVa
   this.default[cfgName] = {value: defaultValue}
 }
 
-Configuration.prototype.setValue = async function (self, sender, text, quiet) {
+Configuration.prototype.setValue = async function (opts) {
   try {
-    var cmd = text.split(' ')[0]
+    var cmd = opts.parameters.split(' ')[0]
     DEBUG_CONFIGURATION_SETVALUE('cmd: %s', cmd)
-    var value = text.replace(text.split(' ')[0], '').trim()
-    var filter = self.cfgL[cmd].filter
-    quiet = _.isBoolean(quiet) ? quiet : false
+    var value = opts.parameters.replace(opts.parameters.split(' ')[0], '').trim()
+    var filter = this.cfgL[cmd].filter
+    opts.quiet = _.isBoolean(opts.quiet) ? opts.quiet : false
 
-    if (value.length === 0) value = self.default[cmd].value
+    if (value.length === 0) value = this.default[cmd].value
     DEBUG_CONFIGURATION_SETVALUE('filter: %s', filter)
     DEBUG_CONFIGURATION_SETVALUE('key: %s', cmd)
     DEBUG_CONFIGURATION_SETVALUE('value to set: %s', value)
-    DEBUG_CONFIGURATION_SETVALUE('text: %s', text)
-    DEBUG_CONFIGURATION_SETVALUE('isQuiet: %s', quiet)
+    DEBUG_CONFIGURATION_SETVALUE('text: %s', opts.parameters)
+    DEBUG_CONFIGURATION_SETVALUE('isQuiet: %s', opts.quiet)
 
     if (_.isString(value)) value = value.trim()
     if (filter === 'number' && Number.isInteger(parseInt(value, 10))) {
       value = parseInt(value, 10)
 
       await global.db.engine.update('settings', { key: cmd }, { key: cmd, value: value })
-      if (!quiet) global.commons.sendToOwners(global.translate(self.cfgL[cmd].success).replace(/\$value/g, value))
+      if (!opts.quiet) global.commons.sendToOwners(global.translate(this.cfgL[cmd].success).replace(/\$value/g, value))
 
-      self.cfgL[cmd].value = value
+      this.cfgL[cmd].value = value
     } else if (filter === 'bool' && (value === 'true' || value === 'false' || _.isBoolean(value))) {
       value = !_.isBoolean(value) ? (value.toLowerCase() === 'true') : value
 
       await global.db.engine.update('settings', { key: cmd }, { key: cmd, value: value })
-      if (!quiet) global.commons.sendToOwners(global.translate(self.cfgL[cmd].success + '.' + value).replace(/\$value/g, value))
+      if (!opts.quiet) global.commons.sendToOwners(global.translate(this.cfgL[cmd].success + '.' + value).replace(/\$value/g, value))
 
-      self.cfgL[cmd].value = value
+      this.cfgL[cmd].value = value
     } else if (filter === 'string' && !(value === 'true' || value === 'false' || _.isBoolean(value)) && !Number.isInteger(parseInt(value, 10))) {
-      self.cfgL[cmd].value = value
+      this.cfgL[cmd].value = value
       await global.db.engine.update('settings', { key: cmd }, { key: cmd, value: value })
       if (cmd === 'lang') {
         process.send({ type: 'lang' })
         await global.lib.translate._load()
-        if (!quiet) global.commons.sendToOwners(global.translate('core.lang-selected'))
+        if (!opts.quiet) global.commons.sendToOwners(global.translate('core.lang-selected'))
       }
-      if (cmd !== 'lang' && !quiet) global.commons.sendToOwners(global.translate(self.cfgL[cmd].success).replace(/\$value/g, value))
-    } else global.commons.sendMessage('Sorry, $sender, cannot parse !set command.', sender)
+      if (cmd !== 'lang' && !opts.quiet) global.commons.sendToOwners(global.translate(this.cfgL[cmd].success).replace(/\$value/g, value))
+    } else global.commons.sendMessage('Sorry, $sender, cannot parse !set command.', opts.sender)
 
     let emit = {}
-    _.each(self.sets(self), async function (key) {
-      emit[key] = await self.getValue(key)
+    _.each(this.sets(this), async function (key) {
+      emit[key] = await this.getValue(key)
     })
   } catch (err) {
-    global.commons.sendMessage('Sorry, $sender, cannot parse !set command.', sender)
+    global.commons.sendMessage('Sorry, $sender, cannot parse !set command.', opts.sender)
   }
 }
 
@@ -109,9 +109,9 @@ Configuration.prototype.sets = function (self) {
   return Object.keys(self.cfgL).map(function (item) { return item })
 }
 
-Configuration.prototype.listSets = function (self, sender, text) {
-  var setL = self.sets(self).join(', ')
-  global.commons.sendMessage(setL.length === 0 ? 'Sorry, $sender, you cannot configure anything' : 'List of possible settings: ' + setL, sender)
+Configuration.prototype.listSets = function (opts) {
+  var setL = this.sets(this).join(', ')
+  global.commons.sendMessage(setL.length === 0 ? 'Sorry, $sender, you cannot configure anything' : 'List of possible settings: ' + setL, opts.sender)
 }
 
 Configuration.prototype.getValue = async function (cfgName) {
@@ -127,10 +127,10 @@ Configuration.prototype.getValue = async function (cfgName) {
   }
 }
 
-Configuration.prototype.enable = async function (self, sender, message) {
-  if (message.trim().length === 0) return
+Configuration.prototype.enable = async function (opts) {
+  if (opts.parameters.trim().length === 0) return
   try {
-    let [type, name] = message.split(' ')
+    let [type, name] = opts.parameters.split(' ')
 
     if (type === 'system') type = 'systems'
     else throw new Error('Not supported')
@@ -142,10 +142,10 @@ Configuration.prototype.enable = async function (self, sender, message) {
     global.log.error(e.message)
   }
 }
-Configuration.prototype.disable = async function (self, sender, message) {
-  if (message.trim().length === 0) return
+Configuration.prototype.disable = async function (opts) {
+  if (opts.parameters.trim().length === 0) return
   try {
-    let [type, name] = message.split(' ')
+    let [type, name] = opts.parameters.split(' ')
 
     if (type === 'system') type = 'systems'
     else throw new Error('Not supported')
