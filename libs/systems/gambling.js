41c38b6a0005d3415cadb13bc8c2a4f95acbc325
Michal Orlik <sogehige@gmail.com>
2018-07-02 15:55:55 +0200

    game/roulette: Move !roulette from gambling system to games

--- libs/systems/gambling.js
+++ libs/systems/gambling.js
@@ -22,7 +22,6 @@ class Gambling {
     this.collection = 'gambling'
     this.timeouts = {}
 
-    global.configuration.register('rouletteTimeout', 'gambling.roulette.timeout', 'number', 10)
     global.configuration.register('fightmeTimeout', 'gambling.fightme.timeout', 'number', 10)
 
     global.configuration.register('duelCooldown', 'gambling.cooldown.duel', 'number', 0)
@@ -41,7 +40,6 @@ class Gambling {
 
     if (isGamblingEnabled) {
       commands.push(
-        {this: this, id: '!roulette', command: '!roulette', fnc: this.roulette, permission: constants.VIEWERS},
         {this: this, id: '!fightme', command: '!fightme', fnc: this.fightme, permission: constants.VIEWERS}
       )
 
@@ -240,29 +238,6 @@ class Gambling {
           global.commons.sendMessage(global.translate('core.error'), opts.sender)
       }
     }
-  }
-
-  async roulette (opts) {
-    opts.sender['message-type'] = 'chat' // force responses to chat
-
-    let isAlive = _.random(0, 1, false)
-    const isMod = await global.commons.isMod(opts.sender)
-
-    global.commons.sendMessage(global.translate('gambling.roulette.trigger'), opts.sender)
-    if (global.commons.isBroadcaster(opts.sender)) {
-      setTimeout(() => global.commons.sendMessage(global.translate('gambling.roulette.broadcaster'), opts.sender), 2000)
-      return
-    }
-
-    if (isMod) {
-      setTimeout(() => global.commons.sendMessage(global.translate('gambling.roulette.mod'), opts.sender), 2000)
-      return
-    }
-
-    setTimeout(async () => {
-      if (!isAlive) global.commons.timeout(opts.sender.username, null, await global.configuration.getValue('rouletteTimeout'))
-      global.commons.sendMessage(isAlive ? global.translate('gambling.roulette.alive') : global.translate('gambling.roulette.dead'), opts.sender)
-    }, 2000)
   }
 
   async fightme (opts) {
