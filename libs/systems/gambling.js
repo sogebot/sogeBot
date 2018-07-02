eacd8f35ff06ffd980c9c87479af4e0d17bfcaaf
Michal Orlik <sogehige@gmail.com>
2018-07-02 16:12:33 +0200

    game/fightme: Move !fightme from gambling system to games

--- libs/systems/gambling.js
+++ libs/systems/gambling.js
@@ -22,10 +22,7 @@ class Gambling {
     this.collection = 'gambling'
     this.timeouts = {}
 
-    global.configuration.register('fightmeTimeout', 'gambling.fightme.timeout', 'number', 10)
-
     global.configuration.register('duelCooldown', 'gambling.cooldown.duel', 'number', 0)
-    global.configuration.register('fightmeCooldown', 'gambling.cooldown.fightme', 'number', 0)
 
     global.configuration.register('duelDuration', 'gambling.duel.duration', 'number', 5)
     global.configuration.register('duelMinimalBet', 'gambling.duel.minimalBet', 'number', 0)
@@ -39,10 +36,6 @@ class Gambling {
     let commands = []
 
     if (isGamblingEnabled) {
-      commands.push(
-        {this: this, id: '!fightme', command: '!fightme', fnc: this.fightme, permission: constants.VIEWERS}
-      )
-
       if (isPointsEnabled) {
         commands.push(
           {this: this, id: '!duel', command: '!duel', fnc: this.duel, permission: constants.VIEWERS}
@@ -66,14 +59,6 @@ class Gambling {
   set duelCooldown (v) {
     if (v === 0) global.db.engine.remove(`${this.collection}.duel`, { key: '_cooldown' })
     else global.db.engine.update(`${this.collection}.duel`, { key: '_cooldown' }, { value: v })
-  }
-
-  get fightmeCooldown () {
-    return new Promise(async (resolve, reject) => resolve(_.get(await global.db.engine.findOne(`${this.collection}.fightme`, { key: '_cooldown' }), 'value', 0)))
-  }
-  set fightmeCooldown (v) {
-    if (v === 0) global.db.engine.remove(`${this.collection}.fightme`, { key: '_cooldown' })
-    else global.db.engine.update(`${this.collection}.fightme`, { key: '_cooldown' }, { value: v })
   }
 
   get duelUsers () {
@@ -237,89 +222,6 @@ class Gambling {
           global.log.error(e.stack)
           global.commons.sendMessage(global.translate('core.error'), opts.sender)
       }
-    }
-  }
-
-  async fightme (opts) {
-    opts.sender['message-type'] = 'chat' // force responses to chat
-    var username
-
-    try {
-      username = opts.parameters.trim().match(/^@?([\S]+)$/)[1].toLowerCase()
-      opts.sender.username = opts.sender.username.toLowerCase()
-    } catch (e) {
-      global.commons.sendMessage(global.translate('gambling.fightme.notEnoughOptions'), opts.sender)
-      return
-    }
-
-    if (opts.sender.username === username) {
-      global.commons.sendMessage(global.translate('gambling.fightme.cannotFightWithYourself'), opts.sender)
-      return
-    }
-
-    // check if you are challenged by user
-    const challenge = await global.db.engine.findOne(`${this.collection}.fightme`, { key: '_users', user: username, challenging: opts.sender.username })
-    const isChallenged = !_.isEmpty(challenge)
-    if (isChallenged) {
-      let winner = _.random(0, 1, false)
-      let isMod = {
-        user: await global.commons.isMod(username),
-        sender: await global.commons.isMod(opts.sender)
-      }
-
-      // vs broadcaster
-      if (global.commons.isBroadcaster(opts.sender) || global.commons.isBroadcaster(username)) {
-        debug('vs broadcaster')
-        global.commons.sendMessage(global.translate('gambling.fightme.broadcaster')
-          .replace(/\$winner/g, global.commons.isBroadcaster(opts.sender) ? opts.sender.username : username), opts.sender)
-        isMod = global.commons.isBroadcaster(opts.sender) ? isMod.user : isMod.opts.sender
-        if (!isMod) global.commons.timeout(global.commons.isBroadcaster(opts.sender) ? username : opts.sender.username, null, await global.configuration.getValue('fightmeTimeout'))
-        global.db.engine.remove(`${this.collection}.fightme`, { _id: challenge._id.toString() })
-        return
-      }
-
-      // mod vs mod
-      if (isMod.user && isMod.opts.sender) {
-        debug('mod vs mod')
-        global.commons.sendMessage(global.translate('gambling.fightme.bothModerators')
-          .replace(/\$challenger/g, username), opts.sender)
-        global.db.engine.remove(`${this.collection}.fightme`, { _id: challenge._id.toString() })
-        return
-      }
-
-      // vs mod
-      if (isMod.user || isMod.opts.sender) {
-        debug('vs mod')
-        global.commons.sendMessage(global.translate('gambling.fightme.oneModerator')
-          .replace(/\$winner/g, isMod.opts.sender ? opts.sender.username : username), opts.sender)
-        global.commons.timeout(isMod.opts.sender ? username : opts.sender.username, null, await global.configuration.getValue('fightmeTimeout'))
-        global.db.engine.remove(`${this.collection}.fightme`, { _id: challenge._id.toString() })
-        return
-      }
-
-      debug('user vs user')
-      global.commons.timeout(winner ? opts.sender.username : username, null, await global.configuration.getValue('fightmeTimeout'))
-      global.commons.sendMessage(global.translate('gambling.fightme.winner')
-        .replace(/\$winner/g, winner ? username : opts.sender.username), opts.sender)
-      global.db.engine.remove(`${this.collection}.fightme`, { _id: challenge._id.toString() })
-    } else {
-      // check if under gambling cooldown
-      const cooldown = await global.configuration.getValue('fightmeCooldown')
-      const isMod = await global.commons.isMod(opts.sender)
-      if (new Date().getTime() - (await this.fightmeCooldown) < cooldown * 1000 &&
-        !(await global.configuration.getValue('gamblingCooldownBypass') && (isMod || global.commons.isBroadcaster(opts.sender)))) {
-        global.commons.sendMessage(global.translate('gambling.fightme.cooldown')
-          .replace(/\$cooldown/g, Math.round(((cooldown * 1000) - (new Date().getTime() - (await this.fightmeCooldown))) / 1000 / 60))
-          .replace(/\$minutesName/g, global.commons.getLocalizedName(Math.round(((cooldown * 1000) - (new Date().getTime() - (await this.fightmeCooldown))) / 1000 / 60), 'core.minutes')), opts.sender)
-        return
-      }
-
-      // save new timestamp if not bypassed
-      if (!(await global.configuration.getValue('gamblingCooldownBypass') && (isMod || global.commons.isBroadcaster(opts.sender)))) this.fightmeCooldown = new Date().getTime()
-
-      const isAlreadyChallenged = !_.isEmpty(await global.db.engine.findOne(`${this.collection}.fightme`, { key: '_users', user: opts.sender.username, challenging: username }))
-      if (!isAlreadyChallenged) await global.db.engine.insert(`${this.collection}.fightme`, { key: '_users', user: opts.sender.username, challenging: username })
-      global.commons.sendMessage(await global.commons.prepare('gambling.fightme.challenge', { username: username }), opts.sender)
     }
   }
 }
