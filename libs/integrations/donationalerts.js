f5c977f4d1e1145782215cb0ad212ea9b64e84e4
sogehelper <anon@privateperson.ga>
2018-07-14 10:57:59 +0300

    integration/donationalerts: Update socket address

--- libs/integrations/donationalerts.js
+++ libs/integrations/donationalerts.js
@@ -42,7 +42,7 @@ class Donationalerts {
 
   async connect () {
     if (_.isNil(this.socket)) {
-      this.socket = require('socket.io-client').connect('http://socket.donationalerts.ru:3001',
+      this.socket = require('socket.io-client').connect('http://socket.donationalerts.ru:80',
         {
           reconnection: true,
           reconnectionDelay: 1000,
