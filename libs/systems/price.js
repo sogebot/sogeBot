995d30bd3c1a5ee2f47e28f32728c1424120a971
Michal Orlik <sogehige@gmail.com>
2018-07-16 11:41:45 +0200

    system/price: Remove redundant interface sockets

--- libs/systems/price.js
+++ libs/systems/price.js
@@ -38,33 +38,6 @@ class Price extends System {
     this.addMenu({category: 'manage', name: 'price', id: 'price/list'})
   }
 
-  sockets () {
-    this.socket.on('connection', (socket) => {
-      socket.on('update', async (items, cb) => {
-        for (let item of items) {
-          const _id = item._id; delete item._id
-          let itemFromDb = item
-          if (_.isNil(_id)) itemFromDb = await global.db.engine.insert(this.collection.data, item)
-          else await global.db.engine.update(this.collection.data, { _id }, item)
-
-          if (_.isFunction(cb)) cb(null, itemFromDb)
-        }
-      })
-      socket.on('delete', async (_id, cb) => {
-        await global.db.engine.remove(this.collection.data, { _id })
-        cb(null)
-      })
-      socket.on('find', async (where, cb) => {
-        where = where || {}
-        cb(null, await global.db.engine.find(this.collection.data, where))
-      })
-      socket.on('findOne', async (where, cb) => {
-        where = where || {}
-        cb(null, await global.db.engine.findOne(this.collection.data, where))
-      })
-    })
-  }
-
   main (opts) {
     global.commons.sendMessage(global.translate('core.usage') + ': !price set <cmd> <price> | !price unset <cmd> | !price list | !price toggle <cmd>', opts.sender)
   }
