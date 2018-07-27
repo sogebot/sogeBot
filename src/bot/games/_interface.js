4708b314900c8455c53629954aace107b2c44f80
Michal Orlik <sogehige@gmail.com>
2018-07-27 14:50:07 +0200

    games/interface: Set games opt-in instead opt-out

--- src/bot/games/_interface.js
+++ src/bot/games/_interface.js
@@ -3,6 +3,7 @@ const Module = require('../_interface')
 class Game extends Module {
   constructor (opts) {
     opts.name = 'games'
+    opts.settings.enabled = typeof opts.settings.enabled !== 'undefined' ? opts.settings.enabled : false
     super(opts)
 
     this.addMenu({category: 'settings', name: 'games', id: 'games'})
