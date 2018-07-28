8280fd43ad599ce55a1fd637287b624ef2d70d66
Michal Orlik <sogehige@gmail.com>
2018-07-28 15:39:23 +0200

    library/configuration: Add !set for interface values

--- src/bot/configuration.js
+++ src/bot/configuration.js
@@ -73,8 +73,41 @@ class Configuration {
     this.default[cfgName] = {value: defaultValue}
   }
 
+  async setValue2 (opts) {
+    // get value so we have a type
+    let splitted = opts.parameters.split(' ')
+    const pointer = splitted.shift()
+    let newValue = splitted.join(' ')
+    let currentValue = await _.get(global, pointer, undefined)
+    if (typeof currentValue !== 'undefined') {
+      if (_.isBoolean(currentValue)) {
+        newValue = newValue.toLowerCase().trim()
+        if (['true', 'false'].includes(newValue)) {
+          _.set(global, pointer, newValue === 'true')
+          global.commons.sendMessage(`$sender, ${pointer} set to ${newValue}`, opts.sender)
+        } else {
+          global.commons.sendMessage('$sender, !set error: bool is expected', opts.sender)
+        }
+      } else if (_.isNumber(currentValue)) {
+        if (_.isFinite(Number(newValue))) {
+          _.set(global, pointer, Number(newValue))
+          global.commons.sendMessage(`$sender, ${pointer} set to ${newValue}`, opts.sender)
+        } else {
+          global.commons.sendMessage('$sender, !set error: number is expected', opts.sender)
+        }
+      } else if (_.isString(currentValue)) {
+        _.set(global, pointer, newValue)
+        global.commons.sendMessage(`$sender, ${pointer} set to '${newValue}'`, opts.sender)
+      } else {
+        global.commons.sendMessage(`$sender, ${pointer} is not supported settings to change`, opts.sender)
+      }
+    } else global.commons.sendMessage(`$sender, ${pointer} settings not exists`, opts.sender)
+  }
+
   async setValue (opts) {
     try {
+      if (opts.parameters.includes('.')) return this.setValue2(opts)
+
       var cmd = opts.parameters.split(' ')[0]
       DEBUG_CONFIGURATION_SETVALUE('cmd: %s', cmd)
       var value = opts.parameters.replace(opts.parameters.split(' ')[0], '').trim()
