22561da1abe12aed9f5e5cc466f65576ec07dd5e
Michal Orlik <sogehige@gmail.com>
2018-09-26 21:20:50 +0200

    db/nedb: Fix worker/master relation

--- src/bot/databases/database.js
+++ src/bot/databases/database.js
@@ -1,18 +1,17 @@
-const _ = require('lodash')
-
 const config = require('@config')
 
 const INeDB = require('./nedb')
 const IMongoDB = require('./mongodb')
-const IMasterController = require('./master')
+const IWorkerController = require('./worker')
 
 class Database {
   constructor (forceIndexes, forceRemoveIndexes) {
     this.engine = null
-    if (!global.mocha && require('cluster').isMaster && (!forceIndexes && !forceRemoveIndexes) && config.database.type === 'nedb') this.engine = new IMasterController()
+
+    if (!global.mocha && require('cluster').isWorker && (!forceIndexes && !forceRemoveIndexes) && config.database.type === 'nedb') this.engine = new IWorkerController()
     else if (config.database.type === 'nedb') this.engine = new INeDB(forceIndexes)
     else if (config.database.type === 'mongodb') this.engine = new IMongoDB(forceIndexes, forceRemoveIndexes)
-    if (_.isNil(this.engine)) {
+    else {
       global.log.warning('No database was selected - fallback to NeDB')
       this.engine = new INeDB(forceIndexes)
     }
