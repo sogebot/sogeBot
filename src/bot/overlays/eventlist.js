34e5d55e7fd9fa43b912f205b1ec01fb84262f32
Michal Orlik <sogehige@gmail.com>
2018-10-15 14:39:02 +0200

    eventlist migrated to vue

--- src/bot/overlays/eventlist.js
+++ src/bot/overlays/eventlist.js
@@ -4,44 +4,56 @@
 const _ = require('lodash')
 const crypto = require('crypto')
 
-function EventList () {
-  if (require('cluster').isMaster) {
-    global.panel.addMenu({ category: 'settings', name: 'overlays', id: 'overlays' })
-    global.panel.socketListening(this, 'overlay.eventlist.get', this._get)
+class EventListOverlay {
+  socket = null
+
+  constructor () {
+    if (require('cluster').isMaster) {
+      global.panel.addMenu({ category: 'settings', name: 'overlays', id: 'overlays' })
+      this.sockets()
+    }
+  }
+
+  sockets () {
+    global.panel.io.of('/overlays/eventlist').on('connection', (socket) => {
+      this.socket = socket
+      socket.on('get', () => this.sendDataToOverlay())
+    })
+  }
+
+  async sendDataToOverlay () {
+    if (!this.socket) return setTimeout(() => this.sendDataToOverlay(), 1000)
+
+    let events = await global.db.engine.find('widgetsEventList')
+    events = _.uniqBy(_.orderBy(events, 'timestamp', 'desc'), o =>
+      (o.username + (o.event === 'cheer' ? crypto.randomBytes(64).toString('hex') : o.event))
+    )
+    this.socket.emit('events', _.chunk(events, 20)[0])
+  }
+
+  async add (data) {
+    if (await global.commons.isBot(data.username)) return // don't save event from a bot
+
+    const newEvent = {
+      event: data.type,
+      timestamp: _.now(),
+      username: data.username,
+      autohost: _.isNil(data.autohost) ? undefined : data.autohost,
+      message: _.isNil(data.message) ? undefined : data.message,
+      amount: _.isNil(data.amount) ? undefined : data.amount,
+      currency: _.isNil(data.currency) ? undefined : data.currency,
+      months: _.isNil(data.months) ? undefined : data.months,
+      bits: _.isNil(data.bits) ? undefined : data.bits,
+      viewers: _.isNil(data.viewers) ? undefined : data.viewers,
+      from: _.isNil(data.from) ? undefined : data.from,
+      tier: _.isNil(data.tier) ? undefined : data.tier,
+      song_title: _.isNil(data.song_title) ? undefined : data.song_title,
+      song_url: _.isNil(data.song_url) ? undefined : data.song_url
+    }
+    await global.db.engine.insert('widgetsEventList', newEvent)
+    global.overlays.eventlist.sendDataToOverlay(global.overlays.eventlist)
+    global.widgets.eventlist._get(global.widgets.eventlist)
   }
 }
 
-EventList.prototype._get = async function (self) {
-  let events = await global.db.engine.find('widgetsEventList')
-
-  events = _.uniqBy(_.orderBy(events, 'timestamp', 'desc'), o =>
-    (o.username + (o.event === 'cheer' ? crypto.randomBytes(64).toString('hex') : o.event))
-  )
-  global.panel.io.emit('overlay.eventlist', _.chunk(events, 20)[0])
-}
-
-EventList.prototype.add = async function (data) {
-  if (await global.commons.isBot(data.username)) return // don't save event from a bot
-
-  const newEvent = {
-    event: data.type,
-    timestamp: _.now(),
-    username: data.username,
-    autohost: _.isNil(data.autohost) ? undefined : data.autohost,
-    message: _.isNil(data.message) ? undefined : data.message,
-    amount: _.isNil(data.amount) ? undefined : data.amount,
-    currency: _.isNil(data.currency) ? undefined : data.currency,
-    months: _.isNil(data.months) ? undefined : data.months,
-    bits: _.isNil(data.bits) ? undefined : data.bits,
-    viewers: _.isNil(data.viewers) ? undefined : data.viewers,
-    from: _.isNil(data.from) ? undefined : data.from,
-    tier: _.isNil(data.tier) ? undefined : data.tier,
-    song_title: _.isNil(data.song_title) ? undefined : data.song_title,
-    song_url: _.isNil(data.song_url) ? undefined : data.song_url
-  }
-  await global.db.engine.insert('widgetsEventList', newEvent)
-  global.overlays.eventlist._get(global.overlays.eventlist)
-  global.widgets.eventlist._get(global.widgets.eventlist)
-}
-
-module.exports = new EventList()
+module.exports = new EventListOverlay()
---
+++ src/overlay/components/eventlist.vue
@@ -0,0 +1,116 @@
+<template>
+<ul>
+  <li
+    v-for="event of events"
+    :key="event._id"
+    class="event"
+    :class="[event.event]">
+    <strong class="username">{{ event.username }}</strong>
+    <span class="event">{{ event.event }}</span>
+  </li>
+</ul>
+</template>
+
+<script>
+  export default {
+    props: ['token'],
+    data: function () {
+      return {
+        socket: io('/overlays/eventlist', {query: "token="+token}),
+        events: []
+      }
+    },
+    created: function () {
+      this.socket.emit('get')
+      this.socket.on('events', (data) => {
+        var order = this.urlParam('order') || 'desc'
+        var display = this.urlParam('display') || 'username,event'; display = display.split(',')
+        var ignore = this.urlParam('ignore') || ''; ignore = ignore.split(',')
+        var count = this.urlParam('count') || 5
+
+        data = _.chunk(
+          _.orderBy(
+            // filter out ignored events
+            _.filter(data, (o) => !_.includes(ignore, o.event))
+            , 'timestamp', 'desc'), count)[0] // order by desc first to get chunk of data
+        data = _.orderBy(data, 'timestamp', order) // re-order as set in order
+
+        for (let event of data) {
+          if (event.event === 'resub') event.event = event.months + 'x ' + translations['overlays-eventlist-resub']
+          else if (event.event === 'cheer') event.event = event.bits + ' ' + translations['overlays-eventlist-cheer']
+          else if (event.event === 'tip') event.event = event.currency + parseFloat(event.amount).toFixed(2)
+          else event.event = translations['overlays-eventlist-' + event.event]
+
+          let generatedOutput = ''
+          for (let toShow of display) {
+            if (toShow === 'username') generatedOutput += `<strong class="username">${event.username}</strong>`
+            else generatedOutput += `<span class="${toShow}">${event[toShow]}</span>`
+          }
+        }
+        this.events = data
+      })
+    },
+    methods: {
+      urlParam: function (name) {
+        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+        if (results == null) {
+          return null
+        } else {
+          return decodeURI(results[1]) || 0;
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=BenchNine');
+
+  html, body {
+    padding: 2px;
+    padding-top: 10px;
+    margin: auto;
+    font-family: 'BenchNine', sans-serif;
+    color: white;
+  }
+
+  ul {
+    list-style-type: none;
+    text-transform: uppercase;
+    font-size: 1.6em;
+    margin: 0;
+    padding: 0;
+    text-align: right;
+  }
+
+  ul li {
+    width: 99%;
+    margin-left: 0;
+    text-shadow: 0 0 10px black, 0 0 20px black, 0 0 30px black;
+  }
+
+  ul li span {
+    font-size: 0.6em;
+  }
+
+  ul li:nth-child(1) {
+    opacity: 1;
+    font-weight: bold;
+  }
+
+  ul li:nth-child(2) {
+    opacity: 0.8;
+  }
+
+  ul li:nth-child(3) {
+    opacity: 0.6;
+  }
+
+  ul li:nth-child(4) {
+    opacity: 0.4;
+  }
+
+  ul li:nth-child(5) {
+    opacity: 0.2;
+  }
+</style>
