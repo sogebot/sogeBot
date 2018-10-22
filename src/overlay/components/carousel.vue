47b1c1a174dcf3111a23e16802ddd89213ec79d5
soge__ <sogehige@gmail.com>
2018-10-22 21:13:01 +0200

    carousel overlay moved to vue

--- src/overlay/components/carousel.vue
+++ src/overlay/components/carousel.vue
@@ -1,143 +1,141 @@
 <template>
-<div>
-</div>
+  <div>
+    <img
+      v-for="e of images"
+      :key="e._id"
+      v-show="currentImage === e.order"
+      :id="e._id"
+      :ref="e._id"
+      :src="'data:' + e.type + ';base64,'  + e.base64"
+    >
+  </div>
 </template>
 
 <script>
-</script>
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Carousel</title>
-    <meta charset="utf-8">
-    <meta name="robots" content="index, follow">
-    <meta name="theme-color" content="#f4f5f6">
-    <meta name="apple-mobile-web-app-status-bar-style" content="#f4f5f6">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
-
-    <script src="/socket.io/socket.io.js"></script>
-    <script type="text/javascript" src="/auth/token.js"></script>
-
-    <style>
-      html, body {
-        width: 99%;
-        height: 99%;
-        overflow: hidden;
-      }
-      img {
-        width: 100%;
-        opacity:0;
-        position: absolute;
-      }
-    </style>
-  </head>
-  <body>
-    <img src=''></img>
-
-    <script src="/dist/jquery/js/jquery.min.js"></script>
-    <script src="/dist/lodash/js/lodash.min.js"></script>
-    <script src="/dist/velocity-animate/js/velocity.min.js"></script>
-
-    <script>
-      const socket = io('/overlays/carousel', { query: "token=" + token })
-      const [PENDING, ACTIVE] = ['0', '1']
-
-      var currentImage = -1
-      var status = PENDING
-      var images = []
-
-      socket.emit('load', (cb) => {
-        console.debug('Loaded images', cb)
-        for (let image of cb) images[image.order] = image
+export default {
+  props: ['token'],
+  data: function () {
+    return {
+      currentImage: null,
+      ready: true,
+      show: true,
+      socket: io('/overlays/carousel', {
+        query: "token=" + token
+      }),
+      images: []
+    }
+  },
+  created: function () {
+    this.socket.emit('load', images => this.images = images)
+    setInterval(() => {
+      this.triggerAnimation()
+    }, 100)
+  },
+  methods: {
+    wait: function (type) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(), this.images[this.currentImage][type])
       })
+    },
+    doEnterAnimation: function (el, done) {
+      let animation = { opacity: 1 }
+      
+      // force refresh of styles
+      this.$refs[this.images[this.currentImage]._id][0].style.filter = 'blur(0px)'
+      this.$refs[this.images[this.currentImage]._id][0].style.top = 0
+      this.$refs[this.images[this.currentImage]._id][0].style.left = 0
+      this.$refs[this.images[this.currentImage]._id][0].style.opacity = 0
 
-      function swapImages() {
-        if (status === PENDING && images.length > 0) {
-          status = ACTIVE
-          currentImage++; if (_.isNil(images[currentImage])) currentImage = 0
-          const image = images[currentImage]
-          console.debug('showing', image)
-          $('img').attr('src', `data:${image.type};base64, ${image.base64}`)
-          // wait before
-          setTimeout(() => {
-            // revert any css changes
-            $('img').css('filter', 'none')
-            $('img').css('opacity', '0')
-            $('img').css('top', '0')
-            $('img').css('left', '0')
-            $('img').css('display', 'inline')
-
-            var animationIn
-            switch (image.animationIn) {
-              case 'blurIn':
-                $('img').css('filter', 'blur(50px)')
-                animationIn = { blur: 0, opacity: 1 }
-                break
-              case 'slideUp':
-                $('img').css('opacity', '1')
-                $('img').css('top', window.innerHeight + 'px')
-                animationIn = { top: 0 }
-                break
-              case 'slideDown':
-                $('img').css('opacity', '1')
-                $('img').css('top', -$('img').innerHeight() + 'px')
-                animationIn = { top: 0 }
-                break
-              case 'slideLeft':
-                $('img').css('opacity', '1')
-                $('img').css('left', window.innerWidth + 'px')
-                animationIn = { left: 0 }
-                break
-              case 'slideRight':
-                $('img').css('opacity', '1')
-                $('img').css('left', -$('img').innerWidth() + 'px')
-                animationIn = { left: 0 }
-                break
-              default:
-                animationIn = image.animationIn
-            }
-
-            var animationOut
-            switch (image.animationOut) {
-              case 'blurOut':
-                animationOut = { blur: 50, opacity: 0 }
-                break
-              case 'slideUp':
-                $('img').css('opacity', '1')
-                animationOut = { top: -$('img').innerHeight() }
-                break
-              case 'slideDown':
-                $('img').css('opacity', '1')
-                animationOut = { top: window.innerHeight + 'px' }
-                break
-              case 'slideLeft':
-                $('img').css('opacity', '1')
-                animationOut = { left: -$('img').innerWidth() }
-                break
-              case 'slideRight':
-                $('img').css('opacity', '1')
-                animationOut = { left: window.innerWidth + 'px' }
-                break
-              default:
-                animationOut = image.animationOut
-            }
-
-            $('img')
-              .velocity(animationIn, { duration: image.animationInDuration })
-              .velocity(animationOut, {
-                delay: image.duration,
-                duration: image.animationOutDuration,
-                complete: function() { setTimeout(() => status = PENDING, image.waitAfter) }})
-          }, image.waitBefore)
-        }
+      switch (this.images[this.currentImage].animationIn) {
+        case 'blurIn':
+          this.$refs[this.images[this.currentImage]._id][0].style.filter = 'blur(50px)'
+          animation = { filter: 'blur(0px)', opacity: 1 }
+          break
+        case 'slideUp':
+          this.$refs[this.images[this.currentImage]._id][0].style.opacity = 1
+          this.$refs[this.images[this.currentImage]._id][0].style.top = window.innerHeight + 'px'
+          animation = { top: 0 }
+          break
+        case 'slideDown':
+          this.$refs[this.images[this.currentImage]._id][0].style.opacity = 1
+          this.$refs[this.images[this.currentImage]._id][0].style.top = -this.$refs[this.images[this.currentImage]._id][0].clientHeight + 'px'
+          animation = { top: 0 }
+          break
+        case 'slideLeft':
+          this.$refs[this.images[this.currentImage]._id][0].style.opacity = 1
+          this.$refs[this.images[this.currentImage]._id][0].style.left = window.innerWidth + 'px'
+          animation = { left: 0 }
+          break
+        case 'slideRight':
+          this.$refs[this.images[this.currentImage]._id][0].style.opacity = 1
+          this.$refs[this.images[this.currentImage]._id][0].style.left = -this.$refs[this.images[this.currentImage]._id][0].clientWidth + 'px'
+          animation = { left: 0 }
+          break
       }
 
-      setInterval(swapImages, 10)
+      return new Promise((resolve, reject) => {
+        TweenLite.to(this.$refs[this.images[this.currentImage]._id], this.images[this.currentImage].animationInDuration / 1000, {
+          ...animation,
+          onComplete: () => {
+            resolve()
+          }
+        })
+      })
+    },
+    doLeaveAnimation: function (el, done) {
+      let animation = { opacity: 0 }
+      
+      switch (this.images[this.currentImage].animationOut) {
+        case 'blurOut':
+          animation = { filter: 'blur(50px)', opacity: 0 }
+          break
+        case 'slideUp':
+          animation = { top: -this.$refs[this.images[this.currentImage]._id][0].clientHeight + 'px' }
+          break
+        case 'slideDown':
+          animation = { top: window.innerHeight + 'px' }
+          break
+        case 'slideLeft':
+          animation = { left: -this.$refs[this.images[this.currentImage]._id][0].clientWidth + 'px' }
+          break
+        case 'slideRight':
+          animation = { left: window.innerWidth + 'px' }
+          break
+      }
 
-    </script>
-  </body>
-</html>
+      return new Promise((resolve, reject) => {
+        TweenLite.to(this.$refs[this.images[this.currentImage]._id], this.images[this.currentImage].animationOutDuration / 1000, {
+          ...animation,
+          onComplete: () => {
+            resolve()
+          }
+        })
+      })
+    },
+    triggerAnimation: async function () {
+      if (this.ready && this.images.length > 0) {
+        this.ready = false
+        if (this.currentImage === null) this.currentImage = 0
+        else this.currentImage++
+        if (this.images.length <= this.currentImage) this.currentImage = 0
+
+        await this.wait('waitBefore')
+        await this.doEnterAnimation()
+        await this.wait('duration')
+        await this.doLeaveAnimation()
+        await this.wait('waitAfter')
+        this.ready = true
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: 100%;
+  opacity: 0;
+  position: absolute;
+}
+</style>
+
