1076a1c4a2a1c5abc18620e88c4cda008a4d517c
Michal Orlik <sogehige@gmail.com>
2018-11-14 15:46:59 +0100

    widgets: chat should be dark if dark theme is used

--- src/panel/widgets/popout.js
+++ src/panel/widgets/popout.js
@@ -1,9 +1,11 @@
-/* globals io token commons _ translations socket page */
+/* globals io token commons _ translations configuration socket page */
 import Vue from 'vue'
 import Popout from './popout.vue'
 
 function initPopout () {
-  if (typeof translations === 'undefined' || _.size(translations) === 0) return setTimeout(() => initPopout(), 10)
+  const isTranslationsLoaded = typeof translations === 'undefined' || _.size(translations) === 0
+  const isConfigurationLoaded = typeof configuration === 'undefined' || _.size(configuration) === 0
+  if (isTranslationsLoaded || isConfigurationLoaded) return setTimeout(() => initPopout(), 10)
 
   new Vue({ // eslint-disable-line no-new
     el: '#popout',
@@ -12,7 +14,7 @@ function initPopout () {
       socket: io({ query: 'token=' + token })
     },
     render: function (createElement) {
-      return createElement(Popout, { props: { commons, socket, page } })
+      return createElement(Popout, { props: { commons, socket, page, configuration } })
     }
   })
 }
