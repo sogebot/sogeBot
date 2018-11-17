89d759a73807641b682f9180d74a56bddbbf3ee1
sogehige <sogehige@gmail.com>
2018-11-17 02:36:49 +0100

    overlays: add simple clips carousel
    PR https://github.com/sogehige/sogeBot/pull/1600

---
+++ src/bot/overlays/clipsCarousel.js
@@ -0,0 +1,53 @@
+// @flow
+'use strict'
+
+const Overlay = require('./_interface')
+
+class ClipsCarousel extends Overlay {
+  constructor () {
+    const settings = {
+      clips: {
+        customPeriodInDays: 31,
+        numOfClips: 20
+      }
+    }
+
+    const ui = {
+      clips: {
+        numOfClips: {
+          type: 'number-input',
+          step: '1',
+          min: '1'
+        },
+        customPeriodInDays: {
+          type: 'number-input',
+          step: '1',
+          min: '1'
+        }
+      },
+      links: {
+        overlay: {
+          type: 'link',
+          href: '/overlays/clipscarousel',
+          class: 'btn btn-primary btn-block',
+          rawText: '/overlays/clipscarousel (1920x1080)',
+          target: '_blank'
+        }
+      }
+    }
+
+    super({ settings, ui })
+  }
+
+  sockets () {
+    global.panel.io
+      .of('/' + this._name + '/' + this.constructor.name.toLowerCase(), (socket) => {
+        socket.on('clips', async (cb) => {
+          const clips = await global.api.getTopClips({ period: 'custom', days: this.settings.clips.customPeriodInDays, first: this.settings.clips.numOfClips })
+          cb(null, { clips })
+        })
+      })
+  }
+}
+
+module.exports = new ClipsCarousel()
---
+++ src/overlay/components/clipscarousel.vue
@@ -0,0 +1,67 @@
+<template>
+<div ref="carousel" style="width: 99999999999%; position: absolute;">
+  <template v-for="clip of clips">
+    <video ref="clips" playsinline autoplay="true" :key="clip.index" muted loop>
+      <source :src="clip.mp4" type="video/mp4">
+    </video>
+  </template>
+</div>
+</template>
+
+<script>
+import { TweenMax } from 'gsap/TweenMax'
+
+export default {
+  props: ['token'],
+  data: function () {
+    return {
+      socket: io('/overlays/clipscarousel', {
+        query: "token=" + token
+      }),
+      isPlaying: false,
+      clips: [],
+      currentClip: 0
+    }
+  },
+  created: function () {
+    this.socket.emit('clips', (err, data) => {
+      data.clips = data.clips
+        .map((a) => ({sort: Math.random(), value: a}))
+        .sort((a, b) => a.sort - b.sort)
+        .map((a) => a.value)
+      for (let i = 0, len = data.clips.length; i < len; i++) {
+        data.clips[i].index = Math.random()
+        this.clips.push(data.clips[i])
+      }
+      this.$nextTick(() => {
+        // center to first clip
+        this.moveToNextClip()
+      })
+      this.volume = data.settings.volume
+    })
+
+    setInterval(() => {
+      this.moveToNextClip()
+    }, 45 * 1000)
+  },
+  methods: {
+    moveToNextClip: function () {
+      let clip = this.$refs.clips[++this.currentClip]
+      if (typeof clip === 'undefined') {
+        clip = this.$refs.clips[0]
+        this.currentClip = 0
+      }
+      const offset = clip.offsetLeft - (window.innerWidth / 4)
+      TweenMax.to(this.$refs.carousel, 1, { left: -offset + 'px',  })
+      for (let i = 0, length = this.$refs.clips.length; i < length; i++) {
+        if (i === this.currentClip) TweenMax.to(this.$refs.clips[i], 1, { filter: 'grayscale(0)', opacity: 1 })
+        else TweenMax.to(this.$refs.clips[i], 1, { filter: 'grayscale(1)', opacity: 0.5 })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  video { width: 40vw; padding: 5vw; display: inline-block }
+</style>
