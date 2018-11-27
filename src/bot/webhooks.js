a5d9efcdbbd7f6ade5f2f5f5d7de2239adfe9ce8
soge__ <sogehige@gmail.com>
2018-11-28 00:14:21 +0100

    fix example data

--- src/bot/webhooks.js
+++ src/bot/webhooks.js
@@ -177,13 +177,13 @@ class Webhooks {
   /*
   {
    "data":
-      [{
+      {
          "from_id":"1336",
          "from_name":"ebi",
          "to_id":"1337",
          "to_name":"oliver0823nagy",
          "followed_at": "2017-08-22T22:55:24Z"
-      }]
+      }
   }
   */
   async follower (aEvent) {
