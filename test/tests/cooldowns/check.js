1cdff023b5b57c1251854622daa16461698d50bb
soge__ <sogehige@gmail.com>
2018-12-13 19:33:39 +0100

    cooldown: set default values if settings are missing
    Minor fixes for lowercased issues

--- test/tests/cooldowns/check.js
+++ test/tests/cooldowns/check.js
@@ -1,4 +1,4 @@
-/* global describe it beforeEach */
+/* global describe it before */
 if (require('cluster').isWorker) process.exit()
 
 require('../../general.js')
@@ -11,146 +11,308 @@ const variable = require('../../general.js').variable
 
 // users
 const owner = { username: 'soge__' }
+const usermod1 = { username: 'usermod1', isModerator: true }
+const subuser1 = { username: 'subuser1', isSubscriber: true }
 const testUser = { username: 'test' }
 const testUser2 = { username: 'test2' }
 
 describe('Cooldowns - check()', () => {
-  beforeEach(async () => {
-    await db.cleanup()
-    await message.prepare()
+  describe('#1658 - cooldown not working on not full cooldown object KonCha', async () => {
+    before(async () => {
+      await db.cleanup()
+      await message.prepare()
 
-    global.games.gamble.settings.enabled = true
-    await variable.isEqual('global.games.gamble.settings.enabled', true)
+      global.games.gamble.settings.enabled = true
+      await variable.isEqual('global.games.gamble.settings.enabled', true)
+    })
+
+    it('Add usermod1 as moderator', async () => {
+      await global.db.engine.insert('users', { id: '2', username: 'usermod1', is: { mod: true } })
+    })
+
+    it('Add global KonCha to cooldown', async () => {
+      await global.db.engine.insert(global.systems.cooldown.collection.data, {
+        'key': 'KonCha',
+        'type': 'global',
+        'quiet': true,
+        'owner': true,
+        'enabled': true,
+        'timestamp': 0,
+        'moderator': true,
+        'miliseconds': 60000
+      })
+    })
+
+    it('Add koncha to keywords', async () => {
+      await global.db.engine.insert(global.systems.keywords.collection.data, {
+        'keyword': 'koncha',
+        'response': '$sender KonCha',
+        'enabled': true
+      })
+    })
+
+    it('First user should PASS', async () => {
+      let isOk = await global.systems.cooldown.check({ sender: testUser, message: 'KonCha' })
+      assert.isTrue(isOk)
+    })
+
+    for (let user of [testUser, testUser2, owner, usermod1, subuser1]) {
+      it('Other users should FAIL', async () => {
+        let isOk = await global.systems.cooldown.check({ sender: user, message: 'koncha' })
+        assert.isFalse(isOk)
+      })
+    }
   })
 
-  it('#1406 - cooldown not working on gamble', async () => {
-    let [command, type, seconds, quiet] = ['!gamble', 'user', '300', true]
-    global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
-    await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
+  describe('#1658 - cooldown not working on !followage', async () => {
+    before(async () => {
+      await db.cleanup()
+      await message.prepare()
 
-    let item = await global.db.engine.findOne('systems.cooldown', { key: '!gamble' })
-    assert.notEmpty(item)
+      global.games.gamble.settings.enabled = true
+      await variable.isEqual('global.games.gamble.settings.enabled', true)
+    })
 
-    let isOk = await global.systems.cooldown.check({ sender: testUser, message: '!gamble 10' })
-    assert.isTrue(isOk)
+    it('Add usermod1 as moderator', async () => {
+      await global.db.engine.insert('users', { id: '2', username: 'usermod1', is: { mod: true } })
+    })
 
-    isOk = await global.systems.cooldown.check({ sender: testUser, message: '!gamble 15' })
-    assert.isFalse(isOk) // second should fail
+    it('Add global !followage to cooldown', async () => {
+      await global.db.engine.insert(global.systems.cooldown.collection.data, {
+        'key': '!followage',
+        'type': 'global',
+        'owner': false,
+        'quiet': true,
+        'enabled': true,
+        'follower': true,
+        'moderator': false,
+        'subscriber': true,
+        'miliseconds': 30000,
+        'timestamp': 1544713598872
+      })
+    })
 
-    isOk = await global.systems.cooldown.check({ sender: testUser2, message: '!gamble 20' })
-    assert.isTrue(isOk)
+    it('First user should PASS', async () => {
+      let isOk = await global.systems.cooldown.check({ sender: testUser, message: '!followage' })
+      assert.isTrue(isOk)
+    })
 
-    isOk = await global.systems.cooldown.check({ sender: testUser2, message: '!gamble 25' })
-    assert.isFalse(isOk) // second should fail
+    it('Owner user should PASS', async () => {
+      let isOk = await global.systems.cooldown.check({ sender: owner, message: '!followage' })
+      assert.isTrue(isOk)
+    })
+
+    it('Moderator user should PASS', async () => {
+      let isOk = await global.systems.cooldown.check({ sender: usermod1, message: '!followage' })
+      assert.isTrue(isOk)
+    })
+
+    for (let user of [testUser, testUser2, subuser1]) {
+      it('Other users should FAIL', async () => {
+        let isOk = await global.systems.cooldown.check({ sender: user, message: '!followage' })
+        assert.isFalse(isOk)
+      })
+    }
   })
 
-  it('#1352 - command in a sentence', async () => {
-    let [command, type, seconds, quiet] = ['!test', 'user', '60', true]
-    global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
-    await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
+  describe('#1406 - cooldown not working on gamble', async () => {
+    before(async () => {
+      await db.cleanup()
+      await message.prepare()
 
-    let item = await global.db.engine.findOne('systems.cooldown', { key: '!test' })
-    assert.notEmpty(item)
+      global.games.gamble.settings.enabled = true
+      await variable.isEqual('global.games.gamble.settings.enabled', true)
+    })
 
-    let isOk = await global.systems.cooldown.check({ sender: testUser, message: 'Lorem Ipsum !test' })
-    assert.isTrue(isOk)
+    it('test', async () => {
+      let [command, type, seconds, quiet] = ['!gamble', 'user', '300', true]
+      global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
+      await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
 
-    isOk = await global.systems.cooldown.check({ sender: testUser, message: 'Lorem Ipsum !test' })
-    assert.isTrue(isOk) // second should fail
+      let item = await global.db.engine.findOne('systems.cooldown', { key: '!gamble' })
+      assert.notEmpty(item)
+
+      let isOk = await global.systems.cooldown.check({ sender: testUser, message: '!gamble 10' })
+      assert.isTrue(isOk)
+
+      isOk = await global.systems.cooldown.check({ sender: testUser, message: '!gamble 15' })
+      assert.isFalse(isOk) // second should fail
+
+      isOk = await global.systems.cooldown.check({ sender: testUser2, message: '!gamble 20' })
+      assert.isTrue(isOk)
+
+      isOk = await global.systems.cooldown.check({ sender: testUser2, message: '!gamble 25' })
+      assert.isFalse(isOk) // second should fail
+    })
   })
 
-  it('command with subcommand - user', async () => {
-    let [command, type, seconds, quiet] = ['!test me', 'user', '60', true]
-    global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
-    await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
+  describe('#1352 - command in a sentence', async () => {
+    before(async () => {
+      await db.cleanup()
+      await message.prepare()
 
-    let item = await global.db.engine.findOne('systems.cooldown', { key: '!test me' })
-    assert.notEmpty(item)
+      global.games.gamble.settings.enabled = true
+      await variable.isEqual('global.games.gamble.settings.enabled', true)
+    })
 
-    assert.isTrue(await global.systems.cooldown.check({ sender: testUser, message: command }), `'${command}' expected to not fail`)
-    assert.isFalse(await global.systems.cooldown.check({ sender: testUser, message: command }), `'${command}' expected to fail`)
-    assert.isTrue(await global.systems.cooldown.check({ sender: testUser2, message: command }), `'${command}' expected to not fail`)
+    it('test', async () => {
+      let [command, type, seconds, quiet] = ['!test', 'user', '60', true]
+      global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
+      await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
 
-    assert.isTrue(await global.systems.cooldown.check({ sender: testUser, message: '!test' }))
-    assert.isTrue(await global.systems.cooldown.check({ sender: testUser, message: '!test' }))
-    assert.isTrue(await global.systems.cooldown.check({ sender: testUser2, message: '!test' }))
+      let item = await global.db.engine.findOne('systems.cooldown', { key: '!test' })
+      assert.notEmpty(item)
+
+      let isOk = await global.systems.cooldown.check({ sender: testUser, message: 'Lorem Ipsum !test' })
+      assert.isTrue(isOk)
+
+      isOk = await global.systems.cooldown.check({ sender: testUser, message: 'Lorem Ipsum !test' })
+      assert.isTrue(isOk) // second should fail
+    })
   })
 
-  it('command - user', async () => {
-    let [command, type, seconds, quiet] = ['!test', 'user', '60', true]
-    global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
-    await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
+  describe('command with subcommand - user', async () => {
+    before(async () => {
+      await db.cleanup()
+      await message.prepare()
 
-    let item = await global.db.engine.findOne('systems.cooldown', { key: '!test' })
-    assert.notEmpty(item)
+      global.games.gamble.settings.enabled = true
+      await variable.isEqual('global.games.gamble.settings.enabled', true)
+    })
 
-    let isOk = await global.systems.cooldown.check({ sender: testUser, message: '!test' })
-    assert.isTrue(isOk)
+    it('test', async () => {
+      let [command, type, seconds, quiet] = ['!test me', 'user', '60', true]
+      global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
+      await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
 
-    isOk = await global.systems.cooldown.check({ sender: testUser, message: '!test' })
-    assert.isFalse(isOk) // second should fail
+      let item = await global.db.engine.findOne('systems.cooldown', { key: '!test me' })
+      assert.notEmpty(item)
 
-    isOk = await global.systems.cooldown.check({ sender: testUser2, message: '!test' })
-    assert.isTrue(isOk)
+      assert.isTrue(await global.systems.cooldown.check({ sender: testUser, message: command }), `'${command}' expected to not fail`)
+      assert.isFalse(await global.systems.cooldown.check({ sender: testUser, message: command }), `'${command}' expected to fail`)
+      assert.isTrue(await global.systems.cooldown.check({ sender: testUser2, message: command }), `'${command}' expected to not fail`)
+
+      assert.isTrue(await global.systems.cooldown.check({ sender: testUser, message: '!test' }))
+      assert.isTrue(await global.systems.cooldown.check({ sender: testUser, message: '!test' }))
+      assert.isTrue(await global.systems.cooldown.check({ sender: testUser2, message: '!test' }))
+    })
   })
 
-  it('command - global', async () => {
-    let [command, type, seconds, quiet] = ['!test', 'global', '60', true]
-    global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
-    await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
+  describe('command - user', async () => {
+    before(async () => {
+      await db.cleanup()
+      await message.prepare()
 
-    let item = await global.db.engine.findOne('systems.cooldown', { key: '!test' })
-    assert.notEmpty(item)
+      global.games.gamble.settings.enabled = true
+      await variable.isEqual('global.games.gamble.settings.enabled', true)
+    })
 
-    let isOk = await global.systems.cooldown.check({ sender: testUser, message: '!test' })
-    assert.isTrue(isOk)
+    it('test', async () => {
+      let [command, type, seconds, quiet] = ['!test', 'user', '60', true]
+      global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
+      await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
 
-    isOk = await global.systems.cooldown.check({ sender: testUser, message: '!test' })
-    assert.isFalse(isOk) // second should fail
+      let item = await global.db.engine.findOne('systems.cooldown', { key: '!test' })
+      assert.notEmpty(item)
 
-    isOk = await global.systems.cooldown.check({ sender: testUser2, message: '!test' })
-    assert.isFalse(isOk) // another user should fail as well
+      let isOk = await global.systems.cooldown.check({ sender: testUser, message: '!test' })
+      assert.isTrue(isOk)
+
+      isOk = await global.systems.cooldown.check({ sender: testUser, message: '!test' })
+      assert.isFalse(isOk) // second should fail
+
+      isOk = await global.systems.cooldown.check({ sender: testUser2, message: '!test' })
+      assert.isTrue(isOk)
+    })
   })
 
-  it('keyword - user', async () => {
-    global.systems.keywords.add({ sender: owner, parameters: 'me (!me)' })
-    await message.isSent('keywords.keyword-was-added', owner, { keyword: 'me', sender: owner.username })
+  describe('command - global', async () => {
+    before(async () => {
+      await db.cleanup()
+      await message.prepare()
 
-    let [command, type, seconds, quiet] = ['me', 'user', '60', true]
-    global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
-    await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
+      global.games.gamble.settings.enabled = true
+      await variable.isEqual('global.games.gamble.settings.enabled', true)
+    })
 
-    let item = await global.db.engine.findOne('systems.cooldown', { key: 'me' })
-    assert.notEmpty(item)
+    it('test', async () => {
+      let [command, type, seconds, quiet] = ['!test', 'global', '60', true]
+      global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
+      await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
 
-    let isOk = await global.systems.cooldown.check({ sender: testUser, message: 'me' })
-    assert.isTrue(isOk)
+      let item = await global.db.engine.findOne('systems.cooldown', { key: '!test' })
+      assert.notEmpty(item)
 
-    isOk = await global.systems.cooldown.check({ sender: testUser, message: 'me' })
-    assert.isFalse(isOk) // second should fail
+      let isOk = await global.systems.cooldown.check({ sender: testUser, message: '!test' })
+      assert.isTrue(isOk)
 
-    isOk = await global.systems.cooldown.check({ sender: testUser2, message: 'me' })
-    assert.isTrue(isOk)
+      isOk = await global.systems.cooldown.check({ sender: testUser, message: '!test' })
+      assert.isFalse(isOk) // second should fail
+
+      isOk = await global.systems.cooldown.check({ sender: testUser2, message: '!test' })
+      assert.isFalse(isOk) // another user should fail as well
+    })
   })
 
-  it('keyword - global', async () => {
-    global.systems.keywords.add({ sender: owner, parameters: 'me (!me)' })
-    await message.isSent('keywords.keyword-was-added', owner, { keyword: 'me', sender: owner.username })
+  describe('keyword - user', async () => {
+    before(async () => {
+      await db.cleanup()
+      await message.prepare()
 
-    let [command, type, seconds, quiet] = ['me', 'global', '60', true]
-    global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
-    await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
+      global.games.gamble.settings.enabled = true
+      await variable.isEqual('global.games.gamble.settings.enabled', true)
+    })
 
-    let item = await global.db.engine.findOne('systems.cooldown', { key: 'me' })
-    assert.notEmpty(item)
+    it('test', async () => {
+      global.systems.keywords.add({ sender: owner, parameters: 'me (!me)' })
+      await message.isSent('keywords.keyword-was-added', owner, { keyword: 'me', sender: owner.username })
 
-    let isOk = await global.systems.cooldown.check({ sender: testUser, message: 'me' })
-    assert.isTrue(isOk)
+      let [command, type, seconds, quiet] = ['me', 'user', '60', true]
+      global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
+      await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
 
-    isOk = await global.systems.cooldown.check({ sender: testUser, message: 'me' })
-    assert.isFalse(isOk) // second should fail
+      let item = await global.db.engine.findOne('systems.cooldown', { key: 'me' })
+      assert.notEmpty(item)
 
-    isOk = await global.systems.cooldown.check({ sender: testUser2, message: 'me' })
-    assert.isFalse(isOk) // another user should fail as well
+      let isOk = await global.systems.cooldown.check({ sender: testUser, message: 'me' })
+      assert.isTrue(isOk)
+
+      isOk = await global.systems.cooldown.check({ sender: testUser, message: 'me' })
+      assert.isFalse(isOk) // second should fail
+
+      isOk = await global.systems.cooldown.check({ sender: testUser2, message: 'me' })
+      assert.isTrue(isOk)
+    })
+  })
+
+  describe('keyword - global', async () => {
+    before(async () => {
+      await db.cleanup()
+      await message.prepare()
+
+      global.games.gamble.settings.enabled = true
+      await variable.isEqual('global.games.gamble.settings.enabled', true)
+    })
+
+    it('test', async () => {
+      global.systems.keywords.add({ sender: owner, parameters: 'me (!me)' })
+      await message.isSent('keywords.keyword-was-added', owner, { keyword: 'me', sender: owner.username })
+
+      let [command, type, seconds, quiet] = ['me', 'global', '60', true]
+      global.systems.cooldown.main({ sender: owner, parameters: `${command} ${type} ${seconds} ${quiet}` })
+      await message.isSent('cooldowns.cooldown-was-set', owner, { command: command, type: type, seconds: seconds, sender: owner.username })
+
+      let item = await global.db.engine.findOne('systems.cooldown', { key: 'me' })
+      assert.notEmpty(item)
+
+      let isOk = await global.systems.cooldown.check({ sender: testUser, message: 'me' })
+      assert.isTrue(isOk)
+
+      isOk = await global.systems.cooldown.check({ sender: testUser, message: 'me' })
+      assert.isFalse(isOk) // second should fail
+
+      isOk = await global.systems.cooldown.check({ sender: testUser2, message: 'me' })
+      assert.isFalse(isOk) // another user should fail as well
+    })
   })
 })
