e27fde9e1d3b209557ca587115b6db88402c4bbd
soge__ <sogehige@gmail.com>
2018-12-22 02:46:50 +0100

    widget: add twitch monitor refresh button

--- src/panel/widgets/components/twitch.vue
+++ src/panel/widgets/components/twitch.vue
@@ -7,6 +7,12 @@
           <font-awesome-icon :icon="['fab', 'twitch']" />
         </a>
       </li>
+      <li role="presentation" class="nav-item">
+        <a href="#" class="nav-link" title="Refresh" @click="refresh">
+          <font-awesome-icon icon="sync-alt" v-if="!isRefreshing"/>
+          <font-awesome-icon icon="sync-alt" spin v-else/>
+        </a>
+      </li>
       <li class="nav-item ml-auto">
         <h6 class="widget-title">{{ commons.translate('widget-title-monitor') }}</h6>
       </li>
@@ -28,22 +34,42 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTwitch } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faTwitch)
+library.add(faTwitch, faSyncAlt)
 
 export default {
+  data: function () {
+    return {
+      room: '',
+      isRefreshing: false
+    }
+  },
   props: ['socket', 'commons'],
   components: {
     'font-awesome-icon': FontAwesomeIcon
   },
   created: function () {
     this.socket.emit('twitch.sendTwitchVideo')
-    this.socket.once('twitchVideo', function (room) {
+    this.socket.once('twitchVideo', (room) => {
+      this.room = room
       $("#twitch-main").append(`<iframe style="width: 100%; height: 100%"
         src="https://player.twitch.tv/?channel=${room}&autoplay=true&muted=true"
         frameborder="0">
       </iframe>`)
     })
+  },
+  methods: {
+    refresh: function () {
+      console.log()
+      this.isRefreshing = true
+      setTimeout(() => (this.isRefreshing = false), 2000)
+      $("#twitch-main").empty()
+      $("#twitch-main").append(`<iframe style="width: 100%; height: 100%"
+        src="https://player.twitch.tv/?channel=${this.room}&autoplay=true&muted=true"
+        frameborder="0">
+      </iframe>`)
+    },
   },
   mounted: function () {
     this.$emit('mounted')
