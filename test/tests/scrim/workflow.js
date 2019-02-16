f0c54c9b02000429dea38d60bf5cefb99bc24547
soge__ <sogehige@gmail.com>
2019-02-16 16:07:32 +0100

    test: fix matches possibilities

--- test/tests/scrim/workflow.js
+++ test/tests/scrim/workflow.js
@@ -201,16 +201,38 @@ describe('Scrim - full workflow', () => {
     })
 
     it('Expecting populated message list', async () => {
-      await message.isSent('systems.scrim.currentMatches', owner, {
-        matches: 'ABC - @user1, @user2, @user3 | ABD - @user4',
-      }, 19000)
+      await message.isSent('systems.scrim.currentMatches', owner, [
+        { matches: 'ABC - @user1, @user2, @user3 | ABD - @user4' },
+        { matches: 'ABC - @user1, @user3, @user2 | ABD - @user4' },
+        { matches: 'ABC - @user2, @user1, @user3 | ABD - @user4' },
+        { matches: 'ABC - @user3, @user1, @user2 | ABD - @user4' },
+        { matches: 'ABC - @user3, @user2, @user1 | ABD - @user4' },
+        { matches: 'ABC - @user2, @user3, @user1 | ABD - @user4' },
+        { matches: 'ABD - @user4 | ABC - @user1, @user2, @user3' },
+        { matches: 'ABD - @user4 | ABC - @user1, @user3, @user2' },
+        { matches: 'ABD - @user4 | ABC - @user2, @user1, @user3' },
+        { matches: 'ABD - @user4 | ABC - @user3, @user1, @user2' },
+        { matches: 'ABD - @user4 | ABC - @user3, @user2, @user1' },
+        { matches: 'ABD - @user4 | ABC - @user2, @user3, @user1' },
+      ], 19000)
     })
 
     it('Check match list by command', async () => {
       global.systems.scrim.match({ sender: owner, parameters: '' })
-      await message.isSent('systems.scrim.currentMatches', owner, {
-        matches: 'ABC - @user1, @user2, @user3 | ABD - @user4',
-      }, 19000)
+      await message.isSent('systems.scrim.currentMatches', owner, [
+        { matches: 'ABC - @user1, @user2, @user3 | ABD - @user4' },
+        { matches: 'ABC - @user1, @user3, @user2 | ABD - @user4' },
+        { matches: 'ABC - @user2, @user1, @user3 | ABD - @user4' },
+        { matches: 'ABC - @user3, @user1, @user2 | ABD - @user4' },
+        { matches: 'ABC - @user3, @user2, @user1 | ABD - @user4' },
+        { matches: 'ABC - @user2, @user3, @user1 | ABD - @user4' },
+        { matches: 'ABD - @user4 | ABC - @user1, @user2, @user3' },
+        { matches: 'ABD - @user4 | ABC - @user1, @user3, @user2' },
+        { matches: 'ABD - @user4 | ABC - @user2, @user1, @user3' },
+        { matches: 'ABD - @user4 | ABC - @user3, @user1, @user2' },
+        { matches: 'ABD - @user4 | ABC - @user3, @user2, @user1' },
+        { matches: 'ABD - @user4 | ABC - @user2, @user3, @user1' },
+      ], 19000)
     })
   })
 })
