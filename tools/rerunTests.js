77d7f1521ab1025946b20ff79735aee08a145c41
sogehige <sogehige@gmail.com>
2019-02-20 10:27:20 +0100

    test: rerun only errored suites

---
+++ tools/rerunTests.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const child_process = require('child_process')
+
+const file = fs.readFileSync('report').toString();
+const regexp = /^  \d\)(.*)$/gm
+const match = file.match(regexp)
+
+let status = 0
+
+if (match) {
+  for (let m of match) {
+    const suite = m.trim().split(/\d\) /)[1]
+    console.log('\n\t => Re-Running ' + suite + ' tests')
+    const p = child_process.spawnSync('npx', [
+      'mocha',
+      '--timeout', '20000',
+      '--exit',
+      '--grep="' + suite + '"',
+      '--recursive',
+      'test/'
+    ], {
+      shell: true,
+    });
+
+    if (p.status === 1) {
+      status = 1;
+      console.log('\t   !!! Failed again :(')
+    } else {
+      console.log('\t   !!! Tests OK! :)')
+    }
+  }
+  console.log('\n\n')
+} else {
+  console.log('\n\t No tests to rerun :)\n\n')
+}
+
+process.exit(status)
