4e1c8d8bf9378a7b14c89ae48af52e12e0e3a069
soge__ <sogehige@gmail.com>
2019-03-12 18:41:25 +0100

    fix: remove comment

--- dist/js/components.js
+++ dist/js/components.js
@@ -179,40 +179,6 @@ window.commandInputWithPermissions = {
       this.permissionsLoaded = true
     })
   },
-  /*template: `
-  <div class="input-group">
-    <div class="input-group-prepend">
-      <span class="input-group-text">{{ command }}</span>
-    </div>
-    <input v-on:keyup="update" v-model="currentValue" class="form-control" type="text" />
-    <div v-if="!permissionsLoaded" class="input-group-append">
-      <div class="spinner-grow spinner-grow-sm" role="status">
-        <span class="sr-only">Loading...</span>
-      </div>
-    </div>
-    <div class="input-group-append" v-else>
-      <div class="dropdown">
-        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
-                :class="{'btn-warning': currentPermissions === null, btn-dark': currentPermissions !== null && getPermissionName(currentPermissions) !== null, 'btn-danger': currentPermissions !== null && getPermissionName(currentPermissions) === null}">
-          <span v-if="getPermissionName(currentPermissions) !== null">{{ getPermissionName(currentPermissions) }}</span>
-          <span v-else>
-            <i class="fas fa-exclamation-triangle"></i> Permission not found
-          </span>
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <button
-          v-for="p of permissionsList"
-          class="dropdown-item"
-          @click="currentPermissions = p.id"
-        >{{p.name}}</button>
-        <button
-          class="dropdown-item"
-          @click="currentPermissions = null"
-        >Disabled</button>
-        </div>
-      </div>
-    </div>
-  </div>`*/
   template: `
     <div class="input-group">
       <div class="input-group-prepend">
