848ec7a386450beacad355f510a07c1340087571
soge__ <sogehige@gmail.com>
2019-03-16 02:21:13 +0100

    test: add permissions tests

--- src/bot/permissions.ts
+++ src/bot/permissions.ts
@@ -8,8 +8,8 @@ const permission = Object.freeze({
   CASTERS: '4300ed23-dca0-4ed9-8014-f5f2f7af55a9',
   MODERATORS: 'b38c5adb-e912-47e3-937a-89fabd12393a',
   SUBSCRIBERS: 'e3b557e7-c26a-433c-a183-e56c11003ab7',
-  VIEWERS: '0efd7b1c-e460-4167-8e06-8aaf2c170311',
   FOLLOWERS: 'c168a63b-aded-4a90-978f-ed357e95b0d2',
+  VIEWERS: '0efd7b1c-e460-4167-8e06-8aaf2c170311',
 });
 
 class Permissions extends Core {
