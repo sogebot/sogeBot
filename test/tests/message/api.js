15c6ac828d1b35a8e730a008f8db5104b8cce344
Michal Orlik <sogehige@gmail.com>
2019-03-18 16:49:15 +0100

    test(message): make message.api test offline
    Fixes #2027

--- test/tests/message/api.js
+++ test/tests/message/api.js
@@ -5,15 +5,30 @@ const db = require('../../general.js').db
 const msg = require('../../general.js').message
 const Message = require('../../../dest/message')
 const assert = require('chai').assert
+const sinon = require('sinon')
+const axios = require('axios')
+const MockAdapter = require('axios-mock-adapter');
+const mock = new MockAdapter(axios);
 
 describe('Message - api filter', () => {
+  before(() => {
+    // add axios mock
+    mock
+      .onGet('http://localhost/get?test=a\\nb').reply(200, {
+        test: 'a\\nb'
+      })
+      .onAny().passThrough(); // pass through others
+  })
+  after(() => {
+    mock.restore();
+  })
   beforeEach(async () => {
     await db.cleanup()
     await msg.prepare()
   })
 
   describe('#1989 - ?test=a\\\\nb should be correctly parsed', () => {
-    let toParse = '(api|https://httpbin.org/get?test=a\\nb) Lorem (api.args.test)'
+    let toParse = '(api|http://localhost/get?test=a\\nb) Lorem (api.test)'
 
     it('Expecting response Lorem a\\\\nb', async () => {
       let message = await new Message(toParse).parse({ })
