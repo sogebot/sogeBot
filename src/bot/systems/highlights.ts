81a49fba7fe2218fdccaf9c7fedd0922cd072061
Michal Orlik <sogehige@gmail.com>
2019-04-11 14:23:13 +0200

    refactor: move highlights to typescript

---
+++ src/bot/systems/highlights.ts
@@ -0,0 +1,132 @@
+import axios from 'axios';
+import { get, isNil, orderBy } from 'lodash';
+import moment from 'moment';
+import 'moment-precise-range-plugin';
+import { isMainThread } from 'worker_threads';
+import { sendMessage } from '../commons';
+import { permission } from '../permissions';
+import System from './_interface';
+
+const ERROR_STREAM_NOT_ONLINE = '1';
+const ERROR_MISSING_TOKEN = '2';
+
+/*
+ * !highlight <?description> - save highlight with optional description
+ * !highlight list           - get list of highlights in current running or latest stream
+ */
+
+class Highlights extends System {
+  constructor() {
+    const options: InterfaceSettings = {
+      settings: {
+        commands: [
+          { name: '!highlight list', permission: permission.CASTERS },
+          { name: '!highlight', permission: permission.CASTERS },
+        ],
+      },
+    };
+    super(options);
+
+    this.addMenu({ category: 'manage', name: 'highlights', id: 'highlights/list' });
+  }
+
+  public sockets() {
+    if (this.socket === null) {
+      return setTimeout(() => this.sockets(), 100);
+    }
+
+    this.socket.on('connection', (socket) => {
+      socket.on('highlight', () => {
+        this.main({ parameters: '', sender: null });
+      });
+      socket.on('list', async (cb) => {
+        cb(null, await global.db.engine.find(this.collection.data));
+      });
+      socket.on('delete', async (_id, cb) => {
+        await global.db.engine.remove(this.collection.data, { _id });
+        cb(null);
+      });
+    });
+  }
+
+  public async main(opts) {
+    const when = await global.cache.when();
+    const token = global.oauth.settings.bot.accessToken;
+    const cid = global.oauth.settings._.channelId;
+    const url = `https://api.twitch.tv/helix/videos?user_id=${cid}&type=archive&first=1`;
+
+    try {
+      if (isNil(when.online)) { throw Error(ERROR_STREAM_NOT_ONLINE); }
+      if (token === '' || cid === '') { throw Error(ERROR_MISSING_TOKEN); }
+
+      // we need to load video id
+      const request = await axios.get(url, {
+        headers: {
+          Authorization: 'Bearer ' + token,
+        },
+      });
+      // save remaining api calls
+      global.api.remainingAPICalls = request.headers['ratelimit-remaining'];
+      global.api.refreshAPICalls = request.headers['ratelimit-reset'];
+
+      const timestamp = moment.preciseDiff(moment.utc(), moment.utc(global.api.streamStartedAt), true);
+      const highlight = {
+        id: request.data.data[0].id,
+        timestamp: { hours: timestamp.hours, minutes: timestamp.minutes, seconds: timestamp.seconds },
+        game: get(await global.db.engine.findOne('api.current', { key: 'game' }), 'value', 'n/a'),
+        title: get(await global.db.engine.findOne('api.current', { key: 'title' }), 'value', 'n/a'),
+        created_at: Date.now(),
+      };
+
+      global.panel.io.emit('api.stats', { data: request.data, timestamp: Date.now(), call: 'highlights', api: 'helix', endpoint: url, code: request.status, remaining: global.api.remainingAPICalls });
+
+      this.add(highlight, timestamp, opts.sender);
+    } catch (e) {
+      global.panel.io.emit('api.stats', { timestamp: Date.now(), call: 'highlights', api: 'helix', endpoint: url, code: e.stack, remaining: global.api.remainingAPICalls });
+      switch (e.message) {
+        case ERROR_STREAM_NOT_ONLINE:
+          global.log.error('Cannot highlight - stream offline');
+          sendMessage(global.translate('highlights.offline'), opts.sender);
+          break;
+        case ERROR_MISSING_TOKEN:
+          global.log.error('Cannot highlight - missing token');
+          break;
+        default:
+          global.log.error(e.stack);
+      }
+    }
+  }
+
+  public async add(highlight, timestamp, sender) {
+    global.api.createMarker();
+    sendMessage(global.translate('highlights.saved')
+      .replace(/\$hours/g, (timestamp.hours < 10) ? '0' + timestamp.hours : timestamp.hours)
+      .replace(/\$minutes/g, (timestamp.minutes < 10) ? '0' + timestamp.minutes : timestamp.minutes)
+      .replace(/\$seconds/g, (timestamp.seconds < 10) ? '0' + timestamp.seconds : timestamp.seconds), sender);
+
+    global.db.engine.insert(this.collection.data, highlight);
+  }
+
+  public async list(opts) {
+    let highlights: any[] = await global.db.engine.find(this.collection.data);
+    const sortedHighlights = orderBy(highlights, 'id', 'desc');
+    const latestStreamId = sortedHighlights.length > 0 ? sortedHighlights[0].id : null;
+
+    if (isNil(latestStreamId)) {
+      sendMessage(global.translate('highlights.list.empty'), opts.sender);
+      return;
+    }
+    highlights = highlights.filter((o) => o.id === latestStreamId);
+    const list: string[] = [];
+
+    for (const highlight of highlights) {
+      list.push(highlight.timestamp.hours + 'h' +
+        highlight.timestamp.minutes + 'm' +
+        highlight.timestamp.seconds + 's');
+    }
+    sendMessage(global.translate(list.length > 0 ? 'highlights.list.items' : 'highlights.list.empty')
+      .replace(/\$items/g, list.join(', ')), opts.sender);
+  }
+}
+
+module.exports = new Highlights();
