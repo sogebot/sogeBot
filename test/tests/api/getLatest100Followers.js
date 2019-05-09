39e3148522b533e37fcbd535d1e019b6cad924f8
Michal Orlik <sogehige@gmail.com>
2019-05-09 13:14:48 +0200

    tests(getLatest100Followers): add axios mock

--- test/tests/api/getLatest100Followers.js
+++ test/tests/api/getLatest100Followers.js
@@ -15,56 +15,8 @@ const sinon = require('sinon')
 const axios = require('axios')
 const assert = require('assert')
 
-const exampleResponse = {
-  headers: {
-    'ratelimit-remaining': 800,
-    'ratelimit-reset': 0,
-    'ratelimit-limit': 800
-  },
-  'status': 200,
-  'data': {
-    'total': 12345,
-    'data': [
-      {
-        'from_id': '171003792',
-        'from_name': 'IIIsutha067III',
-        'to_id': '23161357',
-        'to_name': 'LIRIK',
-        'followed_at': '2017-08-22T22:55:24Z'
-      },
-      {
-        'from_id': '113627897',
-        'from_name': 'Birdman616',
-        'to_id': '23161357',
-        'to_name': 'LIRIK',
-        'followed_at': '2017-08-22T22:55:04Z'
-      },
-      {
-        'from_id': '111',
-        'from_name': 'testfollow',
-        'to_id': '23161357',
-        'to_name': 'LIRIK',
-        'followed_at': String(new Date())
-      },
-      {
-        'from_id': '222',
-        'from_name': 'testfollow2',
-        'to_id': '23161357',
-        'to_name': 'LIRIK',
-        'followed_at': String(new Date())
-      },
-      {
-        'from_id': '333',
-        'from_name': '__bot_username__',
-        'to_id': '23161357',
-        'to_name': 'LIRIK',
-        'followed_at': String(new Date())
-      }
-    ],
-    'pagination': {
-      'cursor': 'eyJiIjpudWxsLCJhIjoiMTUwMzQ0MTc3NjQyNDQyMjAwMCJ9'
-    } }
-}
+const MockAdapter = require('axios-mock-adapter');
+let mock = null;
 
 describe('API - getLatest100Followers()', () => {
   before(async () => {
@@ -89,8 +41,60 @@ describe('API - getLatest100Followers()', () => {
 
   describe('Example data', () => {
     before(() => {
-      const resolved = new Promise((resolve) => resolve(exampleResponse))
-      sinon.stub(axios, 'get').returns(resolved)
+      mock = new MockAdapter(axios);
+      // add axios mock
+      mock.onGet('https://api.twitch.tv/helix/users/follows?to_id=12345&first=100').reply(200, {
+        'total': 12345,
+        'data': [
+          {
+            'from_id': '171003792',
+            'from_name': 'IIIsutha067III',
+            'to_id': '23161357',
+            'to_name': 'LIRIK',
+            'followed_at': '2017-08-22T22:55:24Z'
+          },
+          {
+            'from_id': '113627897',
+            'from_name': 'Birdman616',
+            'to_id': '23161357',
+            'to_name': 'LIRIK',
+            'followed_at': '2017-08-22T22:55:04Z'
+          },
+          {
+            'from_id': '111',
+            'from_name': 'testfollow',
+            'to_id': '23161357',
+            'to_name': 'LIRIK',
+            'followed_at': String(new Date())
+          },
+          {
+            'from_id': '222',
+            'from_name': 'testfollow2',
+            'to_id': '23161357',
+            'to_name': 'LIRIK',
+            'followed_at': String(new Date())
+          },
+          {
+            'from_id': '333',
+            'from_name': '__bot_username__',
+            'to_id': '23161357',
+            'to_name': 'LIRIK',
+            'followed_at': String(new Date())
+          }
+        ],
+        'pagination': {
+          'cursor': 'eyJiIjpudWxsLCJhIjoiMTUwMzQ0MTc3NjQyNDQyMjAwMCJ9'
+        }
+      }, {
+        'ratelimit-remaining': 800,
+        'ratelimit-reset': 0,
+        'ratelimit-limit': 800
+      })
+      .onAny().passThrough(); // pass through others
+    })
+
+    after(() => {
+      mock.restore();
     })
 
     it('should be properly parsed', async () => {
