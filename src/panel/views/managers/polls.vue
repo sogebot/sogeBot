dfdb5cf391395d7b186f6a7224e78fcf817eafb8
soge__ <sogehige@gmail.com>
2019-06-18 00:45:54 +0200

    fix(polls): set correct moment locale

--- src/panel/views/managers/polls.vue
+++ src/panel/views/managers/polls.vue
@@ -204,7 +204,7 @@
       clearInterval(this.interval)
     },
     mounted: function () {
-      this.$moment.locale(this.configuration.core.general.lang)
+      this.$moment.locale(this.configuration.lang)
       this.currentTime = Date.now()
       this.domWidth = (this.$refs['window'] as HTMLElement).clientWidth
       this.refresh();
