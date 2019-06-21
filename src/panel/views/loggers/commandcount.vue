96a2fb35b0fb0b7a74f1c771ae2772d6df773cac
sogehige <sogehige@gmail.com>
2019-06-21 15:47:09 +0200

    feat(stats): add date interval selection (#2313)
    [skip-tests]

--- src/panel/views/loggers/commandcount.vue
+++ src/panel/views/loggers/commandcount.vue
@@ -11,9 +11,16 @@
     </div>
 
     <panel cards></panel>
+    <div class="row">
+      <div class="col">
+        <line-chart :data="generateChartData()"></line-chart>
+      </div>
+      <div class="form-group">
+        <datetime v-model="dateRange" :config="dateTimePicker"></datetime>
+      </div>
+    </div>
 
-    <line-chart :data="generateChartData()"></line-chart>
-
+  {{ timestampSmooth }}
     <table class="table table-striped">
       <thead>
         <tr>
@@ -67,12 +74,16 @@
   library.add(faChartLine)
   Vue.use(Chartkick.use(Chart));
 
+  import VueFlatPickr from 'vue-flatpickr-component';
+  import 'flatpickr/dist/flatpickr.css';
+
   import io from 'socket.io-client';
 
   export default Vue.extend({
     components: {
       panel: () => import('../../components/panel.vue'),
-      'font-awesome-icon': FontAwesomeIcon
+      'font-awesome-icon': FontAwesomeIcon,
+      datetime: VueFlatPickr,
     },
     data: function () {
       const object: {
@@ -83,12 +94,20 @@
           timestamp: number,
         }[],
         showChartCommands: string[],
-        showChartInterval: 'hour' | 'day' | 'week' | 'month' | 'year' | 'all',
+        dateTimePicker: any,
+        dateRange: any,
       } = {
         socket: io('/stats/commandcount', { query: "token=" + this.token }),
         commandsUsage: [],
         showChartCommands: [],
-        showChartInterval: 'year',
+        dateTimePicker: {
+          mode: "range",
+          maxDate: new Date(),
+          inline: true,
+        },
+        dateRange:
+            new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + (new Date().getDate() - 14) + ' to ' +
+            new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate()
       }
       return object
     },
@@ -100,6 +119,65 @@
     computed: {
       commands(): string[] {
         return [...new Set(this.commandsUsage.map(o => o.command))];
+      },
+      fromDate(): number {
+        if (this.dateRange.includes('to')) {
+          return new Date(this.dateRange.split(' to ')[0]).getTime()
+        } else {
+          return new Date(this.dateRange.length > 0 ? this.dateRange : Date.now()).getTime()
+        }
+      },
+      toDate(): number {
+        if (this.dateRange.includes('to')) {
+          return new Date(this.dateRange.split(' to ')[1]).getTime()
+        } else {
+          return new Date().getTime()
+        }
+      },
+      timestampList(): number[] {
+        const from = new Date(this.fromDate).getTime()
+        const to = new Date(this.toDate).getTime();
+
+        const list: number[] = []
+        for (
+          let timestamp = (from / (this.timestampSmooth) * (this.timestampSmooth));
+          timestamp <= (to / (this.timestampSmooth) * (this.timestampSmooth));
+          timestamp = timestamp + (this.timestampSmooth)) {
+            list.push(timestamp);
+        }
+        return list;
+      },
+      timestampSmooth(): number {
+        const from = new Date(this.fromDate).getTime()
+        const to = new Date(this.toDate).getTime();
+
+        const list: number[] = []
+        for (
+          let timestamp = (from / (1000 * 60 * 30) * (1000 * 60 * 30));
+          timestamp <= (to / (1000 * 60 * 30) * (1000 * 60 * 30));
+          timestamp = timestamp + (1000 * 60 * 30)) {
+            list.push(timestamp);
+        }
+
+        if (list.length <= (48 /*day*/ * 1)) {
+          return 1000 * 60 * 30 // half hour
+        } else if (list.length <= (48 /*day*/ * 2)) {
+          return 1000 * 60 * 60 // hour
+        } else if (list.length <= (48 /*day*/ * 3)) {
+          return 1000 * 60 * 60 * 2 // 2 hours
+        } else if (list.length <= (48 /*day*/ * 6)) {
+          return 1000 * 60 * 60 * 4 // 4 hours
+        } else if (list.length <= (48 /*day*/ * 12)) {
+          return 1000 * 60 * 60 * 8 // 8 hours
+        } else if (list.length <= (48 /*day*/ * 21)) {
+          return 1000 * 60 * 60 * 12 // 12 hours
+        } else if (list.length <= (48 /*day*/ * 30)) {
+          return 1000 * 60 * 60 * 24 // day
+        } else if (list.length <= (48 /*day*/ * 365)) {
+          return 1000 * 60 * 60 * 24 * 30 // month
+        } else {
+          return 1000 * 60 * 60 * 24 * 30 * 12 // year
+        }
       }
     },
     methods: {
@@ -122,15 +200,28 @@
             continue;
           }
           const timestamps = this.commandsUsage
-            .filter(o => o.command === command)
-            .sort((a, b) => a.timestamp - b.timestamp)
-            .map(o => {
-              return new Date(Number(Number(o.timestamp / (1000 * 60 * 30)).toFixed()) * (1000 * 60 * 30)).toLocaleString();
+            .filter(o => {
+              const isCommand = o.command === command;
+              const isHigherThanFromDate = o.timestamp >= this.fromDate;
+              const isLowerThanToDate = o.timestamp <= this.toDate;
+              return isCommand && isHigherThanFromDate && isLowerThanToDate;
             })
-          console.log({timestamps})
+            .map(o => {
+              return Number(Number(o.timestamp / (this.timestampSmooth)).toFixed()) * this.timestampSmooth;
+            })
+          const countByTimestamps = this._.countBy(timestamps)
+          for (const t of this.timestampList) {
+            if (!countByTimestamps[t]) {
+              countByTimestamps[t] = 0;
+            }
+          }
+          const countByTimestampsOrdered = {}
+          for (const k of Object.keys(countByTimestamps).sort()) {
+            countByTimestampsOrdered[new Date(Number(k)).toLocaleString()] = countByTimestamps[k];
+          }
           data.push({
             name: command,
-            data: this._.countBy(timestamps),
+            data: countByTimestampsOrdered,
           });
         }
         return data;
@@ -163,4 +254,7 @@
 </script>
 
 <style scoped>
+.flatpickr-input {
+  display: none;
+}
 </style>
