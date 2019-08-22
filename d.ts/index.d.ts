dec23aa11a925a969540baefa90afdf7c1d34cad
Michal Orlik <sogehige@gmail.com>
2019-08-22 16:14:45 +0200

    layout images update

--- d.ts/index.d.ts
+++ d.ts/index.d.ts
@@ -13,6 +13,11 @@ declare module '*.gif' {
   export default content;
 }
 
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
 declare module '*.mp3' {
   const content: string;
   export default content;
--- src/panel/views/registries/alerts/components/layout-picker.vue
+++ src/panel/views/registries/alerts/components/layout-picker.vue
@@ -1,50 +1,23 @@
 <template>
   <b-card-group deck>
-    <b-card :class="{'bg-secondary': layout_number === '1'}" @click="layout_number = '1'">
-      <b-card-text class="text-center m-0">
-        <fa icon="image" size="4x" class="text-muted"/>
-      </b-card-text>
-      <b-card-text class="text-center text pb-1">text</b-card-text>
+    <b-card :class="{'bg-secondary': layout_number === '1'}" @click="layout_number = '1'"
+            :img-src="layout1">
     </b-card>
 
-    <b-card :class="{'bg-secondary': layout_number === '2'}" @click="layout_number = '2'">
-      <b-card-text class="text-center text mb-0">text</b-card-text>
-      <b-card-text class="text-center m-0">
-        <fa icon="image" size="4x" class="text-muted"/>
-      </b-card-text>
+    <b-card :class="{'bg-secondary': layout_number === '2'}" @click="layout_number = '2'"
+            :img-src="layout2">
     </b-card>
 
-    <b-card :class="{'bg-secondary': layout_number === '3'}" @click="layout_number = '3'">
-      <b-card-text class="text-center m-0" style="position: relative;top: 1rem;">
-        <fa icon="image" size="4x" class="text-muted"/>
-      </b-card-text>
-      <b-card-text class="text-center text" style="position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);">text</b-card-text>
+    <b-card :class="{'bg-secondary': layout_number === '3'}" @click="layout_number = '3'"
+            :img-src="layout3">
     </b-card>
 
-    <b-card :class="{'bg-secondary': layout_number === '4'}" @click="layout_number = '4'">
-      <b-card-text class="text-center">
-        <b-row no-gutters style="position: relative;top: 0.9rem;">
-          <b-col class="ml-3" md="auto">
-            <fa icon="image" size="4x" class="text-muted"/>
-          </b-col>
-          <b-col class="text-left text ml-2" style="position: relative;top: 1.2rem;">
-            text
-          </b-col>
-        </b-row>
-      </b-card-text>
+    <b-card :class="{'bg-secondary': layout_number === '4'}" @click="layout_number = '4'"
+            :img-src="layout4">
     </b-card>
 
-    <b-card :class="{'bg-secondary': layout_number === '5'}" @click="layout_number = '5'">
-      <b-card-text class="text-center">
-        <b-row no-gutters style="position: relative;top: 0.9rem;">
-          <b-col class="text-right text mr-2" style="position: relative;top: 1.2rem;">
-            text
-          </b-col>
-          <b-col class="mr-3" md="auto">
-            <fa icon="image" size="4x" class="text-muted"/>
-          </b-col>
-        </b-row>
-      </b-card-text>
+    <b-card :class="{'bg-secondary': layout_number === '5'}" @click="layout_number = '5'"
+            :img-src="layout5">
     </b-card>
   </b-card-group>
 </template>
@@ -52,9 +25,21 @@
 <script lang="ts">
 import { Vue, Component, PropSync } from 'vue-property-decorator';
 
+import layout1 from '../media/layout1.png'
+import layout2 from '../media/layout2.png'
+import layout3 from '../media/layout3.png'
+import layout4 from '../media/layout4.png'
+import layout5 from '../media/layout5.png'
+
 @Component({})
 export default class AlertsEditFollowForm extends Vue {
   @PropSync('layout') readonly layout_number !: number
+
+  layout1 = layout1;
+  layout2 = layout2;
+  layout3 = layout3;
+  layout4 = layout4;
+  layout5 = layout5;
 }
 </script>
 
