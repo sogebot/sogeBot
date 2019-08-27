22bb7c48cb0369e53940e085de0533bd3a8fc1a9
Michal Orlik <sogehige@gmail.com>
2019-08-27 18:06:26 +0200

    add alert message

--- src/panel/views/registries/alerts/components/tts.vue
+++ src/panel/views/registries/alerts/components/tts.vue
@@ -4,12 +4,19 @@
         <b-button block href="#" v-b-toggle.tts-accordion variant="light" class="text-left">{{translate('registry.alerts.tts.setting')}}</b-button>
       </b-card-header>
       <b-collapse id="tts-accordion" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
+        <b-card-body v-if="state.loaded === $state.success">
           <b-form-group label-cols-sm="4" label-cols-lg="3"
               :label="translate('registry.alerts.voices.name')">
             <b-form-select v-model="data.voice" :options="voices" plain></b-form-select>
           </b-form-group>
-          <button type="button" @click="speak()" v-if="state.loaded === $state.success">Test</button>
+          <button type="button" @click="speak()">Test</button>
+        </b-card-body>
+        <b-card-body v-if="state.loaded === $state.fail">
+          <b-alert show variant="info">
+            ResponsiveVoices key is not properly set, go to
+            <a href="#/settings/integrations/responsivevoice">ResponsiveVoice integration settings</a>
+            and set your key.
+          </b-alert>
         </b-card-body>
       </b-collapse>
     </b-card>
@@ -42,14 +49,18 @@ export default class TTS extends Vue {
 
   mounted() {
     this.state.loaded = this.$state.progress;
-    this.$loadScript("https://code.responsivevoice.org/responsivevoice.js?key=" + this.configuration.integrations.responsiveVoice.api.key)
-      .then(() => {
-        window.responsiveVoice.init();
-       this.voices = window.responsiveVoice.getVoices().map(o => {
-          return { text: o.name, value: o.name }
-        });
-        this.state.loaded = this.$state.success;
-      })
+    if (this.configuration.integrations.responsiveVoice.api.key.trim().length === 0) {
+    this.state.loaded = this.$state.fail;
+    } else {
+      this.$loadScript("https://code.responsivevoice.org/responsivevoice.js?key=" + this.configuration.integrations.responsiveVoice.api.key)
+        .then(() => {
+          window.responsiveVoice.init();
+        this.voices = window.responsiveVoice.getVoices().map(o => {
+            return { text: o.name, value: o.name }
+          });
+          this.state.loaded = this.$state.success;
+        })
+    }
   }
 
   speak() {
