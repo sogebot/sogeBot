775e0107b393105cd97ea3d150404d284bba4c57
Michal Orlik <sogehige@gmail.com>
2019-09-09 16:04:03 +0200

    update tests

---
+++ test/helpers/user.js
@@ -0,0 +1,13 @@
+const viewer = {
+  userId: 1,
+  username: '__viewer__',
+  badges: {},
+  emotes: []
+}
+
+module.exports = {
+  prepare: async () => {
+    await global.db.engine.update('users', { id: 1 }, viewer)
+  },
+  viewer
+}
