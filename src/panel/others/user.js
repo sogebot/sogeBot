2c6ba98869bcda92a52180d70e7fce4bb22fd088
sogehige <sogehige@gmail.com>
2019-09-16 15:46:29 +0200

    fix(ui): wait until main is loaded before loggedUser use (#2570)

--- src/panel/others/user.js
+++ src/panel/others/user.js
@@ -1,25 +1,29 @@
-/* globals token commons _ translations configuration systems */
-import Vue from 'vue'
-import User from './user.vue'
+/* globals token commons systems */
+import Vue from 'vue';
+import User from './user.vue';
 
-function initUser () {
-  const isTranslationsLoaded = typeof translations === 'undefined' || _.size(translations) === 0
-  const isConfigurationLoaded = typeof configuration === 'undefined' || _.size(configuration) === 0
-  if (isTranslationsLoaded || isConfigurationLoaded) return setTimeout(() => initUser(), 10)
+import { isAvailableVariable, isMainLoaded } from '../helpers/isAvailableVariable';
+
+async function initUser () {
+  await Promise.all([
+    isAvailableVariable('translations'),
+    isAvailableVariable('configuration'),
+    isMainLoaded(),
+  ]);
 
   new Vue({ // eslint-disable-line no-new
     el: '#user',
     data: {
-      items: []
+      items: [],
     },
     render: function (createElement) {
       if (typeof systems !== 'undefined') {
-        return createElement(User, { props: { commons, token, systems } })
+        return createElement(User, { props: { commons, token, systems } });
       } else {
-        return false
+        return false;
       }
-    }
-  })
+    },
+  });
 }
 
-initUser()
+initUser();
