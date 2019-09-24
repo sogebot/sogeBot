e9b4182dfb3fd1583d6ee684e64bd2ee37b3c6fd
Michal Orlik <sogehige@gmail.com>
2019-09-24 10:52:06 +0200

    refactor(tools): skip tests on doc creation
    [skip-tests]

--- tools/release.js
+++ tools/release.js
@@ -1,29 +1,29 @@
 const chalk = require('chalk');
-const fs = require('fs')
+const fs = require('fs');
 const path = require('path');
-const glob = require('glob')
+const glob = require('glob');
 const { spawnSync } = require('child_process');
 
 
 const argv = require('yargs') // eslint-disable-line
   .usage('node tools/release.js <cmd> [args]')
   .command('build [branch]', 'make a zip file from branch', (yargs) => {
-    yargs.demandOption(['branch'], 'Please provide branch argument to work with this tool')
+    yargs.demandOption(['branch'], 'Please provide branch argument to work with this tool');
     yargs.positional('branch', {
       type: 'string',
-      describe: 'github origin branch'
-    })
+      describe: 'github origin branch',
+    });
   })
   .command('docs', 'move master docs to main docs', (yargs) => {
   })
   .demandCommand()
   .help()
-  .argv
+  .argv;
 
 if (argv._[0] === 'build') {
-  buildZipFile()
+  buildZipFile();
 } else if (argv._[0] === 'docs') {
-  buildDocs()
+  buildDocs();
 }
 
 function buildZipFile() {
@@ -39,12 +39,12 @@ function buildZipFile() {
   console.log(chalk.yellow('3.') + ' Running make');
   spawnSync('cd', [argv.branch]);
   spawnSync('make', {
-    cwd: 'sogeBot-' + argv.branch
+    cwd: 'sogeBot-' + argv.branch,
   });
 
   console.log(chalk.yellow('4.') + ' Creating release package');
   spawnSync('make', ['pack'], {
-    cwd: 'sogeBot-' + argv.branch
+    cwd: 'sogeBot-' + argv.branch,
   });
 
   console.log(chalk.yellow('5.') + ' Copy release package to /');
@@ -56,8 +56,8 @@ function buildZipFile() {
 }
 
 function buildDocs() {
-  const archiveDir = path.join(__dirname, '..', 'docs', '_archive', getLastMajorVersion())
-  const currentBranch = getCurrentBranch()
+  const archiveDir = path.join(__dirname, '..', 'docs', '_archive', getLastMajorVersion());
+  const currentBranch = getCurrentBranch();
 
   console.log(chalk.inverse('Create docs branch'));
   spawnSync('git', ['branch', '-D', 'docs-release']);
@@ -68,13 +68,13 @@ function buildDocs() {
   if (fs.existsSync(archiveDir)) {
     spawnSync('rm', ['-r', archiveDir]);
   }
-  fs.mkdirSync(archiveDir)
+  fs.mkdirSync(archiveDir);
 
   console.log(chalk.yellow('2.') + ' Backup of current docs');
   const archiveDocsFiles = glob.sync('docs/*', {
     ignore: [
-      'docs/_archive', 'docs/_master', 'docs/_navbar.md'
-    ]
+      'docs/_archive', 'docs/_master', 'docs/_navbar.md',
+    ],
   });
   for (const f of archiveDocsFiles) {
     spawnSync('cp', ['-r', f, archiveDir]);
@@ -84,7 +84,7 @@ function buildDocs() {
   console.log(chalk.yellow('3.') + ' Update ' + sidebar + ' paths');
   let sidebarFile = fs.readFileSync(sidebar).toString();
   sidebarFile = sidebarFile.replace(/(\*\s\[.*\]\(\/)(.*\))/g, '$1_archive/' + getLastMajorVersion() + '/$2');
-  fs.writeFileSync(sidebar, sidebarFile)
+  fs.writeFileSync(sidebar, sidebarFile);
 
   console.log(chalk.yellow('4.') + ' Replace current docs with _master');
   const masterDocsFiles = glob.sync('./docs/_master/*');
@@ -95,35 +95,35 @@ function buildDocs() {
   console.log(chalk.yellow('5.') + ' Update ' + path.join('docs', '_sidebar.md') + ' paths');
   sidebarFile = fs.readFileSync(path.join('docs', '_sidebar.md')).toString();
   sidebarFile = sidebarFile.replace(/_master/g, '');
-  fs.writeFileSync(path.join('docs', '_sidebar.md'), sidebarFile)
+  fs.writeFileSync(path.join('docs', '_sidebar.md'), sidebarFile);
 
   const navbar = path.join('docs', '_navbar.md');
   console.log(chalk.yellow('6.') + ' Add archive link to ' + navbar);
   let navbarFile = fs.readFileSync(navbar).toString().split('\n');
-  let newNavbarFile = []
-  for (let line of navbarFile) {
-    newNavbarFile.push(line)
+  let newNavbarFile = [];
+  for (const line of navbarFile) {
+    newNavbarFile.push(line);
     if (line.toLowerCase().includes('archive') && !line.toLowerCase().includes('_archive/')) {
-      newNavbarFile.push(`  * [${getLastMajorVersion()}](/_archive/${getLastMajorVersion()}/)`)
+      newNavbarFile.push(`  * [${getLastMajorVersion()}](/_archive/${getLastMajorVersion()}/)`);
     }
   }
-  fs.writeFileSync(navbar, newNavbarFile.join('\n'))
+  fs.writeFileSync(navbar, newNavbarFile.join('\n'));
 
   console.log(chalk.yellow('7.') + ' Update current version ' + navbar);
   navbarFile = fs.readFileSync(navbar).toString().split('\n');
-  newNavbarFile = []
-  for (let line of navbarFile) {
+  newNavbarFile = [];
+  for (const line of navbarFile) {
     if (line.toLowerCase().includes('current')) {
-      newNavbarFile.push(`* **Current:** [${getCurrentMajorVersion()}](/)`)
+      newNavbarFile.push(`* **Current:** [${getCurrentMajorVersion()}](/)`);
     } else {
-      newNavbarFile.push(line)
+      newNavbarFile.push(line);
     }
   }
-  fs.writeFileSync(navbar, newNavbarFile.join('\n'))
+  fs.writeFileSync(navbar, newNavbarFile.join('\n'));
 
   console.log(chalk.yellow('8.') + ' Create doc commit');
   spawnSync('git', ['add', '-A']);
-  spawnSync('git', ['commit', '-m', 'docs: release docs ' + getReleaseVersion() + '']);
+  spawnSync('git', ['commit', '-m', 'docs: release docs ' + getReleaseVersion(), '-m', '[skip-tests]']);
 
   console.log('\n' + chalk.inverse('PUSHING COMMITS'));
   spawnSync('git', ['push', '-fu', 'origin', 'docs-release']);
@@ -158,8 +158,8 @@ function getCurrentBranch() {
 }
 
 function getReleaseVersion() {
-  const regex = /"version": "(.*-SNAPSHOT)",/g
-  let packageFile = fs.readFileSync('package.json').toString();
+  const regex = /"version": "(.*-SNAPSHOT)",/g;
+  const packageFile = fs.readFileSync('package.json').toString();
   version = regex.exec(packageFile)[1];
-  return version.replace('-SNAPSHOT', '')
+  return version.replace('-SNAPSHOT', '');
 }
