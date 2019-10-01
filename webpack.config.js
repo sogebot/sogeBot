9d5c04b7d13e729fcc2c6fb2f038f65a862ae0ca
Michal Orlik <sogehige@gmail.com>
2019-10-01 17:34:12 +0200

    revert

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,10 @@ const optimization = {
   minimize: process.env.NODE_ENV === 'production',
   moduleIds: 'hashed',
   chunkIds: 'named',
-  usedExports: true,
-  runtimeChunk: true,
   splitChunks: {
-    chunks: 'all',
+    chunks: 'async',
     minSize: 30000,
-    maxSize: 200000,
+    maxSize: 0,
     minChunks: 1,
     maxAsyncRequests: 5,
     maxInitialRequests: 3,
@@ -38,11 +36,7 @@ module.exports = [{
   mode: process.env.NODE_ENV,
   performance: { hints: false },
   optimization,
-  entry: {
-    indeX: './src/panel/index.ts',
-    overlay: './src/overlay/index.ts',
-    login: './src/login/index.ts'
-  },
+  entry: './src/panel/index.ts',
   resolve: {
     extensions: ['.ts', '.js', '.vue', '.json'],
     alias: {
@@ -52,8 +46,8 @@ module.exports = [{
   },
   output: {
     path: path.resolve(__dirname, 'public', 'dist', 'js'),
-    filename: '[name].js',
-    chunkFilename: '[name].[hash].js',
+    filename: 'main.js',
+    chunkFilename: '[chunkhash].js',
     publicPath: '/dist/js/'
   },
   plugins: [
@@ -113,6 +107,184 @@ module.exports = [{
                   modules: true,
                   cacheDirectory: true,
                   localIdentName: '[local]_[hash:base64:8]'
+                }
+              }
+            ]
+          },
+          // this applies to <style> or <style scoped>
+          {
+            use: [
+              'vue-style-loader',
+              'css-loader'
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}, {
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  mode: process.env.NODE_ENV,
+  performance: { hints: false },
+  optimization,
+  entry: ['./src/overlay/index.ts'],
+  resolve: {
+    extensions: ['.ts', '.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      src: path.resolve(__dirname, 'src'),
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, 'public', 'dist', 'js'),
+    filename: 'overlay.js',
+    chunkFilename: '[chunkhash].js',
+    publicPath: '/dist/js/'
+  },
+  plugins: [
+    new VueLoaderPlugin()
+    // new CleanWebpackPlugin(['public/dist/js'])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+      {
+        test: /\.txt$/i,
+        use: 'raw-loader',
+      },
+      {
+        test: /\.ts$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [{
+          loader: 'ts-loader',
+          options: {
+            appendTsSuffixTo: [/\.vue$/],
+            experimentalFileCaching: true
+          }
+        }]
+      },
+      {
+        test: /\.vue$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['vue-loader'],
+      },
+      {
+        test: /\.css$/,
+        oneOf: [
+          // this applies to <style module>
+          {
+            resourceQuery: /module/,
+            use: [
+              'vue-style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true,
+                  localIdentName: '[local]_[hash:base64:8]',
+                  cacheDirectory: true
+                }
+              }
+            ]
+          },
+          // this applies to <style> or <style scoped>
+          {
+            use: [
+              'vue-style-loader',
+              'css-loader'
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}, {
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  mode: process.env.NODE_ENV,
+  performance: { hints: false },
+  optimization,
+  entry: ['./src/login/index.ts'],
+  resolve: {
+    extensions: ['.ts', '.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      src: path.resolve(__dirname, 'src'),
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, 'public', 'dist', 'js'),
+    filename: 'login.js',
+    chunkFilename: '[chunkhash].js',
+    publicPath: '/dist/js/'
+  },
+  plugins: [
+    new VueLoaderPlugin()
+    // new CleanWebpackPlugin(['public/dist/js'])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+      {
+        test: /\.txt$/i,
+        use: 'raw-loader',
+      },
+      {
+        test: /\.ts$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [{
+          loader: 'ts-loader',
+          options: {
+            appendTsSuffixTo: [/\.vue$/],
+            experimentalFileCaching: true
+          }
+        }]
+      },
+      {
+        test: /\.vue$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['vue-loader'],
+      },
+      {
+        test: /\.css$/,
+        oneOf: [
+          // this applies to <style module>
+          {
+            resourceQuery: /module/,
+            use: [
+              'vue-style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true,
+                  localIdentName: '[local]_[hash:base64:8]',
+                  cacheDirectory: true
                 }
               }
             ]
