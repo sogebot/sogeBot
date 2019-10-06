3ff98b3c585a5cc4e181683c0dbbd14102b89cce
soge__ <sogehige@gmail.com>
2019-10-06 14:42:02 +0200

    tests(rerun): use process.stdout to remove newline
    Fixes #2739

--- tools/rerunTests.js
+++ tools/rerunTests.js
@@ -28,11 +28,11 @@ async function retest() {
         });
 
         p.stdout.on('data', (data) => {
-          console.log(data.toString());
+          process.stdout.write(data.toString());
         });
 
         p.stderr.on('data', (data) => {
-          console.error(data.toString());
+          process.stderr.write(data.toString());
         });
 
         p.on('close', (code) => {
