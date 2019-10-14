77b1986d3e939df6c608ec9609402e07a3979963
Michal Orlik <sogehige@gmail.com>
2019-10-14 13:22:28 +0200

    update

--- src/panel/helpers/capitalize.ts
+++ src/panel/helpers/capitalize.ts
@@ -1,5 +1,7 @@
 export function capitalize(value) {
-  if (!value) return ''
-  value = value.toString()
-  return value.charAt(0).toUpperCase() + value.slice(1)
-}
+  if (!value) {
+    return '';
+  }
+  value = value.toString();
+  return value.charAt(0).toUpperCase() + value.slice(1);
+};
