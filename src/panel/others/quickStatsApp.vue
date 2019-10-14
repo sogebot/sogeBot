6b744507b6a4115b5b2ebce9e7a31b82861716a7
Michal Orlik <sogehige@gmail.com>
2019-10-14 13:17:41 +0200

    fix(stats): game column should have correct width on mobile
    Fixes #2783

--- src/panel/others/quickStatsApp.vue
+++ src/panel/others/quickStatsApp.vue
@@ -120,7 +120,7 @@
       </div>
 
       <div class="row">
-        <div class="col-6 col-sm-4 col-md-4 col-lg-4 stream-info" @click="showGameAndTitleDlg">
+        <div class="col-12 col-sm-12 col-md-4 col-lg-4 stream-info" @click="showGameAndTitleDlg">
           <h2>
             <span>{{ translate('game') }}</span>
             <small>{{ translate('click-to-change') }}</small>
