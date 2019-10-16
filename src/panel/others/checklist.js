b55e43066fbc396c7ac21405e8708f34a7fa247f
soge__ <sogehige@gmail.com>
2019-10-16 19:36:36 +0200

    update checklist

--- src/panel/others/checklist.js
+++ src/panel/others/checklist.js
@@ -1,27 +1,23 @@
 /* globals commons _ translations configuration systems */
 import Vue from 'vue'
-import Checklist from './checklist.vue'
+import Component from './checklist.vue'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 Vue.component('fa', FontAwesomeIcon);
 
-function initChecklist () {
-  const isTranslationsLoaded = typeof translations === 'undefined' || Object.keys(translations).length === 0
-  const isConfigurationLoaded = typeof configuration === 'undefined' || Object.keys(configuration).length === 0
-  if (isTranslationsLoaded || isConfigurationLoaded) return setTimeout(() => initChecklist(), 10)
-  new Vue({ // eslint-disable-line no-new
-    el: '#checklist',
-    data: {
-      items: []
-    },
-    render: function (createElement) {
-      if (typeof systems !== 'undefined') {
-        return createElement(Checklist, { props: { systems } })
-      } else {
-        return false
-      }
-    }
-  })
-}
+import { isMainLoaded } from '../helpers/isAvailableVariable';
 
-initChecklist()
+export const init = async () => {
+  await Promise.all([
+    isMainLoaded(),
+  ]);
+
+  new Vue({
+    el: '#checklist',
+    render: function (createElement) {
+      return createElement(Component);
+    },
+  });
+};
+
+init();
