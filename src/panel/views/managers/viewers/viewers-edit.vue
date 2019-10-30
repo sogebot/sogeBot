215938a975f45a86b9308845b488802373be57f6
Michal Orlik <sogehige@gmail.com>
2019-10-30 12:47:39 +0100

    fix(ui): center misaligned inputs
    Fixes #2893

--- src/panel/views/managers/viewers/viewers-edit.vue
+++ src/panel/views/managers/viewers/viewers-edit.vue
@@ -182,46 +182,48 @@
               <table class="table table-sm ml-2 mr-2" v-if="viewer.stats.tips.length !== 0">
                 <tr v-for="tips of orderBy(viewer.stats.tips, 'timestamp', 'desc')" :key="tips.timestamp">
                   <td scope="row">{{ moment(tips.timestamp).format('LLL') }}</td>
-                  <td class="d-flex">
-                    <template v-if="tips.editation">
-                      <input type="number" min="0" v-model="tips.amount" class="form-control">
-                      <select class="form-control" v-model="tips.currency">
-                        <option value="USD">USD</option>
-                        <option value="AUD">AUD</option>
-                        <option value="BGN">BGN</option>
-                        <option value="BRL">BRL</option>
-                        <option value="CAD">CAD</option>
-                        <option value="CHF">CHF</option>
-                        <option value="CNY">CNY</option>
-                        <option value="CZK">CZK</option>
-                        <option value="DKK">DKK</option>
-                        <option value="EUR">EUR</option>
-                        <option value="GBP">GBP</option>
-                        <option value="HKD">HKD</option>
-                        <option value="HRK">HRK</option>
-                        <option value="HUF">HUF</option>
-                        <option value="IDR">IDR</option>
-                        <option value="ILS">ILS</option>
-                        <option value="INR">INR</option>
-                        <option value="ISK">ISK</option>
-                        <option value="JPY">JPY</option>
-                        <option value="KRW">KRW</option>
-                        <option value="MXN">MXN</option>
-                        <option value="MYR">MYR</option>
-                        <option value="NOK">NOK</option>
-                        <option value="NZD">NZD</option>
-                        <option value="PHP">PHP</option>
-                        <option value="PLN">PLN</option>
-                        <option value="RON">RON</option>
-                        <option value="RUB">RUB</option>
-                        <option value="SEK">SEK</option>
-                        <option value="SGD">SGD</option>
-                        <option value="THB">THB</option>
-                        <option value="TRY">TRY</option>
-                        <option value="ZAR">ZAR</option>
-                      </select>
-                    </template>
-                    <strong v-else>{{ Number(tips.amount).toFixed(2) }}{{ tips.currency }}</strong>
+                  <td>
+                    <div class="d-flex">
+                      <template v-if="tips.editation">
+                        <input type="number" min="0" v-model="tips.amount" class="form-control">
+                        <select class="form-control" v-model="tips.currency">
+                          <option value="USD">USD</option>
+                          <option value="AUD">AUD</option>
+                          <option value="BGN">BGN</option>
+                          <option value="BRL">BRL</option>
+                          <option value="CAD">CAD</option>
+                          <option value="CHF">CHF</option>
+                          <option value="CNY">CNY</option>
+                          <option value="CZK">CZK</option>
+                          <option value="DKK">DKK</option>
+                          <option value="EUR">EUR</option>
+                          <option value="GBP">GBP</option>
+                          <option value="HKD">HKD</option>
+                          <option value="HRK">HRK</option>
+                          <option value="HUF">HUF</option>
+                          <option value="IDR">IDR</option>
+                          <option value="ILS">ILS</option>
+                          <option value="INR">INR</option>
+                          <option value="ISK">ISK</option>
+                          <option value="JPY">JPY</option>
+                          <option value="KRW">KRW</option>
+                          <option value="MXN">MXN</option>
+                          <option value="MYR">MYR</option>
+                          <option value="NOK">NOK</option>
+                          <option value="NZD">NZD</option>
+                          <option value="PHP">PHP</option>
+                          <option value="PLN">PLN</option>
+                          <option value="RON">RON</option>
+                          <option value="RUB">RUB</option>
+                          <option value="SEK">SEK</option>
+                          <option value="SGD">SGD</option>
+                          <option value="THB">THB</option>
+                          <option value="TRY">TRY</option>
+                          <option value="ZAR">ZAR</option>
+                        </select>
+                      </template>
+                      <strong v-else>{{ Number(tips.amount).toFixed(2) }}{{ tips.currency }}</strong>
+                    </div>
                   <td>
                     <template v-if="tips.editation">
                       <textarea v-model="tips.message" class="form-control" style="height:38px"></textarea>
