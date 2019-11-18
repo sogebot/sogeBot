fa5476c64d9757731b12741de64d5ad8e0a4af35
sogehige <sogehige@gmail.com>
2019-11-18 12:05:49 +0100

    build(docker): remove db settings, add dev deps (#2989)

--- docker.sh
+++ docker.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 cd /app
-
-export DOCKER_HOST_IP=`route -n | awk '/UG[ \t]/{print $2}'`
-
-if [ "$DB" = 'mongodb' ]; then
-  sed -i "s#nedb#mongodb#g" config.json
-  sed -i 's#mongodb:\/\/localhost:27017\/your-db-name#'$MONGOURI'#g' config.json
-  sed -i 's#localhost:#'$DOCKER_HOST_IP':#g' config.json # set docker host ip instead of localhost
-  npm start
-else
-  echo "Please pass value for the MONGOURI environment variable"
-fi
+npm start
