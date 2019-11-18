8c8d1255286f74ea00cf572db753c249df727f45
sogehige <sogehige@gmail.com>
2019-11-18 12:36:24 +0100

    build(docker): cp ormconfig from conf dir (#2990)
    [skip-tests]

--- docker.sh
+++ docker.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 cd /app
-npm start
+
+$FILE= ./conf/ormconfig.json
+if test -f "$FILE"; then
+  cp $FILE ./ormconfig.json
+  npm start
+else
+  echo "Customized ormconfig.json not found!!!" >& 2
+  exit 1
+fi
+
