3ffd7ef9af0080b2120e19fa2a68be250b8631ce
Michal Orlik <sogehige@gmail.com>
2019-11-19 12:16:44 +0100

    feat(points): add cron interval for points reset

---
+++ src/panel/views/settings/components/interface/cron.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="input-group">
+    <div class="d-flex w-100" style="height: fit-content">
+      <div class="input-group-prepend">
+        <span class="input-group-text">
+          <template v-if="typeof translatedTitle === 'string'">
+            <span v-html="translatedTitle" class="text-left"/>
+          </template>
+          <template v-else-if="typeof translatedTitle === 'object'">
+            {{ translatedTitle.title }}
+            <small style="cursor: help;" class="text-info ml-1" data-toggle="tooltip" data-html="true" :title="translatedTitle.help">[?]</small>
+          </template>
+        </span>
+      </div>
+      <div class="d-block w-100 p-0 border-0" style="height: fit-content">
+        <input v-model="currentValue" v-on:keyup="update" class="form-control" />
+        <b-list-group>
+          <b-list-group-item v-for="timestamp of data" :key="timestamp">{{ new Date(timestamp).toUTCString() }}</b-list-group-item>
+        </b-list-group>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { getSocket } from 'src/panel/helpers/socket';
+
+@Component({})
+export default class cronInput extends Vue {
+  @Prop() readonly emit: any;
+  @Prop() readonly title: any;
+  @Prop() readonly value: any;
+
+  data: number[] = [];
+  currentValue = this.value;
+  translatedTitle = this.translate(this.title);
+
+  mounted() {
+    this.update();
+  }
+
+  update() {
+    getSocket(`/${this.$route.params.type}/${this.$route.params.id}`)
+      .emit(this.emit, this.currentValue, (err, data: number[]) => {
+        if (err) {
+          console.error(err)
+        } else {
+          this.data = data;
+        }
+      });
+    this.$emit('update', { value: this.currentValue });
+  }
+};
+</script>
