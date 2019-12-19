f45a4e922dfbb226339030e25e1693c25a288f84
soge__ <sogehige@gmail.com>
2019-12-19 15:34:55 +0100

    change column name

--- src/bot/database/migration/mysql/1576758423496-commandResponsesResponseToText.ts
+++ src/bot/database/migration/mysql/1576758423496-commandResponsesResponseToText.ts
@@ -4,11 +4,11 @@ export class commandResponsesResponseToText1576758423496 implements MigrationInt
   name = 'commandResponsesResponseToText1576758423496';
 
   public async up(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query('ALTER TABLE `commands_responses` MODIFY COLUMN `responses` text', undefined);
+    await queryRunner.query('ALTER TABLE `commands_responses` MODIFY COLUMN `response` text', undefined);
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query('ALTER TABLE `commands_responses` MODIFY COLUMN `responses` varchar(255)', undefined);
+    await queryRunner.query('ALTER TABLE `commands_responses` MODIFY COLUMN `response` varchar(255)', undefined);
   }
 
 }
