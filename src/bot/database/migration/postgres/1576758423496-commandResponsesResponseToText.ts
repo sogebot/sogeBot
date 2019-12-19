9e729725c89c4c1fdaa1cd344304c710cc39dc7c
soge__ <sogehige@gmail.com>
2019-12-19 13:59:27 +0100

    update

--- src/bot/database/migration/postgres/1576758423496-commandResponsesResponseToText.ts
+++ src/bot/database/migration/postgres/1576758423496-commandResponsesResponseToText.ts
@@ -1,6 +1,7 @@
 import {MigrationInterface, QueryRunner} from 'typeorm';
 
 export class commandResponsesResponseToText1576758423496 implements MigrationInterface {
+  name = 'commandResponsesResponseToText1576758423496';
 
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query('ALTER TABLE "commands_responses" ALTER COLUMN "response" SET DATA TYPE text', undefined);
