3c687122a78518df0dbbac639fa93a722edccdf4
sogehige <sogehige@gmail.com>
2020-01-13 15:28:20 +0100

    refactor(typeorm): dashboard fix inverse side (#3169)
    [skip-tests]

--- src/bot/database/entity/dashboard.ts
+++ src/bot/database/entity/dashboard.ts
@@ -29,7 +29,7 @@ export const Dashboard = new EntitySchema<Readonly<Required<DashboardInterface>>
     widgets: {
       type: 'one-to-many',
       target: 'widget',
-      inverseSide: 'poll',
+      inverseSide: 'dashboard',
       cascade: true,
     },
   },
