e1a1720c6aa0f75fb50244c9ae18226e2608400f
soge__ <sogehige@gmail.com>
2020-01-27 21:18:06 +0100

    fix

--- src/bot/database/migration/mysql/1580154710641-resetPoints.ts
+++ src/bot/database/migration/mysql/1580154710641-resetPoints.ts
@@ -3,7 +3,7 @@ import {MigrationInterface, QueryRunner} from 'typeorm';
 export class resetPoints1580154710641 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query('UPDATE TABLE `users` SET `chatTimeOnline`=0, `chatTimeOffline`=0, `pointsOnlineGivenAt`=0, `pointsOfflineGivenAt`=0, `pointsByMessageGivenAt`=0', undefined);
+    await queryRunner.query('UPDATE `user` SET `chatTimeOnline`=0, `chatTimeOffline`=0, `pointsOnlineGivenAt`=0, `pointsOfflineGivenAt`=0, `pointsByMessageGivenAt`=0', undefined);
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
