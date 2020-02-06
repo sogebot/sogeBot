f0cf8feca395c0e3f2d59418ccc0a9afcd4e7806
sogehige <sogehige@gmail.com>
2020-02-06 14:40:54 +0100

    test(parser): add simple parser time check (#3233)
    * test(parser): add simple parser time check
    
    * remove .only
    
    * updaet
    
    * update time

---
+++ test/tests/parser/time.js
@@ -0,0 +1,46 @@
+/* global describe it before */
+
+
+require('../../general.js');
+
+const db = require('../../general.js').db;
+const message = require('../../general.js').message;
+const assert = require('assert');
+
+const Parser = require('../../../dest/parser').default;
+
+const owner = { username: 'soge__', userId: Math.floor(Math.random() * 100000) };
+
+const { getRepository } = require('typeorm');
+const { User } = require('../../../dest/database/entity/user');
+
+describe('Parser - parse time check', async () => {
+  const tests = [
+    {
+      test: '!me',
+      expected: 200,
+    },
+  ];
+
+  for (const test of tests) {
+    describe(`'${test.test}' expect ${test.expected}ms`, async () => {
+      before(async () => {
+        await db.cleanup();
+        await message.prepare();
+
+        await getRepository(User).save({ username: owner.username, userId: owner.userId });
+      });
+
+      let time;
+      it(`Run command '${test.test}'`, async () => {
+        time = Date.now();
+        const parse = new Parser({ sender: owner, message: test.test, skip: false, quiet: false });
+        await parse.process();
+      });
+
+      it(`Should take to ${test.expected}ms to parse`, async () => {
+        assert(Date.now() - time < test.expected, `${Date.now() - time} > ${test.expected}`);
+      });
+    });
+  }
+});
