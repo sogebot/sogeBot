7f7449edb828246060ad81120a2ea968d9290646
sogehige <sogehige@gmail.com>
2020-02-24 15:37:56 +0100

    feat(ranks): add ranks based on followage/subage (#3289)
    Fixes https://ideas.sogebot.xyz/posts/3/rank-by-sub-lenght

---
+++ src/bot/database/migration/mysql/1582546819777-RanksTypes.ts
@@ -0,0 +1,20 @@
+import {MigrationInterface, QueryRunner} from 'typeorm';
+
+export class RanksTypes1582546819777 implements MigrationInterface {
+  name = 'RanksTypes1582546819777';
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query('DROP INDEX `IDX_93c78c94804a13befdace81904` ON `rank`', undefined);
+    await queryRunner.query('ALTER TABLE `rank` CHANGE `hours` `value` int NOT NULL', undefined);
+    await queryRunner.query('ALTER TABLE `rank` ADD `type` varchar(255) NOT NULL', undefined);
+    await queryRunner.query('CREATE UNIQUE INDEX `IDX_93c78c94804a13befdace81904` ON `rank` (`type`, `value`)', undefined);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query('DROP INDEX `IDX_93c78c94804a13befdace81904` ON `rank`', undefined);
+    await queryRunner.query('ALTER TABLE `rank` DROP COLUMN `type`', undefined);
+    await queryRunner.query('ALTER TABLE `rank` CHANGE `value` `hours` int NOT NULL', undefined);
+    await queryRunner.query('CREATE UNIQUE INDEX `IDX_93c78c94804a13befdace81904` ON `rank` (`hours`)', undefined);
+  }
+
+}
