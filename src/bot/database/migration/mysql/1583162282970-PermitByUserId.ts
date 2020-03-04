45deadbd4c6fa83d8ea0a0e49958dd1b29670d03
Michal Orlik <sogehige@gmail.com>
2020-03-04 15:29:38 +0100

    fixes to migr

--- src/bot/database/migration/mysql/1583162282970-PermitByUserId.ts
+++ src/bot/database/migration/mysql/1583162282970-PermitByUserId.ts
@@ -8,8 +8,8 @@ export class PermitByUserId1583162282970 implements MigrationInterface {
     await queryRunner.query('DROP INDEX `IDX_69499e78c9ee1602baee77b97d` ON `moderation_permit`', undefined);
     await queryRunner.query('DROP TABLE `moderation_warning`', undefined);
     await queryRunner.query('DROP TABLE `moderation_permit`', undefined);
-    await queryRunner.query('CREATE TABLE `moderation_warning` (`id` int NOT NULL AUTO_INCREMENT, `username` int NOT NULL, `timestamp` bigint NOT NULL DEFAULT 0, PRIMARY KEY (`id`)) ENGINE=InnoDB', undefined);
-    await queryRunner.query('CREATE TABLE `moderation_permit` (`id` int NOT NULL AUTO_INCREMENT, `username` int NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB', undefined);
+    await queryRunner.query('CREATE TABLE `moderation_warning` (`id` int NOT NULL AUTO_INCREMENT, `userId` int NOT NULL, `timestamp` bigint NOT NULL DEFAULT 0, PRIMARY KEY (`id`)) ENGINE=InnoDB', undefined);
+    await queryRunner.query('CREATE TABLE `moderation_permit` (`id` int NOT NULL AUTO_INCREMENT, `userId` int NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB', undefined);
     await queryRunner.query('CREATE INDEX `IDX_f941603aef2741795a9108d0d2` ON `moderation_warning` (`userId`)', undefined);
     await queryRunner.query('CREATE INDEX `IDX_69499e78c9ee1602baee77b97d` ON `moderation_permit` (`userId`)', undefined);
   }
