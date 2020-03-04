f3231404c093a2236167673a7738b2ecd87a4577
Michal Orlik <sogehige@gmail.com>
2020-03-04 15:04:57 +0100

    refactor(migration): drop/create moderation_warning/permit tables

--- src/bot/database/migration/mysql/1583162282970-PermitByUserId.ts
+++ src/bot/database/migration/mysql/1583162282970-PermitByUserId.ts
@@ -6,12 +6,10 @@ export class PermitByUserId1583162282970 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query('DROP INDEX `IDX_f941603aef2741795a9108d0d2` ON `moderation_warning`', undefined);
     await queryRunner.query('DROP INDEX `IDX_69499e78c9ee1602baee77b97d` ON `moderation_permit`', undefined);
-    await queryRunner.query('ALTER TABLE `moderation_warning` CHANGE `username` `userId` varchar(255) NOT NULL', undefined);
-    await queryRunner.query('ALTER TABLE `moderation_permit` CHANGE `username` `userId` varchar(255) NOT NULL', undefined);
-    await queryRunner.query('ALTER TABLE `moderation_warning` DROP COLUMN `userId`', undefined);
-    await queryRunner.query('ALTER TABLE `moderation_warning` ADD `userId` int NOT NULL', undefined);
-    await queryRunner.query('ALTER TABLE `moderation_permit` DROP COLUMN `userId`', undefined);
-    await queryRunner.query('ALTER TABLE `moderation_permit` ADD `userId` int NOT NULL', undefined);
+    await queryRunner.query('DROP TABLE `moderation_warning`', undefined);
+    await queryRunner.query('DROP TABLE `moderation_permit`', undefined);
+    await queryRunner.query('CREATE TABLE `moderation_warning` (`id` int NOT NULL AUTO_INCREMENT, `username` int NOT NULL, `timestamp` bigint NOT NULL DEFAULT 0, PRIMARY KEY (`id`)) ENGINE=InnoDB', undefined);
+    await queryRunner.query('CREATE TABLE `moderation_permit` (`id` int NOT NULL AUTO_INCREMENT, `username` int NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB', undefined);
     await queryRunner.query('CREATE INDEX `IDX_f941603aef2741795a9108d0d2` ON `moderation_warning` (`userId`)', undefined);
     await queryRunner.query('CREATE INDEX `IDX_69499e78c9ee1602baee77b97d` ON `moderation_permit` (`userId`)', undefined);
   }
@@ -19,12 +17,10 @@ export class PermitByUserId1583162282970 implements MigrationInterface {
   public async down(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query('DROP INDEX `IDX_69499e78c9ee1602baee77b97d` ON `moderation_permit`', undefined);
     await queryRunner.query('DROP INDEX `IDX_f941603aef2741795a9108d0d2` ON `moderation_warning`', undefined);
-    await queryRunner.query('ALTER TABLE `moderation_permit` DROP COLUMN `userId`', undefined);
-    await queryRunner.query('ALTER TABLE `moderation_permit` ADD `userId` varchar(255) NOT NULL', undefined);
-    await queryRunner.query('ALTER TABLE `moderation_warning` DROP COLUMN `userId`', undefined);
-    await queryRunner.query('ALTER TABLE `moderation_warning` ADD `userId` varchar(255) NOT NULL', undefined);
-    await queryRunner.query('ALTER TABLE `moderation_permit` CHANGE `userId` `username` varchar(255) NOT NULL', undefined);
-    await queryRunner.query('ALTER TABLE `moderation_warning` CHANGE `userId` `username` varchar(255) NOT NULL', undefined);
+    await queryRunner.query('DROP TABLE `moderation_warning`', undefined);
+    await queryRunner.query('DROP TABLE `moderation_permit`', undefined);
+    await queryRunner.query('CREATE TABLE `moderation_warning` (`id` int NOT NULL AUTO_INCREMENT, `username` varchar(255) NOT NULL, `timestamp` bigint NOT NULL DEFAULT 0, PRIMARY KEY (`id`)) ENGINE=InnoDB', undefined);
+    await queryRunner.query('CREATE TABLE `moderation_permit` (`id` int NOT NULL AUTO_INCREMENT, `username` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB', undefined);
     await queryRunner.query('CREATE INDEX `IDX_69499e78c9ee1602baee77b97d` ON `moderation_permit` (`username`)', undefined);
     await queryRunner.query('CREATE INDEX `IDX_f941603aef2741795a9108d0d2` ON `moderation_warning` (`username`)', undefined);
   }
--- src/bot/database/migration/postgres/1583162215893-PermitByUserId.ts
+++ src/bot/database/migration/postgres/1583162215893-PermitByUserId.ts
@@ -6,12 +6,10 @@ export class PermitByUserId1583162215893 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(`DROP INDEX "IDX_f941603aef2741795a9108d0d2"`, undefined);
     await queryRunner.query(`DROP INDEX "IDX_69499e78c9ee1602baee77b97d"`, undefined);
-    await queryRunner.query(`ALTER TABLE "moderation_warning" RENAME COLUMN "username" TO "userId"`, undefined);
-    await queryRunner.query(`ALTER TABLE "moderation_permit" RENAME COLUMN "username" TO "userId"`, undefined);
-    await queryRunner.query(`ALTER TABLE "moderation_warning" DROP COLUMN "userId"`, undefined);
-    await queryRunner.query(`ALTER TABLE "moderation_warning" ADD "userId" integer NOT NULL`, undefined);
-    await queryRunner.query(`ALTER TABLE "moderation_permit" DROP COLUMN "userId"`, undefined);
-    await queryRunner.query(`ALTER TABLE "moderation_permit" ADD "userId" integer NOT NULL`, undefined);
+    await queryRunner.query(`DROP TABLE "moderation_warning"`, undefined);
+    await queryRunner.query(`DROP TABLE "moderation_permit"`, undefined);
+    await queryRunner.query(`CREATE TABLE "moderation_warning" ("id" SERIAL NOT NULL, "userId" integer, "timestamp" bigint NOT NULL DEFAULT 0, CONSTRAINT "PK_0e90c9d7ff04a18218299cfc0e9" PRIMARY KEY ("id"))`, undefined);
+    await queryRunner.query(`CREATE TABLE "moderation_permit" ("id" SERIAL NOT NULL, "userId" integer, CONSTRAINT "PK_ba3b81de5de7feff025898b4a63" PRIMARY KEY ("id"))`, undefined);
     await queryRunner.query(`CREATE INDEX "IDX_f941603aef2741795a9108d0d2" ON "moderation_warning" ("userId") `, undefined);
     await queryRunner.query(`CREATE INDEX "IDX_69499e78c9ee1602baee77b97d" ON "moderation_permit" ("userId") `, undefined);
   }
@@ -19,12 +17,10 @@ export class PermitByUserId1583162215893 implements MigrationInterface {
   public async down(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(`DROP INDEX "IDX_69499e78c9ee1602baee77b97d"`, undefined);
     await queryRunner.query(`DROP INDEX "IDX_f941603aef2741795a9108d0d2"`, undefined);
-    await queryRunner.query(`ALTER TABLE "moderation_permit" DROP COLUMN "userId"`, undefined);
-    await queryRunner.query(`ALTER TABLE "moderation_permit" ADD "userId" character varying NOT NULL`, undefined);
-    await queryRunner.query(`ALTER TABLE "moderation_warning" DROP COLUMN "userId"`, undefined);
-    await queryRunner.query(`ALTER TABLE "moderation_warning" ADD "userId" character varying NOT NULL`, undefined);
-    await queryRunner.query(`ALTER TABLE "moderation_permit" RENAME COLUMN "userId" TO "username"`, undefined);
-    await queryRunner.query(`ALTER TABLE "moderation_warning" RENAME COLUMN "userId" TO "username"`, undefined);
+    await queryRunner.query(`DROP TABLE "moderation_warning"`, undefined);
+    await queryRunner.query(`DROP TABLE "moderation_permit"`, undefined);
+    await queryRunner.query(`CREATE TABLE "moderation_warning" ("id" SERIAL NOT NULL, "username" character varying NOT NULL, "timestamp" bigint NOT NULL DEFAULT 0, CONSTRAINT "PK_0e90c9d7ff04a18218299cfc0e9" PRIMARY KEY ("id"))`, undefined);
+    await queryRunner.query(`CREATE TABLE "moderation_permit" ("id" SERIAL NOT NULL, "username" character varying NOT NULL, CONSTRAINT "PK_ba3b81de5de7feff025898b4a63" PRIMARY KEY ("id"))`, undefined);
     await queryRunner.query(`CREATE INDEX "IDX_69499e78c9ee1602baee77b97d" ON "moderation_permit" ("username") `, undefined);
     await queryRunner.query(`CREATE INDEX "IDX_f941603aef2741795a9108d0d2" ON "moderation_warning" ("username") `, undefined);
   }
