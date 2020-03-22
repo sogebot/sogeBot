7aae16316582e2104e90046d1328f53aff789712
soge__ <sogehige@gmail.com>
2020-03-22 15:25:10 +0100

    fix(quotes): word-wrap long quotes in table
    Fixes https://discordapp.com/channels/317348946144002050/619437014001123338/690941251125772348

--- src/public/views/quotes.vue
+++ src/public/views/quotes.vue
@@ -11,7 +11,8 @@
     loading(v-if="state.loading !== $state.success")
     b-table(v-else striped small :items="items" :fields="fields").table-p-0
       template(v-slot:cell(createdAt)="data") {{ data.item.createdAt | moment('LL')}} {{ data.item.createdAt | moment('LTS') }}
-
+      template(v-slot:cell(quote)="data")
+        span(style="word-break: break-word;") {{ data.item.quote }}
       template(v-slot:cell(tags)="data")
         span(v-for="tag of data.item.tags" v-bind:key="tag" variant="dark").p-2.m-1.text-light.bg-dark {{ tag }}
 </template>
