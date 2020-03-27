a88e04ce81f14cba299cdc2e3075d88704ae48b8
sogehige <sogehige@gmail.com>
2020-03-27 16:23:31 +0100

    docs: fix stable links (#3487)
    [skip-tests]

--- tools/release-docs.sh
+++ tools/release-docs.sh
@@ -9,6 +9,7 @@ cp -r docs/_master/* new-docs/
 cp docs/.nojekyll new-docs/
 cp docs/_navbar.md new-docs/
 cp docs/index.html new-docs/
+sed -i 's/_master//g' new-docs/_sidebar.md
 rm -rf docs
 mv new-docs docs
 git commit -am 'chore: update docs
