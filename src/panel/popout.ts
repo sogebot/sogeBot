1f749aaeb6b30f0b6f00bf642cd070628604c6bd
soge__ <sogehige@gmail.com>
2020-05-11 01:16:30 +0200

    fix(popout): authorize / socket on load
    Fixes #3701
    
    [skip-tests]

--- src/panel/popout.ts
+++ src/panel/popout.ts
@@ -37,7 +37,7 @@ import { isUserLoggedIn } from 'src/panel/helpers/isUserLoggedIn';
 import translate from 'src/panel/helpers/translate';
 import urlParam from 'src/panel/helpers/urlParam';
 import { getListOf } from 'src/panel/helpers/getListOf';
-import { getConfiguration, getTranslations } from 'src/panel/helpers/socket';
+import { getConfiguration, getTranslations, redirectLogin, waitForAuthorizationSocket } from 'src/panel/helpers/socket';
 
 library.add(faVolumeOff, faGripVertical, faImage, faUpload, faCircle2, faCaretRight, faTasks, faCaretDown, faSlash, faFilter, faToggleOn, faToggleOff, faBell, faShareSquare, faExclamationCircle, faQuestion, faVial, faEquals, faGreaterThanEqual, faLongArrowAltLeft, faBan, faPlusSquare, faMusic, faList, faPlay, faPause, faForward, faSpotify, faMoneyBillAlt, faPlus, faSpinner, faGift, faHeadphones, faTh, faDollarSign, faSignInAlt, faSignOutAlt, faUsers, faMusic, faCalendar, faTwitter, faCheck, faMusic, faMusic, faVolumeUp, faVolumeDown, faUsers, faGift, faTrophy, faCog, faExternalLinkAlt, faTrash, faPlus, faSync, faComments, faTwitch, faCircle, faCheckCircle, faLock, faUsers, faUser, faCheck, faTimes, faHeart, faStar, faLockOpen, faHandPointer, faRandom, faEyeSlash, faSignOutAlt, faSignInAlt, faBoxOpen, faEye, faCog, faExternalLinkAlt, faHeart, faTv, faRandom, faGem, faStar, faGift, faDollarSign, faStarHalf, faLongArrowAltRight, faCircleNotch, faCalendar, faDollarSign, faCog, faCode, faAngleUp, faTrashAlt, faAngleDown, faFont, faPlus, faMinus, faDownload, faDollarSign, faTerminal, faCog, faCommentAlt, faUsers, faExternalLinkAlt, faSyncAlt, faClock, faCog, faInfinity, faTrophy, faClone, faGem, faCoins, faExclamation, faStop, faBan, faSpinner, faCheck, faAngleRight, faPlus, faEdit, faEraser, faLink, faTrash, faPlus, faCaretLeft, faExternalLinkAlt, faLink, faSave, faThLarge, faThList, faSearch, faCircleNotch, faCheck, faEllipsisH, faEllipsisV, faPowerOff);
 Vue.component('fa', FontAwesomeIcon);
@@ -94,6 +94,14 @@ const main = async () => {
   Vue.prototype.translate = (v) => translate(v);
   Vue.prototype.urlParam = (v) => urlParam(v);
 
+  /* force first authorize */
+  try {
+    await waitForAuthorizationSocket('/');
+  } catch (e) {
+    console.error(e);
+    redirectLogin();
+    return;
+  }
   Vue.prototype.$loggedUser = await isUserLoggedIn();
   if (Vue.prototype.$loggedUser !== false) {
     await getTranslations();
