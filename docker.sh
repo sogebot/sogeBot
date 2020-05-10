47a2595a39fb51d731dd85282c2686cafbc6a647
sogehige <sogehige@gmail.com>
2020-05-10 20:11:22 +0200

    chore(docker): add DEBUG env to docker.sh (#3700)
    [skip-tests]

--- docker.sh
+++ docker.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 cd /app
 
-# cleanup on start
-rm /etc/cron.d/profiler
-service cron restart
-rm *.log
-
-if [ -n $CPUPROFILER ]
+if [ -n $DEBUG ]
 then
-  echo 'Starting with CPU profiling'
-
-  echo 'Setting up cron for log files'
-  echo '* * * * * cp /app/*.log /app/logs/' > /etc/cron.d/profiler
-  echo '# Mandatory blank line' >> /etc/cron.d/profiler
-  chmod 0644 /etc/cron.d/profiler
-  service cron restart
-
-  npm run cpu-profiler
+  echo 'Starting bot with DEBUG flag, inspect exposed at 0.0.0.0:9229'
+  npm run debug
 else
   npm start
 fi
