b989290d0c16ab8af65ef5901d7a3d42e1994c7a
soge__ <sogehige@gmail.com>
2020-05-12 23:25:40 +0200

    fix(discord): add selectot for channels in ui
    Also added partials and intents to properly cache channels

---
+++ src/panel/views/settings/components/interface/discord-channel.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="d-flex">
+    <div class="input-group-prepend">
+      <span class="input-group-text">
+        <template v-if="typeof translatedTitle === 'string'">{{ translatedTitle }}</template>
+        <template v-else>
+          {{ translatedTitle.title }}
+          <small class="text-info" data-toggle="tooltip" data-html="true" :title="translatedTitle.help">[?]</small>
+        </template>
+      </span>
+    </div>
+    <b-form-select v-model="currentValue" :options="channels"></b-form-select>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
+import { getSocket } from 'src/panel/helpers/socket';
+
+@Component({})
+export default class discordChannel extends Vue {
+  @Prop() readonly value: any;
+  @Prop() readonly title: any;
+
+  socket = getSocket('/integrations/discord')
+  channels: { text: string, value: string }[] = []
+
+  currentValue = this.value;
+  translatedTitle = this.translate(this.title);
+
+  mounted() {
+    this.socket.emit('discord::getChannels', (err, channels) => {
+      console.groupCollapsed('discord::getChannels')
+      console.log({channels});
+      console.groupEnd();
+      if (err) {
+        return console.error(err);
+      }
+
+      // find channel in channels on current or unset current
+      if (!channels.find(o => String(o.value) === String(this.currentValue))) {
+        this.currentValue = '';
+      }
+      this.channels = [{ value: '', text: `-- ${this.translate('integrations.discord.settings.noChannelSelected')} --` }, ...channels];
+    });
+  }
+
+  @Watch('currentValue')
+  onChange() {
+    this.$emit('update', { value: this.currentValue });
+  }
+};
+</script>
