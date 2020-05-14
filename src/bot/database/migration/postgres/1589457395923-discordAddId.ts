b1f3554f228362b69a57debe14913a5640d12cd3
Michal Orlik <sogehige@gmail.com>
2020-05-14 17:56:05 +0200

    fix

--- src/bot/database/migration/postgres/1589457395923-discordAddId.ts
+++ src/bot/database/migration/postgres/1589457395923-discordAddId.ts
@@ -5,12 +5,11 @@ export class discordAddId1589457395923 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP TABLE IF EXISTS "discord_link"`, undefined);
-    await queryRunner.query(`CREATE TABLE "discord_link" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "discordId" character varying NOT NULL, "tag" character varying NOT NULL, "createdAt" bigint NOT NULL, "userId" integer, CONSTRAINT "PK_51c82ec49736e25315b01dad663" PRIMARY KEY ("id"))`, undefined);
+    await queryRunner.query(`CREATE TABLE "discord_link" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "tag" character varying NOT NULL, "discordId" character varying NOT NULL, "createdAt" bigint NOT NULL, "userId" integer, CONSTRAINT "PK_51c82ec49736e25315b01dad663" PRIMARY KEY ("id"))`, undefined);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE IF EXISTS "discord_link"`, undefined);
-    await queryRunner.query(`CREATE TABLE "discord_link" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "tag" character varying NOT NULL, "createdAt" bigint NOT NULL, "userId" integer, CONSTRAINT "PK_51c82ec49736e25315b01dad663" PRIMARY KEY ("id"))`, undefined);
+    await queryRunner.query(`DROP TABLE "discord_link"`, undefined);
   }
 
 }
