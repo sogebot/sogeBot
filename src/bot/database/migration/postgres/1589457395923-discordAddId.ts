6ac1f992228bf7ba5bd6ab5c49965565aced3029
Michal Orlik <sogehige@gmail.com>
2020-05-14 17:50:14 +0200

    fix missing "

--- src/bot/database/migration/postgres/1589457395923-discordAddId.ts
+++ src/bot/database/migration/postgres/1589457395923-discordAddId.ts
@@ -5,7 +5,7 @@ export class discordAddId1589457395923 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP TABLE IF EXISTS "discord_link"`, undefined);
-    await queryRunner.query(`CREATE TABLE "discord_link" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "discordId" character varying NOT NULL, tag" character varying NOT NULL, "createdAt" bigint NOT NULL, "userId" integer, CONSTRAINT "PK_51c82ec49736e25315b01dad663" PRIMARY KEY ("id"))`, undefined);
+    await queryRunner.query(`CREATE TABLE "discord_link" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "discordId" character varying NOT NULL, "tag" character varying NOT NULL, "createdAt" bigint NOT NULL, "userId" integer, CONSTRAINT "PK_51c82ec49736e25315b01dad663" PRIMARY KEY ("id"))`, undefined);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
