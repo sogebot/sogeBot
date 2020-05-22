c3532534f117f5a32ea7c08fc9f4f7ed450ada75
Michal Orlik <sogehige@gmail.com>
2020-05-22 12:07:13 +0200

    fix(ui): permission based toggles should have correct translation
    Fixes #3793

--- src/panel/views/settings/components/interface/toggle-enable.vue
+++ src/panel/views/settings/components/interface/toggle-enable.vue
@@ -2,10 +2,10 @@
   <div class="input-group">
     <div v-if="title" class="input-group-prepend">
       <span class="input-group-text">
-        <template v-if="typeof translatedTitle === 'string'">{{ translatedTitle }}</template>
+        <template v-if="typeof title === 'string'">{{ title }}</template>
         <template v-else>
-          {{ translatedTitle.title }}
-          <small class="text-info pl-1" data-toggle="tooltip" data-html="true" :title="translatedTitle.help">[?]</small>
+          {{ title.title }}
+          <small class="text-info pl-1" data-toggle="tooltip" data-html="true" :title="title.help">[?]</small>
         </template>
       </span>
     </div>
@@ -30,7 +30,6 @@ export default class toggleEnable extends Vue {
   @Prop() readonly disabled !: boolean;
 
   currentValue = this.value;
-  translatedTitle = this.translate(this.title);
 
   update() {
     this.currentValue = !this.currentValue
