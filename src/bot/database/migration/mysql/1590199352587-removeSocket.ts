2ba7b7e41e9663a61f9aeb6590a23606f27b4578
soge__ <sogehige@gmail.com>
2020-05-23 04:03:35 +0200

    migration fix

---
+++ src/bot/database/migration/mysql/1590199352587-removeSocket.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from 'typeorm';
+
+export class removeSocket1590199352587 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query('DROP TABLE `socket`', undefined);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query('CREATE TABLE `socket` (`id` varchar(36) NOT NULL, `userId` int NOT NULL, `type` varchar(10) NOT NULL, `accessToken` varchar(36) NULL, `refreshToken` varchar(36) NULL, `accessTokenTimestamp` bigint NOT NULL DEFAULT 0, `refreshTokenTimestamp` bigint NOT NULL DEFAULT 0, PRIMARY KEY (`id`)) ENGINE=InnoDB', undefined);
+  }
+
+}
---
+++ src/bot/database/migration/sqlite/1590199352587-removeSocket.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from 'typeorm';
+
+export class removeSocket1590199352587 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`DROP TABLE "socket"`, undefined);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`CREATE TABLE "socket" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "userId" integer NOT NULL, "type" character varying(10) NOT NULL, "accessToken" character varying(36), "refreshToken" character varying(36), "accessTokenTimestamp" bigint NOT NULL DEFAULT 0, "refreshTokenTimestamp" bigint NOT NULL DEFAULT 0, CONSTRAINT "PK_54469a3174932555576d81bd656" PRIMARY KEY ("id"))`, undefined);
+  }
+
+}
