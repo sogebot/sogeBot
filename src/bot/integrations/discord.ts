cf73fa562299357b5be797f749fef59dc10efd28
Banonevar <me@banonevar.tk>
2020-05-29 19:48:08 +0300

    feat(discord): add viewers in edited discord embed
    I do not think viewers on first alert will be useful.
    
    However, viewers in edited embed is very useful, but we need change interval from 10 minutes to 1.

--- src/bot/integrations/discord.ts
+++ src/bot/integrations/discord.ts
@@ -121,6 +121,7 @@ class Discord extends Integration {
           { name: prepare('webpanel.responses.variable.game'), value: api.stats.currentGame},
           { name: prepare('webpanel.responses.variable.title'), value: api.stats.currentTitle},
           { name: prepare('integrations.discord.started_at'), value: this.embedStartedAt, inline: true},
+          { name: prepare('webpanel.viewers'), value: api.stats.currentViewers, inline: true},
           { name: prepare('webpanel.views'), value: api.stats.currentViews, inline: true},
           { name: prepare('webpanel.followers'), value: api.stats.currentFollowers, inline: true},
         ]);
