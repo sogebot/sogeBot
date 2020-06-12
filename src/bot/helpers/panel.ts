32fca89ef010cc3a57ec1e19b086d38f3eaf9fb5
Michal Orlik <sogehige@gmail.com>
2020-06-12 14:26:42 +0200

    add CA_CERT, CA_KEY

--- src/bot/helpers/panel.ts
+++ src/bot/helpers/panel.ts
@@ -3,9 +3,12 @@ import https from 'https';
 import http, { Server } from 'http';
 import express from 'express';
 import pem from 'pem';
+import fs from 'fs';
 
 import type { IconName } from '@fortawesome/free-solid-svg-icons';
 import Module from '../_interface';
+import { info } from './log';
+import { normalize } from 'path';
 
 export const menu: { category: string; name: string; id: string; this: Module | null }[] = [];
 export const menuPublic: { name: string; id: string }[] = [];
@@ -42,17 +45,29 @@ export const setServer = () => {
     ioServer = io(server);
     ioServer.sockets.setMaxListeners(200);
 
-    pem.createCertificate({ days: 99999, selfSigned: true }, function (err, keys) {
-      if (err) {
-        throw err;
+    if (process.env.CA_CERT && process.env.CA_KEY) {
+      info(`Using ${process.env.CA_CERT} certificate for HTTPS`);
+      serverSecure = https.createServer({
+        key: fs.readFileSync(normalize(process.env.CA_KEY)),
+        cert: fs.readFileSync(normalize(process.env.CA_CERT)),
+      }, app);
+      if (ioServer) {
+        ioServer.attach(serverSecure);
       }
-      if (app) {
-        serverSecure = https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app);
-        if (ioServer) {
-          ioServer.attach(serverSecure);
+    } else {
+      info('Generating self-signed certificate for HTTPS');
+      pem.createCertificate({ days: 99999, selfSigned: true }, function (err, keys) {
+        if (err) {
+          throw err;
         }
-      }
-    });
+        if (app) {
+          serverSecure = https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app);
+          if (ioServer) {
+            ioServer.attach(serverSecure);
+          }
+        }
+      });
+    }
   }
 };
 
