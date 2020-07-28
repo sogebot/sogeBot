4ab9a23546f4f0f894e61b6dabc50fe0acf9b390
soge__ <sogehige@gmail.com>
2020-07-29 00:42:28 +0200

    feat(spotify): add ban and unban commands

---
+++ src/bot/database/entity/spotify.ts
@@ -0,0 +1,16 @@
+import { EntitySchema } from 'typeorm';
+
+export interface SpotifySongBanInterface {
+  spotifyUri: string;
+  title: string;
+  artists: string[];
+}
+
+export const SpotifySongBan = new EntitySchema<Readonly<Required<SpotifySongBanInterface>>>({
+  name: 'spotify_song_ban',
+  columns: {
+    spotifyUri: { type: String, primary: true },
+    title: { type: String },
+    artists: { type: 'simple-array' },
+  },
+});
