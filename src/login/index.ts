70eb74d69c39967110a263bfbd56bb2182a3229e
soge__ <sogehige@gmail.com>
2020-08-11 03:35:29 +0200

    perf(login): remove unused imports

--- src/login/index.ts
+++ src/login/index.ts
@@ -1,48 +1,20 @@
 import Vue from 'vue';
-
-import translate from '../panel/helpers/translate';
-import LoadScript from 'vue-plugin-load-script';
 import VueRouter from 'vue-router';
-
-import { ButtonStates, states } from '../panel/helpers/buttonStates';
-
 import BootstrapVue from 'bootstrap-vue';
-
-import urlParam from '../panel/helpers/urlParam';
-
 import { library } from '@fortawesome/fontawesome-svg-core';
-
 import {
   faCheckCircle, faSkullCrossbones,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 library.add(faCheckCircle, faSkullCrossbones);
+
 Vue.component('fa', FontAwesomeIcon);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
-
 Vue.use(VueRouter);
-Vue.use(LoadScript);
 Vue.use(BootstrapVue);
 
-export interface Global {
-  translations: any;
-  configuration: any;
-  $loadScript: (script: string) => Promise<void>;
-  $unloadScript: (script: string) => Promise<void>;
-}
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    urlParam(key: string): string | null;
-    $state: states;
-  }
-}
-
 const init = async () => {
-  Vue.prototype.translate = (v: string) => translate(v);
-  Vue.prototype.urlParam = (v: string) => urlParam(v);
-  Vue.prototype.$state = ButtonStates;
-
   const router = new VueRouter({
     mode: 'history',
     base: __dirname,
