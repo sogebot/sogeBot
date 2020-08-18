920000f9eaaf11020ce9da8eddef31e2a72ca534
H4nSolo <H4nSolo@mgc4you.de>
2020-08-18 12:54:42 +0200

    Add Jackpot Command & !gamble timeout
    Added Things:
    
    [] Timeout Function to set up generally Gamble timeout
    [] Jackpot Command to the current Jackpot Value (if Jackpot is enabled)
    
    Changes: 
    [] -

--- src/bot/games/gamble.ts
+++ src/bot/games/gamble.ts
@@ -37,93 +37,147 @@ class Gamble extends Game {
   chanceToTriggerJackpot = 5;
   @shared(true)
   jackpotValue = 0;
+  @settings("timeout")
+  gambletimeout = 30
+
+  timeoutList: Dictionary<number> = {};
+
+  @command('!jackpot')
+  async jackpot (opts: CommandOptions): Promise<CommandResponse[]> {
+    let message;
+
+    if (this.enableJackpot) {
+      const currentPointsOfUser = await pointsSystem.getPointsOf(opts.sender.userId);
+      message = prepare('gambling.gamble.jackpotinfo', {
+        pointsName: await pointsSystem.getPointsName(currentPointsOfUser),
+        jackpotName: await pointsSystem.getPointsName(this.jackpotValue),
+        jackpot: Math.ceil(this.jackpotValue),
+      });
+
+      /*message = prepare('gambling.gamble.winJackpot', {
+        pointsName: await pointsSystem.getPointsName(currentPointsOfUser),
+        points: currentPointsOfUser,
+        jackpotName: await pointsSystem.getPointsName(this.jackpotValue),
+        jackpot: Math.ceil(this.jackpotValue),
+      });
+      */
+      return [{ response: message, ...opts }];
+    } else {
+      return [{ response: 'Jackpot is Disbabled!', ...opts }];
+    }
+  }
+
 
   @command('!gamble')
   async main (opts: CommandOptions): Promise<CommandResponse[]> {
     let points, message;
 
     opts.sender['message-type'] = 'chat'; // force responses to chat
-    try {
-      const parsed = opts.parameters.trim().match(/^([\d]+|all)$/);
-      if (_.isNil(parsed)) {
-        throw Error(ERROR_NOT_ENOUGH_OPTIONS);
+    //mindforger timeout check
+    var has_timeout = 0;
+    var date = new Date();
+    var now = date.getTime();
+    if(opts.sender.username in this.timeoutList)
+    {
+      if(this.timeoutList[opts.sender.username] < now)
+      {
+        delete this.timeoutList[opts.sender.username]; // not timeouted anymore, remove from list
       }
+      else
+      {
+        has_timeout = Math.ceil((this.timeoutList[opts.sender.username] - now) / 1000);
+      }
+    }
 
-      const pointsOfUser = await pointsSystem.getPointsOf(opts.sender.userId);
-      points = parsed[1] === 'all' ? pointsOfUser : Number(parsed[1]);
+    if(has_timeout > 0)
+    {
+      return [{ response: prepare('cooldowns.cooldown-triggered', { sender: opts.sender, command: "!gamble", seconds: has_timeout}), ...opts }];
+    }
+    else
+    {
+      try {
+        const parsed = opts.parameters.trim().match(/^([\d]+|all)$/);
+        if (_.isNil(parsed)) {
+          throw Error(ERROR_NOT_ENOUGH_OPTIONS);
+        }
 
-      if (points === 0) {
-        throw Error(ERROR_ZERO_BET);
-      }
-      if (pointsOfUser < points) {
-        throw Error(ERROR_NOT_ENOUGH_POINTS);
-      }
-      if (points < (this.minimalBet)) {
-        throw Error(ERROR_MINIMAL_BET);
-      }
+        const pointsOfUser = await pointsSystem.getPointsOf(opts.sender.userId);
+        points = parsed[1] === 'all' ? pointsOfUser : Number(parsed[1]);
 
-      await pointsSystem.decrement({ userId: opts.sender.userId }, points);
-      if (this.enableJackpot && _.random(0, 100, false) <= this.chanceToTriggerJackpot) {
-        const incrementPointsWithJackpot = (points * 2) + this.jackpotValue;
-        await getRepository(User).increment({ userId: opts.sender.userId }, 'points', incrementPointsWithJackpot);
-        const currentPointsOfUser = await pointsSystem.getPointsOf(opts.sender.userId);
-        message = prepare('gambling.gamble.winJackpot', {
-          pointsName: await pointsSystem.getPointsName(currentPointsOfUser),
-          points: currentPointsOfUser,
-          jackpotName: await pointsSystem.getPointsName(this.jackpotValue),
-          jackpot: this.jackpotValue,
-        });
-        this.jackpotValue = 0;
-      } else  if (_.random(0, 100, false) <= this.chanceToWin) {
-        await getRepository(User).increment({ userId: opts.sender.userId }, 'points', points * 2);
-        const updatedPoints = await pointsSystem.getPointsOf(opts.sender.userId);
-        message = prepare('gambling.gamble.win', {
-          pointsName: await pointsSystem.getPointsName(updatedPoints),
-          points: updatedPoints,
-        });
-      } else {
-        if (this.enableJackpot) {
+        if (points === 0) {
+          throw Error(ERROR_ZERO_BET);
+        }
+        if (pointsOfUser < points) {
+          throw Error(ERROR_NOT_ENOUGH_POINTS);
+        }
+        if (points < (this.minimalBet)) {
+          throw Error(ERROR_MINIMAL_BET);
+        }
+
+        await pointsSystem.decrement({ userId: opts.sender.userId }, points);
+        if (this.enableJackpot && _.random(0, 100, false) <= this.chanceToTriggerJackpot) {
+          const incrementPointsWithJackpot = (points * 2) + this.jackpotValue;
+          await getRepository(User).increment({ userId: opts.sender.userId }, 'points', incrementPointsWithJackpot);
           const currentPointsOfUser = await pointsSystem.getPointsOf(opts.sender.userId);
-          this.jackpotValue = Math.min(this.jackpotValue + points / this.lostPointsAddedToJackpot, this.maxJackpotValue);
-          message = prepare('gambling.gamble.loseWithJackpot', {
+          message = prepare('gambling.gamble.winJackpot', {
             pointsName: await pointsSystem.getPointsName(currentPointsOfUser),
             points: currentPointsOfUser,
             jackpotName: await pointsSystem.getPointsName(this.jackpotValue),
-            jackpot: this.jackpotValue,
+            jackpot: Math.ceil(this.jackpotValue),
+          });
+          this.jackpotValue = 0;
+        } else  if (_.random(0, 100, false) <= this.chanceToWin) {
+          await getRepository(User).increment({ userId: opts.sender.userId }, 'points', points * 2);
+          const updatedPoints = await pointsSystem.getPointsOf(opts.sender.userId);
+          message = prepare('gambling.gamble.win', {
+            pointsName: await pointsSystem.getPointsName(updatedPoints),
+            points: updatedPoints,
           });
         } else {
-          message = prepare('gambling.gamble.lose', {
-            pointsName: await pointsSystem.getPointsName(await pointsSystem.getPointsOf(opts.sender.userId)),
-            points: await pointsSystem.getPointsOf(opts.sender.userId),
-          });
+          if (this.enableJackpot) {
+            const currentPointsOfUser = await pointsSystem.getPointsOf(opts.sender.userId);
+            this.jackpotValue = Math.floor(Math.min(this.jackpotValue + points * (this.lostPointsAddedToJackpot / 100.0), this.maxJackpotValue));
+            message = prepare('gambling.gamble.loseWithJackpot', {
+              pointsName: await pointsSystem.getPointsName(currentPointsOfUser),
+              points: currentPointsOfUser,
+              jackpotName: await pointsSystem.getPointsName(this.jackpotValue),
+              jackpot: Math.ceil(this.jackpotValue),
+            });
+          } else {
+            message = prepare('gambling.gamble.lose', {
+              pointsName: await pointsSystem.getPointsName(await pointsSystem.getPointsOf(opts.sender.userId)),
+              points: await pointsSystem.getPointsOf(opts.sender.userId),
+            });
+          }
         }
-      }
-      return [{ response: message, ...opts }];
-    } catch (e) {
-      switch (e.message) {
-        case ERROR_ZERO_BET:
-          message = prepare('gambling.gamble.zeroBet', {
-            pointsName: await pointsSystem.getPointsName(0),
-          });
-          return [{ response: message, ...opts }];
-        case ERROR_NOT_ENOUGH_OPTIONS:
-          return [{ response: translate('gambling.gamble.notEnoughOptions'), ...opts }];
-        case ERROR_NOT_ENOUGH_POINTS:
-          message = prepare('gambling.gamble.notEnoughPoints', {
-            pointsName: await pointsSystem.getPointsName(points ? Number(points) : 0),
-            points: points,
-          });
-          return [{ response: message, ...opts }];
-        case ERROR_MINIMAL_BET:
-          points = this.minimalBet;
-          message = prepare('gambling.gamble.lowerThanMinimalBet', {
-            pointsName: await pointsSystem.getPointsName(points),
-            points: points,
-          });
-          return [{ response: message, ...opts }];
-        default:
-          error(e.stack);
-          return [{ response: translate('core.error'), ...opts }];
+        this.timeoutList[opts.sender.username] = now + (this.gambletimeout * 1000);
+        return [{ response: message, ...opts }];
+      } catch (e) {
+        switch (e.message) {
+          case ERROR_ZERO_BET:
+            message = prepare('gambling.gamble.zeroBet', {
+              pointsName: await pointsSystem.getPointsName(0),
+            });
+            return [{ response: message, ...opts }];
+          case ERROR_NOT_ENOUGH_OPTIONS:
+            return [{ response: translate('gambling.gamble.notEnoughOptions'), ...opts }];
+          case ERROR_NOT_ENOUGH_POINTS:
+            message = prepare('gambling.gamble.notEnoughPoints', {
+              pointsName: await pointsSystem.getPointsName(points ? Number(points) : 0),
+              points: points,
+            });
+            return [{ response: message, ...opts }];
+          case ERROR_MINIMAL_BET:
+            points = this.minimalBet;
+            message = prepare('gambling.gamble.lowerThanMinimalBet', {
+              pointsName: await pointsSystem.getPointsName(points),
+              points: points,
+            });
+            return [{ response: message, ...opts }];
+          default:
+            error(e.stack);
+            return [{ response: translate('core.error'), ...opts }];
+        }
       }
     }
   }
