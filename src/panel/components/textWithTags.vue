30693418bdca43535631799b7b551506fb6ba1ec
Michal Orlik <sogehige@gmail.com>
2020-09-07 14:01:12 +0200

    refactor(textWithTags): move to composition api

--- src/panel/components/textWithTags.vue
+++ src/panel/components/textWithTags.vue
@@ -3,34 +3,34 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
+import { defineComponent } from '@vue/composition-api'
 import { sortBy, keys, isNil } from 'lodash-es';
 
 import { flatten } from 'src/bot/helpers/flatten';
 import translate from '../helpers/translate';
 
-export interface Global {
-  translations: any;
+interface Props {
+  value: string;
 }
 
-@Component({})
-export default class textWithTags extends Vue {
-  @Prop() value !: string;
-
-  filter(val: string) {
-    const filtersRegExp = new RegExp('\\$(' + sortBy(keys(flatten(translate('responses.variable', true))), (o) => -o.length).join('|') + ')', 'g');
-    val = val.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-    let matches = val.match(filtersRegExp);
-    let output = val;
-    if (!isNil(matches)) {
-      for (let match of matches) {
-        output = output.replace(match,
-          `<span contenteditable="false" class="editable-variable">
-            ${this.translate('responses.variable.' + match.replace('$', ''))}
-          </span>&nbsp;`);
+export default defineComponent({
+  setup(props: Props, context) {
+    const filter = (val: string) => {
+      const filtersRegExp = new RegExp('\\$(' + sortBy(keys(flatten(translate('responses.variable', true))), (o) => -o.length).join('|') + ')', 'g');
+      val = val.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+      let matches = val.match(filtersRegExp);
+      let output = val;
+      if (!isNil(matches)) {
+        for (let match of matches) {
+          output = output.replace(match,
+            `<span contenteditable="false" class="editable-variable">
+              ${translate('responses.variable.' + match.replace('$', ''))}
+            </span>&nbsp;`);
+        }
       }
+      return output;
     }
-    return output;
+    return { filter }
   }
-}
+});
 </script>
