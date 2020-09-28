86a3c4f78a8e3db60b91058d7d9e34ec1cf17d7e
soge__ <sogehige@gmail.com>
2020-09-28 15:22:41 +0200

    fix

--- src/bot/database/migration/postgres/1601297968526-eventListIsHiddenAttr.ts
+++ src/bot/database/migration/postgres/1601297968526-eventListIsHiddenAttr.ts
@@ -4,7 +4,7 @@ export class eventListIsHiddenAttr1601297968526 implements MigrationInterface {
   name = 'eventListIsHiddenAttr1601297968526';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "event_list" ADD "isHidden" boolean NOT NULL DEFAULT 0`);
+    await queryRunner.query(`ALTER TABLE "event_list" ADD "isHidden" boolean NOT NULL DEFAULT false`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
