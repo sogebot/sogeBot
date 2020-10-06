f41ddf8fb29047eb6fe762fae0eb8c4141f83086
sogehige <sogehige@gmail.com>
2020-10-06 16:18:31 +0200

    fix(migration): use correct object types in data insert (#4189)
    Fixes https://discordapp.com/channels/317348946144002050/317349069024395264/763026097565728798

--- src/bot/database/migration/mysql/1601124530399-eventListUserId.ts
+++ src/bot/database/migration/mysql/1601124530399-eventListUserId.ts
@@ -46,7 +46,7 @@ export class eventListUserId1601124530399 implements MigrationInterface {
     await queryRunner.query('CREATE TABLE `temporary_event_list` (`id` int NOT NULL AUTO_INCREMENT, `event` varchar(255) NOT NULL, `userId` varchar(255) NOT NULL, `timestamp` bigint NOT NULL, `values_json` text NOT NULL, `isTest` tinyint NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB', undefined);
     for (const event of migratedEvents) {
       await queryRunner.query(
-        `INSERT INTO \`temporary_event_list\`(${Object.keys(event).map(o=>`\`${o}\``).join(', ')}) values(${Object.keys(event).map((o: any) => '?')})`, [ ...Object.keys(event).map(key => `'${event[key]}'`) ]
+        `INSERT INTO \`temporary_event_list\`(${Object.keys(event).map(o=>`\`${o}\``).join(', ')}) values(${Object.keys(event).map((o: any) => '?')})`, [ ...Object.keys(event).map(key => event[key]) ]
       );
     }
     await queryRunner.query(`DROP TABLE \`event_list\``);
