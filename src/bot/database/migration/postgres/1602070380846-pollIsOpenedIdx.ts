89b73a21d48693ecd0f51e46fbabe117d7a02dcf
sogehige <sogehige@gmail.com>
2020-10-07 14:09:04 +0200

    refactor(poll): add index to isOpened (#4195)
    * refactor(poll): add index to isOpened
    
    Fixes #4191
    
    * migration

---
+++ src/bot/database/migration/postgres/1602070380846-pollIsOpenedIdx.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from 'typeorm';
+
+export class pollIsOpenedIdx1602070380846 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE INDEX "IDX_poll_isOpened" ON "poll" ("isOpened") `);
+
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "IDX_poll_isOpened"`);
+  }
+
+}
