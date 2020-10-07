7d4b4f04baa1f63971d3d8a1dff5cdfd1de17ee6
Michal Orlik <sogehige@gmail.com>
2020-10-07 17:25:42 +0200

    migration fix

--- src/bot/database/migration/postgres/1602070380846-pollIsOpenedIdx.ts
+++ src/bot/database/migration/postgres/1602070380846-pollIsOpenedIdx.ts
@@ -4,10 +4,13 @@ export class pollIsOpenedIdx1602070380846 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`CREATE INDEX "IDX_poll_isOpened" ON "poll" ("isOpened") `);
+    await queryRunner.query(`ALTER TABLE "poll" ALTER COLUMN "type" TYPE character varying(7) NOT NULL`);
+
 
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "poll" ALTER COLUMN "type" TYPE character varying(6) NOT NULL`);
     await queryRunner.query(`DROP INDEX "IDX_poll_isOpened"`);
   }
 
