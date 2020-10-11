129bdd99a30aba03c1c68a81265d347da910e98b
soge__ <sogehige@gmail.com>
2020-10-11 16:07:40 +0200

    remove duplicate constraint

--- src/bot/database/migration/postgres/1602413333619-keywordMultipleResponses.ts
+++ src/bot/database/migration/postgres/1602413333619-keywordMultipleResponses.ts
@@ -12,7 +12,6 @@ export class keywordMultipleResponses1602413333619 implements MigrationInterface
     await queryRunner.query(`CREATE TABLE "keyword_responses" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "order" integer NOT NULL, "response" text, "stopIfExecuted" boolean NOT NULL, "permission" character varying NOT NULL, "filter" character varying NOT NULL, "keywordId" character varying, CONSTRAINT "PK_3049091cd170cc88ad38bcca63f" PRIMARY KEY ("id"))`, undefined);
     await queryRunner.query(`ALTER TABLE "keyword_responses" ADD CONSTRAINT "FK_d12716a3805d58dd75ab09c8c67" FOREIGN KEY ("keywordId") REFERENCES "keyword"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
 
-    await queryRunner.query(`ALTER TABLE "keyword_responses" ADD CONSTRAINT "FK_d12716a3805d58dd75ab09c8c67" FOREIGN KEY ("keywordId") REFERENCES "keyword"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
     for (const keyword of keywords) {
       await queryRunner.query(
         `INSERT INTO "keyword_responses"("id", "order", "response", "stopIfExecuted", "permission", "filter", "keywordId") values("${uuid()}", 0, "${keyword.response}", 0, "0efd7b1c-e460-4167-8e06-8aaf2c170311", "", "${keyword.id}")`);
