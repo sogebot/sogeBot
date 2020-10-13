7a761c3b19fce5edb313b887d3dc4fa8cdff5df9
Michal Orlik <sogehige@gmail.com>
2020-10-13 12:17:05 +0200

    migration update

--- src/bot/database/migration/mysql/1602499070262-hltbRefactor.ts
+++ src/bot/database/migration/mysql/1602499070262-hltbRefactor.ts
@@ -7,6 +7,8 @@ export class hltbRefactor1602499070262 implements MigrationInterface {
     await queryRunner.query(`DROP TABLE \`how_long_to_beat_game\``, undefined);
     await queryRunner.query('CREATE TABLE `how_long_to_beat_game` (`id` varchar(36) NOT NULL, `game` varchar(255) NOT NULL, `imageUrl` varchar(255) NOT NULL, `startedAt` bigint NOT NULL, `gameplayMain` float(12) NOT NULL DEFAULT 0, `gameplayCompletionist` float(12) NOT NULL DEFAULT 0, UNIQUE INDEX `IDX_301758e0e3108fc902d5436527` (`game`), PRIMARY KEY (`id`)) ENGINE=InnoDB');
     await queryRunner.query('CREATE TABLE `how_long_to_beat_game_item` (`id` varchar(36) NOT NULL, `hltb_id` int NOT NULL, `createdAt` bigint NOT NULL, `timestamp` bigint NOT NULL DEFAULT 0, `offset` bigint NOT NULL DEFAULT 0, `isMainCounted` tinyint NOT NULL DEFAULT 0, `isCompletionistCounted` tinyint NOT NULL DEFAULT 0, INDEX `IDX_hltb_id` (`hltb_id`), PRIMARY KEY (`id`)) ENGINE=InnoDB');
+    await queryRunner.query('ALTER TABLE `how_long_to_beat_game` ADD `gameplayMainExtra` float(12) NOT NULL DEFAULT 0');
+    await queryRunner.query('ALTER TABLE `how_long_to_beat_game_item` ADD `isExtraCounted` tinyint NOT NULL DEFAULT 0');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
--- src/bot/database/migration/postgres/1602499070262-hltbRefactor.ts
+++ src/bot/database/migration/postgres/1602499070262-hltbRefactor.ts
@@ -9,6 +9,8 @@ export class hltbRefactor1602499070262 implements MigrationInterface {
     await queryRunner.query(`CREATE TABLE "how_long_to_beat_game" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "game" character varying NOT NULL, "startedAt" bigint NOT NULL, "gameplayMain" double precision NOT NULL DEFAULT 0, "gameplayCompletionist" double precision NOT NULL DEFAULT 0, "imageUrl" character varying NOT NULL, CONSTRAINT "PK_c6fbf5fc15e97e46c2659dccea1" PRIMARY KEY ("id"))`, undefined);
     await queryRunner.query(`CREATE UNIQUE INDEX "IDX_301758e0e3108fc902d5436527" ON "how_long_to_beat_game" ("game") `, undefined);
     await queryRunner.query(`CREATE INDEX "IDX_hltb_id" ON "how_long_to_beat_game_item" ("hltb_id") `);
+    await queryRunner.query(`ALTER TABLE "how_long_to_beat_game" ADD "gameplayMainExtra" double precision NOT NULL DEFAULT 0`);
+    await queryRunner.query(`ALTER TABLE "how_long_to_beat_game_item" ADD "isExtraCounted" boolean NOT NULL DEFAULT false`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
