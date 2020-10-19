1dfd2fa1ce4396bd88cbb59ddf804522202df915
Michal Orlik <sogehige@gmail.com>
2020-10-19 11:40:34 +0200

    load locales manually
    We need to do this as we will be importing it in panel

--- src/bot/helpers/dayjs.ts
+++ src/bot/helpers/dayjs.ts
@@ -3,22 +3,17 @@ import utc from 'dayjs/plugin/utc';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import { default as tz } from 'dayjs/plugin/timezone';
-import glob from 'glob';
 dayjs.extend(utc);
 dayjs.extend(tz);
 dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
 
-// load locales
-const loadLocales = () => {
-  const list = new Set(glob
-    .sync('./locales/*')
-    .map(o => o.replace('./locales/', '').replace('.json', ''))
-  );
-  for(const key of list) {
-    require('dayjs/locale/' + key);
-  }
-};
-loadLocales();
+import 'dayjs/locale/cs';
+import 'dayjs/locale/de';
+import 'dayjs/locale/en';
+import 'dayjs/locale/fr';
+import 'dayjs/locale/pt';
+import 'dayjs/locale/ru';
+import 'dayjs/locale/uk';
 
 export const timezone = (process.env.TIMEZONE ?? 'system') === 'system' || !process.env.TIMEZONE ? dayjs.tz.guess() : process.env.TIMEZONE;
