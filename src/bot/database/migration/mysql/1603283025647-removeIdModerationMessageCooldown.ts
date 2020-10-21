2ec2d0474d021318b495f3a4a720e7a5290f0390
Michal Orlik <sogehige@gmail.com>
2020-10-21 14:50:05 +0200

    fix

--- src/bot/database/migration/mysql/1603283025647-removeIdModerationMessageCooldown.ts
+++ src/bot/database/migration/mysql/1603283025647-removeIdModerationMessageCooldown.ts
@@ -3,7 +3,7 @@ import {MigrationInterface, QueryRunner} from 'typeorm';
 export class removeModerationMessageCooldown1603283025647 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "moderation_message_cooldown"`);
+    await queryRunner.query('DROP TABLE `moderation_message_cooldown`');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
