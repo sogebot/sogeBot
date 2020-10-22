d115b55eb9c6f1a0f8d84ddda3e2dce9784339de
sogehige <sogehige@gmail.com>
2020-10-22 12:01:51 +0200

    fix(api): ignore missing response data (#4239)
    * fix(api): ignore missing response data
    
    Fixes #4237
    
    * remove AxiosError

--- src/bot/api.ts
+++ src/bot/api.ts
@@ -1049,7 +1049,7 @@ class API extends Core {
 
       warning(`Couldn't find name of game for gid ${id} - fallback to ${this.stats.currentGame}`);
       if (e.isAxiosError) {
-        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response.status}\n${JSON.stringify(e.response.data, null, 4)}\n\n${e.stack}`);
+        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response?.status ?? 0}\n${JSON.stringify(e.response?.data ?? '--nodata--', null, 4)}\n\n${e.stack}`);
         ioServer?.emit('api.stats', { method: e.config.method.toUpperCase(), timestamp: Date.now(), call: 'getGameNameFromId', api: 'helix', endpoint: e.config.url, code: e.response.status ?? 'n/a', data: e.response.data, remaining: this.calls.bot });
       } else {
         error(e.stack);
@@ -1099,7 +1099,7 @@ class API extends Core {
     } catch (e) {
       warning(`Couldn't find name of game for name ${name} - fallback to ${this.stats.currentGame}`);
       if (e.isAxiosError) {
-        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response.status}\n${JSON.stringify(e.response.data, null, 4)}\n\n${e.stack}`);
+        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response?.status ?? 0}\n${JSON.stringify(e.response?.data ?? '--nodata--', null, 4)}\n\n${e.stack}`);
         ioServer?.emit('api.stats', { method: e.config.method.toUpperCase(), timestamp: Date.now(), call: 'getGameIdFromName', api: 'helix', endpoint: e.config.url, code: e.response.status ?? 'n/a', data: e.response.data, remaining: this.calls.bot });
       } else {
         error(e.stack);
@@ -1425,7 +1425,7 @@ class API extends Core {
       ioServer?.emit('api.stats', { method: 'PUT', request: { data: { tag_ids } }, timestamp: Date.now(), call: 'setTags', api: 'helix', endpoint: url, code: request.status, data: request.data, remaining: this.calls.bot });
     } catch (e) {
       if (e.isAxiosError) {
-        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response.status}\n${JSON.stringify(e.response.data, null, 4)}\n\n${e.stack}`);
+        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response?.status ?? 0}\n${JSON.stringify(e.response?.data ?? '--nodata--', null, 4)}\n\n${e.stack}`);
         ioServer?.emit('api.stats', { method: e.config.method.toUpperCase(), timestamp: Date.now(), call: 'setTags', api: 'helix', endpoint: e.config.url, code: e.response.status ?? 'n/a', data: e.response.data, remaining: this.calls.bot });
       } else {
         error(e.stack);
@@ -1527,7 +1527,7 @@ class API extends Core {
       ioServer?.emit('api.stats', { method: 'PATCH', request: requestData, timestamp: Date.now(), call: 'setTitleAndGame', api: 'helix', endpoint: url, code: request.status, remaining: this.calls.bot });
     } catch (e) {
       if (e.isAxiosError) {
-        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response.status}\n${JSON.stringify(e.response.data, null, 4)}\n\n${e.stack}`);
+        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response?.status ?? 0}\n${JSON.stringify(e.response?.data ?? '--nodata--', null, 4)}\n\n${e.stack}`);
         ioServer?.emit('api.stats', { method: e.config.method.toUpperCase(), timestamp: Date.now(), call: 'setTitleAndGame', api: 'helix', endpoint: e.config.url, code: e.response.status ?? 'n/a', data: e.response.data, remaining: this.calls.bot });
       } else {
         error(e.stack);
@@ -1588,7 +1588,7 @@ class API extends Core {
       ioServer?.emit('api.stats', { method: 'GET', data: request.data, timestamp: Date.now(), call: 'sendGameFromTwitch', api: 'helix', endpoint: url, code: request.status, remaining: this.calls.bot });
     } catch (e) {
       if (e.isAxiosError) {
-        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response.status}\n${JSON.stringify(e.response.data, null, 4)}\n\n${e.stack}`);
+        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response?.status ?? 0}\n${JSON.stringify(e.response?.data ?? '--nodata--', null, 4)}\n\n${e.stack}`);
         ioServer?.emit('api.stats', { method: e.config.method.toUpperCase(), timestamp: Date.now(), call: 'sendGameFromTwitch', api: 'helix', endpoint: e.config.url, code: e.response.status ?? 'n/a', data: e.response.data, remaining: this.calls.bot });
       } else {
         error(e.stack);
@@ -1665,7 +1665,7 @@ class API extends Core {
         this.calls.bot.refresh = e.response.headers['ratelimit-reset'];
       }
       if (e.isAxiosError) {
-        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response.status}\n${JSON.stringify(e.response.data, null, 4)}\n\n${e.stack}`);
+        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response?.status ?? 0}\n${JSON.stringify(e.response?.data ?? '--nodata--', null, 4)}\n\n${e.stack}`);
         ioServer?.emit('api.stats', { method: e.config.method.toUpperCase(), timestamp: Date.now(), call: 'checkClips', api: 'helix', endpoint: e.config.url, code: e.response.status ?? 'n/a', data: e.response.data, remaining: this.calls.bot });
       } else {
         error(e.stack);
@@ -1739,7 +1739,7 @@ class API extends Core {
       }
 
       if (e.isAxiosError) {
-        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response.status}\n${JSON.stringify(e.response.data, null, 4)}\n\n${e.stack}`);
+        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response?.status ?? 0}\n${JSON.stringify(e.response?.data ?? '--nodata--', null, 4)}\n\n${e.stack}`);
         ioServer?.emit('api.stats', { method: e.config.method.toUpperCase(), timestamp: Date.now(), call: 'createClip', api: 'helix', endpoint: e.config.url, code: e.response.status ?? 'n/a', data: e.response.data, remaining: this.calls.bot });
       } else {
         error(e.stack);
@@ -1797,7 +1797,7 @@ class API extends Core {
 
       if (logError) {
         if (e.isAxiosError) {
-          error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response.status}\n${JSON.stringify(e.response.data, null, 4)}\n\n${e.stack}`);
+          error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response?.status ?? 0}\n${JSON.stringify(e.response?.data ?? '--nodata--', null, 4)}\n\n${e.stack}`);
           ioServer?.emit('api.stats', { method: e.config.method.toUpperCase(), timestamp: Date.now(), call: 'fetchAccountAge', api: 'helix', endpoint: e.config.url, code: e.response.status ?? 'n/a', data: e.response.data, remaining: this.calls.bot });
         } else {
           error(e.stack);
@@ -1850,7 +1850,7 @@ class API extends Core {
         this.calls.bot.refresh = e.response.headers['ratelimit-reset'];
       }
       if (e.isAxiosError) {
-        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response.status}\n${JSON.stringify(e.response.data, null, 4)}\n\n${e.stack}`);
+        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response?.status ?? 0}\n${JSON.stringify(e.response?.data ?? '--nodata--', null, 4)}\n\n${e.stack}`);
         ioServer?.emit('api.stats', { method: e.config.method.toUpperCase(), timestamp: Date.now(), call: 'isFollowerUpdate', api: 'helix', endpoint: e.config.url, code: e.response.status ?? 'n/a', data: e.response.data, remaining: this.calls.bot });
       } else {
         error(e.stack);
@@ -1949,7 +1949,7 @@ class API extends Core {
         return;
       }
       if (e.isAxiosError) {
-        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response.status}\n${JSON.stringify(e.response.data, null, 4)}\n\n${e.stack}`);
+        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response?.status ?? 0}\n${JSON.stringify(e.response?.data ?? '--nodata--', null, 4)}\n\n${e.stack}`);
         ioServer?.emit('api.stats', { method: e.config.method.toUpperCase(), timestamp: Date.now(), call: 'createMarker', api: 'helix', endpoint: e.config.url, code: e.response.status ?? 'n/a', data: e.response.data, remaining: this.calls.bot });
       } else {
         error(e.stack);
@@ -2045,7 +2045,7 @@ class API extends Core {
       return request.data.data;
     } catch (e) {
       if (e.isAxiosError) {
-        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response.status}\n${JSON.stringify(e.response.data, null, 4)}\n\n${e.stack}`);
+        error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response?.status ?? 0}\n${JSON.stringify(e.response?.data ?? '--nodata--', null, 4)}\n\n${e.stack}`);
         ioServer?.emit('api.stats', { method: e.config.method.toUpperCase(), timestamp: Date.now(), call: 'getClipById', api: 'helix', endpoint: e.config.url, code: e.response.status ?? 'n/a', data: e.response.data, remaining: this.calls.bot });
       } else {
         error(e.stack);
