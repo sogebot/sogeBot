974debd08a8eb01b2dd23c4bb1d3f7feca2736c0
soge__ <sogehige@gmail.com>
2020-11-07 13:34:44 +0100

    fix(tools): prestart should properly fail on migration error
    Fixes #5103

---
+++ tools/migration.js
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const os = require('os');
+const fs = require('fs');
+const logDir = './logs';
+
+const util = require('util');
+const exec = require('child_process').exec;
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const logFile = fs.createWriteStream('./logs/migration.log', { flags: 'w' });
+console.log('... Migration in progress, please wait (see logs/migration.log for progress and error) ...');
+
+async function runMigration() {
+  exec('typeorm migration:run', (error, stdout, stderr) => {
+    logFile.write(stdout + os.EOL);
+    logFile.write(stderr + os.EOL);
+
+    if(error) {
+      console.error('\n!!! Migration FAILED, please check your logs/migration.log for additional information !!! \n');
+      process.exit(1);
+    } else {
+      console.log('\n ... Migration done\n');
+      process.exit(0);
+    }
+  });
+}
+runMigration();
+/*
+typeorm migration:run
+*/
