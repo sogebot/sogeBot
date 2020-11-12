6486729029b55c61f3a458df48dc95114cc1a6a5
soge__ <sogehige@gmail.com>
2020-11-12 22:05:46 +0100

    perf(permissions): be more selectable with recache

--- src/bot/permissions.ts
+++ src/bot/permissions.ts
@@ -18,8 +18,12 @@ import currency from './currency';
 import Expects from './expects';
 import users from './users';
 import { promisify } from 'util';
+import { HOUR, MINUTE } from './constants';
 
 let isWarnedAboutCasters = false;
+let isRecacheRunning = false;
+let rechacheFinishedAt = 0;
+const recacheIds = new Map<number, number>();
 
 class Permissions extends Core {
   constructor() {
@@ -125,15 +129,33 @@ class Permissions extends Core {
   }
 
   recacheOnlineUsersPermission() {
-    const setImmediatePromise = promisify(setImmediate);
-    getRepository(User).find({ isOnline: true }).then(async (users2) => {
-      for (const user of users2) {
-        debug('permissions.recache', `Recaching ${user.username}#${user.userId}`);
-        cleanViewersCache(user.userId);
-        await this.getUserHighestPermission(user.userId);
-        await setImmediatePromise();
+    if (!isRecacheRunning && Date.now() - rechacheFinishedAt > 10 * MINUTE) {
+      const setImmediatePromise = promisify(setImmediate);
+      getRepository(User).find({ isOnline: true }).then(async (users2) => {
+        isRecacheRunning = true;
+        // we need to recache only users not recached in 30 minutes
+        for (const user of users2) {
+          if (!recacheIds.has(user.userId) || (Date.now() - (recacheIds.get(user.userId) ?? 0) > 30 * MINUTE)) {
+            debug('permissions.recache', `Recaching ${user.username}#${user.userId}`);
+            cleanViewersCache(user.userId);
+            await this.getUserHighestPermission(user.userId);
+            await setImmediatePromise();
+            recacheIds.set(user.userId, Date.now());
+          } else {
+            debug('permissions.recache', `Recaching SKIPPED ${user.username}#${user.userId}`);
+          }
+        }
+        isRecacheRunning = false;
+        rechacheFinishedAt = Date.now();
+      });
+    }
+
+    // remove all recacheIds when time is more than HOUR
+    for (const userId of Array.from(recacheIds.keys())) {
+      if (Date.now() - (recacheIds.get(userId) ?? 0) > HOUR) {
+        recacheIds.delete(userId);
       }
-    });
+    }
   }
 
   public async getUserHighestPermission(userId: number): Promise<null | string> {
