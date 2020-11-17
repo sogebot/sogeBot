aa266eb8ad8dc6dac578fd204852ec7bf31fc882
soge__ <sogehige@gmail.com>
2020-11-17 23:55:16 +0100

    ....

--- src/bot/database/migration/mysql/1605640986450-hltbIdFix.ts
+++ src/bot/database/migration/mysql/1605640986450-hltbIdFix.ts
@@ -4,7 +4,7 @@ export class hltbIdFix1605640986450 implements MigrationInterface {
   name = 'hltbIdFix1605640986450';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('ALTER TABLE `how_long_to_beat_game_item` CHANGE `hltb_id` `hltb_id` varchar(36) NOT NULL');
+    await queryRunner.query('ALTER TABLE `how_long_to_beat_game_item` CHANGE `hltb_id` `hltb_id` varchar(255) NOT NULL');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
