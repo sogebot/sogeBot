567697c1a82a70d31f8a35b821ceb86b171558b2
soge__ <sogehige@gmail.com>
2020-11-17 22:00:59 +0100

    update

--- src/bot/database/migration/postgres/1605640986450-hltbIdFix.ts
+++ src/bot/database/migration/postgres/1605640986450-hltbIdFix.ts
@@ -5,13 +5,13 @@ export class hltbIdFix1605640986450 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP INDEX "IDX_hltb_id"`);
-    await queryRunner.query(`ALTER TABLE "how_long_to_beat_game_item" ALTER COLUMN "hltb_id" uuid NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "how_long_to_beat_game_item" ALTER COLUMN "hltb_id" TYPE uuid NOT NULL`);
     await queryRunner.query(`CREATE INDEX "IDX_hltb_id" ON "how_long_to_beat_game_item" ("hltb_id") `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP INDEX "IDX_hltb_id"`);
-    await queryRunner.query(`ALTER TABLE "how_long_to_beat_game_item" ALTER COLUMN "hltb_id" integer NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "how_long_to_beat_game_item" ALTER COLUMN "hltb_id" TYPE integer NOT NULL`);
     await queryRunner.query(`CREATE INDEX "IDX_hltb_id" ON "how_long_to_beat_game_item" ("hltb_id") `);
   }
 
