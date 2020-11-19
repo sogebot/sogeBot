652aaf3f719dc878e8a69a06d0f4f1bff9081a87
Michal Orlik <sogehige@gmail.com>
2020-11-19 16:06:52 +0100

    update

--- src/bot/database/migration/postgres/1605794954571-changeFilterToString.ts
+++ src/bot/database/migration/postgres/1605794954571-changeFilterToString.ts
@@ -4,10 +4,10 @@ export class changeFilterToString1605794954571 implements MigrationInterface {
   name = 'changeFilterToString1605794954571';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "permission_filters" ALTER COLUMN "value" TYPE character varying NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "permission_filters" ALTER COLUMN "value" TYPE character varying`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "permission_filters" ALTER COLUMN "value" TYPE bigint NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "permission_filters" ALTER COLUMN "value" TYPE bigint`);
   }
 }
