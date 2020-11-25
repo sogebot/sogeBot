2771dc56e3249c3a99dc6a44b39096001305a585
soge__ <sogehige@gmail.com>
2020-11-25 23:42:07 +0100

    refactor(tools): add min semver check for node
    Fixes #4373

--- tools/migration.js
+++ tools/migration.js
@@ -11,16 +11,31 @@ if (!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir);
 }
 
-const logFile = fs.createWriteStream('./logs/migration.log', { flags: 'w' });
-console.log('... Migration in progress, please wait (see logs/migration.log for progress and error) ...');
+const logFile = './logs/migration.log';
+try {
+  fs.unlinkSync(logFile);
+} catch (e) {
+  // pass
+}
 
 async function runMigration() {
+  if (+process.versions.node.split('.')[0] < 14) {
+    fs.writeFileSync(logFile, 'Sorry, this app requires Node.js 14.x or later', { flag: 'a'});
+    console.error('Sorry, this app requires Node.js 14.x or later');
+    console.error('\n!!! Pre-run check FAILED, please check your logs/migration.log for additional information !!! \n');
+    process.exit(1);
+  } else {
+    console.log('Pre-run check OK.\n');
+    fs.writeFileSync(logFile, 'Pre-run check OK.' + os.EOL + os.EOL, { flag: 'a'});
+  }
+
+  console.log('... Migration in progress, please wait (see logs/migration.log for progress and error) ...');
   exec('typeorm migration:run', (error, stdout, stderr) => {
-    logFile.write(stdout + os.EOL);
-    logFile.write(stderr + os.EOL);
+    fs.writeFileSync(logFile, stdout + os.EOL, { flag: 'a'});
+    fs.writeFileSync(logFile, stderr + os.EOL, { flag: 'a'});
 
     if(error) {
-      logFile.write(error + os.EOL);
+      fs.writeFileSync(logFile, error + os.EOL, { flag: 'a'});
       console.error('\n!!! Migration FAILED, please check your logs/migration.log for additional information !!! \n');
       process.exit(1);
     } else {
