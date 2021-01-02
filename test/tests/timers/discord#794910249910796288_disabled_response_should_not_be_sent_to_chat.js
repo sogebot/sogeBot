f45eb93490ae241701f382dceb6736ab4cbebe53
soge__ <sogehige@gmail.com>
2021-01-02 20:09:19 +0100

    fix(timers): filter out disabled response
    Fixes https://discord.com/channels/317348946144002050/619437014001123338/794910249910796288

---
+++ test/tests/timers/discord#794910249910796288_disabled_response_should_not_be_sent_to_chat.js
@@ -0,0 +1,115 @@
+/* global describe it beforeEach */
+
+
+const assert = require('assert');
+require('../../general.js');
+
+const db = require('../../general.js').db;
+const message = require('../../general.js').message;
+
+const timers = (require('../../../dest/systems/timers')).default;
+const api = (require('../../../dest/api')).default;
+
+const { getRepository } = require('typeorm');
+const { Timer, TimerResponse } = require('../../../dest/database/entity/timer');
+
+const { linesParsed } = require('../../../dest/helpers/parser');
+
+describe('Timers - disabled response should not be sent to chat - https://discord.com/channels/317348946144002050/619437014001123338/794910249910796288', () => {
+  before(async () => {
+    await db.cleanup();
+    await message.prepare();
+
+    await getRepository(Timer).save({
+      name: 'test',
+      triggerEveryMessage: 0,
+      triggerEverySecond: 1,
+      isEnabled: true,
+      triggeredAtTimestamp: Date.now(),
+      triggeredAtMessage: linesParsed,
+      messages: [
+        { isEnabled: true, response: '1' },
+        { isEnabled: false, response: '2' },
+        { isEnabled: true, response: '3' }
+      ]
+    });
+
+    api.isStreamOnline = true;
+  });
+  after(async () => {
+    api.isStreamOnline = false;
+  });
+
+  it('We should have response 1 in chat in a while', async () => {
+    await message.isSentRaw('1', 'bot', 45000);
+    // we need to wait little more as interval when offline is 30s
+  }).timeout(60000);
+
+  it('We should NOT have response 2 in chat in a while', async () => {
+    await message.isNotSentRaw('2', 'bot', 5000);
+  })
+
+  it('We should have response 3 in chat in a while', async () => {
+    await message.isSentRaw('3', 'bot', 5000);
+  })
+});
+
+describe.only('Timers - disabled responses should not be sent to chat - https://discord.com/channels/317348946144002050/619437014001123338/794910249910796288', () => {
+  let timer;
+  before(async () => {
+    await db.cleanup();
+    await message.prepare();
+
+    timer = await getRepository(Timer).save({
+      name: 'test',
+      triggerEveryMessage: 0,
+      triggerEverySecond: 1,
+      isEnabled: true,
+      triggeredAtTimestamp: Date.now(),
+      triggeredAtMessage: linesParsed,
+      messages: [
+        { isEnabled: false, response: '1' },
+        { isEnabled: false, response: '2' },
+        { isEnabled: false, response: '3' }
+      ]
+    });
+
+    api.isStreamOnline = true;
+  });
+  after(async () => {
+    api.isStreamOnline = false;
+  });
+
+  it('Timer should be updated in DB => checked', async () => {
+    const time = Date.now();
+    const checkTimer = new Promise((resolve) => {
+      const check = async () => {
+        const updatedTimer = await getRepository(Timer).findOne(timer.id);
+        if (timer.triggeredAtTimestamp < updatedTimer.triggeredAtTimestamp) {
+          resolve(true);
+        } else {
+          if (Date.now() - time > 60000) {
+            resolve(false);
+          } else {
+            setTimeout(() => check(), 500)
+          }
+        }
+      }
+    });
+
+    assert(checkTimer, 'Timer was not updated in 60s');
+  }).timeout(65000)
+
+  it('We should NOT have response 1 in chat in a while', async () => {
+    await message.isNotSentRaw('1', 'bot', 2000);
+    // we need to wait little more as interval when offline is 45s (15s should be OK in general)
+  })
+
+  it('We should NOT have response 2 in chat in a while', async () => {
+    await message.isNotSentRaw('2', 'bot', 2000);
+  })
+
+  it('We should NOT have response 3 in chat in a while', async () => {
+    await message.isNotSentRaw('3', 'bot', 2000);
+  })
+});
