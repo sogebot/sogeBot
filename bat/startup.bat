840ae3cc79acaf39118599ea0f6139d5a049bb1c
liadala <62029150+liadala@users.noreply.github.com>
2021-01-02 14:14:58 +0100

    Backup on start for Windows
    backup before start only 1 a day

--- bat/startup.bat
+++ bat/startup.bat
@@ -1,3 +1,6 @@
 @echo off
 cd ..
+if not exist "backup" mkdir "backup"
+if not exist ".\backup\.env_%DATE%" copy ".env" ".\backup\.env_%DATE%"
+if not exist ".\backup\sogebot_%DATE%.db" copy "sogebot.db" ".\backup\sogebot_%DATE%.db"
 npm start
