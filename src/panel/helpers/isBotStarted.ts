00af3b04ad71c0d60433f132c9cdcbd869088bca
Michal Orlik <sogehige@gmail.com>
2021-01-08 13:34:08 +0100

    fix(ui): clearer bot startup message

--- src/panel/helpers/isBotStarted.ts
+++ src/panel/helpers/isBotStarted.ts
@@ -9,11 +9,15 @@ function isBotStarted() {
       socket.emit('botStatus', (status: boolean) => {
         const el = document.getElementById('bot-starting-intro');
         if (status) {
+          if (el) {
+            el.innerHTML = '... registering sockets ...';
+          }
           console.log('Bot is started, continue');
           setTimeout(() => resolve(true), waitAfterStart ? 5000 : 0);
         } else {
           if (el) {
             el.style.display = 'block';
+            el.innerHTML = '... bot is starting ...';
           }
           console.log('Bot not started yet, waiting');
           waitAfterStart = true;
