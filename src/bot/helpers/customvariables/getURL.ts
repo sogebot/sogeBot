a8ac395cafb57c0943decfeb0e70c0530fd7e5c5
Michal Orlik <sogehige@gmail.com>
2021-01-22 15:45:16 +0100

    catch skip

--- src/bot/helpers/customvariables/getURL.ts
+++ src/bot/helpers/customvariables/getURL.ts
@@ -20,8 +20,7 @@ async function getURL(req: any, res: any) {
     } else {
       return res.status(404).send({ error: 'Variable not found', code: 404 });
     }
-  /* istanbul ignore next */
-  } catch (e) {
+  } catch (e) /* istanbul ignore next */ {
     res.status(500).send({ error: 'Internal Server Error', code: 500 });
     throw e;
   }
