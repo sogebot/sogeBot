ac42bffa7486e5c76014cbba7a975b2ac9d68fae
Michal Orlik <sogehige@gmail.com>
2021-01-29 11:10:50 +0100

    fix

--- tools/migrationCheck.js
+++ tools/migrationCheck.js
@@ -8,12 +8,12 @@ async function test() {
   await new Promise((resolve) => {
     exec('npx typeorm migration:run', {
       env: {
-        'TYPEORM_ENTITIES': 'dest/database/entity/*.js',
+        'TYPEORM_ENTITIES':   'dest/database/entity/*.js',
         'TYPEORM_MIGRATIONS': `dest/database/migration/${getMigrationType(process.env.TYPEORM_CONNECTION)}/**/*.js`,
       },        shell: true,
 
     }, (error, stdout, stderr) => {
-      console.log({error, stdout, stderr});
+      console.log({ error, stdout, stderr });
       if (stdout) {
         process.stdout.write(stdout);
       }
@@ -26,22 +26,22 @@ async function test() {
   await new Promise(async (resolve) => {
     exec('npx typeorm migration:generate -n generatedMigration', {
       env: {
-        'TYPEORM_ENTITIES': 'dest/database/entity/*.js',
+        'TYPEORM_ENTITIES':   'dest/database/entity/*.js',
         'TYPEORM_MIGRATIONS': `dest/database/migration/${getMigrationType(process.env.TYPEORM_CONNECTION)}/**/*.js`,
       },        shell: true,
 
     }, (error, stdout, stderr) => {
-      console.log({error, stdout, stderr});
+      console.log({ error, stdout, stderr });
       output += stdout;
       resolve();
     });
   });
   if (output !== expectedOutput) {
     await new Promise((resolve2) => {
-      const cat = exec('cat ./*generatedMigration*', {
+      exec('cat ./*generatedMigration*', {
         shell: true,
       }, (error, stdout, stderr) => {
-        console.log({error, stdout, stderr});
+        console.log({ error, stdout, stderr });
         console.log('\n =================================== generated migration file  =================================== \n');
         console.log(stdout);
         resolve2();
