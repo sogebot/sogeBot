d28312f7c3f11e651bdd7013f7481d1334575b0b
Michal Orlik <sogehige@gmail.com>
2021-01-29 11:44:02 +0100

    asd

--- tools/migrationCheck.js
+++ tools/migrationCheck.js
@@ -11,9 +11,7 @@ fs.appendFileSync('.env', `\ndest/database/entity/*.js`);
 
 async function test() {
   await new Promise((resolve) => {
-    exec('npx typeorm migration:run', {
-      shell: true,
-    }, (error, stdout, stderr) => {
+    exec('npx typeorm migration:run', (error, stdout, stderr) => {
       process.stdout.write(stdout);
       resolve();
     });
@@ -22,22 +20,15 @@ async function test() {
   let output = '';
   const expectedOutput = 'No changes in database schema were found - cannot generate a migration. To create a new empty migration use "typeorm migration:create" command\n';
   await new Promise(async (resolve) => {
-    exec('npx typeorm migration:generate -n generatedMigration', {
-      env: {
-        'TYPEORM_ENTITIES':   'dest/database/entity/*.js',
-        'TYPEORM_MIGRATIONS': `dest/database/migration/${getMigrationType(envConfig.TYPEORM_CONNECTION)}/**/*.js`,
-      },
-      shell: true,
-    }, (error, stdout, stderr) => {
+    exec('npx typeorm migration:generate -n generatedMigration', (error, stdout, stderr) => {
       output += stdout;
       resolve();
     });
   });
+  console.log(output);
   if (output !== expectedOutput) {
     await new Promise((resolve2) => {
-      exec('cat ./*generatedMigration*', {
-        shell: true,
-      }, (error, stdout, stderr) => {
+      exec('cat ./*generatedMigration*', (error, stdout, stderr) => {
         console.log('\n =================================== generated migration file  =================================== \n');
         console.log(stdout);
         resolve2();
