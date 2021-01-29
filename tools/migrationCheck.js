d52addc3b959c220af45d11ec3c7063d918a1b4a
soge__ <sogehige@gmail.com>
2021-01-29 03:27:16 +0100

    .........

--- tools/migrationCheck.js
+++ tools/migrationCheck.js
@@ -12,6 +12,7 @@ async function test() {
         'TYPEORM_MIGRATIONS': `dest/database/migration/${getMigrationType(process.env.TYPEORM_CONNECTION)}/**/*.js`,
       },
     }, (error, stdout, stderr) => {
+      console.log({error, stdout, stderr});
       if (stdout) {
         process.stdout.write(stdout);
       }
@@ -28,6 +29,7 @@ async function test() {
         'TYPEORM_MIGRATIONS': `dest/database/migration/${getMigrationType(process.env.TYPEORM_CONNECTION)}/**/*.js`,
       },
     }, (error, stdout, stderr) => {
+      console.log({error, stdout, stderr});
       output += stdout;
       resolve();
     });
@@ -36,12 +38,10 @@ async function test() {
     await new Promise((resolve2) => {
       const cat = exec('cat ./*test*', {
         shell: true,
-      });
-      console.log('\n =================================== generated migration file  =================================== \n');
-      cat.stdout.on('data', (data) => {
-        process.stdout.write(data.toString());
-      });
-      cat.on('close', () => {
+      }, (error, stdout, stderr) => {
+        console.log({error, stdout, stderr});
+        console.log('\n =================================== generated migration file  =================================== \n');
+        console.log(stdout);
         resolve2();
       });
     });
