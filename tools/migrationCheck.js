cef3cb15c91e2dc4b06468d4ccf9b8fa88dd9212
Michal Orlik <sogehige@gmail.com>
2021-02-05 16:55:49 +0100

    refactor: update migrationCheck env

--- tools/migrationCheck.js
+++ tools/migrationCheck.js
@@ -5,13 +5,15 @@ const dotenv = require('dotenv');
 
 const getMigrationType = require('../dest/helpers/getMigrationType').getMigrationType;
 
-const envConfig = dotenv.parse(fs.readFileSync('.env'));
-fs.appendFileSync('.env', `\nTYPEORM_MIGRATIONS=dest/database/migration/${getMigrationType(envConfig.TYPEORM_CONNECTION)}/**/*.js`);
-fs.appendFileSync('.env', `\nTYPEORM_ENTITIES=dest/database/entity/*.js`);
-
 async function test() {
   await new Promise((resolve) => {
-    exec('npx typeorm migration:run', (error, stdout, stderr) => {
+    exec('npx typeorm migration:run', {
+      env: {
+        ...process.env,
+        'TYPEORM_ENTITIES':   'dest/database/entity/*.js',
+        'TYPEORM_MIGRATIONS': `dest/database/migration/${getMigrationType(process.env.TYPEORM_CONNECTION)}/**/*.js`,
+      },
+    }, (error, stdout, stderr) => {
       process.stdout.write(stdout);
       resolve();
     });
@@ -20,7 +22,13 @@ async function test() {
   let output = '';
   const expectedOutput = 'No changes in database schema were found - cannot generate a migration. To create a new empty migration use "typeorm migration:create" command\n';
   await new Promise(async (resolve) => {
-    exec('npx typeorm migration:generate -n generatedMigration', (error, stdout, stderr) => {
+    exec('npx typeorm migration:generate -n generatedMigration', {
+      env: {
+        ...process.env,
+        'TYPEORM_ENTITIES':   'dest/database/entity/*.js',
+        'TYPEORM_MIGRATIONS': `dest/database/migration/${getMigrationType(process.env.TYPEORM_CONNECTION)}/**/*.js`,
+      },
+    }, (error, stdout, stderr) => {
       output += stdout;
       resolve();
     });
