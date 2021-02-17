1e940fe40fd395bc09b43ae41ab3e7ad15be9baa
Michal Orlik <sogehige@gmail.com>
2021-02-17 10:57:04 +0100

    migration update

---
+++ src/bot/database/migration/mysql/12.7.x/1613554160927-twitchStatsRemoveHosts.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class twitchStatsRemoveHosts1613554160927 implements MigrationInterface {
+  name = 'twitchStatsRemoveHosts1613554160927';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE `twitch_stats` DROP COLUMN `currentHosts`');
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE `twitch_stats` ADD `currentHosts` int NOT NULL DEFAULT \'0\'');
+  }
+
+}
