35ad37650a7ea3197b9f9f9c0a991a6330ff4ff7
soge__ <sogehige@gmail.com>
2021-02-28 13:01:46 +0100

    update

--- src/bot/database/migration/mysql/12.9.x/1614510825911-userIdToString.ts
+++ src/bot/database/migration/mysql/12.9.x/1614510825911-userIdToString.ts
@@ -12,9 +12,14 @@ export class userIdToString1614510825911 implements MigrationInterface {
     await queryRunner.query(`ALTER TABLE \`moderation_permit\` MODIFY \`userId\` varchar(255)`);
     await queryRunner.query(`ALTER TABLE \`points_changelog\` MODIFY \`userId\` varchar(255)`);
     await queryRunner.query(`ALTER TABLE \`quotes\` MODIFY \`quotedBy\` varchar(255)`);
+
+    await queryRunner.query('ALTER TABLE `user_bit` DROP FOREIGN KEY `FK_cca96526faa532e7d20a0f775b0`', undefined);
+    await queryRunner.query('ALTER TABLE `user_tip` DROP FOREIGN KEY `FK_36683fb221201263b38344a9880`', undefined);
     await queryRunner.query(`ALTER TABLE \`user_bit\` MODIFY \`userUserId\` varchar(255)`);
     await queryRunner.query(`ALTER TABLE \`user_tip\` MODIFY \`userUserId\` varchar(255)`);
     await queryRunner.query(`ALTER TABLE \`user\` MODIFY \`userUserId\` varchar(255)`);
+    await queryRunner.query('ALTER TABLE `user_tip` ADD CONSTRAINT `FK_36683fb221201263b38344a9880` FOREIGN KEY (`userUserId`) REFERENCES `user`(`userId`) ON DELETE CASCADE ON UPDATE CASCADE', undefined);
+    await queryRunner.query('ALTER TABLE `user_bit` ADD CONSTRAINT `FK_cca96526faa532e7d20a0f775b0` FOREIGN KEY (`userUserId`) REFERENCES `user`(`userId`) ON DELETE CASCADE ON UPDATE CASCADE', undefined);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
@@ -27,8 +32,13 @@ export class userIdToString1614510825911 implements MigrationInterface {
     await queryRunner.query(`ALTER TABLE \`moderation_permit\` MODIFY \`userId\` int`);
     await queryRunner.query(`ALTER TABLE \`points_changelog\` MODIFY \`userId\` int`);
     await queryRunner.query(`ALTER TABLE \`quotes\` MODIFY \`quotedBy\` int`);
+
+    await queryRunner.query('ALTER TABLE `user_bit` DROP FOREIGN KEY `FK_cca96526faa532e7d20a0f775b0`', undefined);
+    await queryRunner.query('ALTER TABLE `user_tip` DROP FOREIGN KEY `FK_36683fb221201263b38344a9880`', undefined);
     await queryRunner.query(`ALTER TABLE \`user_bit\` MODIFY \`userUserId\` int`);
     await queryRunner.query(`ALTER TABLE \`user_tip\` MODIFY \`userUserId\` int`);
     await queryRunner.query(`ALTER TABLE \`user\` MODIFY \`userUserId\` int`);
+    await queryRunner.query('ALTER TABLE `user_tip` ADD CONSTRAINT `FK_36683fb221201263b38344a9880` FOREIGN KEY (`userUserId`) REFERENCES `user`(`userId`) ON DELETE CASCADE ON UPDATE CASCADE', undefined);
+    await queryRunner.query('ALTER TABLE `user_bit` ADD CONSTRAINT `FK_cca96526faa532e7d20a0f775b0` FOREIGN KEY (`userUserId`) REFERENCES `user`(`userId`) ON DELETE CASCADE ON UPDATE CASCADE', undefined);
   }
 }
