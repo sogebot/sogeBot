834e18df8aba7d4a9531489f253c338f34ef37c0
soge__ <sogehige@gmail.com>
2021-02-28 12:35:03 +0100

    update

--- src/bot/database/migration/mysql/12.9.x/1614510825911-userIdToString.ts
+++ src/bot/database/migration/mysql/12.9.x/1614510825911-userIdToString.ts
@@ -1,34 +1,34 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner } from 'rm';
 
 export class userIdToString1614510825911 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE \`bets_participations\` ALTER COLUMN \`userId\` TYPE varchar(255)`);
-    await queryRunner.query(`ALTER TABLE \`cooldown_viewer\` ALTER COLUMN \`userId\` TYPE varchar(255)`);
-    await queryRunner.query(`ALTER TABLE \`discord_link\` ALTER COLUMN \`userId\` TYPE varchar(255)`);
-    await queryRunner.query(`ALTER TABLE \`duel\` ALTER COLUMN \`id\` TYPE varchar(255)`);
-    await queryRunner.query(`ALTER TABLE \`heist_user\` ALTER COLUMN \`id\` TYPE varchar(255)`);
-    await queryRunner.query(`ALTER TABLE \`moderation_warning\` ALTER COLUMN \`userId\` TYPE varchar(255)`);
-    await queryRunner.query(`ALTER TABLE \`moderation_permit\` ALTER COLUMN \`userId\` TYPE varchar(255)`);
-    await queryRunner.query(`ALTER TABLE \`points_changelog\` ALTER COLUMN \`userId\` TYPE varchar(255)`);
-    await queryRunner.query(`ALTER TABLE \`quotes\` ALTER COLUMN \`quotedBy\` TYPE varchar(255)`);
-    await queryRunner.query(`ALTER TABLE \`user_bit\` ALTER COLUMN \`userUserId\` TYPE varchar(255)`);
-    await queryRunner.query(`ALTER TABLE \`user_tip\` ALTER COLUMN \`userUserId\` TYPE varchar(255)`);
-    await queryRunner.query(`ALTER TABLE \`user\` ALTER COLUMN \`userUserId\` TYPE varchar(255)`);
+    await queryRunner.query(`ALTER TABLE \`bets_participations\` MODIFY \`userId\` varchar(255)`);
+    await queryRunner.query(`ALTER TABLE \`cooldown_viewer\` MODIFY \`userId\` varchar(255)`);
+    await queryRunner.query(`ALTER TABLE \`discord_link\` MODIFY \`userId\` varchar(255)`);
+    await queryRunner.query(`ALTER TABLE \`duel\` MODIFY \`id\` varchar(255)`);
+    await queryRunner.query(`ALTER TABLE \`heist_user\` MODIFY \`id\` varchar(255)`);
+    await queryRunner.query(`ALTER TABLE \`moderation_warning\` MODIFY \`userId\` varchar(255)`);
+    await queryRunner.query(`ALTER TABLE \`moderation_permit\` MODIFY \`userId\` varchar(255)`);
+    await queryRunner.query(`ALTER TABLE \`points_changelog\` MODIFY \`userId\` varchar(255)`);
+    await queryRunner.query(`ALTER TABLE \`quotes\` MODIFY \`quotedBy\` varchar(255)`);
+    await queryRunner.query(`ALTER TABLE \`user_bit\` MODIFY \`userUserId\` varchar(255)`);
+    await queryRunner.query(`ALTER TABLE \`user_tip\` MODIFY \`userUserId\` varchar(255)`);
+    await queryRunner.query(`ALTER TABLE \`user\` MODIFY \`userUserId\` varchar(255)`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE \`bets_participations\` ALTER COLUMN \`userId\` TYPE int`);
-    await queryRunner.query(`ALTER TABLE \`cooldown_viewer\` ALTER COLUMN \`userId\` TYPE int`);
-    await queryRunner.query(`ALTER TABLE \`discord_link\` ALTER COLUMN \`userId\` TYPE int`);
-    await queryRunner.query(`ALTER TABLE \`duel\` ALTER COLUMN \`id\` TYPE int`);
-    await queryRunner.query(`ALTER TABLE \`heist_user\` ALTER COLUMN \`id\` TYPE int`);
-    await queryRunner.query(`ALTER TABLE \`moderation_warning\` ALTER COLUMN \`userId\` TYPE int`);
-    await queryRunner.query(`ALTER TABLE \`moderation_permit\` ALTER COLUMN \`userId\` TYPE int`);
-    await queryRunner.query(`ALTER TABLE \`points_changelog\` ALTER COLUMN \`userId\` TYPE int`);
-    await queryRunner.query(`ALTER TABLE \`quotes\` ALTER COLUMN \`quotedBy\` TYPE int`);
-    await queryRunner.query(`ALTER TABLE \`user_bit\` ALTER COLUMN \`userUserId\` TYPE int`);
-    await queryRunner.query(`ALTER TABLE \`user_tip\` ALTER COLUMN \`userUserId\` TYPE int`);
-    await queryRunner.query(`ALTER TABLE \`user\` ALTER COLUMN \`userUserId\` TYPE int`);
+    await queryRunner.query(`ALTER TABLE \`bets_participations\` MODIFY \`userId\` int`);
+    await queryRunner.query(`ALTER TABLE \`cooldown_viewer\` MODIFY \`userId\` int`);
+    await queryRunner.query(`ALTER TABLE \`discord_link\` MODIFY \`userId\` int`);
+    await queryRunner.query(`ALTER TABLE \`duel\` MODIFY \`id\` int`);
+    await queryRunner.query(`ALTER TABLE \`heist_user\` MODIFY \`id\` int`);
+    await queryRunner.query(`ALTER TABLE \`moderation_warning\` MODIFY \`userId\` int`);
+    await queryRunner.query(`ALTER TABLE \`moderation_permit\` MODIFY \`userId\` int`);
+    await queryRunner.query(`ALTER TABLE \`points_changelog\` MODIFY \`userId\` int`);
+    await queryRunner.query(`ALTER TABLE \`quotes\` MODIFY \`quotedBy\` int`);
+    await queryRunner.query(`ALTER TABLE \`user_bit\` MODIFY \`userUserId\` int`);
+    await queryRunner.query(`ALTER TABLE \`user_tip\` MODIFY \`userUserId\` int`);
+    await queryRunner.query(`ALTER TABLE \`user\` MODIFY \`userUserId\` int`);
   }
 }
