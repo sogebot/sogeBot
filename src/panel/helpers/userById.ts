dda8a486be9582c8b247acdcdf55a1ec3c387d72
soge__ <sogehige@gmail.com>
2021-02-28 17:52:08 +0100

    update

--- src/panel/helpers/userById.ts
+++ src/panel/helpers/userById.ts
@@ -3,7 +3,7 @@ import { getSocket } from './socket';
 const cache: { [id: string]: string } = {};
 const socket = getSocket('/core/users');
 
-export const getUsernameById = async function (id: number) {
+export const getUsernameById = async function (id: string) {
   if (typeof cache[id] === 'undefined') {
     const username = await new Promise<string | null>((resolve, reject) => {
       socket.emit('getNameById', id, (err: string | null, value: string | null) => {
