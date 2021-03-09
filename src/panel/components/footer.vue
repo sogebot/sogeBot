9f017a3aad3a4b3e1b41e2048c42abbfb2b88502
sogehige <sogehige@gmail.com>
2021-03-09 12:20:45 +0100

    fix(parser): remove response time (#4663)
    Fixes #4657

--- src/panel/components/footer.vue
+++ src/panel/components/footer.vue
@@ -1,20 +1,45 @@
 <template>
-  <footer class="footer" @dblclick="setDebug()">
-    <span class="alert" :class="[className(data.API)]" style="padding:0;" :title="'API ' + title(data.API)">API</span>
-    <span class="alert" :class="[className(data.TMI)]" style="padding:0;" :title="'TMI ' + title(data.TMI)">TMI</span>
-    <span class="alert" :class="[classNameMod(data.SOC)]" style="padding:0;" :title="'SOC ' + (data.SOC ? 'connected' : 'disconnected')">SOC</span>
-    <span class="alert" :class="[classNameMod(data.MOD)]" style="padding:0;" :title="'Bot is ' + (data.MOD ? ' ': 'not ') + 'a MOD.'">MOD</span>
-    <span class="alert" :class="[classNameResponse(data.RES)]" style="padding:0;" :title="'Average bot response ' + data.RES + 'ms'">{{data.RES}}ms</span>
+  <footer
+    class="footer"
+    @dblclick="setDebug()"
+  >
+    <span
+      class="alert"
+      :class="[className(data.API)]"
+      style="padding:0;"
+      :title="'API ' + title(data.API)"
+    >API</span>
+    <span
+      class="alert"
+      :class="[className(data.TMI)]"
+      style="padding:0;"
+      :title="'TMI ' + title(data.TMI)"
+    >TMI</span>
+    <span
+      class="alert"
+      :class="[classNameMod(data.SOC)]"
+      style="padding:0;"
+      :title="'SOC ' + (data.SOC ? 'connected' : 'disconnected')"
+    >SOC</span>
+    <span
+      class="alert"
+      :class="[classNameMod(data.MOD)]"
+      style="padding:0;"
+      :title="'Bot is ' + (data.MOD ? ' ': 'not ') + 'a MOD.'"
+    >MOD</span>
     <a href="https://github.com/sogehige/SogeBot">GitHub</a> |
     <a href="https://github.com/sogehige/SogeBot/issues">Issues</a> |
     <a href="https://github.com/sogehige/SogeBot/blob/master/LICENSE">GPL-3.0 License</a> |
-    <span class="alert" style="padding:0;">{{version}}</span>
+    <span
+      class="alert"
+      style="padding:0;"
+    >{{ version }}</span>
   </footer>
 </template>
 
 <script lang="ts">
 import {
-  defineComponent, onMounted, reactive, ref, 
+  defineComponent, onMounted, reactive, ref,
 } from '@vue/composition-api';
 
 import { getSocket } from '../helpers/socket';
@@ -23,16 +48,6 @@ const socket = getSocket('/');
 
 function classNameMod(is: boolean) {
   return is ? 'alert-success' : 'alert-danger';
-}
-
-function classNameResponse(ms: number) {
-  if (ms >= 10000) {
-    return 'alert-danger';
-  } else if (ms < 10000 && ms >= 6000) {
-    return 'alert-warning';
-  } else {
-    return 'alert-success';
-  }
 }
 
 function className (status: 0 | 1 | 2 | 3) {
@@ -76,13 +91,11 @@ export default defineComponent({
     const data: {
       SOC: boolean;
       MOD: boolean;
-      RES: number;
       API: 0 | 1 | 2 | 3;
       TMI: 0 | 1 | 2 | 3;
     } = reactive({
       SOC: false,
       MOD: false,
-      RES: 0,
       API: 0,
       TMI: 0,
     });
@@ -97,7 +110,6 @@ export default defineComponent({
       }) => {
         data.SOC = true;
         data.MOD = dataFromSocket.MOD;
-        data.RES = dataFromSocket.RES;
         data.API = dataFromSocket.API;
         data.TMI = dataFromSocket.TMI;
         setTimeout(() => refresh(), 1000);
@@ -110,7 +122,7 @@ export default defineComponent({
     });
 
     return {
-      version, data, classNameMod, classNameResponse, className, title, setDebug, 
+      version, data, classNameMod, className, title, setDebug,
     };
   },
 });
