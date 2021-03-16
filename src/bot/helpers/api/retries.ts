879d85fa63a08915130d35d2c63eadf4fd71f3bd
sogehige <sogehige@gmail.com>
2021-03-16 13:19:46 +0100

    fix(api): use getChannelInformation on online stream (#4671)
    Before this change we rely on getCurrentStreamData, but
    this caused several issues, namely with getting title outside of a bot and
    trigger proper updates
    
    Fixes https://discord.com/channels/317348946144002050/619437014001123338/821164843406852167

--- src/bot/helpers/api/retries.ts
+++ src/bot/helpers/api/retries.ts
@@ -5,21 +5,14 @@ function setCurrentRetries(value: typeof curRetries) {
   curRetries = value;
 }
 
-let getCurrentStreamData = 0;
 let getChannelInformation = 0;
 let getChannelSubscribers = 0;
 const retries = {
-  set getCurrentStreamData (value: typeof getCurrentStreamData) {
-    getCurrentStreamData = value;
-  },
   set getChannelInformation (value: typeof getChannelInformation) {
     getChannelInformation = value;
   },
   set getChannelSubscribers (value: typeof getChannelSubscribers) {
     getChannelSubscribers = value;
-  },
-  get getCurrentStreamData() {
-    return getCurrentStreamData;
   },
   get getChannelInformation() {
     return getChannelInformation;
@@ -30,5 +23,5 @@ const retries = {
 };
 
 export {
-  maxRetries, curRetries, setCurrentRetries, retries, 
+  maxRetries, curRetries, setCurrentRetries, retries,
 };
