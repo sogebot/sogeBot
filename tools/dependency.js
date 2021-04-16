fcc4caf2cf0ffd2d83d4fcd240473fdbac5b4c8f
Michal Orlik <sogehige@gmail.com>
2021-04-16 16:33:44 +0200

    update

--- tools/dependency.js
+++ tools/dependency.js
@@ -10,6 +10,8 @@ fs.copyFileSync('./package.json', './package.jsonbackup')
 
 const ignored = ['better-sqlite3']
 const unused = [];
+
+console.log(' ==== Production dependencies')
 for (const [dependency, version] of Object.entries(package.dependencies)) {
   if (dependency.startsWith('@sogebot') || ignored.includes(dependency)) {
     console.log('Ignoring ' + dependency)
@@ -19,6 +21,32 @@ for (const [dependency, version] of Object.entries(package.dependencies)) {
 
     // removing package
     packageToCheck.dependencies = Object.fromEntries(Object.entries(packageToCheck.dependencies).filter(o => o[0] !== dependency));
+    fs.writeFileSync('./package.json', JSON.stringify(packageToCheck, null, 2));
+
+    if (fs.existsSync('./package-lock.json')) {
+      fs.unlinkSync('./package-lock.json')
+    }
+
+    try {
+      spawnSync("make dependencies bot", { shell: true });
+      unused.push(dependency)
+      process.stdout.write(' !!!! NOT USED or provided by other package \n\n')
+    } catch(e) {
+      process.stdout.write(' USED\n\n')
+    }
+  }
+}
+
+console.log(' ==== Development dependencies')
+for (const [dependency, version] of Object.entries(package.devDependencies)) {
+  if (dependency.startsWith('@sogebot') || ignored.includes(dependency)) {
+    console.log('Ignoring ' + dependency)
+  } else {
+    process.stdout.write('Checking ' + dependency);
+    const packageToCheck = _.cloneDeep(package);
+
+    // removing package
+    packageToCheck.devDependencies = Object.fromEntries(Object.entries(packageToCheck.devDependencies).filter(o => o[0] !== dependency));
     fs.writeFileSync('./package.json', JSON.stringify(packageToCheck, null, 2));
 
     if (fs.existsSync('./package-lock.json')) {
