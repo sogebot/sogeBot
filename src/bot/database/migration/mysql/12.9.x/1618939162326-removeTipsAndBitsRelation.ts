b5316868c2cdf1e682c20ce2408d764a27fa6d75
soge__ <sogehige@gmail.com>
2021-04-20 20:26:46 +0200

    update

--- src/bot/database/migration/mysql/12.9.x/1618939162326-removeTipsAndBitsRelation.ts
+++ src/bot/database/migration/mysql/12.9.x/1618939162326-removeTipsAndBitsRelation.ts
@@ -8,6 +8,8 @@ export class removeTipsAndBitsRelation1618939162326 implements MigrationInterfac
     await queryRunner.query('ALTER TABLE `user_bit` DROP FOREIGN KEY `FK_cca96526faa532e7d20a0f775b0`');
     await queryRunner.query('DROP INDEX `FK_36683fb221201263b38344a9880` ON `user_tip`');
     await queryRunner.query('DROP INDEX `FK_cca96526faa532e7d20a0f775b0` ON `user_bit`');
+    await queryRunner.query('ALTER TABLE `user_tip` CHANGE `userUserId` `userId` varchar(255) NULL');
+    await queryRunner.query('ALTER TABLE `user_bit` CHANGE `userUserId` `userId` varchar(255) NULL');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
@@ -17,6 +19,8 @@ export class removeTipsAndBitsRelation1618939162326 implements MigrationInterfac
     await queryRunner.query('ALTER TABLE `user_tip` ADD CONSTRAINT `FK_36683fb221201263b38344a9880` FOREIGN KEY (`userUserId`) REFERENCES `user`(`userId`) ON DELETE CASCADE ON UPDATE CASCADE');
     await queryRunner.query('CREATE INDEX `FK_cca96526faa532e7d20a0f775b0` ON `user_bit` (`userId`)');
     await queryRunner.query('CREATE INDEX `FK_36683fb221201263b38344a9880` ON `user_tip` (`userId`)');
+    await queryRunner.query('ALTER TABLE `user_bit` CHANGE `userId` `userUserId` varchar(255) NULL');
+    await queryRunner.query('ALTER TABLE `user_tip` CHANGE `userId` `userUserId` varchar(255) NULL');
   }
 
 }
