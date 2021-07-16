729568f4d13bac122b2eaf9d2610f680ecee42fb
soge__ <sogehige@gmail.com>
2021-07-16 13:28:31 +0200

    build(docker): add npm update to prestart

--- docker.sh
+++ docker.sh
@@ -4,8 +4,8 @@ cd /app
 
 if [ -z "$PROFILER" ]
 then
-  npm update @sogebot/ui-admin @sogebot/ui-overlay @sogebot/ui-helpers @sogebot/ui-oauth @sogebot/ui-public && npm start
+  npm start
 else
   echo 'Starting bot with DEBUG flag, inspect exposed at 0.0.0.0:9229'
-  npm update @sogebot/ui-admin @sogebot/ui-overlay @sogebot/ui-helpers @sogebot/ui-oauth @sogebot/ui-public && npm run debug
+  npm run debug
 fi
