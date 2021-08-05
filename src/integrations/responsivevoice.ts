f69348b628c3ff76657b0c5bebd6504d3f1a862b
liadala <62029150+liadala@users.noreply.github.com>
2021-08-05 12:01:42 +0200

    fix(responsivevoice) fix toogle button
    fix #4756

--- src/integrations/responsivevoice.ts
+++ src/integrations/responsivevoice.ts
@@ -3,7 +3,7 @@ import { settings } from '../decorators';
 import Integration from './_interface';
 
 class ResponsiveVoice extends Integration {
-  _enabled = null; // cannot be enabled / disabled
+  //_enabled = null; // cannot be enabled / disabled
 
   @settings('api')
   key = '';
