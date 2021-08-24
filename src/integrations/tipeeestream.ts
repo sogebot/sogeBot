ee4b36c57f95271941a4f3857f4e7110575c2f34
Michal Orlik <sogehige@gmail.com>
2021-08-24 13:17:20 +0200

    fix(tipeeestream): use API polling and remove legacy socket.io
    Fixes #4782

--- src/integrations/tipeeestream.ts
+++ src/integrations/tipeeestream.ts
@@ -1,149 +1,127 @@
+import * as constants from '@sogebot/ui-helpers/constants';
 import axios from 'axios';
-import chalk from 'chalk';
-import io from 'socket.io-client-legacy';
 import { getRepository } from 'typeorm';
 
 import currency from '../currency';
 import { UserTip, UserTipInterface } from '../database/entity/user';
-import { settings } from '../decorators';
-import { onChange, onStartup } from '../decorators/on.js';
+import { persistent, settings } from '../decorators';
+import { onStartup } from '../decorators/on.js';
 import { isStreamOnline } from '../helpers/api/index.js';
 import { stats } from '../helpers/api/stats.js';
 import { mainCurrency } from '../helpers/currency';
 import { eventEmitter } from '../helpers/events';
 import { triggerInterfaceOnTip } from '../helpers/interface/triggers.js';
-import {
-  error, info, tip,
-} from '../helpers/log.js';
+import { error, tip } from '../helpers/log.js';
 import eventlist from '../overlays/eventlist.js';
 import alerts from '../registries/alerts.js';
 import users from '../users.js';
 import Integration from './_interface';
 
 type TipeeestreamEvent = {
-  appKey: string;
-  event: {
-    type: 'donation'; // is there more?
-    user: {
-      avatar: string;
-      country: string;
-      username: string;
-      providers: {
-        connectedAt: string; code: 'twitch'; id: string; username: string;
-      }[];
-      created_at: string;
-      session_at: string;
-    };
-    ref: string;
-    inserted_at: string;
-    deleted_at: string;
-    created_at: string;
-    parameters: {
-      formattedMessage: string;
-      message: string;
-      username: string;
-      currency: string;
-      amount: number;
-      resub: number;
-      viewers: number;
-    };
-    formattedAmount: string;
-    'parameters.amount': number;
+  message: string,
+  datas: {
+    items: [
+      {
+        id: number,
+        type: 'donation',
+        user: {
+          locked: boolean,
+          hash_id: string,
+          hash: string,
+          avatar: {
+            id: number,
+            is_converted: boolean
+          },
+          hasPayment: string,
+          currency: {
+            code: currency,
+            symbol: string,
+            label: string,
+            available: boolean
+          },
+          country: string,
+          campaignActivation: number,
+          id: number,
+          providers: {
+            connectedAt: string,
+            code: 'twitch' | 'youtube',
+            id: number,
+            username: string,
+            master: boolean,
+            token: string,
+            followers: number,
+            last_follow_update: string,
+            created_at: string,
+            channel: string,
+          }[]
+          ,
+          username: string,
+          pseudo: string,
+          email_confirmation_at: string,
+          created_at: string,
+          session_at: string,
+          parameters: []
+        },
+        ref: string,
+        created_at: string,
+        inserted_at: string,
+        display: boolean,
+        parameters: {
+          amount: number,
+          campaignId: number,
+          currency: currency,
+          fees: number,
+          identifier: string,
+          formattedMessage: string,
+          message?: string,
+          username: string
+        },
+        formattedAmount: string,
+        'parameters.amount': number
+      }
+    ],
+    total_count: number
   }
 };
 
 class TipeeeStream extends Integration {
-  socketToTipeeestream: any | null = null;
-
-  @settings()
-  username = '';
+  @persistent()
+  afterDate = 0;
 
   @settings()
   apiKey = '';
 
   @onStartup()
-  @onChange('enabled')
-  onStateChange (key: string, val: boolean) {
-    if (val) {
-      this.connect();
-    } else {
-      this.disconnect();
-    }
-  }
-
-  async disconnect () {
-    if (this.socketToTipeeestream !== null) {
-      this.socketToTipeeestream.removeAllListeners();
-      this.socketToTipeeestream.disconnect();
-      this.socketToTipeeestream = null;
-    }
-  }
-
-  @onChange('apiKey')
-  async connect () {
-    try {
-      this.disconnect();
-
-      if (this.username.trim() === '' || this.apiKey.trim() === '' || !this.enabled) {
+  interval() {
+    setInterval(async () => {
+      if (this.apiKey.length === 0 || !this.enabled) {
         return;
       }
+      try {
+        const beforeDate = Date.now();
+        const request = await axios.get<TipeeestreamEvent>(`https://api.tipeeestream.com/v1.0/events.json?apiKey=${this.apiKey}&type[]=donation&limit=100000&end=${(new Date(beforeDate)).toISOString()}&start=${(new Date(this.afterDate)).toISOString()}`);
+        if (request.data.message !== 'success') {
+          throw new Error(request.data.message);
+        }
 
-      // get current avaliable host and port
-      // destructured response from https://api.tipeeestream.com/v2.0/site/socket
-      // example response: { "code": 200, "message": "success", "datas": { "port": "443", "host": "https://sso-cf.tipeeestream.com" } }
-      const { data: { datas: { host, port } } } = await axios.get('https://api.tipeeestream.com/v2.0/site/socket');
+        for (const item of request.data.datas.items) {
+          this.parse(item);
+        }
 
-      this.socketToTipeeestream = io(`${host}:${port}`,
-        {
-          reconnection:         true,
-          reconnectionDelay:    1000,
-          reconnectionDelayMax: 5000,
-          reconnectionAttempts: Infinity,
-          query:                { access_token: this.apiKey.trim() },
-        });
-
-      if (this.socketToTipeeestream !== null) {
-        this.socketToTipeeestream.on('connect_error', (e: Error) => {
-          error(chalk.red('TIPEEESTREAM.COM:') + ' error while connecting, ' + e);
-          this.connect(); // rerun all connect process
-        });
-        this.socketToTipeeestream.on('connect', () => {
-          if (this.socketToTipeeestream !== null) {
-            this.socketToTipeeestream.emit('join-room', { room: this.apiKey.trim(), username: this.username.trim() });
-          }
-          info(chalk.yellow('TIPEEESTREAM.COM:') + ' Successfully connected socket to service');
-        });
-        this.socketToTipeeestream.on('reconnect_attempt', () => {
-          info(chalk.yellow('TIPEEESTREAM.COM:') + ' Trying to reconnect to service');
-        });
-        this.socketToTipeeestream.on('disconnect', () => {
-          if (this.enabled) {
-            info(chalk.yellow('TIPEEESTREAM.COM:') + ' Trying to reconnect to service');
-            this.connect();
-          } else {
-            info(chalk.yellow('TIPEEESTREAM.COM:') + ' Socket disconnected from service');
-            this.disconnect();
-            this.socketToTipeeestream = null;
-          }
-        });
-
-        this.socketToTipeeestream.on('new-event', async (data: TipeeestreamEvent) => {
-          this.parse(data);
-        });
+        this.afterDate = beforeDate;
+      } catch (e) {
+        error(e.stack);
       }
-    } catch (e) {
-      error(e);
-    }
+
+    }, constants.MINUTE);
   }
 
-  async parse(data: TipeeestreamEvent) {
-    if (data.event.type !== 'donation') {
-      return;
-    }
+  async parse(data: TipeeestreamEvent['datas']['items'][number]) {
     try {
-      const { amount, message } = data.event.parameters;
-      const username = data.event.parameters.username.toLowerCase();
-      const donationCurrency = data.event.parameters.currency as currency;
+      const amount = data.parameters.amount;
+      const message = data.parameters.message ?? '';
+      const username = data.parameters.username.toLowerCase();
+      const donationCurrency = data.parameters.currency;
 
       if (isStreamOnline.value) {
         stats.value.currentTips = stats.value.currentTips + Number(currency.exchange(amount, donationCurrency, mainCurrency.value));
@@ -214,7 +192,7 @@ class TipeeeStream extends Integration {
           });
         });
     } catch (e) {
-      error(`TIPEESTREAM: Error in parsing event: ${JSON.stringify(data.event)})`);
+      error(`TIPEESTREAM: Error in parsing event: ${JSON.stringify(data)})`);
       error(e);
     }
   }
