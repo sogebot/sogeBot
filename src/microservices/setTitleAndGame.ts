17aa47e2df4678f04b36392d7c72353551d5782b
soge__ <sogehige@gmail.com>
2021-09-06 22:08:08 +0200

    fix(microservices): add missing request data to setTitleAndGame

--- src/microservices/setTitleAndGame.ts
+++ src/microservices/setTitleAndGame.ts
@@ -101,7 +101,7 @@ async function setTitleAndGame (args: { title?: string | null; game?: string | n
     if (e.isAxiosError) {
       error(`API: ${e.config.method.toUpperCase()} ${e.config.url} - ${e.response.status ?? 0}\n${JSON.stringify(e.response?.data ?? '--nodata--', null, 4)}\n\n${e.stack}`);
       ioServer?.emit('api.stats', {
-        method: e.config.method.toUpperCase(), timestamp: Date.now(), call: 'setTitleAndGame', api: 'helix', endpoint: e.config.url, code: e.response.status, data: e.response?.data ?? 'n/a', remaining: calls.bot,
+        method: e.config.method.toUpperCase(), request: requestData, timestamp: Date.now(), call: 'setTitleAndGame', api: 'helix', endpoint: e.config.url, code: e.response.status, data: e.response?.data ?? 'n/a', remaining: calls.bot,
       });
     } else {
       error(e.stack);
