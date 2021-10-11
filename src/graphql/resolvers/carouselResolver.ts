38558417ee7f25625a73aea683faeac20333d023
Michal Orlik <sogehige@gmail.com>
2021-10-11 15:14:56 +0200

    carousel done

--- src/graphql/resolvers/carouselResolver.ts
+++ src/graphql/resolvers/carouselResolver.ts
@@ -1,6 +1,6 @@
-import { Carousel } from '@entity/carousel';
+import { Carousel, CarouselInterface } from '@entity/carousel';
 import {
-  Arg, Query, Resolver,
+  Arg, Authorized, Mutation, Query, Resolver,
 } from 'type-graphql';
 import { getRepository } from 'typeorm';
 
@@ -30,5 +30,49 @@ export class CarouselResolver {
         order: { order: 'ASC' },
       });
     }
+  }
+
+  @Authorized()
+  @Mutation(returns => String)
+  async carouselUpload(
+  @Arg('data') data_json: string,
+  ) {
+    const { base64, type } = JSON.parse(data_json);
+    const order = await getRepository(Carousel).count();
+    const item = await getRepository(Carousel).save({
+      type,
+      base64,
+      // timers in ms
+      waitBefore:            0,
+      waitAfter:             0,
+      duration:              5000,
+      // animation
+      animationInDuration:   1000,
+      animationIn:           'fadeIn',
+      animationOutDuration:  1000,
+      animationOut:          'fadeOut',
+      // order
+      order,
+      // showOnlyOncePerStream
+      showOnlyOncePerStream: false,
+    });
+    return item.id;
+  }
+
+  @Authorized()
+  @Mutation(returns => CarouselObject)
+  async carouselSave(@Arg('data') data_json: string) {
+    const item: CarouselInterface = JSON.parse(data_json);
+    return getRepository(Carousel).save(item);
+  }
+
+  @Authorized()
+  @Mutation(returns => Boolean)
+  async carouselRemove(@Arg('id') id: string) {
+    const item = await getRepository(Carousel).findOne({ id });
+    if (item) {
+      await getRepository(Carousel).remove(item);
+    }
+    return true;
   }
 }
