22a205f60037dc06a29a217e3bc8e7b50eacec62
soge__ <sogehige@gmail.com>
2021-10-30 00:49:01 +0200

    chore: add missing show millis migratin

---
+++ src/database/migration/sqlite/14.0.x/1635453652530-addMissingShowMilliseconds.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class addMissingShowMilliseconds1635453652530 implements MigrationInterface {
+  name = 'addMissingShowMilliseconds1635453652530';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const items = await queryRunner.query('SELECT * FROM overlay_mapper', undefined);
+    for (const item of items) {
+      if(['stopwatch', 'countdown', 'maraton'].includes(item.value)) {
+        const opts: Record<string, any> = JSON.parse(item.opts);
+        if (typeof opts.showMilliseconds === 'undefined') {
+          opts.showMilliseconds = false;
+        }
+        const keys = Object.keys(item);
+        item.opts = JSON.stringify(opts);
+        await queryRunner.query('DELETE FROM "overlay_mapper" WHERE id=?', [item.id]);
+        await queryRunner.query(
+          `INSERT INTO "overlay_mapper"(${keys.map(o => `"${o}"`).join(', ')}) values (${keys.map(o => `?`).join(', ')})`,
+          [keys.map(key => item[key])],
+        );
+
+      }
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return;
+  }
+
+}
