a6fcc4896521475924927205cd8c881b4aefd328
Michal Orlik <sogehige@gmail.com>
2021-11-11 12:19:09 +0100

    chore(getCustomReward): use broadcaster to fetch rewards

--- src/services/twitch/calls/getCustomRewards.ts
+++ src/services/twitch/calls/getCustomRewards.ts
@@ -6,8 +6,8 @@ import { variables } from '~/watchers';
 export const getCustomRewards = async () => {
   try {
     const channelId = variables.get('services.twitch.channelId') as string;
-    const clientBot = await client('bot');
-    return await clientBot.channelPoints.getCustomRewards(channelId);
+    const clientBroadcaster = await client('broadcaster');
+    return await clientBroadcaster.channelPoints.getCustomRewards(channelId);
   } catch (e: unknown) {
     if (e instanceof Error) {
       error(e.stack ?? e.message);
