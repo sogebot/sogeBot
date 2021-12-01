9b4f880667f84cdfc5d994bd92385740ec48d8f9
soge__ <sogehige@gmail.com>
2021-12-01 19:01:10 +0100

    update migration

--- src/database/migration/postgres/14.0.x/1638362272853-responsiveVoiceSetup.ts
+++ src/database/migration/postgres/14.0.x/1638362272853-responsiveVoiceSetup.ts
@@ -4,13 +4,13 @@ export class responsiveVoiceSetup1638362272853 implements MigrationInterface {
   name = 'responsiveVoiceSetup1638362272853';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const responsiveVoiceKey = (await queryRunner.query(`SELECT * from \`settings\``)).find((o: any) => {
+    const responsiveVoiceKey = (await queryRunner.query(`SELECT * from "settings"`)).find((o: any) => {
       return o.namespace === '/integrations/responsivevoice';
     });
     if(responsiveVoiceKey) {
-      await queryRunner.query(`DELETE from \`settings\` WHERE "id"=?`, [responsiveVoiceKey.id]);
-      await queryRunner.query(`INSERT INTO \`settings\`("namespace", "name", "value") VALUES(?, ?, ?)`, ['/core/tts', 'responsiveVoiceKey', responsiveVoiceKey.value]);
-      await queryRunner.query(`INSERT INTO \`settings\`("namespace", "name", "value") VALUES(?, ?, ?)`, ['/core/tts', 'service', JSON.stringify(0)]);
+      await queryRunner.query(`DELETE from "settings" WHERE "id"=?`, [responsiveVoiceKey.id]);
+      await queryRunner.query(`INSERT INTO "settings"("namespace", "name", "value") VALUES(?, ?, ?)`, ['/core/tts', 'responsiveVoiceKey', responsiveVoiceKey.value]);
+      await queryRunner.query(`INSERT INTO "settings"("namespace", "name", "value") VALUES(?, ?, ?)`, ['/core/tts', 'service', JSON.stringify(0)]);
     }
   }
 
