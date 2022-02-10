fd3bc587b73f77c2bd3309d5a11e9a6e43ea80d0
Michal Orlik <sogehige@gmail.com>
2022-02-10 17:14:30 +0100

    fix(migration): fix mysql migration using postgres parameters

--- src/database/migration/mysql/14.0.x/1643061611856-moveAlertMediaToGallery.ts
+++ src/database/migration/mysql/14.0.x/1643061611856-moveAlertMediaToGallery.ts
@@ -8,14 +8,14 @@ export class moveAlertMediaToGallery1643061611856 implements MigrationInterface 
     const mapper = new Map<string, string | null>();
     const items = await queryRunner.query(`SELECT \`b64data\` FROM \`alert_media\` GROUP BY \`b64data\``);
     for (const item of items) {
-      const ids = (await queryRunner.query(`SELECT \`id\` FROM \`alert_media\` WHERE \`b64data\`=$1`, [item.b64data])).map((o: any) => o.id);
+      const ids = (await queryRunner.query(`SELECT \`id\` FROM \`alert_media\` WHERE \`b64data\`=?`, [item.b64data])).map((o: any) => o.id);
 
       if (item.b64data.length > 0) {
         try {
           const type = (item.b64data.match(/^data:([0-9A-Za-z-+/]+);base64,(.+)$/))[1];
           // save new file and get new id
           const id = shortid();
-          await queryRunner.query(`INSERT INTO gallery(id, type, data, name, folder) VALUES($1, $2, $3, $4, $5)`,
+          await queryRunner.query(`INSERT INTO gallery(id, type, data, name, folder) VALUES(?, ?, ?, ?, ?)`,
             [id, type, item.b64data, id, '/alerts/migration']);
 
           // save mapper ids
