9146d3ba4c133139f4da13ed0d7c40f7ef418448
Michal Orlik <sogehige@gmail.com>
2022-02-17 12:29:26 +0100

    update

--- src/database/migration/postgres/14.0.x/1645091113387-allowShortId.ts
+++ src/database/migration/postgres/14.0.x/1645091113387-allowShortId.ts
@@ -8,7 +8,6 @@ export class allowShortId1645091113387 implements MigrationInterface {
     await queryRunner.query(`ALTER TABLE "gallery" ALTER COLUMN "id" TYPE character varying`);
     await queryRunner.query(`ALTER TABLE "gallery" ADD CONSTRAINT "PK_65d7a1ef91ddafb3e7071b188a0" PRIMARY KEY ("id")`);
     await queryRunner.query(`ALTER TABLE "gallery" ALTER COLUMN "id" DROP DEFAULT`);
-    await queryRunner.query(`DROP SEQUENCE "gallery_id_seq"`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
