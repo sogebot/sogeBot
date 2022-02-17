5d06ceb190e94a111401711456ad6f8de53e1fe4
Michal Orlik <sogehige@gmail.com>
2022-02-17 12:03:10 +0100

    update

--- src/database/migration/mysql/14.0.x/1645091113387-allowShortId.ts
+++ src/database/migration/mysql/14.0.x/1645091113387-allowShortId.ts
@@ -4,7 +4,7 @@ export class allowShortId1645091113387 implements MigrationInterface {
   name = 'allowShortId1645091113387';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE \`gallery\` MODIFY COLUMN \`id\` varchar(255) NOT NULL PRIMARY KEY`);
+    await queryRunner.query(`ALTER TABLE \`gallery\` MODIFY COLUMN \`id\` varchar(255) NOT NULL`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
--- src/database/migration/postgres/14.0.x/1645091113387-allowShortId.ts
+++ src/database/migration/postgres/14.0.x/1645091113387-allowShortId.ts
@@ -5,7 +5,7 @@ export class allowShortId1645091113387 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "gallery" DROP CONSTRAINT "PK_65d7a1ef91ddafb3e7071b188a0"`);
-    await queryRunner.query(`ALTER TABLE "gallery" ALTER COLUMN "id" character varying NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "gallery" ALTER COLUMN "id" TYPE character varying NOT NULL`);
     await queryRunner.query(`ALTER TABLE "gallery" ADD CONSTRAINT "PK_65d7a1ef91ddafb3e7071b188a0" PRIMARY KEY ("id")`);
   }
 
