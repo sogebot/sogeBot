302d353928b255cf298bda1d27c63b3201cd097b
Michal Orlik <sogehige@gmail.com>
2022-02-17 12:44:24 +0100

    update

--- src/database/migration/mysql/14.0.x/1645019207858-updateOverlayMappers.ts
+++ src/database/migration/mysql/14.0.x/1645019207858-updateOverlayMappers.ts
@@ -4,10 +4,7 @@ export class updateOverlayMappers1645019207858 implements MigrationInterface {
   name = 'updateOverlayMappers1645019207858';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE TABLE \`temporary_overlay_mapper\` (\`id\` varchar PRIMARY KEY NOT NULL, \`value\` varchar, \`opts\` text, \`groupId\` varchar)`);
-    await queryRunner.query(`INSERT INTO \`temporary_overlay_mapper\`(\`id\`, \`value\`, \`opts\`) SELECT \`id\`, \`value\`, \`opts\` FROM \`overlay_mapper\``);
-    await queryRunner.query(`DROP TABLE \`overlay_mapper\``);
-    await queryRunner.query(`ALTER TABLE \`temporary_overlay_mapper\` RENAME TO \`overlay_mapper\``);
+    await queryRunner.query(`ALTER TABLE \`overlay_mapper\` ADD \`groupId\` varchar(255)`);
     await queryRunner.query(`CREATE INDEX \`IDX_overlay_mapper_groupId\` ON \`overlay_mapper\` (\`groupId\`) `);
 
     // remap group
