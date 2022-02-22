0dbecefe40f1be369163016fc9d877dceeb19419
Michal Orlik <sogehige@gmail.com>
2022-02-22 10:23:38 +0100

    asdadas

--- tools/pre-commit-message.js
+++ tools/pre-commit-message.js
@@ -5,7 +5,7 @@ console.log(process.argv);
 const COMMIT_EDITMSG = process.argv[2];
 const commitMessage = fs.readFileSync(COMMIT_EDITMSG).toString();
 
-console.log(`Cleaning up hard space from commit message: "${COMMIT_EDITMSG}"`);
+console.log(`Cleaning up hard space from commit message: "${commitMessage}"`);
 
 fs.writeFileSync(COMMIT_EDITMSG, commitMessage
   .replace(/[  ]/gm, ' '), // eslint-disable-line no-irregular-whitespace
