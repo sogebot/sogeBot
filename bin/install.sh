51d55c2b2acd93512adafbc91d87596dda6fb87a
Marcelo dos Santos Mafra <msmafra@gmail.com>
2022-03-05 15:04:54 -0300

    Added a bit of bash scripting best practices. Mostly recommended by shellcheck

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+declare DIR
+declare npm_exec
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/..
+npm_exec="$(command -v npm | tee /dev/null 2>&1)"
+
+cd "${DIR}" || exit
 
 rm -rf node_modules &
 echo -n 'Purging old dependencies'
@@ -11,7 +16,7 @@ done
 echo '.DONE'
 
 echo -n 'Installing dependencies'
-npm -q -s install > /dev/null 2>&1 &
+"${npm_exec}" -q -s install > /dev/null 2>&1 &
 while kill -0 $! 2> /dev/null; do
     echo -n '.'
     sleep 1
