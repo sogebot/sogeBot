390a50ddbd63faf781287fc13243b4db0d720345
soge__ <sogehige@gmail.com>
2022-03-11 00:35:59 +0100

    fix(migration): allowShortId migration must run before moveAlertMediaToGallery

--- src/database/migration/postgres/14.0.x/1643061611854-allowShortId.ts
+++ src/database/migration/postgres/14.0.x/1643061611854-allowShortId.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class allowShortId1645091113387 implements MigrationInterface {
-  name = 'allowShortId1645091113387';
+export class allowShortId1643061611854 implements MigrationInterface {
+  name = 'allowShortId1643061611854';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "gallery" DROP CONSTRAINT "PK_65d7a1ef91ddafb3e7071b188a0"`);
