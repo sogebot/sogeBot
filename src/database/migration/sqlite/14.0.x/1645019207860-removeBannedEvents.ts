c8994d1307519d5f5498f3ae63bf7548303fdb5e
soge__ <sogehige@gmail.com>
2022-03-20 10:51:30 +0100

    aaa

--- src/database/migration/sqlite/14.0.x/1645019207860-removeBannedEvents.ts
+++ src/database/migration/sqlite/14.0.x/1645019207860-removeBannedEvents.ts
@@ -4,7 +4,7 @@ export class removeBannedEvents1645019207860 implements MigrationInterface {
   name = 'removeBannedEvents1645019207860';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "bannedEvents"`);
+    await queryRunner.query(`DROP TABLE "banned_events"`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
