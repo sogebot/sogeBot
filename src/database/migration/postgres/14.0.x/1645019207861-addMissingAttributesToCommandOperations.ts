03dabbf3f1f0004bb44863db3b400bac2fad934d
Michal Orlik <sogehige@gmail.com>
2022-03-28 12:20:54 +0200

    feat(events): add timeout to run command operation
    Fixes #5079

---
+++ src/database/migration/postgres/14.0.x/1645019207861-addMissingAttributesToCommandOperations.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class addMissingAttributesToCommandOperations1645019207861 implements MigrationInterface {
+  name = 'addMissingAttributesToCommandOperations1645019207861';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const event_operation = await queryRunner.query(`SELECT * from "event_operation" WHERE "name"='run-command'`);
+    for (const event of event_operation) {
+      const definitions = JSON.stringify({
+        ...JSON.parse(event.definitions) as Record<string, any>,
+        timeout:     0,
+        timeoutType: 'normal',
+      });
+      await queryRunner.query(`UPDATE "event_operation" SET "definitions"=$1 WHERE "id"=$2`,
+        [definitions, event.id]);
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return;
+  }
+
+}
---
+++ src/helpers/events/run-command.ts
@@ -0,0 +1,86 @@
+import { debug } from 'console';
+
+import { isNil, isObject } from 'lodash';
+import _ from 'lodash';
+import { v4 } from 'uuid';
+
+import { getOwner, getUserSender } from '../commons';
+
+import { parserReply } from '~/commons';
+import {
+  Events as EventsEntity,
+} from '~/database/entity/event';
+import { parserEmitter } from '~/helpers/parser/index';
+import Message from '~/message';
+import users from '~/users';
+
+type data = {
+  command: string; userName: string, userId: string, timeout: number; isCommandQuiet: boolean,
+};
+
+const commandsToRun = new Map<string, data>();
+
+export async function fireRunCommand(operation: EventsEntity.OperationDefinitions, attributes: EventsEntity.Attributes) {
+  const userName = isNil(attributes.userName) ? getOwner() : attributes.userName;
+  const userId = attributes.userId ? attributes.userId : await users.getIdByName(userName);
+  operation.timeout ??= 0;
+  operation.timeoutType ??= 'normal';
+  operation.isCommandQuiet ??= false;
+
+  let command = String(operation.commandToRun);
+  for (const key of Object.keys(attributes).sort((a, b) => a.length - b.length)) {
+    const val = attributes[key];
+    if (isObject(val) && Object.keys(val).length === 0) {
+      return;
+    } // skip empty object
+    const replace = new RegExp(`\\$${key}`, 'gi');
+    command = command.replace(replace, val);
+  }
+
+  if (operation.timeoutType === 'normal') {
+    debug('events.runCommand', `Adding new command to queue`);
+    // each event command will be triggered
+    commandsToRun.set(v4(), {
+      userName, userId, command, timeout: Date.now() + Number(operation.timeout), isCommandQuiet: operation.isCommandQuiet as boolean,
+    });
+  } else {
+    const originalCommand = commandsToRun.get(attributes.eventId);
+    if (operation.timeoutType === 'add') {
+      debug('events.runCommand', `Adding timeout for ${attributes.eventId}`);
+      const startTime = originalCommand?.timeout ?? Date.now();
+      commandsToRun.set(attributes.eventId, {
+        userName, userId, command, timeout: startTime + Number(operation.timeout), isCommandQuiet: operation.isCommandQuiet as boolean,
+      });
+    } else if (operation.timeoutType === 'reset') {
+      debug('events.runCommand', `Reseting timeout for ${attributes.eventId}`);
+      commandsToRun.set(attributes.eventId, {
+        userName, userId, command, timeout: Date.now() + Number(operation.timeout), isCommandQuiet: operation.isCommandQuiet as boolean,
+      });
+    }
+  }
+}
+
+setInterval(async () => {
+  for (const [eventId, data] of commandsToRun.entries()) {
+    debug('events.runCommand', `Checking ${eventId}, Time to run '${Date.now() - data.timeout}'`);
+    if (Date.now() - data.timeout > 0) {
+      debug('events.runCommand', `Triggering ${eventId}, running '${data.command}'`);
+      commandsToRun.delete(eventId);
+
+      const command = await new Message(data.command).parse({ userName: data.userName, sender: getUserSender(String(data.userId), data.userName), discord: undefined });
+
+      parserEmitter.emit('process', {
+        sender:  { userName: data.userName, userId: String(data.userId) },
+        message: command,
+        skip:    true,
+        quiet:   data.isCommandQuiet,
+      }, (responses) => {
+        for (let i = 0; i < responses.length; i++) {
+          setTimeout(async () => {
+            parserReply(await responses[i].response, { sender: responses[i].sender, discord: responses[i].discord, attr: responses[i].attr, id: '' });
+          }, 500 * i);
+        }
+      });
+    }
+  }
+}, 100);
