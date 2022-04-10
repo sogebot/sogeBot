27c812fdd946c9b5066de6b83daecf2cfa72b9f2
soge__ <sogehige@gmail.com>
2022-04-10 12:38:20 +0200

    update migrations

---
+++ src/database/migration/mysql/14.0.x/1645091113388-googlePrivateKeysText.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class googlePrivateKeysText1645091113388 implements MigrationInterface {
+  name = 'googlePrivateKeysText1645091113388';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE \`google_private_keys\` MODIFY \`privateKey\` longtext NOT NULL`);
+  }
+
+  public async down() {
+    return true;
+  }
+
+}
