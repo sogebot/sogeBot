c9d6494295d0ab4b53ad0afda306a1cf6e3dc8fe
soge__ <sogehige@gmail.com>
2022-04-10 10:53:10 +0200

    fix(mysql): change privateKey column to text

---
+++ src/database/migration/sqlite/14.0.x/1645019207861-googlePrivateKeysText.ts
@@ -0,0 +1,10 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class googlePrivateKeysText1645019207861 implements MigrationInterface {
+  name = 'googlePrivateKeysText1645019207861';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "google_private_keys" MODIFY COLUMN "privateKey" text NOT NULL`);
+  }
+
+}
