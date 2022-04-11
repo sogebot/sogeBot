c47fae3d9c9c13e1229994134ecaa91bdeda6e37
Michal Orlik <sogehige@gmail.com>
2022-04-11 13:39:32 +0200

    fix

--- src/database/migration/postgres/14.0.x/1645091113388-googlePrivateKeysText.ts
+++ src/database/migration/postgres/14.0.x/1645091113388-googlePrivateKeysText.ts
@@ -4,7 +4,7 @@ export class googlePrivateKeysText1645091113388 implements MigrationInterface {
   name = 'googlePrivateKeysText1645091113388';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "google_private_keys" ALTER COLUMN "privateKey" text NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "google_private_keys" ALTER COLUMN "privateKey" SET DATA TYPE text`);
   }
 
   public async down() {
