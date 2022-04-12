bff16ea079b950d7da6e0cf2bcdefc1dc1c9ef58
sogehige <sogehige@gmail.com>
2022-04-12 15:12:31 +0200

    fix(checkFilter): handle $param and $touser edge-case in filters (#5116)
    Fixes https://discord.com/channels/317348946144002050/317349069024395264/963386192089460767

---
+++ test/tests/helpers/discord#963386192089460767.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+
+require('../../general.js');
+const db = require('../../general.js').db;
+const user = require('../../general.js').user;
+
+describe('checkFilter should properly parse $param in stream - https://discord.com/channels/317348946144002050/317349069024395264/963386192089460767 - @func2', () => {
+  before(async () => {
+    await db.cleanup();
+    await user.prepare();
+  });
+
+  it('returns correct value - true filter', async () => {
+    const { checkFilter } = require('../../../dest/helpers/checkFilter');
+    assert(await checkFilter({ parameters: 'test', sender: user.viewer }, '"(stream|$param|link)" === "twitch.tv/test"'));
+    assert(await checkFilter({ parameters: 'test', sender: user.viewer }, '"(stream|$touser|link)" === "twitch.tv/test"'));
+  });
+
+  it('returns correct value - false filter', async () => {
+    const { checkFilter } = require('../../../dest/helpers/checkFilter');
+    assert(!(await checkFilter({ parameters: 'test2', sender: user.viewer }, '"(stream|$param|link)" === "twitch.tv/test"')));
+    assert(!(await checkFilter({ parameters: 'test2', sender: user.viewer }, '"(stream|$touser|link)" === "twitch.tv/test"')));
+  });
+});
