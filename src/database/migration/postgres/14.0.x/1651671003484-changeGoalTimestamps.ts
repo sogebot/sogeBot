f2ec759cc153f92674645a00ea97d1828b089433
soge__ <sogehige@gmail.com>
2022-05-04 17:27:00 +0200

    update migration

---
+++ src/database/migration/postgres/14.0.x/1651671003484-changeGoalTimestamps.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class changeGoalTimestamps1651671003484 implements MigrationInterface {
+  name = 'changeGoalTimestamps1651671003484';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const goalGroup = await queryRunner.query(`SELECT * from "goal_group"`);
+    const goal = await queryRunner.query(`SELECT * from "goal"`);
+
+    await queryRunner.query(`DELETE "goal_group" WHERE 1=1`);
+    await queryRunner.query(`DELETE "group" WHERE 1=1`);
+
+    await queryRunner.query(`ALTER TABLE "goal_group" DROP COLUMN "createdAt"`);
+    await queryRunner.query(`ALTER TABLE "goal_group" ADD "createdAt" character varying NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "goal" DROP COLUMN "timestamp"`);
+    await queryRunner.query(`ALTER TABLE "goal" ADD "timestamp" character varying`);
+    await queryRunner.query(`ALTER TABLE "goal" DROP COLUMN "endAfter"`);
+    await queryRunner.query(`ALTER TABLE "goal" ADD "endAfter" character varying NOT NULL`);
+
+    for (const item of goalGroup) {
+      const keys = Object.keys(item);
+      item.createdAt = new Date(item.createdAt).toISOString();
+      await queryRunner.query(
+        `INSERT INTO "goal_group"(${keys.map(o => `"${o}"`).join(', ')}) values (${keys.map(o => `?`).join(', ')})`,
+        keys.map(key => item[key]),
+      );
+    }
+
+    for (const item of goal) {
+      const keys = Object.keys(item);
+      item.timestamp = new Date(item.timestamp).toISOString();
+      item.endAfter = new Date(item.endAfter).toISOString();
+      await queryRunner.query(
+        `INSERT INTO "goal"(${keys.map(o => `"${o}"`).join(', ')}) values (${keys.map((o, i) => `$${i+1}`).join(', ')})`,
+        keys.map(key => item[key]),
+      );
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return;
+  }
+
+}
