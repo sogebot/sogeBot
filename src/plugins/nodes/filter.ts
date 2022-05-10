336017fec46ace7588252b8c76065564652b11bc
Michal Orlik <sogehige@gmail.com>
2022-05-10 14:56:00 +0200

    add filter advanced mode

--- src/plugins/nodes/filter.ts
+++ src/plugins/nodes/filter.ts
@@ -2,11 +2,27 @@ import { itemsToEvalPart } from '@sogebot/ui-helpers/queryFilter';
 import { VM }  from 'vm2';
 
 import type { Node } from '~/../d.ts/src/plugins';
+import { error } from '~/helpers/log';
 
 export default async function(pluginId: string, currentNode: Node<string>, parameters: Record<string, any>, variables: Record<string, any>, userstate: ChatUser) {
-  const filter = currentNode.data.value ? JSON.parse(currentNode.data.value) : null;
-  if (filter && filter.items.length > 0) {
-    const script = itemsToEvalPart(filter.items, filter.operator);
+  const advancedMode = JSON.parse(currentNode.data.data).advancedMode ?? false;
+
+  let script = null;
+
+  if (advancedMode) {
+    script = currentNode.data.value;
+  } else {
+    const filter = currentNode.data.value
+      ? JSON.parse(currentNode.data.value) : null;
+    if (filter && filter.items.length > 0) {
+      script = itemsToEvalPart(filter.items, filter.operator);
+    }
+  }
+
+  if (!script) {
+    return false;
+  }
+  try {
     const sandbox = {
       sender: {
         userName: userstate.userName,
@@ -18,6 +34,8 @@ export default async function(pluginId: string, currentNode: Node<string>, param
     const vm = new VM({ sandbox });
     const result = vm.run(`(function () {  return ${script} })`)();
     return !!result;
+  } catch (e) {
+    error(`PLUGINS#${pluginId}: ${(e as Error).stack}`);
+    return false;
   }
-  return true;
 }
