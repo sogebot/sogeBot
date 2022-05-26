9ca5745736ecb26c32ac79cc7d91e392ee4a4851
Michal Orlik <sogehige@gmail.com>
2022-05-26 15:57:56 +0200

    update

--- src/database/migration/mysql/14.0.x/1651843397006-changeHLTBTimestamps.ts
+++ src/database/migration/mysql/14.0.x/1651843397006-changeHLTBTimestamps.ts
@@ -1,26 +1,16 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-// import { insertItemIntoTable } from '~/database/insertItemIntoTable';
+import { insertItemIntoTable } from '~/database/insertItemIntoTable';
 
 export class changeHLTBTimestamps1651843397006 implements MigrationInterface {
   name = 'changeHLTBTimestamps1651843397006';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    /*
     const games = await queryRunner.query(`SELECT * from \`how_long_to_beat_game\``);
     const items = await queryRunner.query(`SELECT * from \`how_long_to_beat_game_item\``);
-    */
 
-    /* await queryRunner.query(`DROP INDEX \`IDX_hltb_id\``); */
-    await queryRunner.query(`DROP TABLE \`how_long_to_beat_game\``);
-    await queryRunner.query(`DROP TABLE \`how_long_to_beat_game_item\``);
-
-    /*
-    await queryRunner.query(`CREATE TABLE "how_long_to_beat_game" ("id" varchar PRIMARY KEY NOT NULL, "game" varchar NOT NULL, "startedAt" varchar NOT NULL, "updatedAt" varchar NOT NULL, "gameplayMain" float NOT NULL DEFAULT (0), "gameplayCompletionist" float NOT NULL DEFAULT (0), "gameplayMainExtra" float NOT NULL DEFAULT (0), "offset" bigint NOT NULL DEFAULT (0))`);
-    await queryRunner.query(`CREATE TABLE "how_long_to_beat_game_item" ("id" varchar PRIMARY KEY NOT NULL, "hltb_id" varchar NOT NULL, "createdAt" varchar NOT NULL, "timestamp" bigint NOT NULL DEFAULT (0), "offset" bigint NOT NULL DEFAULT (0), "isMainCounted" boolean NOT NULL DEFAULT (0), "isExtraCounted" boolean NOT NULL DEFAULT (0), "isCompletionistCounted" boolean NOT NULL DEFAULT (0))`);
-
-    await queryRunner.query(`CREATE INDEX "IDX_hltb_id" ON "how_long_to_beat_game_item" ("hltb_id") `);
-    await queryRunner.query(`CREATE UNIQUE INDEX "IDX_301758e0e3108fc902d5436527" ON "how_long_to_beat_game" ("game") `);
+    await queryRunner.query(`CREATE TABLE \`how_long_to_beat_game\` (\`id\` varchar(36) NOT NULL, \`game\` varchar(255) NOT NULL, \`startedAt\` varchar(255) NOT NULL, \`updatedAt\` varchar(255) NOT NULL, \`offset\` bigint NOT NULL DEFAULT '0', \`gameplayMain\` float(12) NOT NULL DEFAULT '0', \`gameplayMainExtra\` float(12) NOT NULL DEFAULT '0', \`gameplayCompletionist\` float(12) NOT NULL DEFAULT '0', UNIQUE INDEX \`IDX_301758e0e3108fc902d5436527\` (\`game\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+    await queryRunner.query(`CREATE TABLE \`how_long_to_beat_game_item\` (\`id\` varchar(36) NOT NULL, \`hltb_id\` varchar(255) NOT NULL, \`createdAt\` varchar(255) NOT NULL, \`timestamp\` bigint NOT NULL DEFAULT '0', \`offset\` bigint NOT NULL DEFAULT '0', \`isMainCounted\` tinyint NOT NULL DEFAULT 0, \`isCompletionistCounted\` tinyint NOT NULL DEFAULT 0, \`isExtraCounted\` tinyint NOT NULL DEFAULT 0, INDEX \`IDX_hltb_id\` (\`hltb_id\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
 
     for (const item of games) {
       delete item.imageUrl;
@@ -35,7 +25,6 @@ export class changeHLTBTimestamps1651843397006 implements MigrationInterface {
       item.createdAt = new Date(item.createdAt).toISOString();
       await insertItemIntoTable('how_long_to_beat_game_item', item, queryRunner);
     }
-    */
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
