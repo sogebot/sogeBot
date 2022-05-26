71cf22e29ad914567634c595966491eea3222f56
Michal Orlik <sogehige@gmail.com>
2022-05-26 16:03:50 +0200

    fix

--- src/database/migration/mysql/14.0.x/1651843397006-changeHLTBTimestamps.ts
+++ src/database/migration/mysql/14.0.x/1651843397006-changeHLTBTimestamps.ts
@@ -9,6 +9,9 @@ export class changeHLTBTimestamps1651843397006 implements MigrationInterface {
     const games = await queryRunner.query(`SELECT * from \`how_long_to_beat_game\``);
     const items = await queryRunner.query(`SELECT * from \`how_long_to_beat_game_item\``);
 
+    await queryRunner.query(`DROP TABLE \`how_long_to_beat_game\``);
+    await queryRunner.query(`DROP TABLE \`how_long_to_beat_game_item\``);
+
     await queryRunner.query(`CREATE TABLE \`how_long_to_beat_game\` (\`id\` varchar(36) NOT NULL, \`game\` varchar(255) NOT NULL, \`startedAt\` varchar(255) NOT NULL, \`updatedAt\` varchar(255) NOT NULL, \`offset\` bigint NOT NULL DEFAULT '0', \`gameplayMain\` float(12) NOT NULL DEFAULT '0', \`gameplayMainExtra\` float(12) NOT NULL DEFAULT '0', \`gameplayCompletionist\` float(12) NOT NULL DEFAULT '0', UNIQUE INDEX \`IDX_301758e0e3108fc902d5436527\` (\`game\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
     await queryRunner.query(`CREATE TABLE \`how_long_to_beat_game_item\` (\`id\` varchar(36) NOT NULL, \`hltb_id\` varchar(255) NOT NULL, \`createdAt\` varchar(255) NOT NULL, \`timestamp\` bigint NOT NULL DEFAULT '0', \`offset\` bigint NOT NULL DEFAULT '0', \`isMainCounted\` tinyint NOT NULL DEFAULT 0, \`isCompletionistCounted\` tinyint NOT NULL DEFAULT 0, \`isExtraCounted\` tinyint NOT NULL DEFAULT 0, INDEX \`IDX_hltb_id\` (\`hltb_id\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
 
