41a28f2f3a2fa9b8991736dee8f34acb29fdf9e9
soge__ <sogehige@gmail.com>
2022-06-10 23:48:06 +0200

    chore: fix migrations format

--- src/database/migration/sqlite/15.0.x/1651843397007-mergeHLTBCreatedAt.ts
+++ src/database/migration/sqlite/15.0.x/1651843397007-mergeHLTBCreatedAt.ts
@@ -6,7 +6,7 @@ export class mergeHLTBCreatedAt1651843397007 implements MigrationInterface {
   name = 'mergeHLTBCreatedAt1651843397007';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const items = await queryRunner.query(`SELECT * from \`how_long_to_beat_game_item\``);
+    const items = await queryRunner.query(`SELECT * from "how_long_to_beat_game_item"`);
     const map = new Map();
 
     for(const item of items) {
@@ -17,7 +17,7 @@ export class mergeHLTBCreatedAt1651843397007 implements MigrationInterface {
       map.set(item.createdAt, item);
     }
 
-    await queryRunner.query(`DELETE FROM \`how_long_to_beat_game_item\` WHERE 1=1`);
+    await queryRunner.query(`DELETE FROM "how_long_to_beat_game_item" WHERE 1=1`);
     for(const item of map.values()) {
       await insertItemIntoTable('how_long_to_beat_game_item', item, queryRunner);
     }
