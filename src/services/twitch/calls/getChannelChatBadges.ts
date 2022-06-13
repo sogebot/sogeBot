090f1975c5d585d36146c815b1e05b1da8d63613
soge__ <sogehige@gmail.com>
2022-06-13 22:01:06 +0200

    fix(api): fix incorrect refresh of token type

--- src/services/twitch/calls/getChannelChatBadges.ts
+++ src/services/twitch/calls/getChannelChatBadges.ts
@@ -22,7 +22,7 @@ export async function getChannelChatBadges() {
     if (e instanceof Error) {
       if (e.message.includes('Invalid OAuth token')) {
         warning(`${getFunctionName()} => Invalid OAuth token - attempting to refresh token`);
-        await refresh('bot');
+        await refresh('broadcaster');
       } else {
         error(`${getFunctionName()} => ${e.stack ?? e.message}`);
       }
--- src/services/twitch/calls/getModerators.ts
+++ src/services/twitch/calls/getModerators.ts
@@ -38,7 +38,7 @@ export async function getModerators(opts: { isWarned: boolean }) {
     if (e instanceof Error) {
       if (e.message.includes('Invalid OAuth token')) {
         warning(`${getFunctionName()} => Invalid OAuth token - attempting to refresh token`);
-        await refresh('bot');
+        await refresh('broadcaster');
       } else {
         error(`${getFunctionName()} => ${e.stack ?? e.message}`);
       }
