693dd04bb55e15be586544447c1d0687c5bf0236
sogehige <sogehige@gmail.com>
2022-06-14 15:41:10 +0200

    feat(plugins): add botStarted listener (#5258)

--- src/plugins/nodes/twitchSendMessage.ts
+++ src/plugins/nodes/twitchSendMessage.ts
@@ -1,10 +1,11 @@
 import { template } from '../template';
 
 import type { Node } from '~/../d.ts/src/plugins';
+import { getBotSender } from '~/helpers/commons';
 import { sendMessage } from '~/helpers/commons/sendMessage';
 
 export default async function(pluginId: string, currentNode: Node, parameters: Record<string, any>, variablesArg: Record<string, any>, userstate: { userName: string; userId: string; } | null) {
   const message = await template(currentNode.data.value, { parameters, ...variablesArg }, userstate);
-  sendMessage(message, userstate, { parameters, ...variablesArg });
+  sendMessage(message, userstate || getBotSender(), { parameters, ...variablesArg });
   return true;
 }
