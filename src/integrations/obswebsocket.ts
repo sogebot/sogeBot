8b5d52fb56a16cdf1ce993a58ecba7f12d758698
soge__ <sogehige@gmail.com>
2022-08-06 00:27:12 +0200

    chore: remove protocol

--- src/integrations/obswebsocket.ts
+++ src/integrations/obswebsocket.ts
@@ -38,7 +38,7 @@ class OBSWebsocket extends Integration {
   @ui({ type: 'selector', values: ['direct', 'overlay'] })
     accessBy: 'direct' | 'overlay' = 'overlay';
   @settings('connection')
-    address = 'wss://localhost:4455';
+    address = 'localhost:4455';
   @settings('connection')
     password = '';
 
