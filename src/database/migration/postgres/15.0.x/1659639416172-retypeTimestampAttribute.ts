14271ba46071a0f3dcfb5bc1193fe4f49115f116
Michal Orlik <sogehige@gmail.com>
2022-08-08 16:44:27 +0200

    fix

--- src/database/migration/postgres/15.0.x/1659639416172-retypeTimestampAttribute.ts
+++ src/database/migration/postgres/15.0.x/1659639416172-retypeTimestampAttribute.ts
@@ -8,16 +8,9 @@ export class retypeTimestampAttribute1659639416172 implements MigrationInterface
   public async up(queryRunner: QueryRunner): Promise<void> {
     const items = await queryRunner.query(`SELECT * from "cooldown"`);
     const items2 = await queryRunner.query(`SELECT * from "cooldown_viewer"`);
-
-    await queryRunner.query(`ALTER TABLE "cooldown_viewer" DROP CONSTRAINT "FK_5ba6ccf5a51426111e322c80445"`);
-    await queryRunner.query(`ALTER TABLE "cooldown" DROP COLUMN "timestamp"`);
-    await queryRunner.query(`ALTER TABLE "cooldown" ADD "timestamp" character varying(30) NOT NULL`);
-    await queryRunner.query(`ALTER TABLE "cooldown_viewer" DROP COLUMN "timestamp"`);
-    await queryRunner.query(`ALTER TABLE "cooldown_viewer" ADD "timestamp" character varying(30) NOT NULL`);
-    await queryRunner.query(`ALTER TABLE "cooldown_viewer" DROP COLUMN "cooldownId"`);
-    await queryRunner.query(`ALTER TABLE "cooldown_viewer" ADD "cooldownId" character varying`);
-    await queryRunner.query(`ALTER TABLE "cooldown_viewer" ADD CONSTRAINT "FK_5ba6ccf5a51426111e322c80445" FOREIGN KEY ("cooldownId") REFERENCES "cooldown"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-
+    await queryRunner.query(`DROP TABLE "cooldown"`);
+    await queryRunner.query(`DROP TABLE "cooldown_viewer"`);
+    /*
     for (const item of items) {
       await insertItemIntoTable('cooldown', {
         ...item,
@@ -30,7 +23,7 @@ export class retypeTimestampAttribute1659639416172 implements MigrationInterface
         ...item,
         timestamp: new Date(item.timestamp).toISOString(),
       }, queryRunner);
-    }
+    }*/
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
