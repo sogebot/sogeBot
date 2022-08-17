e45d3e8d51e40896d4936408da27a0744299ba5e
Michal Orlik <sogehige@gmail.com>
2022-08-17 12:51:03 +0200

    update migration

---
+++ src/database/migration/mysql/16.0.x/1659639416174-updateCommands.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class updateCommands1659639416174 implements MigrationInterface {
+  name = 'updateCommands1659639416174';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE \`commands_board\``);
+    await queryRunner.query(`DROP TABLE \`commands_count\``);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return;
+  }
+
+}
