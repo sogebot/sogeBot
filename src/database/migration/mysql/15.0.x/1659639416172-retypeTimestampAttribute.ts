966ba3031f1f393f6335a9b9e9aea42baa0d51a8
soge__ <sogehige@gmail.com>
2022-08-27 13:39:27 +0200

    fix(migration): fix legacy migration timestamp retype

--- src/database/migration/mysql/15.0.x/1659639416172-retypeTimestampAttribute.ts
+++ src/database/migration/mysql/15.0.x/1659639416172-retypeTimestampAttribute.ts
@@ -8,8 +8,8 @@ export class retypeTimestampAttribute1659639416172 implements MigrationInterface
   public async up(queryRunner: QueryRunner): Promise<void> {
     const items = await queryRunner.query(`SELECT * from \`cooldown\``);
     await queryRunner.query(`DELETE from \`cooldown\` WHERE 1=1`);
+    await queryRunner.query(`DROP TABLE \`cooldown_viewer\``);
 
-    await queryRunner.query(`ALTER TABLE \`cooldown_viewer\` DROP FOREIGN KEY \`FK_5ba6ccf5a51426111e322c80445\``);
     await queryRunner.query(`ALTER TABLE \`cooldown\` DROP COLUMN \`timestamp\``);
     await queryRunner.query(`ALTER TABLE \`cooldown\` ADD \`timestamp\` varchar(30) NOT NULL`);
 
