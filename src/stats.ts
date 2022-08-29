35b475ea78bb06e61fd14b4e51a2686edfa31026
Michal Orlik <sogehige@gmail.com>
2022-08-29 14:43:29 +0200

    fix(stats): remove cache on average stats

--- src/stats.ts
+++ src/stats.ts
@@ -3,7 +3,7 @@
 import { error } from 'console';
 
 import { DAY, MINUTE } from '@sogebot/ui-helpers/constants';
-import { cloneDeep, isNil } from 'lodash';
+import { isNil } from 'lodash';
 import { getRepository, LessThan } from 'typeorm';
 
 import Core from '~/_interface';
@@ -13,17 +13,6 @@ import { onStreamStart } from '~/decorators/on';
 import { stats } from '~/helpers/api';
 import { debug } from '~/helpers/log';
 import { adminEndpoint } from '~/helpers/socket';
-
-let validStatsUntil = Date.now();
-let cachedStats = {
-  currentViewers: 0,
-  currentBits:    0,
-  currentTips:    0,
-  chatMessages:   0,
-  maxViewers:     0,
-  newChatters:    0,
-  currentWatched: 0,
-};
 
 class Stats extends Core {
   @persistent()
@@ -46,58 +35,49 @@ class Stats extends Core {
   sockets() {
     adminEndpoint('/', 'getLatestStats', async function (cb) {
       try {
-        if (validStatsUntil <= Date.now()) {
-          validStatsUntil = Date.now() + DAY;
+        // cleanup
+        getRepository(TwitchStats).delete({ 'whenOnline': LessThan(Date.now() - (DAY * 31)) });
 
-          // cleanup
-          getRepository(TwitchStats).delete({ 'whenOnline': LessThan(Date.now() - (DAY * 31)) });
-
-          const statsFromDb = await getRepository(TwitchStats)
-            .createQueryBuilder('stats')
-            .offset(1)
-            .limit(Number.MAX_SAFE_INTEGER)
-            .where('stats.whenOnline > :whenOnline', { whenOnline: Date.now() - (DAY * 31) })
-            .orderBy('stats.whenOnline', 'DESC')
-            .getMany();
-          const statsToReturn = {
-            currentViewers:     0,
-            currentSubscribers: 0,
-            currentBits:        0,
-            currentTips:        0,
-            chatMessages:       0,
-            currentFollowers:   0,
-            maxViewers:         0,
-            newChatters:        0,
-            currentWatched:     0,
-          };
-          if (statsFromDb.length > 0) {
-            for (const stat of statsFromDb) {
-              statsToReturn.currentViewers += _self.parseStat(stat.currentViewers);
-              statsToReturn.currentBits += _self.parseStat(stat.currentBits);
-              statsToReturn.currentTips += _self.parseStat(stat.currentTips);
-              statsToReturn.chatMessages += _self.parseStat(stat.chatMessages);
-              statsToReturn.maxViewers += _self.parseStat(stat.maxViewers);
-              statsToReturn.newChatters += _self.parseStat(stat.newChatters);
-              statsToReturn.currentWatched += _self.parseStat(stat.currentWatched);
-            }
-            statsToReturn.currentViewers = Number(Number(statsToReturn.currentViewers / statsFromDb.length).toFixed(0));
-            statsToReturn.currentBits = Number(Number(statsToReturn.currentBits / statsFromDb.length).toFixed(0));
-            statsToReturn.currentTips = Number(Number(statsToReturn.currentTips / statsFromDb.length).toFixed(2));
-            statsToReturn.chatMessages = Number(Number(statsToReturn.chatMessages / statsFromDb.length).toFixed(0));
-            statsToReturn.maxViewers = Number(Number(statsToReturn.maxViewers / statsFromDb.length).toFixed(0));
-            statsToReturn.newChatters = Number(Number(statsToReturn.newChatters / statsFromDb.length).toFixed(0));
-            statsToReturn.currentWatched = Number(Number(statsToReturn.currentWatched / statsFromDb.length).toFixed(0));
-            cachedStats = cloneDeep(statsToReturn);
-            cb(null, {
-              ...statsToReturn, currentFollowers: _self.currentFollowers, currentSubscribers: _self.currentSubscribers,
-            });
-          } else {
-            cb(null, {});
+        const statsFromDb = await getRepository(TwitchStats)
+          .createQueryBuilder('stats')
+          .offset(1)
+          .limit(Number.MAX_SAFE_INTEGER)
+          .where('stats.whenOnline > :whenOnline', { whenOnline: Date.now() - (DAY * 31) })
+          .orderBy('stats.whenOnline', 'DESC')
+          .getMany();
+        const statsToReturn = {
+          currentViewers:     0,
+          currentSubscribers: 0,
+          currentBits:        0,
+          currentTips:        0,
+          chatMessages:       0,
+          currentFollowers:   0,
+          maxViewers:         0,
+          newChatters:        0,
+          currentWatched:     0,
+        };
+        if (statsFromDb.length > 0) {
+          for (const stat of statsFromDb) {
+            statsToReturn.currentViewers += _self.parseStat(stat.currentViewers);
+            statsToReturn.currentBits += _self.parseStat(stat.currentBits);
+            statsToReturn.currentTips += _self.parseStat(stat.currentTips);
+            statsToReturn.chatMessages += _self.parseStat(stat.chatMessages);
+            statsToReturn.maxViewers += _self.parseStat(stat.maxViewers);
+            statsToReturn.newChatters += _self.parseStat(stat.newChatters);
+            statsToReturn.currentWatched += _self.parseStat(stat.currentWatched);
           }
-        } else {
+          statsToReturn.currentViewers = Number(Number(statsToReturn.currentViewers / statsFromDb.length).toFixed(0));
+          statsToReturn.currentBits = Number(Number(statsToReturn.currentBits / statsFromDb.length).toFixed(0));
+          statsToReturn.currentTips = Number(Number(statsToReturn.currentTips / statsFromDb.length).toFixed(2));
+          statsToReturn.chatMessages = Number(Number(statsToReturn.chatMessages / statsFromDb.length).toFixed(0));
+          statsToReturn.maxViewers = Number(Number(statsToReturn.maxViewers / statsFromDb.length).toFixed(0));
+          statsToReturn.newChatters = Number(Number(statsToReturn.newChatters / statsFromDb.length).toFixed(0));
+          statsToReturn.currentWatched = Number(Number(statsToReturn.currentWatched / statsFromDb.length).toFixed(0));
           cb(null, {
-            ...cachedStats, currentFollowers: _self.currentFollowers, currentSubscribers: _self.currentSubscribers,
+            ...statsToReturn, currentFollowers: _self.currentFollowers, currentSubscribers: _self.currentSubscribers,
           });
+        } else {
+          cb(null, {});
         }
       } catch (e: any) {
         error(e);
