f3501c565f2e25733c400354abbd52ed4dfb8d74
Michal Orlik <sogehige@gmail.com>
2022-08-29 12:35:21 +0200

    update

--- src/database/migration/sqlite/16.0.x/1661765436386-timerAttrChange.ts
+++ src/database/migration/sqlite/16.0.x/1661765436386-timerAttrChange.ts
@@ -11,12 +11,11 @@ export class timerAttrChange1661765436386 implements MigrationInterface {
     await queryRunner.query(`DELETE from "timer_response" WHERE 1=1`);
     await queryRunner.query(`DELETE from "timer" WHERE 1=1`);
 
-    await queryRunner.query(`ALTER TABLE "timer" DROP COLUMN "triggeredAtTimestamp"`);
-    await queryRunner.query(`ALTER TABLE "timer" ADD "triggeredAtTimestamp" character varying(30) NOT NULL DEFAULT '1970-01-01T00:00:00.000Z'`);
-    await queryRunner.query(`ALTER TABLE "timer" DROP COLUMN "triggeredAtMessages"`);
-    await queryRunner.query(`ALTER TABLE "timer" ADD "triggeredAtMessages" integer NOT NULL DEFAULT '0'`);
-    await queryRunner.query(`ALTER TABLE "timer_response" DROP COLUMN "timestamp"`);
-    await queryRunner.query(`ALTER TABLE "timer_response" ADD "timestamp" character varying(30) NOT NULL DEFAULT '1970-01-01T00:00:00.000Z'`);
+    await queryRunner.query(`DROP TABLE "timer_response"`);
+    await queryRunner.query(`DROP TABLE "timer"`);
+
+    await queryRunner.query(`CREATE TABLE "timer" ("id" varchar PRIMARY KEY NOT NULL, "name" varchar NOT NULL, "isEnabled" boolean NOT NULL, "tickOffline" boolean NOT NULL DEFAULT (0), "triggerEveryMessage" integer NOT NULL, "triggerEverySecond" integer NOT NULL, "triggeredAtTimestamp" varchar(30) NOT NULL DEFAULT ('1970-01-01T00:00:00.000Z'), "triggeredAtMessages" integer NOT NULL DEFAULT (0))`);
+    await queryRunner.query(`CREATE TABLE "timer_response" ("id" varchar PRIMARY KEY NOT NULL, "timestamp" varchar(30) NOT NULL DEFAULT ('1970-01-01T00:00:00.000Z'), "isEnabled" boolean NOT NULL DEFAULT (1), "response" text NOT NULL, "timerId" varchar, CONSTRAINT "FK_3192b176b66d4375368c9e960de" FOREIGN KEY ("timerId") REFERENCES "timer" ("id") ON DELETE CASCADE ON UPDATE CASCADE)`);
 
     for (const item of items) {
       await insertItemIntoTable('timer', {
