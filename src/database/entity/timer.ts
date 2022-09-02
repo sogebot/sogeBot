97cd0e31da2de6ef713f8554ab4e25d8d173af29
Michal Orlik <sogehige@gmail.com>
2022-09-02 13:43:55 +0200

    update

--- src/database/entity/timer.ts
+++ src/database/entity/timer.ts
@@ -11,21 +11,21 @@ export class Timer extends BaseEntity {
   @IsNotEmpty()
   @MinLength(2)
   @Matches(/^[a-zA-Z0-9_]*$/)
-    name = '';
+    name: string = '';
 
   @Column()
-    isEnabled = true;
+    isEnabled: boolean = true;
 
   @Column({ default: false })
-    tickOffline = false;
+    tickOffline: boolean = false;
 
   @Column()
   @Min(0)
-    triggerEveryMessage = 30;
+    triggerEveryMessage: number = 30;
 
   @Column()
   @Min(0)
-    triggerEverySecond = 60;
+    triggerEverySecond: number = 60;
 
   @Column({ type: 'varchar', length: '2022-07-27T00:30:34.569259834Z'.length, default: '1970-01-01T00:00:00.000Z' })
     triggeredAtTimestamp?: string;
