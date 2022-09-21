bdb1c2f958edc57f11d06b23e2ab93bfd595ff1d
Michal Orlik <sogehige@gmail.com>
2022-09-22 01:28:10 +0200

    update secrets

--- tools/fetchRates.js
+++ tools/fetchRates.js
@@ -9,5 +9,6 @@ axios.get('https://openexchangerates.org/api/latest.json?app_id=' + appId)
   .then(res => {
     const rates = res.data.rates;
     fs.writeFileSync(ratesFile, `export default ${JSON.stringify(rates, null, 2)};`);
+    console.log('Rates fetched OK!')
   })
   .catch(err => console.error(err));
