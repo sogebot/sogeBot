9b7ae4bf8856f208dc19fab921517dabf513ed67
Michal Orlik <sogehige@gmail.com>
2022-10-06 21:46:33 +0200

    fix(plugins): remove filter advanced mode

--- src/plugins/nodes/filter.ts
+++ src/plugins/nodes/filter.ts
@@ -1,27 +1,10 @@
-import { itemsToEvalPart } from '@sogebot/ui-helpers/queryFilter';
 import { VM }  from 'vm2';
 
 import type { Node } from '~/../d.ts/src/plugins';
 import { error } from '~/helpers/log';
 
 export default async function(pluginId: string, currentNode: Node<string>, parameters: Record<string, any>, variables: Record<string, any>, userstate: { userName: string; userId: string; } | null) {
-  const advancedMode = JSON.parse(currentNode.data.data).advancedMode ?? false;
-
-  let script = null;
-
-  if (advancedMode) {
-    script = currentNode.data.value;
-  } else {
-    const filter = currentNode.data.value
-      ? JSON.parse(currentNode.data.value) : null;
-    if (filter && filter.items.length > 0) {
-      script = itemsToEvalPart(filter.items, filter.operator);
-    }
-  }
-
-  if (!script) {
-    return false;
-  }
+  const script = currentNode.data.value;
   try {
     const sandbox = {
       sender: userstate ? {
@@ -32,7 +15,8 @@ export default async function(pluginId: string, currentNode: Node<string>, param
       ...variables,
     };
     const vm = new VM({ sandbox });
-    const result = vm.run(`(function () {  return ${script} })`)();
+
+    const result = vm.run(`(function () { ${script} })`)();
     return !!result;
   } catch (e) {
     error(`PLUGINS#${pluginId}: ${(e as Error).stack}`);
