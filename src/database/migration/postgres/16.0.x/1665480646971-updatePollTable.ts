14cb777decfdcd24fa3708f1ee1d40acabc12c87
Michal Orlik <sogehige@gmail.com>
2022-10-11 17:28:16 +0200

    update

--- src/database/migration/postgres/16.0.x/1665480646971-updatePollTable.ts
+++ src/database/migration/postgres/16.0.x/1665480646971-updatePollTable.ts
@@ -11,6 +11,7 @@ export class updatePollTable1665480646971 implements MigrationInterface {
     await queryRunner.query(`DELETE from "poll" WHERE 1=1`);
     await queryRunner.query(`DELETE from "poll_vote" WHERE 1=1`);
 
+    await queryRunner.query(`DROP TABLE "poll_vote"`);
     await queryRunner.query(`DROP TABLE "poll"`);
     /* await queryRunner.query(`CREATE TABLE "poll" ("id" varchar PRIMARY KEY NOT NULL, "type" varchar(7) NOT NULL, "title" varchar NOT NULL, "openedAt" datetime NOT NULL DEFAULT (datetime('now')), "closedAt" date, "options" text NOT NULL)`);
 
