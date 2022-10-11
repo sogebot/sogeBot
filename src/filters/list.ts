124e2a9770492d17ee12122f784adfb884085263
Michal Orlik <sogehige@gmail.com>
2022-10-11 17:15:56 +0200

    fix relations

--- src/filters/list.ts
+++ src/filters/list.ts
@@ -36,7 +36,7 @@ const list: ResponseFilter = {
           visible: true, enabled: true, group,
         } : { visible: true, enabled: true },
       }),
-      getRepository(Commands).find({ relations: ['responses'], where:     typeof group !== 'undefined' ? {
+      getRepository(Commands).find({ where: typeof group !== 'undefined' ? {
         visible: true, enabled: true, group,
       } : { visible: true, enabled: true } }),
       getRepository(Cooldown).find({ where: { isEnabled: true } }),
