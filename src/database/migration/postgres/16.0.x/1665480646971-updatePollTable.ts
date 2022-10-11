17a5469360613e507d5fd5cd5a03ed2d24be1e92
Michal Orlik <sogehige@gmail.com>
2022-10-11 18:02:15 +0200

    update

--- src/database/migration/postgres/16.0.x/1665480646971-updatePollTable.ts
+++ src/database/migration/postgres/16.0.x/1665480646971-updatePollTable.ts
@@ -15,6 +15,8 @@ export class updatePollTable1665480646971 implements MigrationInterface {
     await queryRunner.query(`DROP TABLE "poll"`);
 
     await queryRunner.query(`CREATE TABLE "poll" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "type" character varying(7) NOT NULL, "title" character varying NOT NULL, "openedAt" TIMESTAMP NOT NULL DEFAULT now(), "closedAt" date, "options" text NOT NULL, CONSTRAINT "PK_03b5cf19a7f562b231c3458527e" PRIMARY KEY ("id"))`);
+    await queryRunner.query(`CREATE TABLE "poll_vote" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "option" integer NOT NULL, "votes" integer NOT NULL, "votedBy" character varying NOT NULL, "pollId" character varying, CONSTRAINT "PK_fd002d371201c472490ba89c6a0" PRIMARY KEY ("id"))`);
+    await queryRunner.query(`ALTER TABLE "poll_vote" ADD CONSTRAINT "FK_99f9db6d3dae2a0aebebbf8e10a" FOREIGN KEY ("pollId") REFERENCES "poll"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
     /*await queryRunner.query(`CREATE TABLE "poll_vote" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "option" integer NOT NULL, "votes" integer NOT NULL, "votedBy" character varying NOT NULL, "pollId" character varying, CONSTRAINT "PK_fd002d371201c472490ba89c6a0" PRIMARY KEY ("id"))`);
     await queryRunner.query(`ALTER TABLE "poll_vote" ADD CONSTRAINT "FK_99f9db6d3dae2a0aebebbf8e10a" FOREIGN KEY ("pollId") REFERENCES "poll"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
 
