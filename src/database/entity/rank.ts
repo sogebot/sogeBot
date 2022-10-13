3aa98018e6d594ddb222845fa70aa4583c58194b
Michal Orlik <sogehige@gmail.com>
2022-10-13 10:45:55 +0200

    entity change

--- src/database/entity/rank.ts
+++ src/database/entity/rank.ts
@@ -1,9 +1,9 @@
 import { IsNotEmpty, Min, MinLength } from 'class-validator';
-import { Column, Entity, PrimaryColumn, Unique } from 'typeorm';
+import { Column, Entity, Index, PrimaryColumn, Unique } from 'typeorm';
 import { BotEntity } from '~/database/entity/_botEntity';
 
 @Entity()
-@Unique('IDX_93c78c94804a13befdace81904', ['type', 'value'])
+@Index('IDX_93c78c94804a13befdace81904', ['type', 'value'])
 export class Rank extends BotEntity {
   @PrimaryColumn({ generated: 'uuid' })
     id: string;
