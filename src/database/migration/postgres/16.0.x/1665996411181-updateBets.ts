d9ec37131b78dc96d6aa028cea5239a59c078d44
Michal Orlik <sogehige@gmail.com>
2022-10-17 12:53:01 +0200

    update

--- src/database/migration/postgres/16.0.x/1665996411181-updateBets.ts
+++ src/database/migration/postgres/16.0.x/1665996411181-updateBets.ts
@@ -11,6 +11,8 @@ export class updateBets1665996411181 implements MigrationInterface {
     await queryRunner.query(`DROP TABLE "bets_participations"`);
     await queryRunner.query(`DROP TABLE "bets"`);
 
+    await queryRunner.query(`CREATE TABLE "bets" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" character varying(30) NOT NULL, "endedAt" character varying(30) NOT NULL, "isLocked" boolean NOT NULL DEFAULT false, "arePointsGiven" boolean NOT NULL DEFAULT false, "options" text NOT NULL, "title" character varying NOT NULL, "participants" json NOT NULL, CONSTRAINT "PK_7ca91a6a39623bd5c21722bcedd" PRIMARY KEY ("id"))`);
+
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
