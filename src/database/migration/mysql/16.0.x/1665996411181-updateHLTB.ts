44ce388e3ee51690fa23da614468b0d0fc78c1ef
Michal Orlik <sogehige@gmail.com>
2022-10-17 21:00:42 +0200

    migr up

---
+++ src/database/migration/mysql/16.0.x/1665996411181-updateHLTB.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class updateHLTB1665996411181 implements MigrationInterface {
+  name = 'updateHLTB1665996411181';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const items = await queryRunner.query(`SELECT * from \`how_long_to_beat_game\``);
+    const items2 = await queryRunner.query(`SELECT * from \`how_long_to_beat_game_item\``);
+
+    await queryRunner.query(`DROP TABLE \`how_long_to_beat_game\``);
+    await queryRunner.query(`DROP TABLE \`how_long_to_beat_game_item\``);
+
+    console.log({ items, items2 });
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return;
+  }
+
+}
