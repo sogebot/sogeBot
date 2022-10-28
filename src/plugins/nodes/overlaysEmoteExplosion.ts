c8782dc26005d335e6149c86a916eb59a9717b37
Michal Orlik <sogehige@gmail.com>
2022-10-29 00:04:00 +0200

    fix(plugins): fix incorrect emote parsing for explosion and fireworks

--- src/plugins/nodes/overlaysEmoteExplosion.ts
+++ src/plugins/nodes/overlaysEmoteExplosion.ts
@@ -2,7 +2,7 @@ import type { Node } from '~/../d.ts/src/plugins';
 import emitter from '~/helpers/interfaceEmitter';
 
 export default async function(pluginId: string, currentNode: Node, parameters: Record<string, any>, variables: Record<string, any>, userstate: { userName: string; userId: string; } | null) {
-  const emotes = JSON.parse(currentNode.data.value).value.trim().split(' ');
+  const emotes = currentNode.data.value.trim().split(' ');
   emitter.emit('services::twitch::emotes', 'explode', emotes);
   return true;
 }
