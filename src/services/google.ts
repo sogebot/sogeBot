ae8faeca86a8fc32fbb52e9e4c525428a15c094a
Michal Orlik <sogehige@gmail.com>
2022-10-31 21:31:15 +0100

    chore(google): expose private keys by api

--- src/services/google.ts
+++ src/services/google.ts
@@ -1,6 +1,23 @@
+import { getRepository } from 'typeorm';
+import { GooglePrivateKeys } from '~/database/entity/google';
+import { app } from '~/helpers/panel';
+import { adminMiddleware } from '~/socket';
 import Service from './_interface';
 
 class Google extends Service {
+  sockets() {
+    if (!app) {
+      setTimeout(() => this.sockets(), 100);
+      return;
+    }
+
+    app.get('/api/services/google/privatekeys', adminMiddleware, async (req, res) => {
+      res.send({
+        data: await getRepository(GooglePrivateKeys).find(),
+      });
+    });
+
+  }
 }
 
 export default new Google();
