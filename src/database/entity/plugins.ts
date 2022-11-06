894195699b8ab08bb6705d3c857c06c730d6be04
Michal Orlik <sogehige@gmail.com>
2022-11-06 22:16:27 +0100

    chore: add boolean type

--- src/database/entity/plugins.ts
+++ src/database/entity/plugins.ts
@@ -22,7 +22,7 @@ export class Plugin extends BaseEntity {
   @Column('simple-json', { nullable: true })
     settings: {
     name: string;
-    type: 'string' | 'number' | 'array';
+    type: 'string' | 'number' | 'array' | 'boolean';
     description: string;
     defaultValue: string | number | string[];
     currentValue: string | number | string[];
