1ef20bcb74da8abd85d4eaf9f035efde2255625a
Michal Orlik <sogehige@gmail.com>
2022-11-08 23:42:41 +0100

    chore: set interval even higher

--- src/services/google.ts
+++ src/services/google.ts
@@ -112,7 +112,7 @@ class Google extends Service {
         if (this.liveChatId) {
           this.getChat();
         }
-      }, 2000);
+      }, 5000);
     } else {
       error(`'YOUTUBE: Couldn't get channel informations.`);
     }
