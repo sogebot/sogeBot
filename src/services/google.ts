a3d8c33699b892ec7ffaddf6835fe4a51cf4a662
Michal Orlik <sogehige@gmail.com>
2022-11-08 14:15:44 +0100

    feat(google): set title same as Twitch title

--- src/services/google.ts
+++ src/services/google.ts
@@ -2,33 +2,54 @@ import { getRepository } from 'typeorm';
 import { GooglePrivateKeys } from '~/database/entity/google';
 import { app } from '~/helpers/panel';
 import { adminMiddleware } from '~/socket';
-import { onStartup } from '~/decorators/on';
+import { onChange, onStartup } from '~/decorators/on';
 import Service from './_interface';
 
-import { google } from 'googleapis';
-import { info } from '~/helpers/log';
+import { google, youtube_v3 } from 'googleapis';
+import { error, info } from '~/helpers/log';
+
+import { OAuth2Client } from 'google-auth-library/build/src/auth/oauth2client';
+import { MINUTE } from '@sogebot/ui-helpers/constants';
+
+import {
+  stats,
+} from '~/helpers/api';
+import { settings } from '~/decorators';
 
 class Google extends Service {
-  clientId = '225380804535-gjd77dplfkbe4d3ct173d8qm0j83f8tr.apps.googleusercontent.com';
-  refreshToken = '';
+  @settings()
+    clientId = '';
+  @settings()
+    refreshToken = '';
+  @settings()
+    channel = '';
 
   expiryDate: null | number = null;
   accessToken: null | string = null;
+  client: OAuth2Client | null = null;
+  liveChatId: string | null = null;
 
+  onStartupInterval: null | NodeJS.Timer = null;
+
+  @onChange('refreshToken')
+  @onChange('clientId')
   @onStartup()
   async onStartup() {
+    if (this.client) {
+      this.client = null;
+    }
+
     // configure a JWT auth client
-    const client = new google.auth.OAuth2({
+    this.client = new google.auth.OAuth2({
       clientId: this.clientId,
     });
-    client.setCredentials({
+    this.client.setCredentials({
       access_token:  this.accessToken,
       refresh_token: this.refreshToken,
       expiry_date:   this.expiryDate,
     });
 
-    client.on('tokens', (tokens) => {
-      console.log({ tokens });
+    this.client.on('tokens', (tokens) => {
       if (tokens.refresh_token) {
         this.refreshToken = tokens.refresh_token;
       }
@@ -40,21 +61,80 @@ class Google extends Service {
       }
     });
 
-    info('GOOGLE: Authentication to Google Service successful.');
-
     const youtube = google.youtube({
-      auth:    client,
+      auth:    this.client,
       version: 'v3',
     });
 
-    // get active broadcasts
-    const list = await youtube.liveBroadcasts.list({
-      part:            ['id','snippet','contentDetails','status'],
-      broadcastStatus: 'active ',
+    const channel = await youtube.channels.list({
+      part: ['snippet,contentDetails'],
+      mine: true,
     });
-    console.log(list.data.items);
+    if (channel.data.items && channel.data.items.length > 0) {
+      const item = channel.data.items[0].snippet!;
+      this.channel = [channel.data.items[0].id, item.title, item.customUrl].filter(String).join(' | ');
+      info(`YOUTUBE: Authentication to Google Service successful as ${this.channel}.`);
+      if (this.onStartupInterval) {
+        clearInterval(this.onStartupInterval);
+      }
+      this.onStartupInterval = setInterval(async () => {
+        const stream = await this.getStream();
+
+        if (stream && stream.snippet) {
+          this.liveChatId = stream.snippet.liveChatId ?? null;
+          const currentTitle = stats.value.currentTitle || 'n/a';
+          if (stream.snippet.title !== currentTitle) {
+            info(`YOUTUBE: Title is not matching current title, changing by bot to "${currentTitle}"`);
+            await this.updateTitle(stream, currentTitle);
+          }
+        }
+      }, MINUTE);
+    } else {
+      error(`'YOUTUBE: Couldn't get channel informations.`);
+    }
   }
 
+  async updateTitle(stream: youtube_v3.Schema$LiveBroadcast, title: string) {
+    if (this.client) {
+      const youtube = google.youtube({
+        auth:    this.client,
+        version: 'v3',
+      });
+
+      // get active broadcasts
+      await youtube.liveBroadcasts.update({
+        part:        ['id','snippet','contentDetails','status'],
+        requestBody: {
+          ...stream,
+          snippet: {
+            ...stream.snippet,
+            title,
+          },
+        },
+      });
+    }
+  }
+
+  async getStream() {
+    if (this.client) {
+      const youtube = google.youtube({
+        auth:    this.client,
+        version: 'v3',
+      });
+
+      // get active broadcasts
+      const list = await youtube.liveBroadcasts.list({
+        part:            ['id','snippet','contentDetails','status'],
+        broadcastStatus: 'active',
+      });
+
+      if (list.data.items && list.data.items.length > 0) {
+        const stream = list.data.items[0];
+        return stream;
+      }
+    }
+    return null;
+  }
 
   sockets() {
     if (!app) {
