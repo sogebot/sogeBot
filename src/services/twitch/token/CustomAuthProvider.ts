be747a810a030383563d5376751217a37b9bd93e
Michal Orlik <sogehige@gmail.com>
2022-11-30 17:00:14 +0100

    fix(CustomAuthProvider): fix incorrent tokenService name

--- src/services/twitch/token/CustomAuthProvider.ts
+++ src/services/twitch/token/CustomAuthProvider.ts
@@ -5,6 +5,7 @@ import { variables } from '../../../watchers.js';
 import { expirationDate, validate } from './validate.js';
 
 export class CustomAuthProvider implements AuthProvider {
+  @Enumerable(false) private _clientId: string;
   @Enumerable(false) private _accessToken: AccessToken;
 
   readonly typeOfToken: 'broadcaster' | 'bot';
@@ -15,9 +16,11 @@ export class CustomAuthProvider implements AuthProvider {
   ) {
     this.typeOfToken = type;
     this._accessToken = variables.get(`services.twitch.${this.typeOfToken}AccessToken`);
+    this.updateClientId();
   }
 
   async getAccessToken(requestedScopes?: string[]): Promise<AccessToken | null> {
+    this.updateClientId();
     if (variables.get(`services.twitch.${this.typeOfToken}AccessToken`) === '') {
       return null;
     }
@@ -35,19 +38,26 @@ export class CustomAuthProvider implements AuthProvider {
     return this._accessToken;
   }
 
+  updateClientId() {
+    const tokenService = variables.get(`services.twitch.tokenService`);
+    switch (tokenService) {
+      case 'SogeBot Token Generator':
+        this._clientId = 't8cney2xkc7j4cu6zpv9ijfa27w027';
+        break;
+      case 'SogeBot Token Generator v2':
+        this._clientId = '89k6demxtifvq0vzgjpvr1mykxaqmf';
+        break;
+      default:
+        this._clientId = variables.get(`services.twitch.tokenServiceCustomClientId`);
+    }
+  }
+
   /**
 	 * The client ID.
 	 */
   get clientId(): string {
-    const tokenService = variables.get(`services.twitch.tokenService`);
-    switch (tokenService) {
-      case 'SogeBot Token Generator':
-        return 't8cney2xkc7j4cu6zpv9ijfa27w027';
-      case 'Sogebot Token Generator v2':
-        return '89k6demxtifvq0vzgjpvr1mykxaqmf';
-      default:
-        return variables.get(`services.twitch.tokenServiceCustomClientId`);
-    }
+    this.updateClientId();
+    return this._clientId;
   }
 
   /**
