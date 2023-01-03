8cfce144f2ea77b477db426842a09381cf290172
Michal Orlik <sogehige@gmail.com>
2023-01-04 00:02:49 +0100

    update migration

---
+++ src/database/migration/mysql/16.0.x/1666167883935-updateCustomVariables.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { insertItemIntoTable } from '~/database/insertItemIntoTable';
+
+export class updateCustomVariables1666167883935 implements MigrationInterface {
+  name = 'updateCustomVariables1666167883935';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const items = await queryRunner.query(`SELECT * from \`variable\``);
+    const items2 = await queryRunner.query(`SELECT * from \`variable_history\``);
+    const items3 = await queryRunner.query(`SELECT * from \`variable_url\``);
+
+    await queryRunner.query(`ALTER TABLE \`variable\` ADD \`history\` json NOT NULL`);
+    await queryRunner.query(`ALTER TABLE \`variable\` ADD \`urls\` json NOT NULL`);
+    await queryRunner.query(`ALTER TABLE \`variable\` ADD UNIQUE INDEX \`IDX_dd084634ad76dbefdca837b8de\` (\`variableName\`)`);
+    await queryRunner.query(`ALTER TABLE \`variable\` DROP COLUMN \`runAt\``);
+    await queryRunner.query(`ALTER TABLE \`variable\` ADD \`runAt\` varchar(30) NOT NULL`);
+
+    await queryRunner.query(`DROP TABLE \`variable_history\``);
+    await queryRunner.query(`DROP TABLE  \`variable_url\``);
+
+    await queryRunner.query(`DELETE FROM \`variable\` WHERE 1=1`);
+
+    for (const item of items) {
+      item.history = JSON.stringify(items2.filter((o: { variableId: any; }) => o.variableId === item.id));
+      item.urls = JSON.stringify(items3.filter((o: { variableId: any; }) => o.variableId === item.id));
+      await insertItemIntoTable('variable', {
+        ...item,
+      }, queryRunner);
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return;
+  }
+
+}
