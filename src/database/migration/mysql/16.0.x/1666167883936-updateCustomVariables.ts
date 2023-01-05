94474923e64c0c195c32d2b24e8be5ebc90c7dde
Michal Orlik <sogehige@gmail.com>
2023-01-05 12:17:19 +0100

    chore: fix migration

---
+++ src/database/migration/mysql/16.0.x/1666167883936-updateCustomVariables.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { insertItemIntoTable } from '~/database/insertItemIntoTable';
+
+export class updateCustomVariables1666167883936 implements MigrationInterface {
+  name = 'updateCustomVariables1666167883936';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE \`variable\` DROP COLUMN \`runEveryTypeValue\``);
+    await queryRunner.query(`ALTER TABLE \`variable\` DROP COLUMN \`runEveryType\``);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return;
+  }
+
+}
