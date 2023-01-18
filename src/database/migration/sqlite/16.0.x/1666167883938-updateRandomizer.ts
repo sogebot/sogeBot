1c8274cd51adc497f094d200f1c6289f3d48994f
Michal Orlik <sogehige@gmail.com>
2023-01-18 21:57:51 +0100

    update

---
+++ src/database/migration/sqlite/16.0.x/1666167883938-updateRandomizer.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class updateRandomizer1666167883938 implements MigrationInterface {
+  name = 'updateRandomizer1666167883938';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "randomizer"`);
+    await queryRunner.query(`DROP TABLE "randomizer_items"`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return;
+  }
+
+}
