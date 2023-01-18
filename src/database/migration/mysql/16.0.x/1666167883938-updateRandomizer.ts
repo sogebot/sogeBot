baba308ea12a1800d99b0d59005d636a9131e84b
Michal Orlik <sogehige@gmail.com>
2023-01-18 22:59:27 +0100

    update

--- src/database/migration/mysql/16.0.x/1666167883938-updateRandomizer.ts
+++ src/database/migration/mysql/16.0.x/1666167883938-updateRandomizer.ts
@@ -4,8 +4,8 @@ export class updateRandomizer1666167883938 implements MigrationInterface {
   name = 'updateRandomizer1666167883938';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE \`randomizer\``);
     await queryRunner.query(`DROP TABLE \`randomizer_items\``);
+    await queryRunner.query(`DROP TABLE \`randomizer\``);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
