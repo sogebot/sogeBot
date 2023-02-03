e0048129a53c65b7385bf1c17576c3c822e63273
Michal Orlik <sogehige@gmail.com>
2023-02-03 15:29:25 +0100

    fix(countdown): add missing default value for showMilliseconds

--- src/helpers/overlaysDefaultValues.ts
+++ src/helpers/overlaysDefaultValues.ts
@@ -14,6 +14,7 @@ const values = {
     isPersistent:               false,
     isStartedOnSourceLoad:      true,
     showMessageWhenReachedZero: false,
+    showMilliseconds:           false,
     countdownFont:              {
       family:      'PT Sans',
       size:        50,
