9f9bb6bae2f24e6c43dd9864b094af0513f4e092
sogehige <sogehige@gmail.com>
2023-03-03 18:43:27 +0100

    chore: fix migration zero (#5642)

--- src/database/migration/postgres/1000000000001-initialize.ts
+++ src/database/migration/postgres/1000000000001-initialize.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class initialize0 implements MigrationInterface {
-  name = 'initialize0';
+export class initialize1000000000001 implements MigrationInterface {
+  name = 'initialize1000000000001';
 
   public async up(queryRunner: QueryRunner): Promise<any> {
     return;
