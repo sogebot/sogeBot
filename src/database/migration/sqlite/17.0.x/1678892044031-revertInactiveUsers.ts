04947d82aa795b9012f92e2fa7485e20036aa657
Michal Orlik <sogehige@gmail.com>
2023-04-11 17:58:31 +0200

    chore: revert inactive users

---
+++ src/database/migration/sqlite/17.0.x/1678892044031-revertInactiveUsers.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class revertInactiveUsers1678892044031 implements MigrationInterface {
+  name = 'revertInactiveUsers1678892044031';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const users = await queryRunner.query(`SELECT * FROM "user"`);
+    for (const user of users) {
+      await queryRunner.query(`UPDATE "user" SET "userName"=? WHERE "userId"=?`,
+        [user.userName.replace(/__inactive__/g, ''), user.userId]);
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return;
+  }
+
+}
