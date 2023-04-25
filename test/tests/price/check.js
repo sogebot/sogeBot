5978ae7c13282d910e2963d968af442381a16192
Michal Orlik <sogehige@gmail.com>
2023-04-25 16:51:40 +0200

    fix test

--- test/tests/price/check.js
+++ test/tests/price/check.js
@@ -61,14 +61,14 @@ describe('Price - check() - @func3', () => {
     await message.prepare();
     await user.prepare();
 
-    await AppDataSource.getRepository(User).save({ user_login: user.viewer.userName, userId: user.viewer.userId });
+    await AppDataSource.getRepository(User).save({ userName: user.viewer.userName, userId: user.viewer.userId });
   });
 
   for (const test of tests) {
     it(`${test.user} with ${test.points} points calls ${test.command}, price on ${test.priceOn} set to ${test.price} and should ${test.expected ? 'pass' : 'fail'}`, async () => {
-      await AppDataSource.getRepository(User).update({ userId: user.viewer.userId }, { points: test.points });
+      await AppDataSource.getRepository(User).update({ userId: test.user_id }, { points: test.points });
       await AppDataSource.getRepository(Price).save({ command: test.command, price: test.price });
-      const haveEnoughPoints = await price.check({ sender: { userName: test.user, userId: test.userId }, message: test.command });
+      const haveEnoughPoints = await price.check({ sender: { userName: test.user, userId: test.user_id }, message: test.command });
       assert(haveEnoughPoints === test.expected);
     });
   }
@@ -77,7 +77,7 @@ describe('Price - check() - @func3', () => {
     await AppDataSource.getRepository(Price).save({
       command: '!me', price: 0, priceBits: 10,
     });
-    const haveEnoughPoints = await price.check({ sender: { user_login: user.viewer.userName, userId: user.viewer.userId }, message: '!me' });
+    const haveEnoughPoints = await price.check({ sender: { userName: user.viewer.userName, userId: user.viewer.userId }, message: '!me' });
     assert(haveEnoughPoints === false);
     await message.isSentRaw('Sorry, @__viewer__, but you need to redeem command by 10 bits to use !me', user.viewer.userName, 20000);
   });
@@ -87,7 +87,7 @@ describe('Price - check() - @func3', () => {
     await AppDataSource.getRepository(Price).save({
       command: '!me', price: 100, priceBits: 10,
     });
-    const haveEnoughPoints = await price.check({ sender: { user_login: user.viewer.userName, userId: user.viewer.userId }, message: '!me' });
+    const haveEnoughPoints = await price.check({ sender: { userName: user.viewer.userName, userId: user.viewer.userId }, message: '!me' });
     assert(haveEnoughPoints === false);
     await message.isSentRaw('Sorry, @__viewer__, but you don\'t have 100 points or redeem command by 10 bits to use !me', user.viewer.userName, 20000);
   });
@@ -97,7 +97,7 @@ describe('Price - check() - @func3', () => {
     await AppDataSource.getRepository(Price).save({
       command: '!me', price: 100, priceBits: 10,
     });
-    const haveEnoughPoints = await price.check({ sender: { user_login: user.viewer.userName, userId: user.viewer.userId }, message: '!me' });
+    const haveEnoughPoints = await price.check({ sender: { userName: user.viewer.userName, userId: user.viewer.userId }, message: '!me' });
     assert(haveEnoughPoints === true);
   });
 
