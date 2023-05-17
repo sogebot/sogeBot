6ae1a74c4a51244935600929c8f34081c6c14943
Michal Orlik <sogehige@gmail.com>
2023-05-17 21:14:15 +0200

    update

---
+++ src/database/migration/mysql/1000000000001-initialize.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class initialize1000000000001 implements MigrationInterface {
+  transaction?: boolean | undefined;
+  name = 'initialize1000000000001';
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    const migrations = await queryRunner.query(`SELECT * FROM \`migrations\``);
+    if (migrations.length > 0) {
+      console.log('Skipping migration zero, migrations are already in bot');
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    return;
+  }
+
+}
