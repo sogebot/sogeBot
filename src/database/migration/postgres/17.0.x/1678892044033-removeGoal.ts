4ef23ee599e2d5ebd5d56443df28ada6fb84a244
Michal Orlik <sogehige@gmail.com>
2023-05-26 00:22:21 +0200

    swap migration table remove

--- src/database/migration/postgres/17.0.x/1678892044033-removeGoal.ts
+++ src/database/migration/postgres/17.0.x/1678892044033-removeGoal.ts
@@ -4,8 +4,8 @@ export class removeGoal1678892044033 implements MigrationInterface {
   name = 'removeGoal1678892044033';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "goal_group"`);
     await queryRunner.query(`DROP TABLE "goal"`);
+    await queryRunner.query(`DROP TABLE "goal_group"`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
