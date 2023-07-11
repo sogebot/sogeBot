fb25f68517cbd85c6eb9689c8b973ee9f2e1fa7d
Michal Orlik <sogehige@gmail.com>
2023-07-11 12:21:34 +0200

    fix(checkFilter): update checkFilter behavior to include $

--- src/helpers/checkFilter.ts
+++ src/helpers/checkFilter.ts
@@ -95,24 +95,24 @@ class HelpersFilter {
     const uptime = vars.get('services.twitch.uptime') as number;
 
     const variables: Record<string, any> = {
-      game:            stats.value.currentGame,
-      language:        stats.value.language,
-      viewers:         isStreamOnline.value ? stats.value.currentViewers : 0,
-      followers:       stats.value.currentFollowers,
-      subscribers:     stats.value.currentSubscribers,
-      bits:            isStreamOnline.value ? stats.value.currentBits : 0,
-      title:           stats.value.currentTitle,
-      source:          opts.sender && typeof opts.discord !== 'undefined' ? 'discord' : 'twitch',
-      isBotSubscriber: isBotSubscriber(),
-      isStreamOnline:  isStreamOnline.value,
-      uptime:          getTime(Date.now() - uptime, false),
+      $game:            stats.value.currentGame,
+      $language:        stats.value.language,
+      $viewers:         isStreamOnline.value ? stats.value.currentViewers : 0,
+      $followers:       stats.value.currentFollowers,
+      $subscribers:     stats.value.currentSubscribers,
+      $bits:            isStreamOnline.value ? stats.value.currentBits : 0,
+      $title:           stats.value.currentTitle,
+      $source:          opts.sender && typeof opts.discord !== 'undefined' ? 'discord' : 'twitch',
+      $isBotSubscriber: isBotSubscriber(),
+      $isStreamOnline:  isStreamOnline.value,
+      $uptime:          getTime(Date.now() - uptime, false),
     };
 
     if (message.includes('$thumbnail') && stats.value.currentGame) {
       const gameFromDb = await AppDataSource.getRepository(CacheGames).findOneBy({ name: stats.value.currentGame });
       if (gameFromDb && gameFromDb.thumbnail) {
         // replace $thumbnail
-        variables.thumbnail = gameFromDb.thumbnail;
+        variables.$thumbnail = gameFromDb.thumbnail;
 
         // replace $thumbnail with width and height defined
         const regex = /\$thumbnail\((?<width>\d+)x(?<height>\d+)\)/gm;
@@ -140,12 +140,12 @@ class HelpersFilter {
     if (message.includes('$version')) {
       const version = _.get(process, 'env.npm_package_version', 'x.y.z');
       const commitFile = existsSync('./.commit') ? readFileSync('./.commit').toString() : null;
-      variables.version = version.replace('SNAPSHOT', commitFile && commitFile.length > 0 ? commitFile : gitCommitInfo().shortHash || 'SNAPSHOT');
+      variables.$version = version.replace('SNAPSHOT', commitFile && commitFile.length > 0 ? commitFile : gitCommitInfo().shortHash || 'SNAPSHOT');
     }
 
     if (message.includes('$latestFollower')) {
       const latestFollower = await AppDataSource.getRepository(EventList).findOne({ order: { timestamp: 'DESC' }, where: { event: 'follow' } });
-      variables.latestFollower = !_.isNil(latestFollower) ? await getNameById(latestFollower.userId) : 'n/a';
+      variables.$latestFollower = !_.isNil(latestFollower) ? await getNameById(latestFollower.userId) : 'n/a';
     }
 
     // latestSubscriber
@@ -157,27 +157,27 @@ class HelpersFilter {
 
       if (latestSubscriber && (message.includes('$latestSubscriberMonths') || message.includes('$latestSubscriberStreak'))) {
         const latestSubscriberUser = await changelog.get(latestSubscriber.userId);
-        variables.latestSubscriberMonths = latestSubscriberUser ? String(latestSubscriberUser.subscribeCumulativeMonths) : 'n/a';
-        variables.latestSubscriberStreak = latestSubscriberUser ? String(latestSubscriberUser.subscribeStreak) : 'n/a';
+        variables.$latestSubscriberMonths = latestSubscriberUser ? String(latestSubscriberUser.subscribeCumulativeMonths) : 'n/a';
+        variables.$latestSubscriberStreak = latestSubscriberUser ? String(latestSubscriberUser.subscribeStreak) : 'n/a';
       }
-      variables.latestSubscriber = !_.isNil(latestSubscriber) ? await getNameById(latestSubscriber.userId) : 'n/a';
+      variables.$latestSubscriber = !_.isNil(latestSubscriber) ? await getNameById(latestSubscriber.userId) : 'n/a';
     }
 
     // latestTip, latestTipAmount, latestTipCurrency, latestTipMessage
     if (message.includes('$latestTip')) {
       const latestTip = await AppDataSource.getRepository(EventList).findOne({ order: { timestamp: 'DESC' }, where: { event: 'tip', isTest: false } });
-      variables.latestTipAmount = !_.isNil(latestTip) ? parseFloat(JSON.parse(latestTip.values_json).amount).toFixed(2) : 'n/a';
-      variables.latestTipCurrency = !_.isNil(latestTip) ? JSON.parse(latestTip.values_json).currency : 'n/a';
-      variables.latestTipMessage = !_.isNil(latestTip) ? JSON.parse(latestTip.values_json).message : 'n/a';
-      variables.latestTip = !_.isNil(latestTip) ? await getNameById(latestTip.userId) : 'n/a';
+      variables.$latestTipAmount = !_.isNil(latestTip) ? parseFloat(JSON.parse(latestTip.values_json).amount).toFixed(2) : 'n/a';
+      variables.$latestTipCurrency = !_.isNil(latestTip) ? JSON.parse(latestTip.values_json).currency : 'n/a';
+      variables.$latestTipMessage = !_.isNil(latestTip) ? JSON.parse(latestTip.values_json).message : 'n/a';
+      variables.$latestTip = !_.isNil(latestTip) ? await getNameById(latestTip.userId) : 'n/a';
     }
 
     // latestCheer, latestCheerAmount, latestCheerCurrency, latestCheerMessage
     if (message.includes('$latestCheer')) {
       const latestCheer = await AppDataSource.getRepository(EventList).findOne({ order: { timestamp: 'DESC' }, where: { event: 'cheer' } });
-      variables.latestCheerAmount = !_.isNil(latestCheer) ? JSON.parse(latestCheer.values_json).bits : 'n/a';
-      variables.latestCheerMessage = !_.isNil(latestCheer) ? JSON.parse(latestCheer.values_json).message : 'n/a';
-      variables.latestCheer = !_.isNil(latestCheer) ? await getNameById(latestCheer.userId) : 'n/a';
+      variables.$latestCheerAmount = !_.isNil(latestCheer) ? JSON.parse(latestCheer.values_json).bits : 'n/a';
+      variables.$latestCheerMessage = !_.isNil(latestCheer) ? JSON.parse(latestCheer.values_json).message : 'n/a';
+      variables.$latestCheer = !_.isNil(latestCheer) ? await getNameById(latestCheer.userId) : 'n/a';
     }
 
     const spotifySong = JSON.parse(spotify.currentSong);
@@ -188,12 +188,12 @@ class HelpersFilter {
         spotifySong.song = spotifySong.song.replace(new RegExp(opts.escape, 'g'), `\\${opts.escape}`);
         spotifySong.artist = spotifySong.artist.replace(new RegExp(opts.escape, 'g'), `\\${opts.escape}`);
       }
-      variables.spotifySong = format.replace(/\$song/g, spotifySong.song).replace(/\$artist/g, spotifySong.artist);
+      variables.$spotifySong = format.replace(/\$song/g, spotifySong.song).replace(/\$artist/g, spotifySong.artist);
     } else {
-      variables.spotifySong = translate('songs.not-playing');
+      variables.$spotifySong = translate('songs.not-playing');
     }
 
-    variables.lastfmSong = lastfm.currentSong ? lastfm.currentSong : translate('songs.not-playing');
+    variables.$lastfmSong = lastfm.currentSong ? lastfm.currentSong : translate('songs.not-playing');
 
     if (songs.enabled
         && message.includes('$ytSong')
@@ -202,9 +202,9 @@ class HelpersFilter {
       if (opts.escape) {
         currentSong = currentSong.replace(new RegExp(opts.escape, 'g'), `\\${opts.escape}`);
       }
-      variables.ytSong = currentSong;
+      variables.$ytSong = currentSong;
     } else {
-      variables.ytSong = translate('songs.not-playing');
+      variables.$ytSong = translate('songs.not-playing');
     }
 
     const variablesSortedByKey: Record<string, string> = {};
