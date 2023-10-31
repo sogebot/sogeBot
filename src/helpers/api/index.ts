af410720db48ece0d97c06561089f2e78c29e6a5
Michal Orlik <sogehige@gmail.com>
2023-10-31 23:53:29 +0100

    chore: remove unused files

--- src/helpers/api/index.ts
+++ src/helpers/api/index.ts
@@ -2,7 +2,6 @@ export * from './cache.js';
 export * from './chatMessagesAtStart.js';
 export * from './currentStreamTags.js';
 export * from './gameOrTitleChangedManually.js';
-export * from './getToken.js';
 export * from './isStreamOnline.js';
 // exclude from index
 // export * from './parseTitle';
