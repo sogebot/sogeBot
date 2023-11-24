7aab879bb8f6ab349ee1cb857b8fa2e340bc1808
Michal Orlik <sogehige@gmail.com>
2023-11-24 16:20:50 +0100

    fix

--- src/database/migration/mysql/21.x/1678892044036-eventsUpdate.ts
+++ src/database/migration/mysql/21.x/1678892044036-eventsUpdate.ts
@@ -5,8 +5,8 @@ export class eventsUpdate1678892044036 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP INDEX \`IDX_b535fbe8ec6d832dde22065ebd\` ON \`event\``);
-    await queryRunner.query(`DROP TABLE \`event\``);
     await queryRunner.query(`DROP TABLE \`event_operation\``);
+    await queryRunner.query(`DROP TABLE \`event\``);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
--- src/database/migration/sqlite/21.x/1678892044036-eventsUpdate.ts
+++ src/database/migration/sqlite/21.x/1678892044036-eventsUpdate.ts
@@ -7,6 +7,7 @@ export class eventsUpdate1678892044036 implements MigrationInterface {
     await queryRunner.query(`DROP INDEX "IDX_b535fbe8ec6d832dde22065ebd"`);
     await queryRunner.query(`DROP TABLE "event"`);
     await queryRunner.query(`DROP TABLE "event_operation"`);
+    await queryRunner.query(`CREATE TABLE "event" ("id" varchar PRIMARY KEY NOT NULL, "isEnabled" boolean NOT NULL, "filter" varchar NOT NULL, "operations" text NOT NULL, "eventName" text NOT NULL, "eventTriggered" text NOT NULL, "eventDefinitions" text NOT NULL)`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
