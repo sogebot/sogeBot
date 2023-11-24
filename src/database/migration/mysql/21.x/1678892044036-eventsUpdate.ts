054fadbbd013a952457b922778d057f7d7f8b3c6
Michal Orlik <sogehige@gmail.com>
2023-11-24 16:31:54 +0100

    update

--- src/database/migration/mysql/21.x/1678892044036-eventsUpdate.ts
+++ src/database/migration/mysql/21.x/1678892044036-eventsUpdate.ts
@@ -7,6 +7,7 @@ export class eventsUpdate1678892044036 implements MigrationInterface {
     await queryRunner.query(`DROP INDEX \`IDX_b535fbe8ec6d832dde22065ebd\` ON \`event\``);
     await queryRunner.query(`DROP TABLE \`event_operation\``);
     await queryRunner.query(`DROP TABLE \`event\``);
+    await queryRunner.query(`CREATE TABLE \`event\` (\`id\` varchar(36) NOT NULL, \`isEnabled\` tinyint NOT NULL, \`filter\` varchar(255) NOT NULL, \`operations\` json NOT NULL, \`eventName\` text NOT NULL, \`eventTriggered\` json NOT NULL, \`eventDefinitions\` json NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
--- src/database/migration/postgres/21.x/1678892044036-eventsUpdate.ts
+++ src/database/migration/postgres/21.x/1678892044036-eventsUpdate.ts
@@ -7,6 +7,7 @@ export class eventsUpdate1678892044036 implements MigrationInterface {
     await queryRunner.query(`DROP INDEX "IDX_b535fbe8ec6d832dde22065ebd"`);
     await queryRunner.query(`DROP TABLE "event_operation"`);
     await queryRunner.query(`DROP TABLE "event"`);
+    await queryRunner.query(`CREATE TABLE "event" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "isEnabled" boolean NOT NULL, "filter" character varying NOT NULL, "operations" json NOT NULL, "eventName" text NOT NULL, "eventTriggered" json NOT NULL, "eventDefinitions" json NOT NULL, CONSTRAINT "PK_30c2f3bbaf6d34a55f8ae6e4614" PRIMARY KEY ("id"))`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
