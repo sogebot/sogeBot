ecdf686322a52590b64c5e60b487407afca3a4f0
Michal Orlik <sogehige@gmail.com>
2023-11-24 17:06:39 +0100

    mysql fix

--- src/database/migration/mysql/21.x/1678892044036-eventsUpdate.ts
+++ src/database/migration/mysql/21.x/1678892044036-eventsUpdate.ts
@@ -6,8 +6,8 @@ export class eventsUpdate1678892044036 implements MigrationInterface {
   name = 'eventsUpdate1678892044036';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const operations  = await queryRunner.query(`SELECT * FROM "event_operation"`);
-    const events = await queryRunner.query(`SELECT * FROM "event"`);
+    const operations  = await queryRunner.query(`SELECT * FROM \`event_operation\``);
+    const events = await queryRunner.query(`SELECT * FROM \`event\``);
     await queryRunner.query(`DROP INDEX \`IDX_b535fbe8ec6d832dde22065ebd\` ON \`event\``);
     await queryRunner.query(`DROP TABLE \`event_operation\``);
     await queryRunner.query(`DROP TABLE \`event\``);
