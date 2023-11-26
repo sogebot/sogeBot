f5834f22f85b518f1fac83cd22c17c748dcb00d0
Michal Orlik <sogehige@gmail.com>
2023-11-26 16:34:00 +0100

    fix test

--- test/tests/keywords/list.js
+++ test/tests/keywords/list.js
@@ -25,21 +25,21 @@ describe('Keywords - list() - @func2', () => {
   });
 
   it('populated list', async () => {
-    const r = await keywords.add({ sender: owner, parameters: '-p casters -k a -r me' });
-    assert.strictEqual(r[0].response, `$sender, keyword a (${r[0].id}) was added`);
+    const r = await keywords.add({ sender: owner, parameters: '-p casters -k aaa -r me' });
+    assert.strictEqual(r[0].response, `$sender, keyword aaa (${r[0].id}) was added`);
 
-    const r2 = await keywords.add({ sender: owner, parameters: '-p moderators -s true -k a -r me2' });
-    assert.strictEqual(r2[0].response, `$sender, keyword a (${r2[0].id}) was added`);
+    const r2 = await keywords.add({ sender: owner, parameters: '-p moderators -s true -k aaa -r me2' });
+    assert.strictEqual(r2[0].response, `$sender, keyword aaa (${r2[0].id}) was added`);
 
-    const r3 = await keywords.add({ sender: owner, parameters: '-k b -r me' });
-    assert.strictEqual(r3[0].response, `$sender, keyword b (${r3[0].id}) was added`);
+    const r3 = await keywords.add({ sender: owner, parameters: '-k bbb -r me' });
+    assert.strictEqual(r3[0].response, `$sender, keyword bbb (${r3[0].id}) was added`);
 
     const r4 = await keywords.list({ sender: owner, parameters: '' });
-    assert.strictEqual(r4[0].response, '$sender, list of keywords: a, b');
+    assert.strictEqual(r4[0].response, '$sender, list of keywords: aaa, bbb');
 
-    const r5 = await keywords.list({ sender: owner, parameters: 'a' });
-    assert.strictEqual(r5[0].response, 'a#1 (Casters) v| me');
-    assert.strictEqual(r5[1].response, 'a#2 (Moderators) _| me2');
+    const r5 = await keywords.list({ sender: owner, parameters: 'aaa' });
+    assert.strictEqual(r5[0].response, 'aaa#1 (Casters) v| me');
+    assert.strictEqual(r5[1].response, 'aaa#2 (Moderators) _| me2');
 
     const r6 = await keywords.list({ sender: owner, parameters: 'asdsad' });
     assert.strictEqual(r6[0].response, '$sender, asdsad have no responses or doesn\'t exists');
