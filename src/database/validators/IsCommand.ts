568d98b7b2ae5a73838257b486f8e30a4b00ef61
Michal Orlik <sogehige@gmail.com>
2023-11-26 18:09:53 +0100

    fixes

--- src/database/validators/IsCommand.ts
+++ src/database/validators/IsCommand.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
 
 export function command() {
-  return z.custom(value => typeof value === 'string' && value.trim().length > 1 && value.startsWith('!'), 'isCommand');
+  return z.custom<string>(value => typeof value === 'string' && value.trim().length > 1 && value.startsWith('!'), 'isCommand');
 }
--- src/database/validators/IsCommandOrCustomVariable.ts
+++ src/database/validators/IsCommandOrCustomVariable.ts
@@ -2,5 +2,5 @@ import { z } from 'zod';
 
 export function commandOrCustomVariable () {
   return z.custom(value => typeof value === 'string'
-  && (value.startsWith('!') || (value.length > 2 && value.startsWith('$_'))), 'IsCommandOrCustomVariable');
+  && (value.startsWith('!') || (value.length > 2 && value.startsWith('$_'))), 'isCommandOrCustomVariable');
 }
