5c7568bad47af3ed6497d7d1f70c96055985e53f
Michal Orlik <sogehige@gmail.com>
2023-12-11 21:32:12 +0100

    fix(ui): expose tts settings on public

--- src/ui.ts
+++ src/ui.ts
@@ -101,6 +101,17 @@ class UI extends Core {
       try {
         const data: any = {};
 
+        for (const system of ['tts']) {
+          if (typeof data.core === 'undefined') {
+            data.core = {};
+          }
+          const self = find('core', system);
+          if (!self) {
+            throw new Error(`core.${system} not found in list`);
+          }
+          data.core[system] = await self.getAllSettings(true);
+        }
+
         for (const dir of ['systems', 'games']) {
           for (const system of list(dir as any)) {
             set(data, `${dir}.${system.__moduleName__}`, await system.getAllSettings(true));
