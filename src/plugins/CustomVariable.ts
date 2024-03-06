d707101055f4e7e25c089a6e88108b6424b7fc07
Michal Orlik <sogehige@gmail.com>
2024-03-06 18:35:24 +0100

    fix(plugins): custom variable must start with $_

--- src/plugins/CustomVariable.ts
+++ src/plugins/CustomVariable.ts
@@ -3,9 +3,19 @@ import { setValueOf } from '~/helpers/customvariables/setValueOf.js';
 
 export const CustomVariableGenerator = (pluginId: string) => ({
   async set(variableName: string, value: any) {
-    await setValueOf(String(variableName), value, {});
+    variableName = String(variableName).trim();
+    if (variableName.startsWith('$_')) {
+      await setValueOf(variableName, value, {});
+    } else {
+      throw new Error('Variable name must start with "$_".');
+    }
   },
   async get(variableName: string) {
-    return getValueOf(String(variableName));
+    variableName = String(variableName).trim();
+    if (variableName.startsWith('$_')) {
+      return getValueOf(String(variableName));
+    } else {
+      throw new Error('Variable name must start with "$_".');
+    }
   },
 });
