26b92f234e3a11b9fc3d4e91cc4aac43bece6f40
Michal Orlik <sogehige@gmail.com>
2024-03-06 12:44:05 +0100

    migration done

--- src/database/migration/postgres/22.x/1678892044040-changeKeywordsResponses.ts
+++ src/database/migration/postgres/22.x/1678892044040-changeKeywordsResponses.ts
@@ -1,17 +1,29 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
+import { insertItemIntoTable } from '../../../insertItemIntoTable.js';
+
 export class changeKeywordsResponses1678892044040 implements MigrationInterface {
   name = 'changeKeywordsResponses1678892044040';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // const items = await queryRunner.query(`SELECT * from "keyword"`);
-    // const items2 = await queryRunner.query(`SELECT * from "keyword_responses"`);
+    const items = await queryRunner.query(`SELECT * from "keyword"`);
+    const items2 = await queryRunner.query(`SELECT * from "keyword_responses"`);
 
     await queryRunner.query(`DELETE from "keyword_responses" WHERE 1=1`);
     await queryRunner.query(`DELETE from "keyword" WHERE 1=1`);
 
     await queryRunner.query(`DROP TABLE "keyword_responses"`);
     await queryRunner.query(`DROP TABLE "keyword"`);
+
+    await queryRunner.query(`CREATE TABLE "keyword" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "keyword" character varying NOT NULL, "enabled" boolean NOT NULL, "group" character varying, "responses" json NOT NULL, CONSTRAINT "PK_affdb8c8fa5b442900cb3aa21dc" PRIMARY KEY ("id"))`);
+    await queryRunner.query(`CREATE INDEX "IDX_35e3ff88225eef1d85c951e229" ON "keyword" ("keyword") `);
+
+    for (const item of items) {
+      item.responses = JSON.stringify(items2.filter((o: any) => o.keywordId === item.id));
+      await insertItemIntoTable('keyword', {
+        ...item,
+      }, queryRunner);
+    }
 
     return;
   }
--- src/database/migration/sqlite/22.x/1678892044040-changeKeywordsResponses.ts
+++ src/database/migration/sqlite/22.x/1678892044040-changeKeywordsResponses.ts
@@ -1,11 +1,13 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
+import { insertItemIntoTable } from '../../../insertItemIntoTable.js';
+
 export class changeKeywordsResponses1678892044040 implements MigrationInterface {
   name = 'changeKeywordsResponses1678892044040';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // const items = await queryRunner.query(`SELECT * from "keyword"`);
-    // const items2 = await queryRunner.query(`SELECT * from "keyword_responses"`);
+    const items = await queryRunner.query(`SELECT * from "keyword"`);
+    const items2 = await queryRunner.query(`SELECT * from "keyword_responses"`);
 
     await queryRunner.query(`DELETE from "keyword_responses" WHERE 1=1`);
     await queryRunner.query(`DELETE from "keyword" WHERE 1=1`);
@@ -13,7 +15,15 @@ export class changeKeywordsResponses1678892044040 implements MigrationInterface 
     await queryRunner.query(`DROP TABLE "keyword_responses"`);
     await queryRunner.query(`DROP TABLE "keyword"`);
 
-    return;
+    await queryRunner.query(`CREATE TABLE "keyword" ("id" varchar PRIMARY KEY NOT NULL, "keyword" varchar NOT NULL, "enabled" boolean NOT NULL, "group" varchar, "responses" text NOT NULL)`);
+    await queryRunner.query(`CREATE INDEX "IDX_35e3ff88225eef1d85c951e229" ON "keyword" ("keyword") `);
+
+    for (const item of items) {
+      item.responses = JSON.stringify(items2.filter((o: any) => o.keywordId === item.id));
+      await insertItemIntoTable('keyword', {
+        ...item,
+      }, queryRunner);
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
