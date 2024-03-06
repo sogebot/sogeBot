8d54dec3f5e561a4b6ae519566d266ae7c916b6a
Michal Orlik <sogehige@gmail.com>
2024-03-06 12:35:21 +0100

    ...

--- src/database/migration/mysql/22.x/1678892044040-changeKeywordsResponses.ts
+++ src/database/migration/mysql/22.x/1678892044040-changeKeywordsResponses.ts
@@ -4,14 +4,14 @@ export class changeKeywordsResponses1678892044040 implements MigrationInterface 
   name = 'changeKeywordsResponses1678892044040';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // const items = await queryRunner.query(`SELECT * from \`keywords\``);
-    // const items2 = await queryRunner.query(`SELECT * from \`keywords_responses\``);
+    // const items = await queryRunner.query(`SELECT * from \`keyword\``);
+    // const items2 = await queryRunner.query(`SELECT * from \`keyword_responses\``);
 
-    await queryRunner.query(`DELETE from \`keywords_responses\` WHERE 1=1`);
-    await queryRunner.query(`DELETE from \`keywords\` WHERE 1=1`);
+    await queryRunner.query(`DELETE from \`keyword_responses\` WHERE 1=1`);
+    await queryRunner.query(`DELETE from \`keyword\` WHERE 1=1`);
 
-    await queryRunner.query(`DROP TABLE \`keywords_responses\``);
-    await queryRunner.query(`DROP TABLE \`keywords\``);
+    await queryRunner.query(`DROP TABLE \`keyword_responses\``);
+    await queryRunner.query(`DROP TABLE \`keyword\``);
 
     return;
   }
