a146704e503570b9cb9c0255b1c5c1f5b4eb1cda
Michal Orlik <sogehige@gmail.com>
2024-11-09 00:32:26 +0100

    fix: add with instead of assert

--- tools/changePackageVersion.js
+++ tools/changePackageVersion.js
@@ -1,5 +1,5 @@
 import fs from 'fs'
 
-import pkg from '../package.json';
+import pkg from '../package.json' with { type: "json" };
 
 fs.writeFileSync('./package.json', JSON.stringify({ ...pkg, version: process.argv[2] }, null, 2));
