a3cd76e4fc8551e4f107bce3cefadfdc2fef2e49
Michal Orlik <sogehige@gmail.com>
2024-11-09 00:30:24 +0100

    fix: fix changePackageVersion

--- tools/changePackageVersion.js
+++ tools/changePackageVersion.js
@@ -1,5 +1,5 @@
 import fs from 'fs'
 
-import pkg from '../package.json' assert { type: "json" };
+import pkg from '../package.json';
 
 fs.writeFileSync('./package.json', JSON.stringify({ ...pkg, version: process.argv[2] }, null, 2));
